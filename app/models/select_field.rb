2719c99d3d135135972cd5daf46736f9bb89cc86
Francis Marineau <frank.marineau@gmail.com>
2015-08-26 13:50:50 -0400

    Fixed SelectField match score

--- app/models/select_field.rb
+++ app/models/select_field.rb
@@ -11,8 +11,8 @@ class SelectField < Field
   #   - Community size contrast (a small cluster with a large cluster will be worth more than 2 small clusters)
   #   - Community size (small clusters are worth more)
   def match_score_between(e1, e2)
-    e1_value = e1.info[self.id]
-    e2_value = e2.info[self.id]
+    e1_value = e1.info[self]
+    e2_value = e2.info[self]
 
     # Returns nil if we don't have all the employee info necessary to get a score
     return nil unless e1_value && e2_value
@@ -22,9 +22,6 @@ class SelectField < Field
 
     e1_popularity = e1_option.popularity
     e2_popularity = e2_option.popularity
-
-    pp(e1_popularity)
-    pp(e2_popularity)
 
     popularity_total = e1_popularity + e2_popularity
 
