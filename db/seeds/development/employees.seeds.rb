e24d45ec58e87731efb393608c056a09d9ac86c9
Francis Marineau <frank.marineau@gmail.com>
2015-08-28 17:19:54 -0400

    Better seeds

--- db/seeds/development/employees.seeds.rb
+++ db/seeds/development/employees.seeds.rb
@@ -1,14 +1,19 @@
 nb_employees = 200
 
 enterprise = Enterprise.first
-age_field = NumericField.where(enterprise: enterprise).first
-name_field = TextField.where(enterprise: enterprise).first
-disabilities_field = CheckboxField.where(enterprise: enterprise).first
 domain_name = enterprise.name.parameterize + ".com"
+first_name_field = TextField.where(title: "First name").first
+last_name_field = TextField.where(title: "Last name").first
+gender_field = SelectField.where(title: "Gender").first
+age_field = NumericField.where(title: "Age").first
+disabilities_field = CheckboxField.where(title: "Disabilities").first
 
 nb_employees.times do |i|
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+
   employee = Employee.create(
-    email: Faker::Internet.user_name(name = Faker::Name.name, %w(. _ -)) + "@#{domain_name}",
+    email: Faker::Internet.user_name("#{first_name} #{last_name}", %w(. _ -)) + "@#{domain_name}",
     auth_source: "manual",
     enterprise: enterprise,
     invited_by_id: enterprise.admins.first.id,
@@ -20,21 +25,27 @@ nb_employees.times do |i|
     password_confirmation: password
   )
 
-  invite_time
-
   employee.info[age_field] = Faker::Number.between(18, 65)
-  employee.info[name_field] = name
+  employee.info[first_name_field] = first_name
+  employee.info[last_name_field] = last_name
 
   # Have a chance to pick a random disability
   if rand(100) < 5
     offset = rand(disabilities_field.options.count)
-    employee.info[disabilities_field] = disabilities_field.options.offset(offset).first.id
+    employee.info[disabilities_field] = disabilities_field.options.offset(offset).first.title
 
     # Chance to pick another random disability
     if rand(100) < 5
       offset = rand(disabilities_field.options.count)
-      employee.info[disabilities_field] << disabilities_field.options.offset(offset).first.id
+      employee.info[disabilities_field] << disabilities_field.options.offset(offset).first.title
     end
+  end
+
+  # Pick gender with a 70-30 repartition
+  if rand(100) > 30
+    employee.info[gender_field] = "Male"
+  else
+    employee.info[gender_field] = "Female"
   end
 
   if employee.save
