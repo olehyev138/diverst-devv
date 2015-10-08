60d3cabd7bfa51c6cd9b981ece6389fae83c86d3
Francis Marineau <frank.marineau@gmail.com>
2015-10-08 03:21:15 -0400

    save poll responses

--- app/controllers/poll_responses_controller.rb
+++ app/controllers/poll_responses_controller.rb
@@ -1,5 +1,6 @@
 class PollResponsesController < ApplicationController
   before_action :authenticate_admin!
+  before_action :authenticate_employee!, only: [:new]
   before_action :set_poll
   before_action :set_response, only: [:edit, :update, :destroy, :show]
   skip_before_action :verify_authenticity_token, only: [:create]
@@ -13,7 +14,9 @@ class PollResponsesController < ApplicationController
   end
 
   def create
-    @response = @poll.responses.new(response_params)
+    @response = @poll.responses.new()
+
+    # TODO: merge_params here!
 
     if @response.save
       redirect_to action: :thank_you
@@ -43,15 +46,5 @@ class PollResponsesController < ApplicationController
 
   def set_response
     @response = @poll.responses.find(params[:id])
-  end
-
-  def response_params
-    params
-    .require(:response)
-    .permit(
-      :data,
-      :description,
-      member_ids: []
-    )
   end
 end
