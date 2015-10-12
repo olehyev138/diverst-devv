989da666d159a2134342c628e4c0443eae26da90
Francis Marineau <frank.marineau@gmail.com>
2015-10-12 13:50:09 -0400

    Poll seed

---
+++ db/seeds/segments.seeds.rb
@@ -0,0 +1,11 @@
+s = Enterprise.first.segments.new(
+  name: "Females"
+)
+
+s.rules.new(
+  field: SelectField.where(title: "Gender").first,
+  operator: SegmentRule.operators[:contains_any_of],
+  values: ["Female"]
+)
+
+s.save
