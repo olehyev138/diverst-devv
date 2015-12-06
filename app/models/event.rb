41f6865bc173a73eff6b6eda0dbc6dd6f50ba42e
Francis Marineau <frank.marineau@gmail.com>
2015-12-06 16:11:47 -0500

    More Event tests

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,8 +2,9 @@ class Event < ActiveRecord::Base
   has_and_belongs_to_many :segments
   belongs_to :group
 
-  scope :past, -> { where('end < ?', Time.now).order(start: :desc) }
-  scope :upcoming, -> { where('end >= ?', Time.now).order(start: :desc) }
+  scope :past, -> { where('end < ?', DateTime.now).order(start: :desc) }
+  scope :upcoming, -> { where('start > ?', DateTime.now).order(start: :desc) }
+  scope :ongoing, -> { where('start <= ?', DateTime.now).where('end >= ?', DateTime.now).order(start: :desc) }
 
   def time_string
     if self.start.to_date === self.end.to_date # If the event starts and ends on the same day
--- spec/model/event_spec.rb
+++ spec/model/event_spec.rb
@@ -1,19 +1,58 @@
 RSpec.describe Event do
-  context "#time_string" do
-    it "prints a well formatted time when the event occurs on a single day" do
-      event_start = DateTime.new(2015, 11, 19, 12, 30)
-      event_end = DateTime.new(2015, 11, 19, 14, 30)
-      event = build(:event, start: event_start, end: event_end)
+  describe "#time_string" do
+    context "when the event occurs on a single day" do
+      it "prints a well formatted time" do
+        event_start = DateTime.new(2015, 11, 19, 12, 30)
+        event_end = DateTime.new(2015, 11, 19, 14, 30)
+        event = build(:event, start: event_start, end: event_end)
 
-      expect(event.time_string).to eq "November 19, 2015 from 12:30 PM to 2:30 PM"
+        expect(event.time_string).to eq "November 19, 2015 from 12:30 PM to 2:30 PM"
+      end
     end
 
-    it "prints a well formatted time when the event spans multiple days" do
+    context "when the event spans multiple days" do
       event_start = DateTime.new(2015, 11, 19, 12, 30)
       event_end = DateTime.new(2015, 11, 20, 14, 30)
-      event = build(:event, start: event_start, end: event_end)
+      let(:event) { build(:event, start: event_start, end: event_end) }
 
-      expect(event.time_string).to eq "From November 19, 2015 at 12:30 PM to November 20, 2015 at 2:30 PM"
+      it "prints a well formatted time" do
+        expect(event.time_string).to eq "From November 19, 2015 at 12:30 PM to November 20, 2015 at 2:30 PM"
+      end
+    end
+  end
+
+  describe "scopes" do
+    current_datetime = DateTime.new(2015, 11, 10, 13, 00)
+    let(:past_event) { create(:event, start: DateTime.new(2015, 11, 9, 12, 30), end: DateTime.new(2015, 11, 9, 2, 30)) }
+    let(:ongoing_event) { create(:event, start: DateTime.new(2015, 11, 10, 12, 30), end: DateTime.new(2015, 11, 10, 13, 30)) }
+    let(:upcoming_event) { create(:event, start: DateTime.new(2015, 11, 11, 12, 30), end: DateTime.new(2015, 11, 11, 13, 30)) }
+
+    before do
+      allow(DateTime).to receive(:now).and_return(current_datetime)
+    end
+
+    context ".past" do
+      subject { Event.past }
+
+      it "only include the past event" do
+        expect(subject).to eq [past_event]
+      end
+    end
+
+    context ".ongoing" do
+      subject { Event.ongoing }
+
+      it "only include the ongoing event" do
+        expect(subject).to eq [ongoing_event]
+      end
+    end
+
+    context ".upcoming" do
+      subject { Event.upcoming }
+
+      it "only include the upcoming event" do
+        expect(subject).to eq [upcoming_event]
+      end
     end
   end
 end
