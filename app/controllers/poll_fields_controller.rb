c4ba9dc52af5db3a748506d9e822c0a804811f04
Francis Marineau <frank.marineau@gmail.com>
2015-12-10 11:31:52 -0500

    Textual answers in market scope poll#show

--- app/controllers/poll_fields_controller.rb
+++ app/controllers/poll_fields_controller.rb
@@ -1,7 +1,9 @@
 class PollFieldsController < ApplicationController
   before_action :authenticate_admin!
-  before_action :set_field, only: [:answer_popularities]
+  before_action :set_field, only: [:answer_popularities, :show]
   before_action :set_poll
+
+  layout "market_scope"
 
   def answer_popularities
     render json: @field.answer_popularities(entries: @poll.responses)
