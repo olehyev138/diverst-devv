15b055e2e39095f3bfd75f51160a36a957335134
Francis Marineau <frank.marineau@gmail.com>
2015-12-12 18:52:37 -0500

    Use Time instead of DateTime in spec

--- spec/model/event_spec.rb
+++ spec/model/event_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe Event do
   describe "#time_string" do
     context "when the event occurs on a single day" do
       it "prints a well formatted time" do
-        event_start = DateTime.new(2015, 11, 19, 12, 30)
-        event_end = DateTime.new(2015, 11, 19, 14, 30)
+        event_start = Time.new(2015, 11, 19, 12, 30)
+        event_end = Time.new(2015, 11, 19, 14, 30)
         event = build(:event, start: event_start, end: event_end)
 
         expect(event.time_string).to eq "November 19, 2015 from 12:30 PM to 2:30 PM"
@@ -13,8 +13,8 @@ RSpec.describe Event do
     end
 
     context "when the event spans multiple days" do
-      event_start = DateTime.new(2015, 11, 19, 12, 30)
-      event_end = DateTime.new(2015, 11, 20, 14, 30)
+      event_start = Time.new(2015, 11, 19, 12, 30)
+      event_end = Time.new(2015, 11, 20, 14, 30)
       let(:event) { build(:event, start: event_start, end: event_end) }
 
       it "prints a well formatted time" do
@@ -24,13 +24,13 @@ RSpec.describe Event do
   end
 
   describe "scopes" do
-    let(:past_event) { create(:event, start: DateTime.new(2015, 11, 9, 12, 30), end: DateTime.new(2015, 11, 9, 2, 30)) }
-    let(:ongoing_event) { create(:event, start: DateTime.new(2015, 11, 10, 12, 30), end: DateTime.new(2015, 11, 10, 13, 30)) }
-    let(:upcoming_event) { create(:event, start: DateTime.new(2015, 11, 11, 12, 30), end: DateTime.new(2015, 11, 11, 13, 30)) }
+    let(:past_event) { create(:event, start: Time.new(2015, 11, 9, 12, 30), end: Time.new(2015, 11, 9, 2, 30)) }
+    let(:ongoing_event) { create(:event, start: Time.new(2015, 11, 10, 12, 30), end: Time.new(2015, 11, 10, 13, 30)) }
+    let(:upcoming_event) { create(:event, start: Time.new(2015, 11, 11, 12, 30), end: Time.new(2015, 11, 11, 13, 30)) }
 
     before do
-      current_datetime = DateTime.new(2015, 11, 10, 13, 00)
-      allow(DateTime).to receive(:now).and_return(current_datetime)
+      current_time = Time.new(2015, 11, 10, 13, 00)
+      allow(Time).to receive(:now).and_return(current_time)
     end
 
     describe ".past" do
