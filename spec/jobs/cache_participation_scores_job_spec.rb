3c8ae145bbbe12325f3633b41b201476679926bc
Francis Marineau <frank.marineau@gmail.com>
2015-12-14 15:30:17 -0500

    Spec for score job + yammer sync accross enterprises

--- spec/jobs/cache_participation_scores_job_spec.rb
+++ spec/jobs/cache_participation_scores_job_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe CacheParticipationScoresJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let! (:employee) { create(:employee) }
+  let! (:group) { create(:group) }
+
+  before do
+    allow_any_instance_of(Employee).to receive(:participation_score).and_return(10)
+    allow_any_instance_of(Group).to receive(:participation_score).and_return(10)
+  end
+
+  it "updates employee scores" do
+    Sidekiq::Testing.inline! do
+      worker = CacheParticipationScoresJob.new
+      worker.perform
+      expect(Employee.first.participation_score_7days).to eq 10
+    end
+  end
+
+  it "updates group scores" do
+    Sidekiq::Testing.inline! do
+      worker = CacheParticipationScoresJob.new
+      worker.perform
+      expect(Group.first.participation_score_7days).to eq 10
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
 require 'capybara/poltergeist'
+require 'sidekiq/testing'
 
 Capybara.javascript_driver = :poltergeist
 
