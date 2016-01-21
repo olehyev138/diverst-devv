064dbf32169a9ec00840cef08c7f64f868bd79ca
Francis Marineau <frank.marineau@gmail.com>
2016-01-21 14:11:47 -0500

    Link to LinkedIn profile

--- app/controllers/omni_auth_controller.rb
+++ app/controllers/omni_auth_controller.rb
@@ -1,5 +1,5 @@
 class OmniAuthController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_employee!
 
   def callback
     @oauth = request.env['omniauth.auth']
@@ -7,8 +7,16 @@ class OmniAuthController < ApplicationController
   end
 
   def linkedin
+    current_employee.update(
+      linkedin_profile_url: @oauth['info']['urls']['public_profile']
+    )
+
+    ap @oauth['info']['urls']['public_profile']
+
+    # Set the user's avatar
     picture_url = @oauth['info']['image']
     SaveEmployeeAvatarFromUrlJob.perform_later(current_employee, picture_url)
+
     redirect_to '/'
   end
 end
