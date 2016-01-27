0a6d563a92d1eac253031ee834317ece4fe03a1c
Francis Marineau <frank.marineau@gmail.com>
2016-01-27 11:08:04 -0500

    ads

--- app/models/invitation_segments_group.rb
+++ app/models/invitation_segments_group.rb
@@ -1,4 +1,4 @@
 class InvitationSegmentsGroup < ActiveRecord::Base
-  belongs_to :invitation_segment, class_name: "Segment"
+  belongs_to :invitation_segment, class_name: "Segment", foreign_key: :segment_id
   belongs_to :group
 end
