30f3d6a4e2105daed2fef8f9ccc96e26a5ace857
Francis Marineau <frank.marineau@gmail.com>
2016-01-31 05:44:36 -0500

    Better-looking campaigns

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,6 @@ class Question < ActiveRecord::Base
   belongs_to :campaign
   has_many :answers, inverse_of: :question, dependent: :destroy
   has_many :answer_comments, through: :answers, source: :comments
+
+  scope :solved, -> { where.not(solved_at: nil) }
 end
