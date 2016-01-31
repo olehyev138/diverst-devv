9d812c4f5c9e61c27eebe447648ee7b8f72c4b6e
Francis Marineau <frank.marineau@gmail.com>
2016-01-31 18:30:33 -0500

    Update resource file extension spec

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Resource do
   describe '#extension' do
-    it "returns the file's extension in caps without the dot" do
+    it "returns the file's lowercase extension without the dot" do
       resource = build(:resource)
-      expect(resource.file_extension).to eq 'PDF'
+      expect(resource.file_extension).to eq 'pdf'
     end
   end
 end
