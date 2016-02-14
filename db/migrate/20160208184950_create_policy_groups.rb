2707da701f8015243fc5fe32994e837b44fbf9ac
Francis Marineau <frank.marineau@gmail.com>
2016-02-13 23:49:06 -0500

    Added defaults to policy groups

--- db/migrate/20160208184950_create_policy_groups.rb
+++ db/migrate/20160208184950_create_policy_groups.rb
@@ -4,47 +4,47 @@ class CreatePolicyGroups < ActiveRecord::Migration
       t.string :name
       t.belongs_to :enterprise
 
-      t.boolean :campaigns_index
-      t.boolean :campaigns_create
-      t.boolean :campaigns_manage
+      t.boolean :campaigns_index, default: false
+      t.boolean :campaigns_create, default: false
+      t.boolean :campaigns_manage, default: false
 
-      t.boolean :polls_index
-      t.boolean :polls_create
-      t.boolean :polls_manage
+      t.boolean :polls_index, default: false
+      t.boolean :polls_create, default: false
+      t.boolean :polls_manage, default: false
 
-      t.boolean :events_index
-      t.boolean :events_create
-      t.boolean :events_manage
+      t.boolean :events_index, default: false
+      t.boolean :events_create, default: false
+      t.boolean :events_manage, default: false
 
-      t.boolean :group_messages_index
-      t.boolean :group_messages_create
-      t.boolean :group_messages_manage
+      t.boolean :group_messages_index, default: false
+      t.boolean :group_messages_create, default: false
+      t.boolean :group_messages_manage, default: false
 
-      t.boolean :groups_index
-      t.boolean :groups_create
-      t.boolean :groups_manage
-      t.boolean :groups_members_index
-      t.boolean :groups_members_manage
+      t.boolean :groups_index, default: false
+      t.boolean :groups_create, default: false
+      t.boolean :groups_manage, default: false
+      t.boolean :groups_members_index, default: false
+      t.boolean :groups_members_manage, default: false
 
-      t.boolean :metrics_dashboards_index
-      t.boolean :metrics_dashboards_create
+      t.boolean :metrics_dashboards_index, default: false
+      t.boolean :metrics_dashboards_create, default: false
 
-      t.boolean :news_links_index
-      t.boolean :news_links_create
-      t.boolean :news_links_manage
+      t.boolean :news_links_index, default: false
+      t.boolean :news_links_create, default: false
+      t.boolean :news_links_manage, default: false
 
-      t.boolean :enterprise_resources_index
-      t.boolean :enterprise_resources_create
-      t.boolean :enterprise_resources_manage
+      t.boolean :enterprise_resources_index, default: false
+      t.boolean :enterprise_resources_create, default: false
+      t.boolean :enterprise_resources_manage, default: false
 
-      t.boolean :segments_index
-      t.boolean :segments_create
-      t.boolean :segments_manage
+      t.boolean :segments_index, default: false
+      t.boolean :segments_create, default: false
+      t.boolean :segments_manage, default: false
 
-      t.boolean :users_index
-      t.boolean :users_manage
+      t.boolean :users_index, default: false
+      t.boolean :users_manage, default: false
 
-      t.boolean :global_settings_manage
+      t.boolean :global_settings_manage, default: false
 
       t.timestamps null: false
     end
