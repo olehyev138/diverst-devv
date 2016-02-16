4c35baf5f3bd5fc374584e9bcccff02b9010d47f
Francis Marineau <frank.marineau@gmail.com>
2016-02-15 23:53:42 -0500

    Fixed enterprise resources

--- app/controllers/enterprises/resources_controller.rb
+++ app/controllers/enterprises/resources_controller.rb
@@ -1,20 +1,11 @@
 class Enterprises::ResourcesController < ApplicationController
   include IsResources
 
-  layout :resolve_layout
-
-  def new
-    authorize Resource
-  end
+  layout 'erg_manager'
 
   protected
 
   def set_container
     @container = current_user.enterprise
-  end
-
-  def resolve_layout
-    # return 'erg_manager' if current_user(:admin).is_a? Admin TODO
-    'user'
   end
 end
