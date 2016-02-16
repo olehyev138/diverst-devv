9d783fe485c64fa5c69804143a236baa08a3c5ee
Francis Marineau <frank.marineau@gmail.com>
2016-02-16 00:35:34 -0500

    More fixes and adjustments

--- app/policies/news_link_policy.rb
+++ app/policies/news_link_policy.rb
@@ -1,2 +1,13 @@
 class NewsLinkPolicy < ApplicationPolicy
+  def index?
+    @policy_group.group_messages_index?
+  end
+
+  def create?
+    @policy_group.group_messages_create?
+  end
+
+  def destroy?
+    return true if @policy_group.polls_manage?
+  end
 end
