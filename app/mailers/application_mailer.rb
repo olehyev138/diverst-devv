d97d5933906cd58a257cd3ce60781b37c34a160d
Francis Marineau <frank.marineau@gmail.com>
2016-02-19 17:53:00 -0500

    New staging IP, migrate to SendGrid aka 'nooow we haave him'

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,7 +2,7 @@ class ApplicationMailer < ActionMailer::Base
   helper Rails.application.routes.url_helpers # Allows us to call url helpers
   include MailerHelper
 
-  from_address = Mail::Address.new 'le-facteur@volume7.ca'
+  from_address = Mail::Address.new 'nowwehavehim@diverst.com'
   from_address.display_name = 'Diverst'
   default from: from_address
 
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server '52.90.224.202', user: 'deploy', roles: %w(web app db)
+server '54.175.76.236', user: 'deploy', roles: %w(web app db)
 
 # role-based syntax
 # ==================
