f9961aa29446f38891f3f55bc92c385c93d747d5
Francis Marineau <frank.marineau@gmail.com>
2016-02-28 03:44:24 -0500

    Fix theme recompiling task

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ set :sidekiq_processes, 1
 namespace :deploy do
 
   desc 'Recompile all enterprise themes'
-  task :recompile_themes, [:command] => 'deploy:set_rails_env' do |task, args|
+  task 'themes:recompile', [:command] => 'deploy:set_rails_env' do |task, args|
     on primary(:app) do
       within current_path do
         with :rails_env => fetch(:rails_env) do
