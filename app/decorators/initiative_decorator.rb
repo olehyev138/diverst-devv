3e7c7b19bbb091a656318134595e903bf01425d6
Francis Marineau <frank.marineau@gmail.com>
2016-03-14 13:56:28 -0400

    More precise time progress bar

--- app/decorators/initiative_decorator.rb
+++ app/decorators/initiative_decorator.rb
@@ -1,3 +1,8 @@
 class InitiativeDecorator < Draper::Decorator
   decorates_association :updates
+
+  def progress_percentage
+    return 100 if Time.current >= initiative.end
+    (Time.current - initiative.start) / (initiative.end - initiative.start) * 100
+  end
 end
