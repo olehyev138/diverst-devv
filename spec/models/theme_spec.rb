110bb040269d9349d6e769e743ee56bb8be3d431
Anatolii Borysov <borisano@gmail.com>
2016-06-20 15:39:37 +0200

    Refactor Theme model spec

--- spec/models/theme_spec.rb
+++ spec/models/theme_spec.rb
@@ -1,23 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Theme, type: :model do
-  it 'doesnt validate with an invalid color' do
-    expect(build(:theme, primary_color: '#zz1122')).to be_invalid
+  describe 'color validation' do
+    it 'is invalid with incorrect color' do
+      expect(build(:theme, primary_color: '#zz1122')).to be_invalid
+    end
+
+    it 'is valid with correct color' do
+      expect(build(:theme, primary_color: '#f15e58')).to be_valid
+    end
   end
 
-  it 'auto-appends a hash before the color if none is present' do
-    theme = build(:theme, primary_color: 'f15e58')
-    theme.valid?
-    expect(theme.primary_color).to eq '#f15e58'
-  end
+  describe 'appending hash to colors' do
+    it 'triggers if hash is not present' do
+      theme = build(:theme, primary_color: 'f15e58')
+      theme.valid?
+      expect(theme.primary_color).to eq '#f15e58'
+    end
 
-  it 'doesnt auto-append a hash if one is already present' do
-    theme = build(:theme, primary_color: '#f15e58')
-    theme.valid?
-    expect(theme.primary_color).to eq '#f15e58'
-  end
-
-  it 'validates with a valid color' do
-    expect(build(:theme, primary_color: '#f15e58')).to be_valid
+    it 'does not trigger if hash is present' do
+      theme = build(:theme, primary_color: '#f15e58')
+      theme.valid?
+      expect(theme.primary_color).to eq '#f15e58'
+    end
   end
 end
