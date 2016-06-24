e2b3ad70ff2d5a2715cac3373e9eaa0b6a7416eb
Anatolii Borysov <borisano@gmail.com>
2016-06-24 19:29:46 +0200

    Add client logic to color setting

--- app/assets/javascripts/enterprise/edit_branding.js
+++ app/assets/javascripts/enterprise/edit_branding.js
@@ -1,14 +1,33 @@
 $(document).ready(function(){
-  $('#enterprise_theme_primary_color').change(function(){
-    var $useSecondaryColor = $('#enterprise_theme_use_secondary_color');
-    var $secondaryColorInput = $('#enterprise_theme_secondary_color');
+  var $primaryColorInput = $('#enterprise_theme_primary_color')
+  var $secondaryColorInput = $('#enterprise_theme_secondary_color');
+  var $useSecondaryColor = $('#enterprise_theme_use_secondary_color');
 
+  $primaryColorInput.change(function(){
     // If user prefer to not use separate color for charts,
     // Set charts color to be equal to primary color
     if( !$useSecondaryColor.is(':checked')) {
-      $secondaryColorInput.val( $(this).val() );
-      document.getElementById('enterprise_theme_secondary_color')
-        .jscolor.fromString( $(this).val())
+      changeInputColor($secondaryColorInput, $(this).val() );
+    }
+  }); //endof .change
+
+  $secondaryColorInput.change(function(){
+    if( !$useSecondaryColor.is(':checked')) {
+      changeInputColor($secondaryColorInput, $primaryColorInput.val() );
+    }
+  }); //endof change
+
+  $useSecondaryColor.change(function(){
+    if( !$(this).is(':checked') ) {
+      changeInputColor($secondaryColorInput, $primaryColorInput.val() );
     }
   });
+
+
+
+  function changeInputColor($input, colorString) {
+    $input.val( colorString );
+    document.getElementById( $input.attr('id') )
+      .jscolor.fromString( colorString );
+  }
 });
