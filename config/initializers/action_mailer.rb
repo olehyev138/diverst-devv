f4884ee62b29abe6ee721036da6a02d294a33aa3
borisano <borisano@gmail.com>
2016-08-26 12:31:55 +0300

    Add mailgun_rails gem and configure mailgin via api

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,12 +1,3 @@
 ActionMailer::Base.default charset: 'utf-8'
-ActionMailer::Base.smtp_settings = {
-  port: 587,
-  address: ENV['SMTP_HOST'],
-  user_name: ENV['SMTP_USERNAME'],
-  password: ENV['SMTP_PASSWORD'],
-  domain: ENV['SMTP_DOMAIN'],
-  authentication: :plain,
-  enable_starttls_auto: true
-}
 
 ActionMailer::Base.register_interceptor(PreventMailInterceptor)
