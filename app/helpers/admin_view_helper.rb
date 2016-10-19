49165e6477069fa0f9d998d36131e70b5b067a96
borisano <borisano@gmail.com>
2016-10-19 12:23:18 +0300

    Move admin links highlight logic to helper
    * Also fixed several bugs related to incorrect links hightlighting

---
+++ app/helpers/admin_view_helper.rb
@@ -0,0 +1,51 @@
+module AdminViewHelper
+  def active_manage_erg_link?
+   return false unless ['enterprises',
+                        'groups',
+                        'resources',
+                        'segments',
+                        'metrics_dashboards'].include? controller_name
+
+
+   return false unless ['index', 'edit', 'show', 'new', 'calendar'].include? action_name
+
+   true
+  end
+
+  def active_champaigns_link?
+    return false unless ['campaigns', 'questions', 'expenses', 'expense_categories'].include? controller_name
+
+    true
+  end
+
+  def active_plan_link?
+    if controller_name == 'groups' && action_name == 'index'
+      return false
+    end
+
+    return false unless ['groups', 'initiatives', 'outcomes', 'updates'].include? controller_name
+
+    return false if ['calendar'].include? action_name
+
+    true
+  end
+
+  def active_global_settings_link?
+    return false unless ['users',
+                          'enterprises',
+                          'integrations',
+                          'policy_groups',
+                          'emails'
+                        ].include? controller_name
+
+    return false unless ['index',
+                          'edit',
+                          'show',
+                          'new',
+                          'edit_auth',
+                          'edit_fields',
+                          'edit_branding'].include? action_name
+
+    true
+  end
+end
