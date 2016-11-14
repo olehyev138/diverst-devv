d549b25d3fdd304f02a7cac4ed3726e39081cf4f
Anatolii Borysov <borisano@gmail.com>
2016-11-14 18:09:26 +0200

    Add Budget method for approval

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -6,6 +6,30 @@ class Budget < ActiveRecord::Base
   has_many :checklist_items, as: :container
   accepts_nested_attributes_for :checklist_items, reject_if: :all_blank, allow_destroy: true
 
+  scope :approved, -> { where(is_approved: true) }
+  scope :not_approved, -> { where(is_approved: false )}
+  scope :pending, -> { where(is_approved: nil )}
+
+  scope :with_available_funds, -> { where('available_amount > 0')}
+
+  def approve(amount = nil)
+    if amount.nil? # approve full
+      self.agreed_amount = self.requested_amount
+    else
+      # approve partial amount
+      self.agreed_amount = amount
+    end
+
+    self.available_amount = self.agreed_amount
+    self.is_approved = true
+
+    self.save
+  end
+
+  def decline
+    self.update(is_approved: false)
+  end
+
   def status_title
     return 'Pending' if is_approved.nil?
 
