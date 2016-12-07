1ffd48b9fac3f995356ab68b5e82e4ad2cc4583f
borisano <borisano@gmail.com>
2016-12-07 19:07:18 +0200

    Fix empty progress bars on expences

--- app/decorators/initiative_decorator.rb
+++ app/decorators/initiative_decorator.rb
@@ -5,4 +5,15 @@ class InitiativeDecorator < Draper::Decorator
     return 100 if Time.current >= initiative.end
     (Time.current - initiative.start) / (initiative.end - initiative.start) * 100
   end
+
+  def budget_percentage
+    #Show empty bar if no funds is allocated
+    return 0 if initiative.estimated_funding == 0
+
+    #Show just barely visible bar if expences are expected but not published yet.
+    initiative_expences = initiative.expenses.sum(:amount).to_f
+    return 2 if initiative_expences == 0
+
+    initiative_expences / initiative.estimated_funding * 100
+  end
 end
