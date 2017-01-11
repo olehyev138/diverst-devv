e5a51fa55e0f66a808509ec7be35e8370bfa32ce
Anatolii Borysov <borisano@gmail.com>
2017-01-11 23:02:40 +0200

    Bugfix - correctly form poll emails

--- app/mailers/poll_mailer.rb
+++ app/mailers/poll_mailer.rb
@@ -1,7 +1,7 @@
 class PollMailer < ApplicationMailer
   def invitation(poll, user)
     @link = "http://#{ENV['DOMAIN']}#{new_poll_poll_response_path(poll)}"
-    mail(to: user.email, subject: 'Market Scope campaign invitation')
     @poll = poll
+    mail(to: user.email, subject: 'Market Scope campaign invitation')
   end
 end
