d231fca4e63d762eeba68c2d05779adaef88f58c
Anatolii Borysov <borisano@gmail.com>
2017-01-18 17:09:37 +0200

    Fix attendences
    * Add timestamps to initiative_users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117155659) do
+ActiveRecord::Schema.define(version: 20170118150249) do
 
   create_table "answer_comments", force: :cascade do |t|
     t.text     "content",    limit: 65535
@@ -494,8 +494,10 @@ ActiveRecord::Schema.define(version: 20170117155659) do
   end
 
   create_table "initiative_users", force: :cascade do |t|
-    t.integer "initiative_id", limit: 4
-    t.integer "user_id",       limit: 4
+    t.integer  "initiative_id", limit: 4
+    t.integer  "user_id",       limit: 4
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "initiatives", force: :cascade do |t|
