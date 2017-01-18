56ae09a8609e543b2d59589bc12010fd57d73112
Anatolii Borysov <borisano@gmail.com>
2017-01-18 15:08:45 +0200

    Fix poll mailer

--- app/mailers/poll_mailer.rb
+++ app/mailers/poll_mailer.rb
@@ -1,6 +1,5 @@
 class PollMailer < ApplicationMailer
   def invitation(poll, user)
-    @link = "http://#{ENV['DOMAIN']}#{new_poll_poll_response_path(poll)}"
     @poll = poll
     mail(to: user.email, subject: 'Market Scope campaign invitation')
   end
