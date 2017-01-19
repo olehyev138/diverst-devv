ec9783531f6fc10430eada6522a3ea37d5f0368f
Gabriel Givigier <gabriel@givigier.com>
2017-01-19 11:31:49 -0200

    Refactoring of admin_view_helpers

--- app/helpers/admin_view_helper.rb
+++ app/helpers/admin_view_helper.rb
@@ -1,55 +1,28 @@
 module AdminViewHelper
   def active_manage_erg_link?
-   return false unless ['enterprises',
-                        'groups',
-                        'resources',
-                        'segments',
-                        'metrics_dashboards'].include? controller_name
+    return true if controller_name == 'groups' &&
+      ['calendar', 'index', 'import_csv', 'edit', 'new'].include?(action_name)
+    return true if controller_name == 'enterprises' && ['resources'].include?(action_name)
 
-
-   return false unless ['index', 'edit', 'show', 'new', 'calendar'].include? action_name
-
-   true
+    ['resources', 'segments', 'metrics_dashboards'].include? controller_name
   end
 
-  def active_champaigns_link?
-    return false unless ['campaigns', 'questions', 'expenses', 'expense_categories'].include? controller_name
-
-    true
+  def active_engage_link?
+    ['campaigns', 'expenses', 'expense_categories'].include? controller_name
   end
 
   def active_plan_link?
-    if controller_name == 'groups' && (action_name == 'index' || action_name == 'edit' || action_name == 'new')
-      return false
-    end
+    return true if controller_name == 'groups' &&
+      ['edit_fields', 'plan_overview', 'metrics', 'budgets', 'request_budget', 'view_budget'].include?(action_name)
 
-    return false unless ['groups', 'initiatives', 'outcomes', 'updates'].include? controller_name
-
-    return false if ['calendar', 'edit_annual_budget'].include? action_name
-
-    true
+    ['initiatives', 'outcomes', 'updates'].include? controller_name
   end
 
   def active_global_settings_link?
-    return false unless ['users',
-                          'groups',
-                          'enterprises',
-                          'integrations',
-                          'policy_groups',
-                          'emails'
-                        ].include? controller_name
+    return true if controller_name == 'groups' && [].include?(action_name)
+    return true if controller_name == 'enterprises' &&
+      ['edit_auth', 'edit_fields', 'edit_branding'].include?(action_name)
 
-    return false unless ['index',
-                          'edit',
-                          'show',
-                          'new',
-                          'edit_auth',
-                          'edit_fields',
-                          'edit_branding',
-                          'edit_budgeting',
-                          'edit_annual_budget'
-                          ].include? action_name
-
-    true
+    ['users', 'integrations', 'policy_groups', 'emails'].include? controller_name
   end
 end
