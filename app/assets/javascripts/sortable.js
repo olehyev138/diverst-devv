28102223c22f886c7014bbb79f5275695a921b14
Gabriel Givigier <gabriel@givigier.com>
2017-01-19 14:01:08 -0200

    Add option to sort fields on surveys

---
+++ app/assets/javascripts/sortable.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  Sortable.create(document.getElementById('fields'), {
+    onSort: function() {
+      $("#fields .nested-fields").each(function(i) {
+        $(this).find("input").each(function() {
+          $(this).attr(
+            "name",
+            this.name.replace(/\[fields_attributes\]\[\d+\]/, "[fields_attributes]["+i+"]")
+          );
+        });
+      });
+    }
+  });
+});
