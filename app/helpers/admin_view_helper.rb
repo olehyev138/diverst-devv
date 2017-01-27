b3e8aea8569ad575718c200af1d14ecd50d97d3e
Anatolii Borysov <borisano@gmail.com>
2017-01-27 15:28:24 +0200

    Fix admin sidebar links highlight

--- app/helpers/admin_view_helper.rb
+++ app/helpers/admin_view_helper.rb
@@ -3,8 +3,9 @@ module AdminViewHelper
     return true if controller_name == 'groups' &&
       ['calendar', 'index', 'import_csv', 'edit', 'new'].include?(action_name)
     return true if controller_name == 'enterprises' && ['resources'].include?(action_name)
+    return true if ['groups/resources', 'enterprises/resources'].include? params[:controller]
 
-    ['resources', 'segments', 'metrics_dashboards'].include? controller_name
+    ['segments', 'metrics_dashboards'].include? controller_name
   end
 
   def active_engage_link?
@@ -14,6 +15,8 @@ module AdminViewHelper
   def active_plan_link?
     return true if controller_name == 'groups' &&
       ['edit_fields', 'plan_overview', 'metrics', 'budgets', 'request_budget', 'view_budget'].include?(action_name)
+
+    return true if params[:controller] == 'initiatives/resources'
 
     ['initiatives', 'outcomes', 'updates'].include? controller_name
   end
