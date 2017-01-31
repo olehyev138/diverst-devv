e2ae50029058dee54bd0b9afeb04514e7d370a64
Gabriel Givigier <gabriel@givigier.com>
2017-01-31 15:11:34 -0200

    Update view of mailer

--- spec/mailers/user_group_mailer_spec.rb
+++ spec/mailers/user_group_mailer_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe UserGroupMailer, type: :mailer do
   let!(:user){ create(:user) }
-  let!(:groups){ [{ group: create(:group), messages_count: 2, news_count: 1 }] }
+  let!(:groups){ [{ group: create(:group), messages_count: 2, news_count: 0 }] }
 
   let!(:mail) { described_class.notification(user, groups).deliver_now }
 
@@ -29,6 +29,10 @@ RSpec.describe UserGroupMailer, type: :mailer do
 
     it 'shows a message with number of comments in group' do
       expect(mail.body.encoded).to include("2 new messages")
+    end
+
+    it 'does not shows a message with number of news when there is news' do
+      expect(mail.body.encoded).to_not include("news")
     end
   end
 end
