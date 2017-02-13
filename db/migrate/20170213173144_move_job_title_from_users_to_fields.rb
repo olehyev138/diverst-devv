1f49dda2b0a788e0f3f6c80f965a2ae07604ba8f
Gabriel Givigier <gabriel@givigier.com>
2017-02-13 18:02:40 -0200

    Migrate job_title to regular fields

---
+++ db/migrate/20170213173144_move_job_title_from_users_to_fields.rb
@@ -0,0 +1,25 @@
+class MoveJobTitleFromUsersToFields < ActiveRecord::Migration
+  def up
+    User.find_in_batches(batch_size: 200) do |users|
+      users.each do |user|
+        job_title = user.job_title
+        field = TextField.find_or_create_by(container_id: user.enterprise_id, title: "Job title",
+          type: "TextField", container_type: "Enterprise")
+        user.info[field] = field.process_field_value job_title
+        user.save!
+      end
+    end
+    remove_column :users, :job_title
+  end
+
+  def down
+    add_column :users, :job_title, :string
+    field = TextField.find_by(title: "Job title")
+    User.find_in_batches(batch_size: 200) do |users|
+      users.each do |user|
+        info = user.info
+        user.update(job_title: info.fetch(field.id))
+      end
+    end
+  end
+end
