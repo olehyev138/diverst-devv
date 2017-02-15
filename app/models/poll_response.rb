3c2e73926b5c86e2f0b8ccbe0c25f0c126279ab5
Gabriel Givigier <gabriel@givigier.com>
2017-02-15 11:32:50 -0200

    Reindex after a new poll_response

--- app/models/poll_response.rb
+++ app/models/poll_response.rb
@@ -3,4 +3,13 @@ class PollResponse < ActiveRecord::Base
 
   belongs_to :poll
   belongs_to :user
+
+  after_commit on: [:create] do
+    IndexElasticsearchJob.perform_later(
+      model_name: 'User',
+      operation: 'index',
+      index: User.es_index_name(enterprise: poll.enterprise),
+      record_id: user.id
+    )
+  end
 end
