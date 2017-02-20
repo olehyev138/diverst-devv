17c66bd165ed7c1af724f3c24397d3dfced27e78
Gabriel Givigier <gabriel@givigier.com>
2017-02-20 10:56:56 -0300

    Format date of timeseries graphs

--- app/services/reports/graph_timeseries.rb
+++ app/services/reports/graph_timeseries.rb
@@ -24,10 +24,14 @@ class Reports::GraphTimeseries
     dates_info = {}
     @graph_content.each_with_index do |content, i|
       content[:data].each do |date_info|
-        dates_info[date_info.first] = [] unless dates_info[date_info.first]
-        dates_info[date_info.first] << { i => date_info.second }
+        dates_info[to_date(date_info.first)] = [] unless dates_info[date_info.first]
+        dates_info[to_date(date_info.first)] << { i => date_info.second }
       end
     end
     dates_info
+  end
+
+  def to_date(miliseconds)
+    DateTime.strptime((miliseconds/1000).to_s,'%s')
   end
 end
--- app/services/reports/graph_timeseries_generic.rb
+++ app/services/reports/graph_timeseries_generic.rb
@@ -9,6 +9,11 @@ class Reports::GraphTimeseriesGeneric
   end
 
   def get_body
-    @data
+    @data.map{ |data| [to_date(data[0]), data[1]] }
+  end
+
+  private
+  def to_date(miliseconds)
+    DateTime.strptime((miliseconds/1000).to_s,'%s')
   end
 end
