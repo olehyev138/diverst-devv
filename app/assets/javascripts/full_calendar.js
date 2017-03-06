a2858aa58311b8fa224c8c0492fcfc5bcdce834d
Gabriel Givigier <gabriel@givigier.com>
2017-03-06 17:44:33 -0300

    Add filters to calendar

--- app/assets/javascripts/full_calendar.js
+++ app/assets/javascripts/full_calendar.js
@@ -1,19 +1,38 @@
-function applyFullCalendar(selector) {
-  selector = selector || '#calendar';
+$(document).on('ready page:load', function(){
+  listenCalendarVisibility();
+  applyFullCalendar();
 
-  var $calendar = $(selector);
-  var calendarDataUrl = $calendar.data('calendar-data-url');
-
-  $calendar.fullCalendar({
-    header: {
-      left:   'title',
-      center: 'month,agendaWeek',
-      right:  'prev,today,next'
-    },
-    eventSources: [
-      {
-        url: calendarDataUrl
+  function listenCalendarVisibility() {
+    var hidden = false;
+    $('.calendar-filters-visibility').on('click', function() {
+      hidden = $(this).text() === 'Show filters' ? false : true;
+      if(hidden) {
+        $(this).text('Show filters');
       }
-    ]
-  }); //endof fullcalendar
-}
+      else {
+        $(this).text('Hide filters');
+      }
+      $('.calendar-filters').toggle();
+    });
+  }
+
+  function applyFullCalendar(selector) {
+    selector = selector || '#calendar';
+
+    var $calendar = $(selector);
+    var calendarDataUrl = $calendar.data('calendar-data-url');
+
+    $calendar.fullCalendar({
+      header: {
+        left:   'title',
+        center: 'month,agendaWeek',
+        right:  'prev,today,next'
+      },
+      eventSources: [
+        {
+          url: calendarDataUrl
+        }
+      ]
+    }); //endof fullcalendar
+  }
+});
