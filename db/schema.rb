c3f4114f9575a3360ea6f769374fbe56cc8d1ac4
Gabriel Givigier <gabriel@givigier.com>
2017-04-03 11:53:36 -0300

    Add anonymous field to polls

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403112708) do
+ActiveRecord::Schema.define(version: 20170403135333) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id",   limit: 4
@@ -686,8 +686,9 @@ ActiveRecord::Schema.define(version: 20170403112708) do
     t.integer  "poll_id",    limit: 4
     t.integer  "user_id",    limit: 4
     t.text     "data",       limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.boolean  "anonymous",                default: false
   end
 
   create_table "polls", force: :cascade do |t|
