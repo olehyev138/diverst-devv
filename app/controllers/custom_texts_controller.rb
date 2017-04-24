fe572e46545151cb2677d21bee144a611bc58e74
Gabriel Givigier <gabriel@givigier.com>
2017-04-24 09:50:56 -0300

    Add translations for Symantec

--- app/controllers/custom_texts_controller.rb
+++ app/controllers/custom_texts_controller.rb
@@ -25,6 +25,6 @@ class CustomTextsController < ApplicationController
   end
 
   def custom_texts_params
-    params.require(:custom_text).permit(:erg, :program)
+    params.require(:custom_text).permit(CustomText.keys)
   end
 end
