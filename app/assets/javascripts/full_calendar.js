f66a12a59aff602ce75447cf6f71cf69e4caac65
Anatolii Borysov <borisano@gmail.com>
2017-05-19 20:24:16 +0300

    Make calendar filters always visible

--- app/assets/javascripts/full_calendar.js
+++ app/assets/javascripts/full_calendar.js
@@ -1,20 +1,5 @@
 $(document).on('ready page:load', function(){
-  $('.calendar-filters').toggle();
-  listenCalendarVisibility();
   applyFullCalendar();
-
-  function listenCalendarVisibility() {
-    $('.calendar-filters-visibility').on('click', function() {
-      $('.select2').css('width', '100%');
-      $('.calendar-filters').toggle();
-      if($('.calendar-filters').is(':visible')) {
-        $(this).text($(this).data('hide-text'));
-      }
-      else {
-        $(this).text($(this).data('show-text'));
-      }
-    });
-  }
 
   function applyFullCalendar(selector) {
     selector = selector || '#calendar';
