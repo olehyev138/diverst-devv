358a19eec20dd9b260b1b82c2a04c0eb41d7ef96
Anatolii Borysov <borisano@gmail.com>
2017-06-06 17:17:30 +0300

    Add saml-for_enterprise to links in mailers

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -1,6 +1,7 @@
 class CampaignMailer < ApplicationMailer
   def invitation(inv)
     @invitation = inv
+    @enterprise_id = @invitation.user.enterprise.id
     mail(to: inv.user.email, subject: 'Help your coworkers solve a problem')
     inv.update(email_sent: true)
   end
