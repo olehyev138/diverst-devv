ee62c9f8856ade60f0b6d5a611848643feb50c52
Anatolii Borysov <borisano@gmail.com>
2017-06-06 16:45:03 +0300

    Add redirect option to Sessions#new action

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,17 @@ class Users::SessionsController < Devise::SessionsController
 
   after_filter :after_login, :only => :create
 
+  def new
+    if params[:saml_for_enterprise]
+      enterprise_id = params[:saml_for_enterprise]
+      redirect_to sso_enterprise_saml_index_url(enterprise_id: enterprise_id)
+
+      return
+    end
+
+    super
+  end
+
   def after_login
     track_activity(current_user, :login)
   end
