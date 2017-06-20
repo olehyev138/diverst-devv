3a8818c3479b4888593a2c0d9d70382fb0c611f4
Anatolii Borysov <borisano@gmail.com>
2017-06-20 09:52:10 +0300

    Add OEmbed gem. Use it to fetch the info

--- app/services/social_media/importer.rb
+++ app/services/social_media/importer.rb
@@ -1,13 +1,16 @@
+require 'oembed'
+
 class SocialMedia::Importer
   def self.url_to_embed(url)
     return nil unless self.valid_url? url
 
-<<-eos
- <div class="fb-post" data-href="https://www.facebook.com/LGBTQ/photos/a.559035344122874.147187.545385972154478/1885612558131806/?type=3&amp;theater" data-width="500" data-show-text="true"><blockquote cite="https://www.facebook.com/LGBTQ/posts/1885612558131806:0" class="fb-xfbml-parse-ignore"><p>Celebrating Pride? Now you can add a colorful background to your text posts! To access these limited-edition...</p>Posted by <a href="https://www.facebook.com/LGBTQ/">LGBTQ&#064;Facebook</a> on&nbsp;<a href="https://www.facebook.com/LGBTQ/posts/1885612558131806:0">Friday, June 16, 2017</a></blockquote></div>
-eos
+    OEmbed::Providers.register_all
+    resource = OEmbed::Providers.get(url)
+
+    resource.html
   end
 
   def self.valid_url?(url)
-    true
+    OEmbed::Providers.find(url).present?
   end
 end
