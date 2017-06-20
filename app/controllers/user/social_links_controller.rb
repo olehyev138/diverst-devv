851359b3bc8b6956336fcb3f4d2b234d22b6df3d
Anatolii Borysov <borisano@gmail.com>
2017-06-20 18:53:35 +0300

    Implement social link deletion procedure

--- app/controllers/user/social_links_controller.rb
+++ app/controllers/user/social_links_controller.rb
@@ -1,6 +1,8 @@
 class User::SocialLinksController < ApplicationController
   before_action :authenticate_user!
 
+  before_action :set_social_link, only: [:destroy]
+
   layout 'user'
 
   def index
@@ -23,6 +25,18 @@ class User::SocialLinksController < ApplicationController
     end
   end
 
+  def destroy
+    authorize @social_link
+
+    if @social_link.destroy
+      flash[:notice] = "Your social post was removed"
+    else
+      flash[:alert] = "An error occured while deleting your social post"
+    end
+
+    redirect_to action: :index
+  end
+
   protected
 
   def social_links_params
@@ -31,5 +45,9 @@ class User::SocialLinksController < ApplicationController
       .permit(
         :url
       )
+  end
+
+  def set_social_link
+    @social_link = current_user.social_links.find(params[:id])
   end
 end
