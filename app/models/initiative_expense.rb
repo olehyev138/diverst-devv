cdc1361314cbc4560e8a8171cdd3ed5b7a917aa6
Gabriel Givigier <gabriel@givigier.com>
2017-08-08 14:12:33 -0300

    Add validation to comments models

--- app/models/initiative_expense.rb
+++ app/models/initiative_expense.rb
@@ -1,4 +1,8 @@
 class InitiativeExpense < ActiveRecord::Base
   belongs_to :initiative
   belongs_to :owner, class_name: "User"
+
+  validates :initiative, presence: true
+  validates :owner, presence: true
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
