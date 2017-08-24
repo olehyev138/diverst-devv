3d38dc4e1db7dc53f9fdacf6a3d24191c45f4930
Girgy <girgalicious@gmail.com>
2017-08-24 02:38:44 +0000

    Tests - CampaignsGroup model spec tests

--- spec/models/campaigns_group_spec.rb
+++ spec/models/campaigns_group_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe CampaignsGroup, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    describe 'when validating' do
+        let(:campaigns_group) { build_stubbed(:campaigns_group) }
+        
+        it { expect(campaigns_group).to belong_to(:campaign) }
+        it { expect(campaigns_group).to belong_to(:group) }
+    end
 end
