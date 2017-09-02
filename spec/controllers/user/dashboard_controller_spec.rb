bb75d07f99bb23f80db5984acf2727d46c9f4dd1
Girgy <girgalicious@gmail.com>
2017-09-02 13:36:48 +0000

    Tests - User::DashboardController spec tests

--- spec/controllers/user/dashboard_controller_spec.rb
+++ spec/controllers/user/dashboard_controller_spec.rb
@@ -1,13 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User::DashboardController, type: :controller do
-  let(:user) { create :user }
-
-  describe 'GET #home' do
-    it 'should be implemented'
-  end
-
-  describe 'GET #rewards' do
-    it 'should be implemented'
-  end
+    let(:user) { create :user }
+    
+    login_user_from_let
+    
+    describe 'GET #home' do
+        it "returns success" do
+            get :home
+            expect(response).to be_success
+        end
+    end
+    
+    describe 'GET #rewards' do
+        it "returns success" do
+            get :rewards
+            expect(response).to be_success
+        end
+    end
 end
