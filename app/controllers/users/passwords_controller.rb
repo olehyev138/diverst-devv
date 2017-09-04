c12bcf4f879908347c54d98a2cac075d8297f9f1
Girgy <girgalicious@gmail.com>
2017-09-04 19:22:28 +0000

    Tests - Users::PasswordsController spec tests

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,13 +1,14 @@
 class Users::PasswordsController < Devise::PasswordsController
 
-  # This was overwritten to behave the same way for correct and incorrect emails
-  # to prevent brootforse username lookup
-  # POST /resource/password
-  def create
-    self.resource = resource_class.send_reset_password_instructions(resource_params)
-    yield resource if block_given?
+    # This was overwritten to behave the same way for correct and incorrect emails
+    # to prevent brute force username lookup
+    # POST /resource/password
 
-    flash[:notice] = "You will shortly receive password reset instructions to email"
-    respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
-  end
+    def create
+        self.resource = resource_class.send_reset_password_instructions(resource_params)
+        yield resource if block_given?
+
+        flash[:notice] = "You will shortly receive password reset instructions to email"
+        respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
+    end
 end
