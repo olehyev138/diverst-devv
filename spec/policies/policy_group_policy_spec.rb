be1fb0ea9c804a34b62286b41ecd7f84d975cfb0
Girgy <girgalicious@gmail.com>
2017-09-28 21:54:38 +0000

    Tests - PolicyGroupPolicy spec tests

--- spec/policies/policy_group_policy_spec.rb
+++ spec/policies/policy_group_policy_spec.rb
@@ -1,28 +1,52 @@
 require 'rails_helper'
 
-RSpec.describe PolicyGroupPolicy do
+RSpec.describe PolicyGroupPolicy, :type => :policy do
+    
+    let(:policy_group){ create(:policy_group, :global_settings_manage => true)}
+    let(:enterprise) {create(:enterprise, :policy_groups => [policy_group])}
+    let(:user){ create(:user, :enterprise => enterprise) }
+    let(:policy_group_2){ create(:policy_group, :global_settings_manage => false)}
+    let(:no_access) { create(:user, :policy_group => policy_group_2) }
+    
+    subject { described_class }
 
-  let(:user) { User.new }
+    permissions :show? do
+        it "allows access" do
+            expect(subject).to permit(user, policy_group)
+        end
 
-  subject { described_class }
+        it "doesn't allow access" do
+            expect(subject).to_not permit(no_access, policy_group_2)
+        end
+    end
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+    permissions :create? do
+        it "allows access" do
+            expect(subject).to permit(user, policy_group)
+        end
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+        it "doesn't allow access" do
+            expect(subject).to_not permit(no_access, policy_group_2)
+        end
+    end
 
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+    permissions :update? do
+        it "allows access" do
+            expect(subject).to permit(user, policy_group)
+        end
 
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+        it "doesn't allow access" do
+            expect(subject).to_not permit(no_access, policy_group_2)
+        end
+    end
 
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+    permissions :destroy? do
+        it "allows access" do
+            expect(subject).to permit(user, policy_group)
+        end
+
+        it "doesn't allow access" do
+            expect(subject).to_not permit(no_access, policy_group_2)
+        end
+    end
 end
