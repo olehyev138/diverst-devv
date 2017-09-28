fc303fd3008cc3e1df7c8547d02a05c49ade1832
Girgy <girgalicious@gmail.com>
2017-09-28 22:42:33 +0000

    Tests - EventPolicy spec tests

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,29 +1,31 @@
 class Event < ActiveRecord::Base
-  has_many :budgets, as: :subject
-  has_many :events_segments
-  has_many :segments, through: :events_segments
-  belongs_to :group
-  has_many :event_attendances
-  has_many :attendees, through: :event_attendances, source: :user
-  has_many :event_invitees
-  has_many :invitees, through: :event_invitees, source: :user
-  has_many :comments, class_name: "EventComment"
-  has_many :fields, as: :container
+    
+    has_many :budgets, as: :subject
+    has_many :events_segments
+    has_many :segments, through: :events_segments
+    belongs_to :group
+    belongs_to :owner, :class_name => "User"
+    has_many :event_attendances
+    has_many :attendees, through: :event_attendances, source: :user
+    has_many :event_invitees
+    has_many :invitees, through: :event_invitees, source: :user
+    has_many :comments, class_name: "EventComment"
+    has_many :fields, as: :container
 
-  scope :past, -> { where('end < ?', Time.current).order(start: :desc) }
-  scope :upcoming, -> { where('start > ?', Time.current).order(start: :asc) }
-  scope :ongoing, -> { where('start <= ?', Time.current).where('end >= ?', Time.current).order(start: :desc) }
+    scope :past, -> { where('end < ?', Time.current).order(start: :desc) }
+    scope :upcoming, -> { where('start > ?', Time.current).order(start: :asc) }
+    scope :ongoing, -> { where('start <= ?', Time.current).where('end >= ?', Time.current).order(start: :desc) }
 
-  has_attached_file :picture, styles: { medium: '1000x300>', thumb: '100x100>' }, default_url: ActionController::Base.helpers.image_path('/assets/missing.png'), s3_permissions: "private"
-  validates_attachment_content_type :picture, content_type: %r{\Aimage\/.*\Z}
+    has_attached_file :picture, styles: { medium: '1000x300>', thumb: '100x100>' }, default_url: ActionController::Base.helpers.image_path('/assets/missing.png'), s3_permissions: "private"
+    validates_attachment_content_type :picture, content_type: %r{\Aimage\/.*\Z}
 
-  accepts_nested_attributes_for :fields, reject_if: :all_blank, allow_destroy: true
+    accepts_nested_attributes_for :fields, reject_if: :all_blank, allow_destroy: true
 
-  def time_string
-    if start.to_date == self.end.to_date # If the event starts and ends on the same day
-      "#{start.to_s :dateonly} from #{start.to_s :ampmtime} to #{self.end.to_s :ampmtime}"
-    else
-      "From #{start.to_s :datetime} to #{self.end.to_s :datetime}"
+    def time_string
+        if start.to_date == self.end.to_date # If the event starts and ends on the same day
+            "#{start.to_s :dateonly} from #{start.to_s :ampmtime} to #{self.end.to_s :ampmtime}"
+        else
+            "From #{start.to_s :datetime} to #{self.end.to_s :datetime}"
+        end
     end
-  end
 end
--- spec/policies/event_policy_spec.rb
+++ spec/policies/event_policy_spec.rb
@@ -1,28 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe EventPolicy do
+RSpec.describe EventPolicy, :type => :policy do
+    
+    let(:policy_group){ create(:policy_group, :global_settings_manage => true)}
+    let(:enterprise) {create(:enterprise, :policy_groups => [policy_group])}
+    let(:user){ create(:user, :enterprise => enterprise) }
+    let(:policy_group_2){ create(:policy_group, :events_index => false, :events_create => false, :events_manage => false )}
+    let(:no_access) { create(:user, :policy_group => policy_group_2) }
+    let(:event){ create(:event)}
+    
+    subject { described_class }
 
-  let(:user) { User.new }
+    permissions :create?, :update?, :destroy? do
+        it "allows access" do
+            expect(subject).to permit(user, event)
+        end
 
-  subject { described_class }
-
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+        it "doesn't allow access" do
+            expect(subject).to_not permit(no_access, event)
+        end
+    end
 end
