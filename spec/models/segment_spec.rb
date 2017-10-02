d3c9c6a0c1eb3913f472242e9921a642f753de47
Girgy <girgalicious@gmail.com>
2017-10-02 21:32:50 +0000

    Model-Validations - remove skip in segment model spec test

--- spec/models/segment_spec.rb
+++ spec/models/segment_spec.rb
@@ -40,7 +40,7 @@ RSpec.describe Segment, type: :model do
         end
     end
 
-    describe 'when describing callbacks', :skip => "Need to fix - written by Gabriel" do
+    describe 'when describing callbacks' do
         it "should reindex users on elasticsearch after create" do
             segment = build(:segment)
             TestAfterCommit.with_commits(true) do
