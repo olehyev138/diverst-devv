31204870b48bfa31af1e9e0ab99eb9d2bc6c9861
Girgy <girgalicious@gmail.com>
2017-10-17 22:19:44 +0000

    usergroupnotificationfix - user group scope

--- app/jobs/user_group_notification_job.rb
+++ app/jobs/user_group_notification_job.rb
@@ -5,31 +5,15 @@ class UserGroupNotificationJob < ActiveJob::Base
     User.includes(user_groups: :group).find_in_batches(batch_size: 200) do |users|
       users.each do |user|
         groups = []
-        user.user_groups.notifications_status(notifications_frequency).each do |user_group|
+        user.user_groups.accepted_users.notifications_status(notifications_frequency).each do |user_group|
           group = user_group.group
-          # check if pending_users is enabled for the group
-          # we automatically add the group if not
-          if !group.pending_users.enabled?
-            frequency_range = get_frequency_range(user_group.notifications_frequency)
-            groups << {
-              group: user_group.group,
-              events_count: get_events_count(user, group, frequency_range),
-              messages_count: get_messages_count(user, group, frequency_range),
-              news_count: get_news_count(user, group, frequency_range)
-            }
-          else
-            # pending_users is enabled and we need to check if user
-            # is not a pending member
-            if user_group.accepted_member?
-              frequency_range = get_frequency_range(user_group.notifications_frequency)
-              groups << {
-                group: user_group.group,
-                events_count: get_events_count(group, frequency_range),
-                messages_count: get_messages_count(group, frequency_range),
-                news_count: get_news_count(group, frequency_range)
-              }
-            end
-          end
+          frequency_range = get_frequency_range(user_group.notifications_frequency)
+          groups << {
+            group: user_group.group,
+            events_count: get_events_count(user, group, frequency_range),
+            messages_count: get_messages_count(user, group, frequency_range),
+            news_count: get_news_count(user, group, frequency_range)
+          }
         end
         if have_updates?(groups)
           UserGroupMailer.notification(user, groups).deliver_now
