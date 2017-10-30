7f6683c0e0e073d5f999c3096a25002ba1302d6a
Anatolii Borysov <borisano@gmail.com>
2017-10-30 18:40:18 +0200

    Add privacy statement page

--- app/controllers/user/dashboard_controller.rb
+++ app/controllers/user/dashboard_controller.rb
@@ -1,24 +1,30 @@
 class User::DashboardController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_enterprise
 
   layout 'user'
 
   def home
-    @enterprise = current_user.enterprise
     @upcoming_events = current_user.initiatives.upcoming.includes(:owner_group).limit(4) + current_user.invited_initiatives.upcoming.includes(:owner_group).limit(3)
     @posts = posts
     @messages = current_user.messages.includes(:group, :owner).limit(3)
   end
 
   def rewards
-    @enterprise = current_user.enterprise
     @reward_actions = @enterprise.reward_actions.order(points: :asc)
     @rewards = @enterprise.rewards.order(points: :asc)
     @badges = @enterprise.badges.order(points: :asc)
   end
-  
+
+  def privacy_statement
+  end
+
   private
-  
+
+  def set_enterprise
+    @enterprise = current_user.enterprise
+  end
+
   def posts
     posts = NewsFeedLink.joins(:news_feed)
               .joins(joins)
