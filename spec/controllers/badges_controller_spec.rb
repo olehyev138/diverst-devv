b8c347d3f6bdcac778be1ce4c79a3d7eb44269a1
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-11-02 12:46:53 +0000

    Improve specs in badges_controller_spec

--- spec/controllers/badges_controller_spec.rb
+++ spec/controllers/badges_controller_spec.rb
@@ -4,26 +4,49 @@ RSpec.describe BadgesController, type: :controller do
   let(:enterprise){ create(:enterprise) }
   let(:user){ create(:user, enterprise: enterprise) }
   
+  def c_t(type)
+    @custom_text ||= current_user.enterprise.custom_text rescue CustomText.new
+    @custom_text.send("#{type}_text")
+  end
+
   describe "POST#new" do
     describe "with logged in user" do
       login_user_from_let
+      before { post :new }
+
         it "returns success" do
-          post :new
           expect(response).to be_success
         end
 
         it "returns a new badge object" do 
-          post :new
           expect(assigns[:badge]).to be_a_new(Badge)
         end
+
+        it "renders new template" do 
+          expect(response).to render_template :new
+        end
+    end
+
+    describe "without logged in user" do 
+      before { post :new }
+
+      it "redirect user to users/sign_in path " do 
+        expect(response).to redirect_to new_user_session_path
+      end
+
+      it 'returns status code of 302' do
+        expect(response).to have_http_status(302)
+      end
     end
   end
 
   describe "POST#create" do
     describe "with logged in user" do
+
       login_user_from_let
 
       context "with valid parameters" do
+
         it "creates a new badge" do
           expect{ post :create, badge: attributes_for(:badge_params) }
             .to change(enterprise.badges, :count).by(1)
@@ -34,8 +57,10 @@ RSpec.describe BadgesController, type: :controller do
           expect(response).to redirect_to rewards_path
         end
         
-        it "flashes notice" do
-          expect(flash[:notice])
+        it "render a notice flash message" do
+          post :create, badge: attributes_for(:badge_params)
+
+          expect(flash[:notice]).to eq "Your #{c_t(:badge)} was created"
         end
       end
 
@@ -50,9 +75,22 @@ RSpec.describe BadgesController, type: :controller do
           expect(response).to render_template :new
         end
         
-        it "flashes alert" do
-          expect(flash[:alert])
+        it "renders a flash alert message" do
+          post :create, badge: attributes_for(:badge_params, label: "")
+          expect(flash[:alert]).to eq "Your #{ c_t(:badge) } was not created. Please fix the errors"
         end
+      end
+    end
+
+    describe "without a logged in user" do 
+      before { post :create, badge: attributes_for(:badge_params, label: "") }
+
+      it "redirect user to users/sign_in path " do 
+        expect(response).to redirect_to new_user_session_path
+      end
+
+      it 'returns status code of 302' do
+        expect(response).to have_http_status(302)
       end
     end
   end
@@ -86,6 +124,18 @@ RSpec.describe BadgesController, type: :controller do
         end
       end
     end
+
+    describe "without a logged in user" do 
+      before { get :edit, id: badge.id }
+
+      it "redirect user to users/sign_in path " do 
+        expect(response).to redirect_to new_user_session_path
+      end
+
+      it 'returns status code of 302' do
+        expect(response).to have_http_status(302)
+      end
+    end
   end
 
   describe "PATCH#update" do
@@ -95,7 +145,7 @@ RSpec.describe BadgesController, type: :controller do
       login_user_from_let
 
       context "with valid parameters" do
-        before(:each){ patch :update, id: badge.id, badge: attributes_for(:badge, points: 20) }
+        before { patch :update, id: badge.id, badge: attributes_for(:badge, points: 20) }
 
         it "updates the badge" do
           badge.reload
@@ -106,13 +156,13 @@ RSpec.describe BadgesController, type: :controller do
           expect(response).to redirect_to rewards_path
         end
         
-        it "flashes notice" do 
-          expect(flash[:notice])
+        it "returns a flash notice message" do 
+          expect(flash[:notice]).to eq "Your #{ c_t(:badge) } was updated"
         end
       end
 
       context "with invalid parameters" do
-        before(:each){ patch :update, id: badge.id, badge: attributes_for(:badge, points: "") }
+        before { patch :update, id: badge.id, badge: attributes_for(:badge, points: "") }
 
         it "does not update the badge" do
           badge.reload
@@ -123,17 +173,31 @@ RSpec.describe BadgesController, type: :controller do
           expect(response).to render_template :edit
         end
         
-        it "flashes alert" do
-          expect(flash[:alert])
+        it "render an alert flash message" do
+          expect(flash[:alert]).to eq "Your #{ c_t(:badge) } was not updated. Please fix the errors"
         end
       end
     end
+
+    describe "with user not logged in" do 
+      before { patch :update, id: badge.id, badge: attributes_for(:badge, points: 10) }
+
+      it "redirect user to users/sign_in path " do 
+        expect(response).to redirect_to new_user_session_path
+      end
+
+      it 'returns status code of 302' do
+        expect(response).to have_http_status(302)
+      end
+    end
+
   end
 
   describe "DELETE#destroy" do
+    let!(:badge){ create(:badge, enterprise: enterprise) }
+    
     describe "with logged in user" do
       login_user_from_let
-      let!(:badge){ create(:badge, enterprise: enterprise) }
 
       it "destroy the badge" do
         expect{ delete :destroy, id: badge.id }.to change(enterprise.badges, :count).by(-1)
@@ -144,8 +208,21 @@ RSpec.describe BadgesController, type: :controller do
         expect(response).to redirect_to rewards_path
       end
       
-      it "flashes notice" do
-        expect(flash[:notice])
+      it "renders flash notice message" do
+        delete :destroy, id: badge.id
+        expect(flash[:notice]).to eq "Your #{ c_t(:badge) } was deleted"
+      end
+    end
+
+    describe "with a user not logged in" do 
+      before { delete :destroy, id: badge.id }
+
+      it "redirect user to users/sign_in path " do 
+        expect(response).to redirect_to new_user_session_path
+      end
+
+      it 'returns status code of 302' do
+        expect(response).to have_http_status(302)
       end
     end
   end
