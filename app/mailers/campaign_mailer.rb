1a1137727662126433d18e787bd4d893f04486b9
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-11-09 07:50:30 +0000

    Add Group name to mailer classes

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -1,11 +1,11 @@
 class CampaignMailer < ApplicationMailer
-    
+
     def invitation(inv)
         @invitation = inv
         @enterprise_id = @invitation.user.enterprise.id
         @group = Group.find_by(owner_id: @invitation.user.id)
         mail(to: inv.user.email, subject: ' You are invited to join a #{@group.name} online conversation in Diverst!')
-        
+
         inv.email_sent = true
         inv.save!
     end
--- app/mailers/poll_mailer.rb
+++ app/mailers/poll_mailer.rb
@@ -1,6 +1,7 @@
 class PollMailer < ApplicationMailer
   def invitation(poll, user)
     @poll = poll
-    mail(to: user.email, subject: "You are invited to participate in a <Group name> survey!")
+    @group = Group.find_by(owner_id: user.id)
+    mail(to: user.email, subject: "You are invited to participate in a #{@group.name} survey!")
   end
 end
