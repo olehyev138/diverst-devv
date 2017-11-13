f2121734b3471dea65a9e926d642b26781671a71
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-11-13 15:45:21 +0000

    Remove again

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -4,7 +4,6 @@ class CampaignMailer < ApplicationMailer
         @invitation = inv
         @enterprise_id = @invitation.user.enterprise.id
         mail(to: inv.user.email, subject: ' You are invited to join a <Group name > online conversation in Diverst!')
-
         inv.email_sent = true
         inv.save!
     end
