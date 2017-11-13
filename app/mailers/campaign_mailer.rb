dc22c8d8c0be10ee7cf0c10867ca984ddca4a37f
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-11-13 16:45:12 +0000

    Revert changes

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -3,7 +3,7 @@ class CampaignMailer < ApplicationMailer
     def invitation(inv)
         @invitation = inv
         @enterprise_id = @invitation.user.enterprise.id
-        mail(to: inv.user.email, subject: ' You are invited to join a <Group name > online conversation in Diverst!')
+        mail(to: inv.user.email, subject: 'Help your coworkers solve a problem')
         inv.email_sent = true
         inv.save!
     end
--- app/mailers/poll_mailer.rb
+++ app/mailers/poll_mailer.rb
@@ -1,6 +1,6 @@
 class PollMailer < ApplicationMailer
   def invitation(poll, user)
     @poll = poll
-    mail(to: user.email, subject: "You are invited to participate in a <Group name> survey!")
+    mail(to: user.email, subject: "You are invited to participate in a #{@poll.title} survey!")
   end
 end
