00bdd4a5d23eb2b0ede9b320660e3ff310924a49
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-11-13 15:26:33 +0000

    Remove incorrect logic from mailer

--- app/mailers/poll_mailer.rb
+++ app/mailers/poll_mailer.rb
@@ -1,7 +1,6 @@
 class PollMailer < ApplicationMailer
   def invitation(poll, user)
     @poll = poll
-    @group = Group.find_by(owner_id: user.id)
-    mail(to: user.email, subject: "You are invited to participate in a #{@group.name} survey!")
+    mail(to: user.email, subject: "You are invited to participate in a <Group name> survey!")
   end
 end
