abc2c9c7bb35b47835b9489051bac0930432b692
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-11-14 10:13:55 +0000

    Run migrations on development and test environment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20171113195713) do
 
   create_table "activities", force: :cascade do |t|
