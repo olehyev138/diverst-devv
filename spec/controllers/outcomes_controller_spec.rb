76e3c93c969f195b9491dd123495ac4b6732b1b1
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-11-20 22:26:22 +0000

    Remove trailing spaces

--- spec/controllers/outcomes_controller_spec.rb
+++ spec/controllers/outcomes_controller_spec.rb
@@ -7,81 +7,81 @@ RSpec.describe OutcomesController, type: :controller do
     let!(:user) { create :user, enterprise: enterprise}
     let!(:group) { create :group, enterprise: user.enterprise }
     let!(:outcome) {create :outcome, group_id: group.id}
-    
+
     describe 'GET #index' do
         def get_index(group_id = -1)
             get :index, group_id: group_id
         end
-        
+
         context 'with logged user' do
             login_user_from_let
-            
+
             before { get_index(group.id) }
-            
-            it "render index template" do 
+
+            it "render index template" do
                 expect(response).to render_template :index
             end
         end
 
-        context "with non-logged in user" do 
+        context "with non-logged in user" do
             before { get_index(group.id) }
             it_behaves_like "redirect user to users/sign_in path"
         end
     end
 
-    
+
     describe 'GET #new', skip: "missing template" do
         def get_new(group_id = -1)
             get :new, group_id: group_id
         end
-        
+
         context 'with logged user' do
             login_user_from_let
-            
+
             before { get_new(group.id) }
-            
-            it "returns a new Outcome object" do 
+
+            it "returns a new Outcome object" do
                 expect(assigns[:outcome]).to be_a_new(Outcome)
             end
         end
     end
 
-    
+
     describe 'GET #edit', skip: "missing template" do
         def get_edit(group_id = -1)
             get :edit, group_id: group_id, :id => outcome.id
         end
-        
+
         context 'with logged user' do
             login_user_from_let
-            
+
             before { get_edit(group.id) }
-            
+
             it 'return success' do
                 expect(response).to be_success
             end
         end
     end
 
-    
+
     describe 'POST #create' do
         def post_create(group_id = -1)
             post :create, group_id: group_id, :outcome => {:name => "created", :group_id => group.id}
         end
-        
+
         context 'with logged user' do
             login_user_from_let
-            
+
             before { post_create(group.id) }
-            
+
             it 'redirects to index' do
                 expect(response).to redirect_to action: :index
             end
-            
+
             it 'creates the outcome' do
                 expect(Outcome.count).to eq(2)
             end
-            
+
             it 'flashes a notice message' do
                 expect(flash[:notice]).to eq "Your #{ c_t(:outcome) } was created"
             end
@@ -91,61 +91,61 @@ RSpec.describe OutcomesController, type: :controller do
             login_user_from_let
             before {post :create, group_id: group.id, :outcome => {:name => "", :group_id => group.id}}
 
-            it "flashes an alert message" do 
+            it "flashes an alert message" do
                 expect(flash[:alert]).to eq "Your #{ c_t(:outcome) } was not created. Please fix the errors"
             end
 
-            it "render new template" do 
+            it "render new template" do
                 expect(response).to render_template :new
             end
         end
 
-        context "without logged in user" do 
+        context "without logged in user" do
             before { post :create, group_id: group.id, :outcome => {:name => "created", :group_id => group.id} }
             it_behaves_like "redirect user to users/sign_in path"
         end
     end
-    
+
     describe 'PATCH #update' do
         def patch_update(group_id = -1)
             patch :update, group_id: group_id, :id => outcome.id, :outcome => {:name => "updated"}
         end
-        
+
         context 'with logged user' do
             login_user_from_let
-            
+
             before { patch_update(group.id) }
-            
+
             it 'redirects to index' do
                 expect(response).to redirect_to action: :index
             end
-            
+
             it 'updates the outcome' do
                 outcome.reload
                 expect(outcome.name).to eq("updated")
             end
-            
+
             it 'flashes' do
                 expect(flash[:notice])
             end
         end
     end
-    
+
     describe 'DELETE #destroy' do
         def delete_destroy(group_id = -1)
             delete :destroy, group_id: group_id, :id => outcome.id
         end
-        
+
         context 'with logged user' do
             login_user_from_let
-            
+
             it 'redirects to index' do
                 delete_destroy(group.id)
                 expect(response).to redirect_to action: :index
             end
 
-            it "destroys outcome" do 
-                expect{ delete_destroy(group.id) }.to change(Outcome, :count).by(-1)                
+            it "destroys outcome" do
+                expect{ delete_destroy(group.id) }.to change(Outcome, :count).by(-1)
             end
         end
     end
--- spec/controllers/policy_groups_controller_spec.rb
+++ spec/controllers/policy_groups_controller_spec.rb
@@ -4,82 +4,82 @@ RSpec.describe PolicyGroupsController, type: :controller do
     let(:enterprise) {create :enterprise}
     let(:user) { create :user, enterprise: enterprise}
     let!(:policy_group) { create :policy_group, enterprise: enterprise }
-    
+
     describe 'GET #index' do
         context 'with logged user' do
             login_user_from_let
-            
+
             before { get :index }
-            
+
             it 'return success' do
                 expect(response).to be_success
             end
         end
     end
-    
+
     describe 'GET #new' do
         context 'with logged user' do
             login_user_from_let
-            
+
             before { get :new }
-            
+
             it 'return success' do
                 expect(response).to be_success
             end
         end
     end
-    
+
     describe 'POST #create' do
         context 'with logged user' do
             login_user_from_let
-            
+
             before { post :create, :policy_group => {:name => "name"}}
-            
+
             it 'redirect_to' do
                 expect(response).to redirect_to action: :index
             end
-            
+
             it 'changes the policy_group count' do
                 expect(PolicyGroup.count).to eq(3)
             end
-            
+
             it 'creates the policy_group' do
                 expect(PolicyGroup.last.name).to eq("name")
             end
-            
+
             it 'flashes' do
                 expect(flash[:notice])
             end
         end
     end
-    
+
     describe 'PATCH #update' do
         context 'with logged user' do
             login_user_from_let
-            
+
             before { patch :update, :id => policy_group.id, :policy_group => {:name => "updated"}}
-            
+
             it 'redirect_to' do
                 expect(response).to redirect_to action: :index
             end
-            
+
             it 'updates the policy_group' do
                 policy_group.reload
                 expect(policy_group.name).to eq("updated")
             end
-            
+
             it 'flashes' do
                 expect(flash[:notice])
             end
         end
     end
-    
+
     describe 'DELETE #destroy' do
         context 'with logged user' do
             login_user_from_let
-            
+
             before { delete :destroy, :id => policy_group.id}
-            
+
             it 'redirect_to' do
                 expect(response).to redirect_to action: :index
             end
