25b2ad31b81b3c9307465c7608b4280dc3d71980
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-11-21 09:13:36 +0000

    Improve policy groups spec file

--- spec/controllers/policy_groups_controller_spec.rb
+++ spec/controllers/policy_groups_controller_spec.rb
@@ -11,11 +11,21 @@ RSpec.describe PolicyGroupsController, type: :controller do
 
             before { get :index }
 
-            it 'return success' do
-                expect(response).to be_success
+            it "returns enterprise policy groups" do 
+                expect(assigns[:policy_groups].count).to eq 1
+            end
+
+            it "render index template" do 
+                expect(response).to render_template :index
             end
         end
+
+        context "without a logged in user" do 
+            before { get :index }
+            it_behaves_like "redirect user to users/sign_in path"
+        end
     end
+
 
     describe 'GET #new' do
         context 'with logged user' do
@@ -23,66 +33,127 @@ RSpec.describe PolicyGroupsController, type: :controller do
 
             before { get :new }
 
-            it 'return success' do
-                expect(response).to be_success
+            it "return a new policy group" do 
+                expect(assigns[:policy_group]).to be_a_new(PolicyGroup)
+            end
+
+            it "render new template" do 
+                expect(response).to render_template :new
             end
         end
+
+        context "without a logged in user" do 
+            before { get :new }
+            it_behaves_like "redirect user to users/sign_in path"
+        end
     end
+
 
     describe 'POST #create' do
         context 'with logged user' do
-            login_user_from_let
+            context "with correct params" do 
+                login_user_from_let
 
-            before { post :create, :policy_group => {:name => "name"}}
+                before { post :create, :policy_group => {:name => "name"}}
 
-            it 'redirect_to' do
-                expect(response).to redirect_to action: :index
+                it 'redirect_to' do
+                    expect(response).to redirect_to action: :index
+                end
+
+                it 'changes the policy_group count' do
+                    expect(PolicyGroup.count).to eq(3)
+                end
+
+                it 'creates the policy_group' do
+                    expect(PolicyGroup.last.name).to eq("name")
+                end
+
+                it 'flashes a notice message' do
+                    expect(flash[:notice]).to eq "Your policy group was created"
+                end
             end
 
-            it 'changes the policy_group count' do
-                expect(PolicyGroup.count).to eq(3)
-            end
+            context "with incorrect params" do 
+                login_user_from_let
 
-            it 'creates the policy_group' do
-                expect(PolicyGroup.last.name).to eq("name")
-            end
+                before {  post :create, :policy_group => { :name => nil }}
 
-            it 'flashes' do
-                expect(flash[:notice])
+                it "flashes an alert message" do 
+                    expect(flash[:alert]).to eq "Your policy group was not created. Please fix the errors"
+                end
+
+                it "render new template" do 
+                    expect(response).to render_template :new
+                end
             end
         end
+
+        context "without a logged in user" do 
+            before { post :create, :policy_group => { :name => "name" } }
+            it_behaves_like "redirect user to users/sign_in path"
+        end
     end
+
 
     describe 'PATCH #update' do
         context 'with logged user' do
-            login_user_from_let
+            context "with correct params" do
+                login_user_from_let
 
-            before { patch :update, :id => policy_group.id, :policy_group => {:name => "updated"}}
+                before { patch :update, :id => policy_group.id, :policy_group => {:name => "updated"}}
 
-            it 'redirect_to' do
-                expect(response).to redirect_to action: :index
+                it 'redirect_to' do
+                    expect(response).to redirect_to action: :index
+                end
+
+                it 'updates the policy_group' do
+                    policy_group.reload
+                    expect(policy_group.name).to eq("updated")
+                end
+
+                it 'flashes a notice message' do
+                    expect(flash[:notice]).to eq "Your policy group was updated"
+                end
             end
 
-            it 'updates the policy_group' do
-                policy_group.reload
-                expect(policy_group.name).to eq("updated")
-            end
+            context "with incorrect params" do 
+                login_user_from_let
+                before { patch :update, :id => policy_group.id, :policy_group => {:name => nil}}
 
-            it 'flashes' do
-                expect(flash[:notice])
+                it "flashes an alert message" do 
+                    expect(flash[:alert]).to eq "Your policy group was not updated. Please fix the errors"
+                end
+
+                it "render edit template" do 
+                    expect(response).to render_template :edit
+                end
             end
         end
+
+        context "without a logged in user" do 
+            before { patch :update, :id => policy_group.id, :policy_group => {:name => "updated"}}
+            it_behaves_like "redirect user to users/sign_in path"
+        end
     end
+
 
     describe 'DELETE #destroy' do
         context 'with logged user' do
             login_user_from_let
 
-            before { delete :destroy, :id => policy_group.id}
-
-            it 'redirect_to' do
+            it 'redirect_to action index' do
+                delete :destroy, :id => policy_group.id
                 expect(response).to redirect_to action: :index
             end
+
+            it "destroys policy_group object" do 
+                expect{delete :destroy, :id => policy_group.id}.to change(PolicyGroup, :count).by(-1)
+            end
+        end
+
+        context "without a logged in user" do 
+           before { delete :destroy, :id => policy_group.id }
+            it_behaves_like "redirect user to users/sign_in path"
         end
     end
 end
