4390a67935f3f0def0a5e544737db7f22e90818a
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-11-21 16:35:38 +0000

    Improve poll_fields_controller_spec.rb file

--- spec/controllers/poll_fields_controller_spec.rb
+++ spec/controllers/poll_fields_controller_spec.rb
@@ -1,22 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe PollFieldsController, type: :controller do
-    let(:enterprise) {create :enterprise}
-    let(:user) { create :user, enterprise: enterprise}
-    let(:poll) {create :poll}
-    let(:field) {create :field, type: "CheckboxField"}
-    
+    let(:enterprise) { create :enterprise }
+    let(:user) { create :user, enterprise: enterprise }
+    let(:poll) { create :poll }
+    let(:field) { create :field, type: "CheckboxField" }
+
     describe 'GET #answer_popularities' do
         context 'with logged user' do
             login_user_from_let
             
-            before { get :answer_popularities, :poll_id => poll.id, :id => field.id}
+            before { get :answer_popularities, :poll_id => poll.id, :id => field.id }
             
-            it 'return success' do
-                expect(response).to be_success
+            it "renders a response in json" do 
+                expect(response.content_type).to eq "application/json" 
             end
         end
+
+        context 'without a logged in user' do 
+            before { get :answer_popularities, :poll_id => poll.id, :id => field.id }
+            it_behaves_like "redirect user to users/sign_in path"
+        end
     end
+
     
     describe 'GET #show' do
         context 'with logged user' do
@@ -24,9 +30,19 @@ RSpec.describe PollFieldsController, type: :controller do
             
             before { get :show, :poll_id => poll.id, :id => field.id}
             
-            it 'return success' do
-                expect(response).to be_success
+            it 'renders show template' do 
+                expect(response).to render_template :show
             end
+
+            it "returns response of a particular poll" do 
+                2.times { FactoryGirl.create :poll_response, poll: poll }
+                expect(assigns[:responses].count).to eq 2
+            end
+        end
+
+        context "without a logged in user" do 
+            before { get :show, :poll_id => poll.id, :id => field.id }
+            it_behaves_like "redirect user to users/sign_in path"
         end
     end
 end
