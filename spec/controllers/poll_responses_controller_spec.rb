9300796015047ccf649ccf26e725cba82a3c073b
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-11-22 10:52:15 +0000

    commit changes before pull

--- spec/controllers/poll_responses_controller_spec.rb
+++ spec/controllers/poll_responses_controller_spec.rb
@@ -4,25 +4,45 @@ RSpec.describe PollResponsesController, type: :controller do
     let(:user){ create(:user) }
     let(:poll){ create(:poll) }
     let(:poll_response){ create :poll_response, poll: poll}
-    login_user_from_let
     
     describe "GET#index", :skip => "MISSING TEMPLATE" do
         context "with logged user" do
+            login_user_from_let
             it "gets the index" do
                 get :index, poll_id: poll.id
                 expect(response).to be_success
             end
         end
     end
+
     
     describe "GET#new" do
         context "with logged user" do
-            it "creates a new poll_response" do
-                get :new, poll_id: poll.id
-                expect(response).to be_success
+            login_user_from_let
+            before { get :new, poll_id: poll.id }
+
+            context "if user already answered" do 
+                before do 
+                    user.poll_responses << poll_response
+                    response = user.poll_responses.where(poll: poll).first
+                end
+                
+                it "redirect to thank you action" do 
+                    expect(response).to redirect_to action: "thank_you"
+                end
             end
+
+            it "creates a new poll_response object" do
+              expect(assigns[:response]).to be_a_new(PollResponse)
+          end
+        end
+
+        context "without a logged in user" do 
+            before { get :new, poll_id: poll.id }
+            it_behaves_like "redirect user to users/sign_in path"
         end
     end
+
 
     describe "POST#create" do
         context "with logged user" do
