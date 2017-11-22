176c4dc81469176992d0c75fb9b8b76ed02b3c10
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-11-22 14:09:42 +0000

    Remove trailing spaces from spec files

--- spec/controllers/poll_fields_controller_spec.rb
+++ spec/controllers/poll_fields_controller_spec.rb
@@ -9,38 +9,38 @@ RSpec.describe PollFieldsController, type: :controller do
     describe 'GET #answer_popularities' do
         context 'with logged user' do
             login_user_from_let
-            
+
             before { get :answer_popularities, :poll_id => poll.id, :id => field.id }
-            
-            it "renders a response in json" do 
-                expect(response.content_type).to eq "application/json" 
+
+            it "renders a response in json" do
+                expect(response.content_type).to eq "application/json"
             end
         end
 
-        context 'without a logged in user' do 
+        context 'without a logged in user' do
             before { get :answer_popularities, :poll_id => poll.id, :id => field.id }
             it_behaves_like "redirect user to users/sign_in path"
         end
     end
 
-    
+
     describe 'GET #show' do
         context 'with logged user' do
             login_user_from_let
-            
+
             before { get :show, :poll_id => poll.id, :id => field.id}
-            
-            it 'renders show template' do 
+
+            it 'renders show template' do
                 expect(response).to render_template :show
             end
 
-            it "returns response of a particular poll" do 
+            it "returns response of a particular poll" do
                 2.times { FactoryGirl.create :poll_response, poll: poll }
                 expect(assigns[:responses].count).to eq 2
             end
         end
 
-        context "without a logged in user" do 
+        context "without a logged in user" do
             before { get :show, :poll_id => poll.id, :id => field.id }
             it_behaves_like "redirect user to users/sign_in path"
         end
--- spec/controllers/poll_responses_controller_spec.rb
+++ spec/controllers/poll_responses_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe PollResponsesController, type: :controller do
     let(:user){ create(:user) }
     let(:poll){ create(:poll) }
     let(:poll_response){ create :poll_response, poll: poll}
-    
+
     describe "GET#index", :skip => "MISSING TEMPLATE" do
         context "with logged user" do
             login_user_from_let
@@ -16,13 +16,13 @@ RSpec.describe PollResponsesController, type: :controller do
         end
     end
 
-    
+
     describe "GET#new" do
         context "with logged user" do
             login_user_from_let
             before { get :new, poll_id: poll.id }
 
-            it "render a new template" do 
+            it "render a new template" do
                 expect(response).to render_template :new
             end
 
@@ -31,7 +31,7 @@ RSpec.describe PollResponsesController, type: :controller do
           end
         end
 
-        context "without a logged in user" do 
+        context "without a logged in user" do
             before { get :new, poll_id: poll.id }
             it_behaves_like "redirect user to users/sign_in path"
         end
@@ -66,7 +66,7 @@ RSpec.describe PollResponsesController, type: :controller do
             end
         end
 
-        context "without a logged in user" do 
+        context "without a logged in user" do
             let!(:poll_response) { attributes_for(:poll_response) }
             let!(:reward_action) { create(:reward_action, enterprise: user.enterprise, key: "survey_response", points: 40) }
             before { post :create, poll_id: poll.id, poll_response: poll_response }
@@ -75,40 +75,40 @@ RSpec.describe PollResponsesController, type: :controller do
         end
     end
 
-    
+
     describe "PATCH#update" do
         context "with valid params" do
             before {patch :update, poll_id: poll.id, id: poll_response.id, poll_response: {anonymous: false}}
 
             it "updates a poll_response" do
-                poll_response.reload 
+                poll_response.reload
                 expect(poll_response.anonymous).to eq false
             end
 
-            it "renders edit template" do 
+            it "renders edit template" do
                 expect(response).to redirect_to assigns[:poll]
             end
         end
     end
 
-    describe "GET#thank_you" do 
-        context "with logged in user" do 
+    describe "GET#thank_you" do
+        context "with logged in user" do
             login_user_from_let
 
             before { get :thank_you, poll_id: poll.id, id: poll_response.id }
 
-            it "renders thank_you template" do 
+            it "renders thank_you template" do
                 expect(response).to render_template :thank_you
             end
         end
 
-        context "without logged in user" do 
+        context "without logged in user" do
             before { get :thank_you, poll_id: poll.id, id: poll_response.id }
             it_behaves_like "redirect user to users/sign_in path"
         end
     end
 
-    
+
     describe "DELETE#destroy" do
         context "successfull delete poll response" do
             it "redirect to index action" do
@@ -116,7 +116,7 @@ RSpec.describe PollResponsesController, type: :controller do
                 expect(response.status).to eq(302)
             end
 
-            it "deletes poll response" do 
+            it "deletes poll response" do
                 poll_response
                 expect{ delete :destroy, poll_id: poll.id, id: poll_response.id }.to change(PollResponse, :count).by(-1)
             end
