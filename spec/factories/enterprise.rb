faac46f2a15951a72ddee0ebca01b2268ac4c9b1
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-11-28 21:49:27 +0000

    Attempt to fix errors on Circle CI

--- spec/factories/enterprise.rb
+++ spec/factories/enterprise.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     name { Faker::Company.name }
     created_at { Date.today }
     cdo_name {Faker::Name.name}
+    sponsor_media { File.new("#{Rails.root}/spec/support/fixtures/files/verizon_logo.png") }
     theme nil
   end
 end
