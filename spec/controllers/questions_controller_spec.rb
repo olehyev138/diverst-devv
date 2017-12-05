edf106a1efab91ce092fe9ca10dc8af4de7a20e0
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-12-05 16:18:06 +0000

    Commit changes and resolve PR issues

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -4,38 +4,87 @@ RSpec.describe QuestionsController, type: :controller do
     let(:user){ create(:user) }
     let(:campaign){ create(:campaign, enterprise: user.enterprise) }
     let(:question){ create(:question, campaign: campaign) }
-    login_user_from_let
 
     describe "GET#index" do
         context "with logged user" do
+            login_user_from_let
+            before { get :index, campaign_id: campaign.id }
+
             it "gets the index" do
-                get :index, campaign_id: campaign.id
-                expect(response).to be_success
+                expect(response).to render_template :index 
+            end
+
+            it "returns a valid campaign object" do 
+                expect(assigns[:campaign]).to be_valid
+            end
+
+            it "list campaign questions in descending order by created_at" do 
+                question
+                question1 = create(:question, campaign: campaign)
+                question2  = create(:question, campaign: campaign)
+
+                expect(assigns[:campaign].questions).to eq [question, question1, question2]
             end
         end
+
+        context "without a logged in user" do 
+            before { get :index, campaign_id: campaign.id }
+            it_behaves_like "redirect user to users/sign_in path"
+        end
     end
+
     
     describe "GET#new" do
         context "with logged user" do
+            login_user_from_let
+            before { get :new, campaign_id: campaign.id }
+
             it "gets the new page" do
-                get :new, campaign_id: campaign.id
-                expect(response).to be_success
+                expect(response).to render_template :new 
+            end
+
+            it "returns a valid campaign object" do 
+                expect(assigns[:question].campaign).to be_valid
+            end
+
+            it "returns a new question object" do 
+                expect(assigns[:question]).to be_a_new(Question)
             end
         end
+
+        context "without a logged in user" do 
+            before { get :new, campaign_id: campaign.id }
+            it_behaves_like "redirect user to users/sign_in path"
+        end
     end
+
     
     describe "GET#show" do
         context "with logged user" do
+            login_user_from_let
+            before { get :show, id: question.id }
+
             it "gets the show page" do
-                get :show, id: question.id
-                expect(response).to be_success
+                expect(response).to render_template :show
+            end
+
+            it "returns a valid campaign object" do 
+                expect(assigns[:question].campaign).to be_valid
             end
         end
+
+        context "without a logged in user" do 
+            before { get :show, id: question.id }
+            it_behaves_like "redirect user to users/sign_in path"
+        end
     end
+
     
     describe "POST#create" do
         context "with logged user" do
+            login_user_from_let
             before{ post :create, campaign_id: campaign.id, question: {title: "Title", description: "description"}}
+            
             it "redirects" do
                 expect(response).to redirect_to action: :index
             end
@@ -45,8 +94,8 @@ RSpec.describe QuestionsController, type: :controller do
                 expect(campaign.questions.count).to eq(1)
             end
             
-            it "flashes" do
-                expect(flash[:notice])
+            it "flashes a notice message" do
+                expect(flash[:notice]).to eq "Your question was created"
             end
         end
     end
