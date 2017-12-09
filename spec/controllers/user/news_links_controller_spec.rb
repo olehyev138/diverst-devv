1330fb1ee65c20dbf2538edba8fc28968cce7458
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-12-09 22:04:18 +0000

    improve news_links_controller_spec

--- spec/controllers/user/news_links_controller_spec.rb
+++ spec/controllers/user/news_links_controller_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User::NewsLinksController, type: :controller do
-    let(:user) { create :user}
+    let!(:user) { create :user }
+    let!(:groups) { create_list(:group, 2, owner: user) }
+    let!(:news_links) { create_list(:news_link, 12, author: user, group: groups.first) }
+    let!(:news_feeds) { create_list(:news_feed, 12, group: groups.first) }
+    let!(:news_feed_links) { create_list(:news_feed_link, 12, news_feed: news_feeds.first) }
+    let!(:segment) { create(:segment, owner: user) }
+    let!(:news_feed_link_segments) { create_list(:news_feed_link_segment, 12, news_feed_link: news_feed_links.first, segment: segment) }
+ 
+    before do 
+        user.groups = groups 
+        user.segments << segment
+    end
 
+    
     describe 'GET #index' do
        describe  "when user is logged in" do 
          login_user_from_let
@@ -13,7 +25,12 @@ RSpec.describe User::NewsLinksController, type: :controller do
             expect(response).to render_template :index
          end
 
-         it "debug" do 
+         it "return posts limited to 5" do 
+            expect(assigns[:posts].count).to eq 5
+         end
+
+         it "assert @count to be greater than limited(5)" do 
+            expect(assigns[:count]).to be > assigns[:posts].count
          end
        end
 
