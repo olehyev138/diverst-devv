0a392d1bea76774118bc0706107d323bb2c6a5c9
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-12-10 00:58:19 +0000

    Revert changes to spec file

--- spec/controllers/user/questions_controller_spec.rb
+++ spec/controllers/user/questions_controller_spec.rb
@@ -1,70 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "User::QuestionsController", type: :controller do
-     def setup
+    let(:user) { create :user}
+    let(:campaign){ create(:campaign, enterprise: user.enterprise) }
+	let(:question){ create(:question, campaign: campaign) }
+    
+    def setup
         @controller = User::QuestionsController.new
-     end
-
-    let!(:user) { create :user}
-    let!(:campaign) { create(:campaign, enterprise: user.enterprise, owner: user) }
-	let!(:question1) { create(:question, campaign: campaign, created_at: Time.current, updated_at: Time.current) }
-    let!(:question2) { create(:question, campaign: campaign, created_at: Time.current + 1.days, updated_at: Time.current + 1.days) }
-    let!(:answer1) { create(:answer, question: question1, author: user, upvote_count: 4) }
-    let!(:answer2) { create(:answer, question: question1, author: user, upvote_count: 8) }
-    let!(:other_answer) { create(:answer, question: question2, author: user) }
-
-    before { setup }
-
+    end
+    
+    before {setup}
+    
+    login_user_from_let
+    
     describe 'GET #index' do
-        describe "when user is logged in" do
-            login_user_from_let
-            before { get :index, user_campaign_id: campaign.id }
-
-            it "return success" do
-                expect(response).to be_success
-            end
-
-            it "render index template" do
-                expect(response).to render_template :index
-            end
-
-            it "return campaign questions in descending order of created_at" do 
-                expect(assigns[:questions]).to eq [question2, question1]
-            end
-        end
-
-        describe "when user is not logged in" do 
-            before { get :index, user_campaign_id: campaign.id }
-            it_behaves_like "redirect user to users/sign_in path"
+        it "returns success" do
+            get :index, user_campaign_id: campaign.id
+            expect(response).to be_success
         end
     end
-
-
+    
     describe 'GET #show' do
-        describe "when user is logged in" do
-           login_user_from_let
-           before { get :show, id: question1.id }
-
-            it "return success" do
-                expect(response).to be_success
-            end
-
-            it "render show template" do
-                expect(response).to render_template :show
-            end
-
-            it "return answers to a question1 in descending order of upvote_count" do 
-                expect(assigns[:answers]).to eq [answer2, answer1]
-            end
-
-            it "returns a new answer object for question1" do 
-                expect(assigns[:answer]).to be_a_new(Answer)
-            end
-        end
-
-        describe "when user is not logged in" do 
-            before { get :show, id: question1.id }
-            it_behaves_like "redirect user to users/sign_in path"
+        it "returns success" do
+            get :show, id: question.id
+            expect(response).to be_success
         end
     end
 end
