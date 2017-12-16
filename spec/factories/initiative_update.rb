fb6b1eeb67553600d9c21c7e711bcf7cd108d672
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-12-16 01:22:03 +0000

    commit work in progress

--- spec/factories/initiative_update.rb
+++ spec/factories/initiative_update.rb
@@ -1,4 +1,9 @@
 FactoryGirl.define do
     factory :initiative_update do
+    	association :owner, factory: :user
+    	association :initiative
+    	comments { Faker::Lorem.sentence }
+    	data { Faker::Lorem.sentence }
+    	report_date { Faker::Date.forward(2) }
     end
 end
