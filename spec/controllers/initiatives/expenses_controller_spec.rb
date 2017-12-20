21a160bb861db408fa2eb4f858df1830d4fd1466
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-12-20 16:16:43 +0000

    Improve initiatives/expenses_controller_spec.rb

--- spec/controllers/initiatives/expenses_controller_spec.rb
+++ spec/controllers/initiatives/expenses_controller_spec.rb
@@ -5,63 +5,163 @@ RSpec.describe Initiatives::ExpensesController, type: :controller do
     let(:group){ create(:group, enterprise: user.enterprise) }
     let(:initiative){ initiative_of_group(group) }
     let(:initiative_expense){create(:initiative_expense, initiative: initiative)}
-    
-    login_user_from_let
-    
+
+
+
     describe 'GET#index' do
-        it "gets the expenses" do
-            get :index, group_id: group.id, initiative_id: initiative.id
-            expect(response).to be_success
+        describe 'with user logged in' do
+            login_user_from_let
+            before { get :index, group_id: group.id, initiative_id: initiative.id }
+
+            it "set valid group object" do
+                expect(assigns[:group]).to be_valid
+            end
+
+            it "set valid initiative object" do
+                expect(assigns[:initiative]).to be_valid
+            end
+
+            it "gets the expenses" do
+                expect(assigns[:expenses]).to eq [initiative_expense]
+            end
+
+            it "renders index template" do
+                expect(response).to render_template :index
+            end
+        end
+
+        describe 'without user logged in' do
+            before { get :index, group_id: group.id, initiative_id: initiative.id }
+            it_behaves_like "redirect user to users/sign_in path"
         end
     end
-    
+
+
     describe 'GET#new' do
-        it "gets a new expense" do
-            get :new, group_id: group.id, initiative_id: initiative.id
-            expect(response).to be_success
+        describe 'with user logged in' do
+            login_user_from_let
+            before { get :new, group_id: group.id, initiative_id: initiative.id }
+
+            it "set valid group object" do
+                expect(assigns[:group]).to be_valid
+            end
+
+            it "set initiative object" do
+                expect(assigns[:initiative]).to eq initiative
+            end
+
+            it "gets a new expense" do
+                expect(assigns[:expense]).to be_a_new(InitiativeExpense)
+            end
+
+            it "render a new template" do
+                expect(response).to render_template :new
+            end
+        end
+
+        describe 'with user not logged in' do
+            before { get :new, group_id: group.id, initiative_id: initiative.id }
+            it_behaves_like "redirect user to users/sign_in path"
         end
     end
-    
+
+
     describe 'GET#show', :skip => "Missing template" do
         it "show an expense" do
             get :show, group_id: group.id, initiative_id: initiative.id, id: initiative_expense.id
             expect(response).to be_success
         end
     end
-    
+
+
     describe 'GET#edit' do
-        it "edits an expense" do
-            get :edit, group_id: group.id, initiative_id: initiative.id, id: initiative_expense.id
-            expect(response).to be_success
+        context 'with user logged in' do
+            login_user_from_let
+            before { get :edit, group_id: group.id, initiative_id: initiative.id, id: initiative_expense.id }
+
+
+            it 'returns valid expense object' do
+                expect(assigns[:expense]).to be_valid
+            end
+
+            it "set valid group object" do
+                expect(assigns[:group]).to be_valid
+            end
+
+            it "set valid initiative object" do
+                expect(assigns[:initiative]).to be_valid
+            end
+
+            it 'renders edit template' do
+                expect(response).to render_template :edit
+            end
+        end
+
+        context 'with user not logged in' do
+            before { get :edit, group_id: group.id, initiative_id: initiative.id, id: initiative_expense.id }
+            it_behaves_like "redirect user to users/sign_in path"
         end
     end
-    
+
+
     describe 'PATCH#update' do
-        before {patch :update, group_id: group.id, initiative_id: initiative.id, id: initiative_expense.id, initiative_expense: {amount: 1000}}
-        
-        it "redirects" do
-            expect(response).to redirect_to action: :index
+        describe 'with user logged in' do
+            login_user_from_let
+
+            context 'with valid attributes' do
+                before { patch :update, group_id: group.id, initiative_id: initiative.id, id: initiative_expense.id, initiative_expense: {amount: 1000} }
+
+                it "redirects to action index" do
+                    expect(response).to redirect_to action: :index
+                end
+
+                it "flashes a notice message" do
+                    expect(flash[:notice]).to eq "Your expense was updated"
+                end
+
+                it "updates an expense" do
+                    initiative_expense.reload
+                    expect(initiative_expense.amount).to eq(1000)
+                end
+            end
+
+            context 'with invalid attributes' do
+                before {patch :update, group_id: group.id, initiative_id: initiative.id, id: initiative_expense.id, initiative_expense: {amount: -1}}
+
+                it 'flashes an alert message' do
+                    expect(flash[:alert]).to eq "Your expense was not updated. Please fix the errors"
+                end
+
+                it 'renders edit template' do
+                    expect(response).to render_template :edit
+                end
+            end
         end
-        
-        it "flashes" do
-            expect(flash[:notice]).to eq "Your expense was updated"
-        end
-        
-        it "updates an expense" do
-            initiative_expense.reload
-            expect(initiative_expense.amount).to eq(1000)
+
+        describe 'with a user not logged in' do
+            before { patch :update, group_id: group.id, initiative_id: initiative.id, id: initiative_expense.id, initiative_expense: {amount: 1000} }
+            it_behaves_like "redirect user to users/sign_in path"
         end
     end
-    
+
     describe 'DELETE#destroy' do
-        before {delete :destroy, group_id: group.id, initiative_id: initiative.id, id: initiative_expense.id, initiative_expense: {amount: 1000}}
-        
-        it "redirects" do
-            expect(response).to redirect_to action: :index
+        context 'with a logged in user' do
+            login_user_from_let
+
+            it "redirects to action index" do
+                delete :destroy, group_id: group.id, initiative_id: initiative.id, id: initiative_expense.id, initiative_expense: {amount: 1000}
+                expect(response).to redirect_to action: :index
+            end
+
+            it "updates an expense" do
+                delete :destroy, group_id: group.id, initiative_id: initiative.id, id: initiative_expense.id, initiative_expense: {amount: 1000}
+                expect(InitiativeExpense.where(:id => initiative_expense.id).count).to eq(0)
+            end
         end
-        
-        it "updates an expense" do
-            expect(InitiativeExpense.where(:id => initiative_expense.id).count).to eq(0)
+
+        describe 'with user not logged in' do
+            before { delete :destroy, group_id: group.id, initiative_id: initiative.id, id: initiative_expense.id, initiative_expense: {amount: 1000} }
+            it_behaves_like "redirect user to users/sign_in path"
         end
     end
 end
