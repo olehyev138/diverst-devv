45866fdc9f8c20907b249564dc38a248bf3f5f26
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-12-23 04:40:24 +0000

    Commit changes

--- spec/controllers/groups/social_links_controller_spec.rb
+++ spec/controllers/groups/social_links_controller_spec.rb
@@ -7,14 +7,14 @@ RSpec.describe Groups::SocialLinksController, type: :controller do
 
 
     describe 'GET#index' do
-        before do 
+        before do
             allow(SocialMedia::Importer).to receive(:valid_url?).and_return(true)
         end
-        context 'with logged in user' do 
+        context 'with logged in user' do
             login_user_from_let
             before { get :index, group_id: group.id }
 
-            it 'returns social links belonging to group object' do 
+            it 'returns social links belonging to group object' do
                 byebug
             end
 
@@ -24,13 +24,13 @@ RSpec.describe Groups::SocialLinksController, type: :controller do
         end
 
     end
-    
+
 
     describe 'GET #new' do
         def get_new(group_id)
             get :new, group_id: group_id
         end
-        
+
         context 'with logged user' do
             login_user_from_let
             before { get_new(group.to_param) }
@@ -41,11 +41,11 @@ RSpec.describe Groups::SocialLinksController, type: :controller do
             end
 
             it 'return new group social link object' do
-                expect(assigns[:social_link]).to be_a_new(SocialLink) 
+                expect(assigns[:social_link]).to be_a_new(SocialLink)
             end
         end
 
-        context 'with user not logged in' do 
+        context 'with user not logged in' do
             before { get_new(group.to_param) }
             it_behaves_like "redirect user to users/sign_in path"
         end
@@ -55,7 +55,7 @@ RSpec.describe Groups::SocialLinksController, type: :controller do
         before :each do
             post :create, group_id: group.id, social_link: attributes_for(:social_link, :url => "https://twitter.com/realDonaldTrump/status/912848241535971331")
         end
-        
+
         it "redirect back" do
             expect(response).to redirect_to group_posts_path(group)
         end
@@ -63,11 +63,11 @@ RSpec.describe Groups::SocialLinksController, type: :controller do
 
     describe 'DELETE#destroy' do
         let!(:social_link){ create(:social_link, group: group) }
-        
+
         before :each do
             delete :destroy, group_id: group.id, id: social_link.id
         end
-        
+
         it "redirects back" do
             expect(response).to redirect_to group_posts_path(group)
         end
