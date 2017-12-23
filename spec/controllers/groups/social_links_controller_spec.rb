f20d94764c01d0fefaac5c6fdcf2f8bc988c3d3d
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-12-23 05:49:28 +0000

    Improve groups/social_links_controller_spec.rb

--- spec/controllers/groups/social_links_controller_spec.rb
+++ spec/controllers/groups/social_links_controller_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Groups::SocialLinksController, type: :controller do
-    let(:user) { create :user }
+    let(:enterprise) { create(:enterprise, enable_rewards: true) }
+    let(:user) { create :user, enterprise: enterprise }
     let(:group) { create(:group, enterprise: user.enterprise) }
 
 
@@ -58,28 +59,81 @@ RSpec.describe Groups::SocialLinksController, type: :controller do
 
 
     describe 'POST#create' do
-        before :each do
-            post :create, group_id: group.id, social_link: attributes_for(:social_link, :url => "https://twitter.com/realDonaldTrump/status/912848241535971331")
+        describe 'with logged in user' do
+            login_user_from_let
+            let!(:reward_action) { create(:reward_action, enterprise: user.enterprise, key: "social_media_posts", points: 75) }
+
+            context 'with valid params' do
+
+                it 'creates a social link object' do 
+                    expect{post :create, group_id: group.id, social_link: attributes_for(:social_link, :url => "https://twitter.com/realDonaldTrump/status/912848241535971331")}
+                    .to change(SocialLink, :count).by(1)
+                end
+
+                it "redirect back" do
+                    post :create, group_id: group.id, social_link: attributes_for(:social_link, :url => "https://twitter.com/realDonaldTrump/status/912848241535971331")
+                    expect(response).to redirect_to group_posts_path(group)
+                end
+
+                it 'rewards a user with points of this action' do
+                    expect(user.points).to eq 0
+                    post :create, group_id: group.id, social_link: attributes_for(:social_link, :url => "https://twitter.com/realDonaldTrump/status/912848241535971331")
+                    user.reload
+                    expect(user.points).to eq 75
+                end
+
+                it 'flashes a reward message' do
+                    post :create, group_id: group.id, social_link: attributes_for(:social_link, :url => "https://twitter.com/realDonaldTrump/status/912848241535971331")
+                    user.reload
+                    expect(flash[:reward]).to eq "Your social_link was created. Now you have #{ user.credits } points"
+                end
+            end
+
+            context 'with invalid params' do
+                before { request.env["HTTP_REFERER"] = "back" }
+
+                it 'does not create social link object' do 
+                    expect{post :create, group_id: group.id, social_link: attributes_for(:social_link, :url => "https://example.com/912848")}
+                    .to change(SocialLink, :count).by(0)
+                end
+
+                it 'flashes an alert message' do
+                    post :create, group_id: group.id, social_link: attributes_for(:social_link, :url => "https://example.com/912848")
+                    expect(flash[:alert]).to eq "Your social link was not created. Please fix the errors"
+                end
+
+                it 'redirects to back' do
+                    post :create, group_id: group.id, social_link: attributes_for(:social_link, :url => "https://example.com/912848")
+                    expect(response).to redirect_to "back"
+                end
+            end
         end
 
-        it "redirect back" do
-            expect(response).to redirect_to group_posts_path(group)
-        end
-
-        it 'debug' do 
-            byebug
+        describe "with a user not logged in" do
+            before { post :create, group_id: group.id, social_link: attributes_for(:social_link, :url => "https://twitter.com/realDonaldTrump/status/912848241535971331") }
+            it_behaves_like "redirect user to users/sign_in path"
         end
     end
 
+
     describe 'DELETE#destroy' do
-        let!(:social_link){ create(:social_link, group: group) }
+        describe 'with user logged in' do
+            login_user_from_let
+            let!(:social_link){ create(:social_link, group: group, url: "https://twitter.com/realDonaldTrump/status/912848241535971331") }
 
-        before :each do
-            delete :destroy, group_id: group.id, id: social_link.id
-        end
+            it 'destroys social link object' do 
+                expect{delete :destroy, group_id: group.id, id: social_link.id}.to change(SocialLink, :count).by(-1)
+            end
 
-        it "redirects back" do
-            expect(response).to redirect_to group_posts_path(group)
+            it 'flashes notice message' do 
+                delete :destroy, group_id: group.id, id: social_link.id
+                expect(flash[:notice]).to eq "Your social link was removed."
+            end
+
+            it "redirects back" do
+                delete :destroy, group_id: group.id, id: social_link.id
+                expect(response).to redirect_to group_posts_path(group)
+            end
         end
     end
 end
