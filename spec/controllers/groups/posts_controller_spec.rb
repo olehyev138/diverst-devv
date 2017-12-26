213cfaa9aabe49fd3f0252bf8fa6dc72fc91e96b
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-12-26 13:02:21 +0000

    Commit WIP

--- spec/controllers/groups/posts_controller_spec.rb
+++ spec/controllers/groups/posts_controller_spec.rb
@@ -2,27 +2,39 @@ require 'rails_helper'
 
 RSpec.describe Groups::PostsController, type: :controller do
     include ActiveJob::TestHelper
-    
+
     let(:user) { create :user }
     let(:group) { create(:group, enterprise: user.enterprise) }
-    let(:news_link) { create(:news_link, :group => group)}
-    
-    login_user_from_let
+    let!(:news_feed_link1) { create(:news_feed_link, link: news_link1, approved: true, created_at: Time.now - 5.hours) }
+    let!(:news_feed_link2) { create(:news_feed_link, link: news_link2, approved: true, created_at: Time.now - 2.hours) }
+    let!(:news_feed_link3) { create(:news_feed_link, link: news_link3, approved: true, created_at: Time.now) }
+    let!(:news_link1) { create(:news_link, :group => group)}
+    let!(:news_link2) { create(:news_link, :group => group)}
+    let!(:news_link3) { create(:news_link, :group => group)}
+   
 
     describe 'GET #index' do
-        it 'return success' do
-            get :index, group_id: group.id
-            expect(response).to be_success
+        context 'with user logged in' do
+            login_user_from_let
+            before { get :index, group_id: group.id }
+
+            it 'render index template' do
+                expect(response).to render_template :index
+            end
+
+            it 'debug' do 
+                byebug
+            end
         end
     end
-    
+
     describe 'GET #pending' do
         it 'return success' do
             get :pending, group_id: group.id
             expect(response).to be_success
         end
     end
-    
+
     describe 'PATCH #approve' do
         before :each do
             # ensure the job is performed and that
@@ -32,7 +44,7 @@ RSpec.describe Groups::PostsController, type: :controller do
                 patch :approve, group_id: group.id, link_id: news_link.news_feed_link.id
             end
         end
-        
+
         it 'return success' do
             expect(response).to redirect_to "back"
         end
