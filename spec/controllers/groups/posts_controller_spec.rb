e0f317dfc9f996137a74a611cbebb5e5e60622fc
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-12-31 20:19:54 +0000

    commit changes before experiment

--- spec/controllers/groups/posts_controller_spec.rb
+++ spec/controllers/groups/posts_controller_spec.rb
@@ -3,15 +3,16 @@ require 'rails_helper'
 RSpec.describe Groups::PostsController, type: :controller do
     include ActiveJob::TestHelper
 
-    let(:user) { create :user }
-    let(:group) { create(:group, enterprise: user.enterprise) }
-    let!(:news_feed_link1) { create(:news_feed_link, link: news_link1, approved: true, created_at: Time.now - 5.hours) }
-    let!(:news_feed_link2) { create(:news_feed_link, link: news_link2, approved: true, created_at: Time.now - 2.hours) }
-    let!(:news_feed_link3) { create(:news_feed_link, link: news_link3, approved: true, created_at: Time.now) }
+    let!(:user) { create :user }
+    let!(:group) { create(:group, enterprise: user.enterprise, owner: user) }
+    let!(:news_feed) { create(:news_feed, group: group) }
+    let!(:news_feed_link1) { create(:news_feed_link, link: news_link1, news_feed: news_feed, approved: true, created_at: Time.now - 5.hours) }
+    let!(:news_feed_link2) { create(:news_feed_link, link: news_link2, news_feed: news_feed, approved: true, created_at: Time.now - 2.hours) }
+    let!(:news_feed_link3) { create(:news_feed_link, link: news_link3, news_feed: news_feed, approved: true, created_at: Time.now) }
     let!(:news_link1) { create(:news_link, :group => group)}
     let!(:news_link2) { create(:news_link, :group => group)}
     let!(:news_link3) { create(:news_link, :group => group)}
-   
+
 
     describe 'GET #index' do
         context 'with user logged in' do
@@ -22,11 +23,46 @@ RSpec.describe Groups::PostsController, type: :controller do
                 expect(response).to render_template :index
             end
 
-            it 'debug' do 
-                byebug
+            describe 'policy(@group).erg_leader_permissions? returns true' do
+                let!(:group_leader) { create(:group_leader, user: user, group: group, visible: true, notifications_enabled: false) }
+
+                it 'return count 3' do
+                    expect(assigns[:count]).to eq 3
+                end
+
+                it 'return 3 posts' do 
+                    expect(assigns[:posts].count).to eq 3
+                end
+            end
+
+            describe 'if current user' do
+                let!(:segment) { create(:segment, enterprise: user.enterprise, owner: user) }
+                let!(:news_link4) { create(:news_link, :group => group)}
+                let!(:news_feed_link4) { create(:news_feed_link, link: news_link4, news_feed: news_feed, approved: true, created_at: Time.now - 3.hours) }
+                let!(:news_link_segment) { create(:news_link_segment, segment: segment, news_link: news_link4) }
+                let!(:news_feed_link_segment) { create(:news_feed_link_segment, segment: segment, news_feed_link: news_feed_link4, link_segment: news_link_segment ) }
+                let!(:base_query) { group.news_feed_links.approved }
+                
+                
+                context 'is an active member of group' do 
+                    let!(:user_group) { create(:user_group, user: user, group: group) }
+                    
+
+                    it 'return 4 posts' do
+                        expect(assigns[:posts].count).to eq 4
+                    end
+                end
+
+                context 'is not an active member of group' do
+                    it 'returns 0 counts' do 
+                        allow(group.active_members).to receive(:include?).with(user).and_return(false)
+                        byebug
+                    end
+                end
             end
         end
     end
+
 
     describe 'GET #pending' do
         it 'return success' do
