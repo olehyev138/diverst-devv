2cfcfc8565b47ff278e98e2e3c0460db4df61023
Girgy <girgalicious@gmail.com>
2018-01-02 20:44:10 +0000

    Tests - Initiatives::UpdatesController

--- spec/controllers/initiatives/updates_controller_spec.rb
+++ spec/controllers/initiatives/updates_controller_spec.rb
@@ -22,24 +22,51 @@ RSpec.describe Initiatives::UpdatesController, type: :controller do
         end
     end
     
+    describe 'GET#edit' do
+        it "edits an update" do
+            get :edit, group_id: group.id, initiative_id: initiative.id, id: initiative_update.id
+            expect(response).to be_success
+        end
+    end
+    
     describe 'POST#create' do
-        
-        before{post :create, group_id: group.id, initiative_id: initiative.id, initiative_update: {report_date: Date.today}}
-        
-        it "redirects" do
-            expect(response).to redirect_to action: :index
+        context "when successful" do 
+            before{post :create, group_id: group.id, initiative_id: initiative.id, initiative_update: {report_date: Date.today}}
+            
+            it "redirects" do
+                expect(response).to redirect_to action: :index
+            end
+            
+            it "creates the initiative_update" do
+                expect(InitiativeUpdate.count).to eq(1)
+            end
+            
+            it "flashes" do
+                expect(flash[:notice]).to eq("Your initiative update was created")
+            end
+            
+            it "sets the owner" do
+                expect(InitiativeUpdate.last.owner).to eq(user)
+            end
         end
         
-        it "creates the initiative_update" do
-            expect(InitiativeUpdate.count).to eq(1)
-        end
-        
-        it "flashes" do
-            expect(flash[:notice]).to eq("Your initiative update was created")
-        end
-        
-        it "sets the owner" do
-            expect(InitiativeUpdate.last.owner).to eq(user)
+        context "when unsuccessful" do 
+            before{
+                allow_any_instance_of(InitiativeUpdate).to receive(:save).and_return(false)
+                post :create, group_id: group.id, initiative_id: initiative.id, initiative_update: {report_date: Date.today}
+            }
+            
+            it "renders new" do
+                expect(response).to render_template :new
+            end
+            
+            it "does not create the initiative_update" do
+                expect(InitiativeUpdate.count).to eq(0)
+            end
+            
+            it "flashes" do
+                expect(flash[:alert]).to eq("Your initiative update was not created. Please fix the errors")
+            end
         end
     end
     
@@ -52,19 +79,36 @@ RSpec.describe Initiatives::UpdatesController, type: :controller do
     
     describe 'PATCH#update' do
         
-        before{patch :update, group_id: group.id, initiative_id: initiative.id, id: initiative_update.id, initiative_update: {report_date: Date.today}}
-        
-        it "redirects" do
-            expect(response).to redirect_to action: :index
+        context "when successful" do
+            before{patch :update, group_id: group.id, initiative_id: initiative.id, id: initiative_update.id, initiative_update: {report_date: Date.today}}
+            
+            it "redirects" do
+                expect(response).to redirect_to action: :index
+            end
+            
+            it "updates the initiative_update" do
+                initiative_update.reload
+                expect(initiative_update.report_date).to eq(Date.today)
+            end
+            
+            it "flashes" do
+                expect(flash[:notice]).to eq("Your initiative update was updated")
+            end
         end
         
-        it "updates the initiative_update" do
-            initiative_update.reload
-            expect(initiative_update.report_date).to eq(Date.today)
-        end
-        
-        it "flashes" do
-            expect(flash[:notice]).to eq("Your initiative update was updated")
+        context "when usuccessful" do
+            before{
+                allow_any_instance_of(InitiativeUpdate).to receive(:update).and_return(false)
+                patch :update, group_id: group.id, initiative_id: initiative.id, id: initiative_update.id, initiative_update: {report_date: Date.today}
+            }
+            
+            it "renders edit" do
+                expect(response).to render_template :edit
+            end
+            
+            it "flashes" do
+                expect(flash[:alert]).to eq("Your initiative update was not updated. Please fix the errors")
+            end
         end
     end
     
