dc8f7bdbbaf143c15cfc1bb9a232ba90d2e571ec
Anatolii Borysov <borisano@gmail.com>
2018-01-05 18:57:16 +0200

    Check that table exists before trying to create it in migration

--- db/migrate/20171228133035_create_news_link_photos.rb
+++ db/migrate/20171228133035_create_news_link_photos.rb
@@ -1,12 +1,14 @@
 class CreateNewsLinkPhotos < ActiveRecord::Migration
   def change
     # create photos
-    create_table :news_link_photos do |t|
-      t.attachment :file
-      t.belongs_to :news_link
-      t.timestamps null: false
+    if !ActiveRecord::Base.connection.table_exists? :news_link_photos
+      create_table :news_link_photos do |t|
+        t.attachment :file
+        t.belongs_to :news_link
+        t.timestamps null: false
+      end
     end
-    
+
     # migrate existing newsLink pictures to news_link_photos
     NewsLink.find_each do |news_link|
       if news_link.picture.exists?
