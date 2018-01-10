cd31d8796476356f704fec1901a08729766ec109
Girgy <girgalicious@gmail.com>
2018-01-10 14:35:45 +0000

    Tests - undo focus

--- spec/jobs/generate_enterprise_matches_job_spec.rb
+++ spec/jobs/generate_enterprise_matches_job_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe GenerateEnterpriseMatchesJob, type: :job do
   include ActiveJob::TestHelper
   
-  fdescribe "#perform" do
+  describe "#perform" do
     it "calls update_match_scores" do
       user = create(:user)
       expect_any_instance_of(User).to receive(:update_match_scores)
