b584a9fb727685c977b7a8fc41062490765830b8
Girgy <girgalicious@gmail.com>
2018-01-10 19:40:20 +0000

    Tests - Groups::NewsLinkCommentController

--- spec/controllers/groups/group_news_link_comment_controller_spec.rb
+++ spec/controllers/groups/group_news_link_comment_controller_spec.rb
@@ -16,18 +16,41 @@ RSpec.describe Groups::NewsLinkCommentController, type: :controller do
   end
 
   describe 'PATCH#update' do
-    before(:each) do
-      patch :update, group_id: group.id, news_link_id: news_link.id, id: news_link_comment.id, news_link_comment: {content: "updated", approved: true}
+    context "when successful" do
+      before(:each) do
+        patch :update, group_id: group.id, news_link_id: news_link.id, id: news_link_comment.id, news_link_comment: {content: "updated", approved: true}
+      end
+  
+      it "updates the comment" do
+        news_link_comment.reload
+        expect(news_link_comment.content).to eq "updated"
+        expect(news_link_comment.approved).to be true
+      end
+  
+      it "redirect to message" do
+        expect(response).to redirect_to comments_group_news_link_url(:id => news_link.id, :group_id => group.id)
+      end
     end
-
-    it "updates the comment" do
-      news_link_comment.reload
-      expect(news_link_comment.content).to eq "updated"
-      expect(news_link_comment.approved).to be true
-    end
-
-    it "redirect to message" do
-      expect(response).to redirect_to comments_group_news_link_url(:id => news_link.id, :group_id => group.id)
+    
+    context "when unsuccessful" do
+      before {
+        allow_any_instance_of(NewsLinkComment).to receive(:update).and_return(false)
+        patch :update, group_id: group.id, news_link_id: news_link.id, id: news_link_comment.id, news_link_comment: {content: "updated", approved: true}
+      }
+  
+      it "does not update the comment" do
+        news_link_comment.reload
+        expect(news_link_comment.content).to_not eq "updated"
+        expect(news_link_comment.approved).to_not be true
+      end
+  
+      it "renders edit" do
+        expect(response).to render_template :edit
+      end
+      
+      it "flashes" do
+        expect(flash[:alert]).to eq "Your comment was not updated. Please fix the errors"
+      end
     end
   end
 
