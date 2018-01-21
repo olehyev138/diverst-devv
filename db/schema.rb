a1ebb94a97377290e671bfd2a9fa9898e64d03c8
Derek Owusu-Frimpong <deepsky_5@live.com>
2018-01-21 19:05:16 +0000

    commit changes to schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20180118201836) do
 
-ActiveRecord::Schema.define(version: 20180104150641) do
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id",   limit: 4
     t.string   "trackable_type", limit: 191
@@ -34,8 +34,9 @@ ActiveRecord::Schema.define(version: 20180104150641) do
     t.text     "content",    limit: 65535
     t.integer  "author_id",  limit: 4
     t.integer  "answer_id",  limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.boolean  "approved",                 default: false
   end
 
   create_table "answer_expenses", force: :cascade do |t|
@@ -343,6 +344,8 @@ ActiveRecord::Schema.define(version: 20180104150641) do
     t.string   "onboarding_sponsor_media_content_type", limit: 191
     t.integer  "onboarding_sponsor_media_file_size",    limit: 4
     t.datetime "onboarding_sponsor_media_updated_at"
+    t.boolean  "disable_sponsor_message",                             default: false
+    t.boolean  "enable_pending_comments",                             default: false
   end
 
   create_table "event_attendances", force: :cascade do |t|
@@ -480,21 +483,24 @@ ActiveRecord::Schema.define(version: 20180104150641) do
   end
 
   create_table "group_leaders", force: :cascade do |t|
-    t.integer  "group_id",              limit: 4
-    t.integer  "user_id",               limit: 4
-    t.string   "position_name",         limit: 191
-    t.datetime "created_at",                                        null: false
-    t.datetime "updated_at",                                        null: false
-    t.boolean  "visible",                           default: true
-    t.boolean  "notifications_enabled",             default: false
+    t.integer  "group_id",                               limit: 4
+    t.integer  "user_id",                                limit: 4
+    t.string   "position_name",                          limit: 191
+    t.datetime "created_at",                                                         null: false
+    t.datetime "updated_at",                                                         null: false
+    t.boolean  "visible",                                            default: true
+    t.boolean  "pending_member_notifications_enabled",               default: false
+    t.boolean  "pending_comments_notifications_enabled",             default: false
+    t.boolean  "pending_posts_notifications_enabled",                default: false
   end
 
   create_table "group_message_comments", force: :cascade do |t|
     t.text     "content",    limit: 65535
     t.integer  "author_id",  limit: 4
     t.integer  "message_id", limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.boolean  "approved",                 default: false
   end
 
   create_table "group_messages", force: :cascade do |t|
@@ -731,8 +737,9 @@ ActiveRecord::Schema.define(version: 20180104150641) do
     t.text     "content",      limit: 65535
     t.integer  "author_id",    limit: 4
     t.integer  "news_link_id", limit: 4
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
+    t.boolean  "approved",                   default: false
   end
 
   create_table "news_link_photos", force: :cascade do |t|
@@ -1019,6 +1026,7 @@ ActiveRecord::Schema.define(version: 20180104150641) do
     t.integer  "notifications_frequency", limit: 4,     default: 2
     t.integer  "total_weekly_points",     limit: 4,     default: 0
     t.text     "data",                    limit: 65535
+    t.integer  "notifications_date",      limit: 4,     default: 1
   end
 
   create_table "user_reward_actions", force: :cascade do |t|
