5910fade1d3301c2ba8f54d068e7af9081acdf19
Derek Owusu-Frimpong <deepsky_5@live.com>
2018-01-29 16:07:39 +0000

    remove unwanted controller specs

--- spec/controllers/groups/leaders_controller_spec.rb
+++ spec/controllers/groups/leaders_controller_spec.rb
@@ -108,54 +108,6 @@ RSpec.describe Groups::LeadersController, type: :controller do
           leader = group.group_leaders.first
           expect(leader.pending_member_notifications_enabled).to eq(false)
         end
-
-        context 'when more than one group_leader object is set as group contact' do
-          let!(:another_group_leader) { create(:group_leader, group: group, default_group_contact: true) }
-          let!(:leader_attrs) { attributes_for :group_leader, user_id: leader_user.to_param, default_group_contact: true }
-
-          it 'flash an alert message' do
-            post_create(group.to_param, leader_attrs)
-            expect(flash[:notice]).to eq "Leaders were updated"
-          end
-
-          it 'redirect to action index' do 
-            post_create(group.to_param, leader_attrs)
-            expect(response).to redirect_to action: :index
-          end
-
-          xit 'picks the first group leader with default_group_contact attribute set to true' do 
-            post_create(group.to_param, leader_attrs)
-            group_leader = assigns[:group].group_leaders.find_by(default_group_contact: true).user
-            expect(assigns[:group].contact_email).to eq group_leader.email
-          end
-        end
-
-        context 'when no group leader is set as group contact' do 
-          it 'flash a notice message' do 
-            post_create(group.to_param, leader_attrs)
-            expect(flash[:notice]).to eq "Leaders were updated"
-          end
-
-          it 'redirect to action index' do 
-            post_create(group.to_param, leader_attrs)
-            expect(response).to redirect_to action: :index
-          end
-        end
-
-        context 'when ONLY ONE group leader is set as group contact' do
-          let!(:leader_attrs) { attributes_for :group_leader, user_id: leader_user.to_param, default_group_contact: true }
-
-          it 'flash a notice message' do
-            post_create(group.to_param, leader_attrs)
-            group_leader = group.group_leaders.find_by(default_group_contact: true).user
-            expect(flash[:notice]).to eq "Leaders were updated"
-          end
-
-          it 'redirect to action index' do 
-            post_create(group.to_param, leader_attrs)
-            expect(response).to redirect_to action: :index
-          end
-        end
       end
 
       context 'with incorrect params' do
