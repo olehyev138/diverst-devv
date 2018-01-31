735654181dea014d0d4499f6f13b803e20650015
Anatolii Borysov <borisano@gmail.com>
2018-01-31 16:33:51 +0200

    Add is_private to budget items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180129090107) do
+ActiveRecord::Schema.define(version: 20180131142119) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id",   limit: 4
@@ -136,6 +136,7 @@ ActiveRecord::Schema.define(version: 20180129090107) do
     t.integer  "budget_id",        limit: 4
     t.string   "title",            limit: 191
     t.date     "estimated_date"
+    t.boolean  "is_private",                                           default: false
     t.boolean  "is_done",                                              default: false
     t.datetime "created_at",                                                           null: false
     t.datetime "updated_at",                                                           null: false
