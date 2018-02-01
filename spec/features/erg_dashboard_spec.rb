4c1b27dc5e1ef05f1f8f821ffb1e8804796a7649
Derek Owusu-Frimpong <deepsky_5@live.com>
2018-02-01 19:34:36 +0000

    update spec/features/erg_dashboard_spec.rb

--- spec/features/erg_dashboard_spec.rb
+++ spec/features/erg_dashboard_spec.rb
@@ -17,6 +17,7 @@ RSpec.feature 'An ERG dashboard' do
   scenario 'shows the upcoming events' do
     initiative = create :initiative, owner_group: group, start: 2.days.from_now
     group.outcomes.first.pillars.first.initiatives << initiative
+    group.members << user
 
     visit group_path(group)
 
@@ -102,18 +103,19 @@ RSpec.feature 'An ERG dashboard' do
   end
 
   context 'in the events section' do
-    scenario 'does not show the upcoming events' do
+    scenario 'does not show the upcoming events if user is a guest' do
+      group.members.delete(user)
       initiative = create(:initiative, owner_group: group, start: 1.day.from_now, end: 1.day.from_now + 2.hours)
-
+      
       visit group_events_path(group)
 
-      expect(page).to have_content initiative.name
+      expect(page).to_not have_content initiative.name
     end
-
+    
     scenario 'shows the upcoming events' do
       initiative = create(:initiative, owner_group: group, start: 1.day.from_now, end: 1.day.from_now + 2.hours)
       create(:user_group, group: group, user: user, accepted_member: true)
-
+      
       visit group_events_path(group)
 
       expect(page).to have_content initiative.name
@@ -126,11 +128,11 @@ RSpec.feature 'An ERG dashboard' do
 
       expect(page).to have_content past_initiative.name
     end
-
+    
     scenario 'shows the past events for erg members' do
       past_initiative = create(:initiative, owner_group: group, start: 1.day.ago, end: 1.day.ago + 2.hours)
       create(:user_group, group: group, user: user, accepted_member: true)
-
+      
       visit group_events_path(group)
 
       expect(page).to have_content past_initiative.name
