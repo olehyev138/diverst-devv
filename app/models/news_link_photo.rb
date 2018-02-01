551b3bd556480920bce9b7d4a465ead873371ba1
Girgy <girgalicious@gmail.com>
2018-02-01 12:25:14 +0000

    NewsLinkPhotos - add default url to news link photos

--- app/models/news_link_photo.rb
+++ app/models/news_link_photo.rb
@@ -3,7 +3,7 @@ class NewsLinkPhoto < ActiveRecord::Base
 
   validates :news_link, presence: true, :on => :update
   
-  has_attached_file :file, styles: { medium: '1000x300>', thumb: '100x100>' }, s3_permissions: :private
+  has_attached_file :file, styles: { medium: '1000x300>', thumb: '100x100>' }, default_url: ActionController::Base.helpers.image_path('/assets/missing_user.png'), s3_permissions: :private
   validates_attachment_content_type :file, content_type: %r{\Aimage\/.*\Z}
 
   def group
