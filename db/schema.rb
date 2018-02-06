168e90f392f7b9e09dde61a5c58d21ee6e21dde5
Derek Owusu-Frimpong <deepsky_5@live.com>
2018-02-06 19:44:54 +0000

    add boolean attribute to initiative_comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180131142119) do
+ActiveRecord::Schema.define(version: 20180206193539) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id",   limit: 4
@@ -593,6 +593,7 @@ ActiveRecord::Schema.define(version: 20180131142119) do
     t.integer "initiative_id", limit: 4
     t.integer "user_id",       limit: 4
     t.text    "content",       limit: 65535
+    t.boolean "approved",                    default: false
   end
 
   create_table "initiative_expenses", force: :cascade do |t|
