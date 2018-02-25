3b00d5f50c1ab62fdb98e55929e26f1ed6f27b78
Derek Owusu-Frimpong <deepsky_5@live.com>
2018-02-25 20:30:44 +0000

    fix errors on Circle CI

--- spec/models/initiative_invitee_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe InitiativeInvitee, type: :model do
-  let!(:initiative_invitee) { build(:initiative_invitee) }
-
-  it{ expect(initiative_invitee).to belong_to(:user) }
-  it{ expect(initiative_invitee).to belong_to(:initiative) }
-end
