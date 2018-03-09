4d6b6deb72e834abbe43ec849aca8c4f52ca8fa7
Derek Owusu-Frimpong <deepsky_5@live.com>
2018-03-09 12:50:01 +0000

    remove google calendar link

--- app/services/google_calendar.rb
+++
@@ -1,8 +0,0 @@
-class GoogleCalendar
-	attr_reader :build_link
-
-	def self.build_link(event)
-		"https://calendar.google.com/calendar/r/eventedit?#{{ dates: CGI.unescape("#{event.start.utc.strftime('%Y%m%dT%H%M%SZ')}/#{event.end.utc.strftime('%Y%m%dT%H%M%SZ')}"),
-		 text:"#{event.name}", details:"#{event.description}", location:"#{event.location}"}.to_query}"
-	end
-end
