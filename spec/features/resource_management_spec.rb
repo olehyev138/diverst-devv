205dc156a17f8889d8b8013c1c144c975b8a2201
Derek Owusu-Frimpong <deepsky_5@live.com>
2018-03-27 14:33:52 +0000

    feature specs for folder creation

--- spec/features/resource_management_spec.rb
+++ spec/features/resource_management_spec.rb
@@ -5,19 +5,41 @@ RSpec.feature 'Resource management' do
 
 	before do
 		login_as(user, scope: :user)
-		visit enterprise_folders_url(user.enterprise)
 	end
 
-	context 'create a new resource' do
-		scenario 'successfully' do
+	context 'create a new folder' do
+		before do
+			visit enterprise_folders_url(user.enterprise)
 			expect(page).to have_content 'Folders'
-
 			visit new_enterprise_folder_url(user.enterprise)
 
 			expect(current_url).to eq new_enterprise_folder_url(user.enterprise)
 			expect(page).to have_content 'Create a folder'
+		end
 
+		scenario 'with password protection' do
 			fill_in 'folder[name]', with: 'Top Secret'
+			check 'Password Protected?'
+			fill_in 'folder[password]', with: 'password_1'
+
+			click_on 'Create Folder'
+
+			expect(current_url).to eq enterprise_folders_url(user.enterprise)
+			expect(page).to have_content  'Top Secret'
+			folder = Folder.find_by(name: 'Top Secret')
+			expect(folder.password_protected?).to eq true
+
+		end
+
+		scenario 'without password protection' do
+			fill_in 'folder[name]', with: 'Top Secret'
+
+			click_on 'Create Folder'
+
+			expect(current_url).to eq enterprise_folders_url(user.enterprise)
+			expect(page).to have_content 'Top Secret'
+			folder = Folder.find_by(name: 'Top Secret')
+			expect(folder.password_protected?).to eq false
 		end
 	end
 end
