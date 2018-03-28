285cb6436047119e3631f47d43c000f1e32a8403
Derek Owusu-Frimpong <deepsky_5@live.com>
2018-03-28 03:13:53 +0000

    rspec feature test for folder share with group

--- spec/features/resource_management_spec.rb
+++ spec/features/resource_management_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.feature 'Resource management' do
 	let!(:user) { create(:user) }
+	let!(:folder_with_pp) { create(:folder, container: user.enterprise, name: "Company Archives", password_protected: true, password: "password_2") }
+	let!(:folder_without_pp) { create(:folder, container: user.enterprise, name: "Company Documents", password_protected: false) }
+	let!(:group) { create(:group, name: "New Group", enterprise_id: user.enterprise_id ) }
 
 	before do
 		login_as(user, scope: :user)
@@ -41,5 +44,26 @@ RSpec.feature 'Resource management' do
 			folder = Folder.find_by(name: 'Top Secret')
 			expect(folder.password_protected?).to eq false
 		end
+
+		scenario 'and share folder with group' do
+			fill_in 'folder[name]', with: 'Top Secret'
+			check 'Password Protected?'
+			fill_in 'folder[password]', with: 'password_1'
+
+			select group.name, from: 'folder[group_ids][]'
+
+			click_on 'Create Folder'
+
+			visit group_folders_url(group)
+			expect(page).to have_content 'Top Secret'
+			expect(page).not_to have_content 'Company Archives'
+			expect(page).not_to have_content 'Company Documents'
+		end
+	end
+
+	context 'update existing folder' do
+	end
+
+	context 'delete existing folder' do
 	end
 end
