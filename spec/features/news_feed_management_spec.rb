e9012150d958fcfab7dccc7c79d394fff066d556
Derek Owusu-Frimpong <deepsky_5@live.com>
2018-03-31 23:40:28 +0000

    feature spec for updating group message

--- spec/features/news_feed_management_spec.rb
+++ spec/features/news_feed_management_spec.rb
@@ -30,6 +30,27 @@ RSpec.feature 'News Feed Management' do
 				expect(page).to have_content "by #{group_message.owner.name_with_status}"
 				expect(page).to have_link 'Comments(0)'
 			end
+
+			scenario 'when updating group message' do
+				group_message = create(:group_message, subject: 'First Group Message',
+					content: 'This is the first group message :)', group_id: group.id, owner_id: user.id)
+
+				visit group_posts_url(group)
+
+				expect(page).to have_content group_message.subject
+
+				click_on 'Edit'
+
+				expect(current_url).to eq edit_group_group_message_url(group, group_message)
+
+				fill_in 'group_message[subject]', with: 'Updated Group Message!!!'
+
+				click_on 'Update Group message'
+
+				expect(current_url).to eq group_posts_url(group)
+				expect(page).to_not have_content 'First Group Message'
+				expect(page).to have_content 'Updated Group Message!!!'
+			end
 		end
 	end
 
