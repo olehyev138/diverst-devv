8d1a2c28b13718b1d96183fd8ad32062181d2f46
Girgy <girgalicious@gmail.com>
2018-05-05 22:45:33 +0000

    Integrity - ensure email destroy callback destroys child objects

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,5 +1,8 @@
 class Email < ActiveRecord::Base
+  # associations
   belongs_to :enterprise
-  has_many :variables, class_name: 'EmailVariable'
+  has_many :variables, class_name: 'EmailVariable', dependent: :destroy
+  
+  # validations
   validates :name, :subject, presence: true
 end
--- spec/models/email_spec.rb
+++ spec/models/email_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe Email do
   it { expect(subject).to respond_to(:use_custom_templates) }
   it { expect(subject).to respond_to(:custom_html_template) }
   it { expect(subject).to respond_to(:custom_txt_template) }
+  
+  describe "#destroy_callbacks" do
+    it "removes the child objects" do
+        email = create(:email)
+        email_variable = create(:email_variable, :email => email)
+        
+        email.destroy
+        
+        expect{Email.find(email.id)}.to raise_error(ActiveRecord::RecordNotFound)
+        expect{EmailVariable.find(email_variable.id)}.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
 end
