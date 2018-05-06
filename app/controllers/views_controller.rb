612f31b0c39269ab7fba7ddebf0bc351810aca67
Matt <mattb1397@protonmail.com>
2018-05-06 15:22:54 -0400

    Update views controller for new associations

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -1,9 +1,7 @@
 class ViewsController < ApplicationController
   def track
-    # In order to keep this generic we are passed the news_feed_link id
-    # We then look it up and get its 'link' association which is the concrete news feed object
     news_feed_link = NewsFeedLink.find(params[:id])
-    View.increment_view(news_feed_link.link, current_user)
+    news_feed_link.increment_view(current_user)
 
     render nothing: true
   end
