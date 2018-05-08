148b9e4b21cf8647b87fa55b7a26f852f7b02d32
Girgy <girgalicious@gmail.com>
2018-05-08 14:36:37 +0000

    Integrity - ensure expense category destroy callback destroys child objects

--- spec/models/expense_category_spec.rb
+++ spec/models/expense_category_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Expense, type: :model do
+RSpec.describe ExpenseCategory, type: :model do
     
     describe 'validations' do
         let(:expense_category) { FactoryGirl.build_stubbed(:expense_category) }
@@ -11,5 +11,17 @@ RSpec.describe Expense, type: :model do
         it { expect(expense_category).to belong_to(:enterprise) }
 
         it { expect(expense_category).to have_many(:expenses) }
+    end
+    
+    describe "#destroy_callbacks" do
+        it "removes the child objects" do
+            expense_category = create(:expense_category)
+            expense = create(:expense, :category => expense_category)
+            
+            expense_category.destroy!
+            
+            expect{ExpenseCategory.find(expense_category.id)}.to raise_error(ActiveRecord::RecordNotFound)
+            expect{Expense.find(expense.id)}.to raise_error(ActiveRecord::RecordNotFound)
+        end
     end
 end
