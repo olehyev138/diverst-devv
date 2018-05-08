e820a80bc02b50c563a88b2ea9ca06854e85050f
Matt <mattb1397@protonmail.com>
2018-05-08 14:40:42 -0400

    Tweak password reset to resend invitations

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,14 +1,36 @@
 class Users::PasswordsController < Devise::PasswordsController
+  # This was overwritten to:
+  # 1) Behave the same way for correct and incorrect emails
+  #    to prevent brute force username lookup
+  #    POST /resource/password
+  #
+  # 2) Resend an invite email for invited but not accepted users
+  #
 
-    # This was overwritten to behave the same way for correct and incorrect emails
-    # to prevent brute force username lookup
-    # POST /resource/password
+  def create
+    self.resource = resource_class.find_by(email: resource_params[:email])
+    message = ''
 
-    def create
-        self.resource = resource_class.send_reset_password_instructions(resource_params)
-        yield resource if block_given?
-
-        flash[:notice] = "You will shortly receive password reset instructions to email"
-        respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
+    if resend_invite?
+      # Resend invite
+      resource_class.invite!(email: resource.email)
+      message = 'You have a pending invitation. Please check your email to accept the invitation and sign in'
+    else
+      # Send reset password instructions
+      self.resource = resource_class.send_reset_password_instructions(resource_params)
+      message = 'You will shortly receive password reset instructions to email'
     end
+
+    yield resource if block_given?
+
+    flash[:notice] = message
+    respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
+  end
+
+  private
+
+  def resend_invite?
+    # Determine whether this user is in the system but has not yet accepted there invitation
+    self.resource != nil and self.resource.invitation_accepted_at == nil
+  end
 end
