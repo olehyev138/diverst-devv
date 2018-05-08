7d995e2919d6549b0d69cd50c0a41b4d86dffaea
Matt <mattb1397@protonmail.com>
2018-05-08 16:55:54 -0400

    Add specs for onboard tweaks

--- spec/controllers/users/passwords_controller_spec.rb
+++ spec/controllers/users/passwords_controller_spec.rb
@@ -1,21 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Users::PasswordsController, type: :controller do
-    let(:user){ create(:user) }
+  let(:user){ create(:user) }
 
-    before :each do
-        @request.env["devise.mapping"] = Devise.mappings[:user]
+  before :each do
+    @request.env['devise.mapping'] = Devise.mappings[:user]
+  end
+
+  describe 'POST#create' do
+    it 'renders success' do
+      post :create
+      expect(response).to redirect_to '/users/sign_in'
     end
 
-    describe "POST#create" do
-        before {post :create}
+    it 'flashes invite message when user hasnt accepted' do
+      user.invitation_accepted_at = nil
+      user.save
 
-        it "renders success" do
-            expect(response).to redirect_to '/users/sign_in'
-        end
-
-        it "flashes" do
-            expect(flash[:notice]).to eq("You will shortly receive password reset instructions to email")
-        end
+      post :create, { user: { email: user.email } }
+      expect(flash[:notice]).to eq('You have a pending invitation. Please check your email to accept the invitation and sign in')
     end
+
+    it 'flashes password reset message when user has accepted' do
+      post :create, email: user.email
+      expect(flash[:notice]).to eq('You will shortly receive password reset instructions to email')
+    end
+
+    it 'flashes password reset message when user doesnt exist' do
+      post :create
+      expect(flash[:notice]).to eq('You will shortly receive password reset instructions to email')
+    end
+  end
 end
