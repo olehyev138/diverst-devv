baab5c5dc6ad9de7b63a945b43e715e585f7dd07
Girgy <girgalicious@gmail.com>
2018-05-10 14:58:42 +0000

    Integrity - ensure group_messages_segment destroy callback destroys child objects

--- spec/models/group_messages_segment_spec.rb
+++ spec/models/group_messages_segment_spec.rb
@@ -18,4 +18,16 @@ RSpec.describe GroupMessagesSegment, type: :model do
             expect(group_messages_segment.news_feed_link_segment).to_not be_nil
         end
     end
+    
+    describe "#destroy_callbacks" do
+        it "removes the child objects" do
+            group_messages_segment = create(:group_messages_segment)
+            news_feed_link_segment = group_messages_segment.news_feed_link_segment
+            
+            group_messages_segment.destroy!
+            
+            expect{GroupMessagesSegment.find(group_messages_segment.id)}.to raise_error(ActiveRecord::RecordNotFound)
+            expect{NewsFeedLinkSegment.find(news_feed_link_segment.id)}.to raise_error(ActiveRecord::RecordNotFound)
+        end
+    end
 end
