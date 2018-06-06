d27e1f7fe8b29489f59546ee9b5873ec98257d8c
Girgy <girgalicious@gmail.com>
2018-06-06 00:58:04 +0000

    Integrity - destroy callback spec tests for Topic model

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe Topic, type: :model do
   it { expect(topic).to belong_to(:enterprise) }
   # it { expect(topic).to belong_to(:admin) }
   it { expect(topic).to have_many(:feedbacks).class_name('TopicFeedback') }
+  
+  describe "#destroy_callbacks" do
+    it "removes the child objects" do
+      topic = create(:topic)
+      topic_feedback = create(:topic_feedback, :topic => topic)
+
+      topic.destroy
+
+      expect{Topic.find(topic.id)}.to raise_error(ActiveRecord::RecordNotFound)
+      expect{TopicFeedback.find(topic_feedback.id)}.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
 end
