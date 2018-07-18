1f0150b14d834ce3a90e68ae1040a68e29b8892e
Anatolii Borysov <borisano@gmail.com>
2018-07-18 17:56:56 +0300

    Remove obsolete clock jobs, increase interval between Segment updates

--- clock.rb
+++ clock.rb
@@ -4,12 +4,10 @@ require './config/environment'
 
 include Clockwork
 
-every(1.hour, 'Update cached segment members') { Segment.update_all_members }
+every(5.hours, 'Update cached segment members') { Segment.update_all_members }
 every(1.hour, 'Send hourly notifications of groups to users'){ UserGroupNotificationJob.perform_later('hourly') }
 every(1.day, 'Reset weekly rewards', at: '00:00') { ResetWeeklyRewardsJob.perform_later if Date.today.sunday? }
-every(10.minutes, 'Sync Yammer users with Diverst users') { SyncYammerUsersJob.perform_later }
-every(30.minutes, 'Sync Yammer members') { Group.all.each { |group| SyncYammerGroupJob.perform_later(group) } }
-every(30.minutes, 'Save employee data samples') { SaveUserDataSamplesJob.perform_later }
+
 every(1.day, 'Send daily notifications of groups to users', at: '00:00'){ UserGroupNotificationJob.perform_later('daily') }
 every(1.day, 'Send daily notifications of pending users to group leaders', at: '00:00'){ Group.where(:pending_users => "enabled").find_each { |group| GroupLeaderMemberNotificationsJob.perform_later(group) } }
 every(1.day, 'Send daily notifications of pending comments to group leaders', at: '00:00'){ Group.find_each { |group| GroupLeaderCommentNotificationsJob.perform_later(group) } }
