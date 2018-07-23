1bbd6e1e19c395ba79fa4ca1e8076fcdd0d728e3
Mark Roy <markr4848@gmail.com>
2018-07-23 18:58:04 -0400

    Removed budget decline reason

--- db/migrate/20180721020102_add_budget_decline_reason_to_budgets.rb
+++
@@ -1,5 +0,0 @@
-class AddBudgetDeclineReasonToBudgets < ActiveRecord::Migration
-  def change
-    add_column :budgets, :budget_decline_reason, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180723221837) do
+ActiveRecord::Schema.define(version: 20180723221838) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id",   limit: 4
@@ -153,7 +153,6 @@ ActiveRecord::Schema.define(version: 20180723221837) do
     t.integer  "requester_id",          limit: 4
     t.integer  "event_id",              limit: 4
     t.integer  "group_id",              limit: 4
-    t.string   "budget_decline_reason", limit: 191
   end
 
   add_index "budgets", ["approver_id"], name: "fk_rails_a057b1443a", using: :btree
