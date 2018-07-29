c6fc58d001d470552dc9dc115a353296825194d4
Derek Owusu-Frimpong <deepsky_5@live.com>
2018-07-29 12:25:50 +0000

    display group type on join/leave button, i.e parent or sub group

--- spec/features/group_membership_management_spec.rb
+++ spec/features/group_membership_management_spec.rb
@@ -19,7 +19,7 @@ RSpec.feature 'Group Membership Management' do
 		scenario 'when a user joins a parent group with children', js: true do
 			visit group_path(group)
 
-			click_button "Join this #{c_t(:erg)}"
+			click_button "Join this #{c_t(:parent)}"
 
 			expect(page).to have_content "Thanks for joining the #{c_t(:parent)}! Do you also want to join a #{c_t(:sub_erg)}?"
 
@@ -169,7 +169,7 @@ RSpec.feature 'Group Membership Management' do
 		scenario 'when a user joins a parent group with children', js: true do
 			visit group_path(group)
 
-			click_button "Join this #{c_t(:erg)}"
+			click_button "Join this #{c_t(:parent)}"
 
 			expect(page).to have_content "Thanks for joining the #{c_t(:parent)}! Do you also want to join a #{c_t(:sub_erg)}?"
 
@@ -197,7 +197,7 @@ RSpec.feature 'Group Membership Management' do
 		end
 
 		context 'when a user' do
-			let!(:parent_group) { create(:group, name: 'Group ONE', enterprise: enterprise, parent_id: nil) }
+			let!(:parent_group) { create(:group, name: 'Parent Group', enterprise: enterprise, parent_id: nil) }
 
 			scenario 'joins a standard group(group with no parent or child)' do
 				visit group_path(parent_group)
@@ -216,9 +216,9 @@ RSpec.feature 'Group Membership Management' do
 			scenario 'a parent group', js: true do
 				visit group_path(group)
 
-				click_link "Leave this #{c_t(:erg)}"
+				click_link "Leave this #{c_t(:parent)}"
 
-				expect(page).to have_button "Join this #{c_t(:erg)}"
+				expect(page).to have_button "Join this #{c_t(:parent)}"
 				expect(group.members).not_to include guest_user
 				expect(sub_group.members).not_to include guest_user
 			end
