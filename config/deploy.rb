987af4c6dfc768638d3a9098f6ecb2a1036f5545
Sergey Kukunin <sergey.kukunin@gmail.com>
2018-08-15 15:01:31 +0300

    Generate puma.rb with capistrano. Don't forget `cap puma:config`

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :application, 'diverst'
 set :repo_url, 'git@github.com:TeamDiverst/diverst-development.git'
 set :deploy_to, '/home/newdeploy/diverst'
 set :pty, false
-set :linked_files, %w(config/application.yml config/database.yml)
+set :linked_files, %w(config/application.yml config/database.yml config/puma.rb)
 set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads)
 set :keep_releases, 5
 set :rvm_type, :user
@@ -13,7 +13,7 @@ set :rvm_ruby_version, '2.3.0'
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, 'tcp://0.0.0.0:3000' #"unix://#{shared_path}/tmp/sockets/puma.sock" # accept array for multi-bind
+set :puma_bind, 'tcp://0.0.0.0:3000'
 set :puma_conf, "#{shared_path}/config/puma.rb"
 set :puma_access_log, "#{shared_path}/log/puma_error.log"
 set :puma_error_log, "#{shared_path}/log/puma_access.log"
@@ -22,6 +22,7 @@ set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 set :puma_preload_app, true
+set :puma_workers, 2
 
 set :sidekiq_config, 'config/sidekiq.yml'
 set :sidekiq_log, '/dev/null'
