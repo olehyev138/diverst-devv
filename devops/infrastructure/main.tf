a09c25e43d3b54c46e621c2cbe5c57f121ba1451
Sergey Kukunin <sergey.kukunin@gmail.com>
2018-08-26 17:37:23 +0300

    Extract BigBrother to a separate file

--- devops/infrastructure/main.tf
+++ devops/infrastructure/main.tf
@@ -20,31 +20,3 @@ resource "aws_security_group" "ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-resource "aws_security_group" "big_brother" {
-  name        = "big_brother"
-  description = "Security group for the BigBrother instance"
-}
-
-resource "aws_eip" "big_brother" {
-  instance = "${aws_instance.big_brother.id}"
-}
-
-resource "aws_instance" "big_brother" {
-  ami = "ami-04169656fea786776"
-  instance_type = "t2.micro"
-  key_name = "${aws_key_pair.main.key_name}"
-
-  security_groups = [
-    "${aws_security_group.ssh.name}",
-    "${aws_security_group.big_brother.name}"
-  ]
-
-  tags {
-    Name = "BigBrother"
-  }
-
-  root_block_device {
-    volume_size = 100
-  }
-}
