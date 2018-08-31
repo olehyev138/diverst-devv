2e1610163016d85357f6743b5d6bfbc5e6c01cd2
Sergey Kukunin <sergey.kukunin@gmail.com>
2018-09-01 00:13:59 +0300

    Add instances to Ansible

--- devops/infrastructure/instance/output.tf
+++ devops/infrastructure/instance/output.tf
@@ -5,3 +5,7 @@ output "webserver_ips" {
 output "workers_ips" {
   value = ["${aws_instance.worker.*.public_ip}"]
 }
+
+output "hosts" {
+  value = "${zipmap(concat(aws_instance.webserver.*.tags.Name, aws_instance.worker.*.tags.Name), concat(aws_eip.webserver.*.public_ip, aws_instance.worker.*.public_ip))}"
+}
--- devops/infrastructure/output.tf
+++ devops/infrastructure/output.tf
@@ -9,3 +9,17 @@ output "staging_ips" {
 output "kp_ips" {
   value = "${map("webservers", module.kp.webserver_ips, "workers", module.kp.workers_ips)}"
 }
+
+output "ssh_hosts" {
+  value = <<VALUE
+
+${join("\n\n",
+       formatlist("Host %s.diverst.com\n  HostName %s\n  User ubuntu",
+                  concat(list("bigbrother"),
+                         keys(module.kp.hosts),
+                         keys(module.staging.hosts)),
+                  concat(list("${aws_eip.big_brother.public_ip}"),
+                         values(module.kp.hosts),
+                         values(module.staging.hosts))))}
+VALUE
+}
