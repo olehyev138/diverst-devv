9369f7e6a347c75613621c09765f930cd5672a76
Sergey Kukunin <sergey.kukunin@gmail.com>
2018-09-02 12:12:45 +0300

    Run Grafana on 80 port, proxy by CloudFlare

--- devops/infrastructure/big_brother.tf
+++ devops/infrastructure/big_brother.tf
@@ -38,8 +38,8 @@ resource "aws_security_group" "big_brother" {
   }
 
   ingress {
-    from_port   = 3000
-    to_port     = 3000
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
