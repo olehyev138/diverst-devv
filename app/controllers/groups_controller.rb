ec8a722c3561d7688b607d4e8c446865bbb969b6
Derek Owusu-Frimpong <deepsky_5@live.com>
2018-09-10 09:32:04 +0000

    display sub ergs in global calendar

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -30,7 +30,7 @@ class GroupsController < ApplicationController
     def calendar
         authorize Group, :index?
         enterprise = current_user.enterprise
-        @groups = enterprise.groups.all_parents
+        @groups = enterprise.groups
         @segments = enterprise.segments
         @q_form_submit_path = calendar_groups_path
         @q = Initiative.ransack(params[:q])
