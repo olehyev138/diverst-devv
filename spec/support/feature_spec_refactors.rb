7e1ce5a3840106c0f055b843cc5deb7f8d10203f
Derek Owusu-Frimpong <deepsky_5@live.com>
2018-09-12 07:58:11 +0000

    use capybara matcher have_current_path for asserting current_path

--- spec/support/feature_spec_refactors.rb
+++ spec/support/feature_spec_refactors.rb
@@ -125,7 +125,7 @@ module FeatureSpecRefactors
 			fill_in 'user_password', with: 'wh4t3v3r'
 			click_on 'Log in'
 
-			expect(current_path).to eq new_user_session_path
+			expect(page).to have_current_path new_user_session_path
 		end
 
 		def expect_new_text_field_form
