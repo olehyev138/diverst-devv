6a3f9a28e1610ad7964108b00795beb6e16ea13a
Girgy <girgalicious@gmail.com>
2018-09-19 07:07:36 +0000

    AutoDeployment - comment out deploy job for now and undo focus on spec test

--- spec/models/view_spec.rb
+++ spec/models/view_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe View, type: :model do
   let(:view) { FactoryGirl.create :view }
 
   describe 'factory' do
-    fit 'is valid' do
+    it 'is valid' do
       expect(view).to be_valid
     end
   end
