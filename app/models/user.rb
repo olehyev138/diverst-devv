06afbf6b21fd974ebd69605e1c5dc02bec2add64
Derek Owusu-Frimpong <deepsky_5@live.com>
2018-09-27 13:36:41 +0000

    add ability to set groups notifications for all groups a user is a member of on user edit/update

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,9 @@ class User < ActiveRecord::Base
 
     @@fb_token_generator = Firebase::FirebaseTokenGenerator.new(ENV['FIREBASE_SECRET'].to_s)
 
+    enum groups_notifications_frequency: [:hourly, :daily, :weekly, :disabled]
+    enum groups_notifications_date: [:sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday]
+
     scope :active,              -> { where(active: true).distinct }
     scope :enterprise_mentors,  -> ( user_ids = []) { where(mentor: true).where.not(:id => user_ids) }
     scope :enterprise_mentees,  -> ( user_ids = []) { where(mentee: true).where.not(:id => user_ids) }
