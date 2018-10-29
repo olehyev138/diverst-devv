943af1facffd0baa066762a6d4a2a7f7a3ec9b32
Derek Owusu-Frimpong <deepsky_5@live.com>
2018-10-29 11:49:15 +0000

    validate against Group Leader

--- spec/models/group_leader_spec.rb
+++ spec/models/group_leader_spec.rb
@@ -4,11 +4,12 @@ RSpec.describe GroupLeader, type: :model do
 
   describe 'when validating' do
     let!(:enterprise) {create(:enterprise)}
-    let(:group_leader) { build_stubbed(:group_leader) }
+    let(:group_leader) { build(:group_leader) }
 
     it{ expect(group_leader).to validate_presence_of(:position_name) }
     it{ expect(group_leader).to validate_presence_of(:group) }
     it{ expect(group_leader).to validate_presence_of(:user) }
+    it{ expect(group_leader).to validate_uniqueness_of(:user_id) }
     it{ expect(group_leader).to belong_to(:user) }
     it{ expect(group_leader).to belong_to(:group) }
   end
