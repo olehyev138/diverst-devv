eb041fcefceb4b71519182f46af0c176f19a4095
Derek Owusu-Frimpong <deepsky_5@live.com>
2018-10-30 22:21:04 +0000

    use appropriate validation message for duplicate group leaders and add tests for the validation

--- spec/models/group_leader_spec.rb
+++ spec/models/group_leader_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe GroupLeader, type: :model do
     it{ expect(group_leader).to validate_presence_of(:position_name) }
     it{ expect(group_leader).to validate_presence_of(:group) }
     it{ expect(group_leader).to validate_presence_of(:user) }
-    it{ expect(group_leader).to validate_uniqueness_of(:user_id) }
+    it{ expect(group_leader).to validate_uniqueness_of(:user_id).with_message('already exists as a group leader').case_insensitive }
     it{ expect(group_leader).to belong_to(:user) }
     it{ expect(group_leader).to belong_to(:group) }
   end
