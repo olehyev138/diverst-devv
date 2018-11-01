bcc89126903140f4e110b440cd223c5a789e2104
Matthew Baldwin <mattb1397@protonmail.com>
2018-10-31 20:57:08 -0500

    Add spec for group_base_policy

--- app/policies/group_budget_policy.rb
+++ app/policies/group_budget_policy.rb
@@ -1,24 +1,24 @@
 class GroupBudgetPolicy < GroupBasePolicy
-  
-    def base_index_permission
-      "groups_budgets_index"
-    end
-    
-    def base_create_permission
-      "groups_budgets_request"
-    end
-    
-    def approve?
-      return true if update?
-      @policy_group.budget_approval?
-    end
-    
-    def manage_all_budgets?
-      return true if user.policy_group.manage_all?
-      user.policy_group.groups_budgets_manage? && user.policy_group.groups_manage?
-    end
-    
-    def base_manage_permission
-      "groups_budgets_manage"
-    end
+
+  def base_index_permission
+    "groups_budgets_index"
+  end
+
+  def base_create_permission
+    "groups_budgets_request"
+  end
+
+  def approve?
+    return true if update?
+    @policy_group.budget_approval?
+  end
+
+  def manage_all_budgets?
+    return true if user.policy_group.manage_all?
+    user.policy_group.groups_budgets_manage? && user.policy_group.groups_manage?
+  end
+
+  def base_manage_permission
+    "groups_budgets_manage"
+  end
 end
