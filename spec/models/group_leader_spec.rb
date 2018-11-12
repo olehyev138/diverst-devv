b73ca9e9eaf61c5aa7190cc3d7b69bf52acce11d
Girgy <girgalicious@gmail.com>
2018-11-12 21:50:45 +0000

    GroupLeaderValidation - update group leader unique validation to ensure group leader scope is per group and not per enterprise

--- spec/models/group_leader_spec.rb
+++ spec/models/group_leader_spec.rb
@@ -13,5 +13,29 @@ RSpec.describe GroupLeader, type: :model do
     it{ expect(group_leader).to belong_to(:user) }
     it{ expect(group_leader).to belong_to(:group) }
   end
+  
+  describe "#user_id" do
+    it "validates user cannot be added as group leader to group twice" do
+      user = create(:user)
+      group = create(:group, :enterprise => user.enterprise)
+      group_2 = create(:group, :enterprise => user.enterprise)
+      create(:user_group, :group => group, :user => user)
+      create(:user_group, :group => group_2, :user => user)
+      
+      # validate that the first group leader is valid
+      group_leader = create(:group_leader, :user => user, :group => group)
+      expect(group_leader.valid?).to be(true)
+      
+      # validate that the second group leader is not valid since a user cannot be 
+      # a group leader in 1 group twice
+      group_leader_2 = build(:group_leader, :user => user, :group => group)
+      expect(group_leader_2.valid?).to_not be(true)
+      
+      # validate that the second group leader is valid since a user can be 
+      # a group leader in 2 groups once
+      group_leader_3 = create(:group_leader, :user => user, :group => group_2)
+      expect(group_leader_3.valid?).to be(true)
+    end
+  end
 
 end
