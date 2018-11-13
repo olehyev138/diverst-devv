36fab20d8dbdf08f7398e3f28bd9e3fd81645e7f
Matthew Baldwin <mattb1397@protonmail.com>
2018-11-13 18:44:05 -0500

    Make user selects async

---
+++ app/assets/javascripts/async_select.js
@@ -0,0 +1,30 @@
+function async_select(url) {
+    $('.select2-field').select2({
+        ajax: {
+            url: url,
+            theme: 'bootstrap',
+            allowClear: true,
+            type: "GET",
+            dataType: "json",
+            minimumInputLength: 3,
+            multiple:true,
+            delay: 1000,
+            data: function (params) {
+                // Query parameters will be ?search=[term]&type=public
+                return { search: { value: params.term } }
+            },
+            processResults: function (data) {
+                var int = 1;
+
+                // Tranforms the top-level key of the response object from 'items' to 'results'
+                return {
+                    results: $.map(data.data, function(item){
+                        return {id: item[0], text: item[1] + " " + item[2] + " - " + item[3] } } )
+                };
+            },
+            templateSelection: function(item) {
+                return item[0]
+            }
+        }
+    });
+}
