177da727d668749352c822657352d68796f80757
Derek Owusu-Frimpong <deepsky_5@live.com>
2018-12-31 21:37:43 +0000

    create seed data for MentorshipAvailability, MentoringSession, MentoringRequest

--- db/seeds/development/mentors.seeds.rb
+++ db/seeds/development/mentors.seeds.rb
@@ -17,5 +17,18 @@ after 'development:enterprise' do
             {name: "People Skills"},
             {name: "Public Speaking"}
         ])
+
+        days_of_the_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+        if enterprise.name == "Diverst Inc"
+            enterprise.users.each do |user|
+                MentorshipAvailability.create(user_id: user.id, start: "1:30", end: "2:30", day: days_of_the_week.sample)
+            end
+            
+            admin = enterprise.users.where(user_role_id: 1).first #find first admin in enterprise
+            user = enterprise.users.where(user_role_id: 7).first
+            FactoryGirl.create_list(:mentoring_session, rand(5..10), enterprise_id: enterprise.id, creator_id: admin.id )
+
+            FactoryGirl.create(:mentoring_request, enterprise_id: enterprise.id, sender_id: user.id, receiver_id: admin.id )
+        end
     end
 end
