75171f5117cb58215c86633ce11215676942f89f
ImOnMars <markr4848@gmail.com>
2019-01-16 12:06:52 -0500

    Update mentorship sessions controller spec

--- spec/controllers/mentorship_sessions_controller_spec.rb
+++ spec/controllers/mentorship_sessions_controller_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe MentorshipSessionsController, type: :controller do
-  let(:user){ create(:user) }
-  let(:creator) { create(:user) }
-  let(:mentoring_session){ create(:mentoring_session, creator: creator, start: Date.today, end: Date.tomorrow + 1.day, status: "scheduled", format: "Video") }
+  let(:enterprise) { create :enterprise }
+  let(:user){ create(:user, enterprise: enterprise) }
+  let(:creator) { create(:user, enterprise: enterprise) }
+  let(:mentoring_session){ create(:mentoring_session, enterprise: enterprise, creator: creator, start: Date.today, end: Date.tomorrow + 1.day, status: "scheduled", format: "Video") }
   let!(:mentorship_session){ mentoring_session.mentorship_sessions.create(:user => creator, :mentoring_session => mentoring_session, :role => "presenter") }
   let!(:mentorship_session2){ mentoring_session.mentorship_sessions.create(:user => user, :mentoring_session => mentoring_session, :role => "viewer") }
 
@@ -12,7 +13,7 @@ RSpec.describe MentorshipSessionsController, type: :controller do
 
     before {
       request.env["HTTP_REFERER"] = "back"
-      get :accept, mentoring_session_id: mentoring_session.id, id: mentorship_session2.id
+      post :accept, mentoring_session_id: mentoring_session.id, id: mentorship_session2.id
     }
 
     context 'when user is logged in' do
@@ -35,6 +36,29 @@ RSpec.describe MentorshipSessionsController, type: :controller do
   end
 
   describe 'POST#decline' do
+    login_user_from_let
 
+    before {
+      request.env["HTTP_REFERER"] = "back"
+      post :decline, mentoring_session_id: mentoring_session.id, id: mentorship_session2.id
+    }
+
+    context 'when user is logged in' do
+      it 'sets the status to declined' do
+        expect(mentorship_session2.declined?).to eq true
+      end
+
+      it 'flashes a notice message' do
+        expect(flash[:notice]).to eq "Session invitation declined"
+      end
+
+      it 'redirects to previous page' do
+        expect(response).to redirect_to "back"
+      end
+    end
+
+    context 'without logged user' do
+      it_behaves_like "redirect user to users/sign_in path"
+    end
   end
 end
