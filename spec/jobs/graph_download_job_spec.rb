bd4e704bcc0ba1d61eecb1256090cae487ae80d8
Matthew Baldwin <matt@diverst.com>
2019-03-19 17:42:53 -0400

    Update graph download job spec

--- spec/jobs/graph_download_job_spec.rb
+++ spec/jobs/graph_download_job_spec.rb
@@ -10,23 +10,20 @@ RSpec.describe GraphDownloadJob, type: :job do
     let(:field1) { create(:field, type: "NumericField", poll: poll) }
     let(:field2) { create(:field, type: "NumericField", poll: poll) }
     let(:field3) { create(:field, type: "CheckboxField", poll: poll) }
-    let(:metrics_graph) { create(:graph, metrics_dashboard: metrics_dashboard, field: field1) }
-    let(:poll_graph) { create(:graph, poll: poll, field: field2) }
+    let(:metrics_graph) { create(:graph_with_metrics_dashboard, metrics_dashboard: metrics_dashboard, field: field1) }
+    let(:poll_graph) { create(:graph_with_metrics_dashboard, poll: poll, field: field2) }
 
     describe "#perform" do
-        before { User.__elasticsearch__.create_index!(index: User.es_index_name(enterprise: enterprise)) }
-        after { User.__elasticsearch__.delete_index!(index: User.es_index_name(enterprise: enterprise)) }
-
         context "poll graph" do
           it "creates a downloadable csv file" do
-              expect{ subject.perform(user.id, poll_graph.id) }
+              expect{ subject.perform(user.id, poll_graph.id, '', []) }
                 .to change(CsvFile, :count).by(1)
           end
         end
 
         context "metrics graph" do
           it "creates a downloadable csv file" do
-              expect{ subject.perform(user.id, metrics_graph.id) }
+              expect{ subject.perform(user.id, metrics_graph.id, '', []) }
                 .to change(CsvFile, :count).by(1)
           end
         end
