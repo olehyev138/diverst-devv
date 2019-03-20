b429d1047079de81aed60b85a2839b0fe4750507
Derek Owusu-Frimpong <deepsky_5@live.com>
2019-03-20 00:14:59 +0000

    add instructions close expenses and export budget when reset annual budget link is clicked

--- app/services/annual_budget_manager.rb
+++ app/services/annual_budget_manager.rb
@@ -4,7 +4,7 @@ class AnnualBudgetManager
 	end
 
 	def reset
-      @group.initiatives.update_all(:estimated_funding => 0, :actual_funding => 0)
+      @group.initiatives.update_all(:estimated_funding => 0, :actual_funding => 0, :budget_item_id => nil)
 	  @group.initiatives.each { |initiative|  initiative.expenses.destroy_all }
       @group.budgets.destroy_all
 	  return true if @group.update({:annual_budget => 0, :leftover_money => 0})		
