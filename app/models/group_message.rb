1953f7f4b9267e311d04986481b24af034a70a2c
Mark Roy <markr4848@gmail.com>
2019-03-21 00:27:47 -0400

    Fix group and group message specs

--- app/models/group_message.rb
+++ app/models/group_message.rb
@@ -1,39 +1,5 @@
 class GroupMessage < BaseClass
   include PublicActivity::Common
-    has_many :group_messages_segments, dependent: :destroy
-
-    has_many :segments, through: :group_messages_segments, :before_remove => :remove_segment_association
-    has_many :comments, class_name: 'GroupMessageComment', foreign_key: :message_id, dependent: :destroy
-    has_many :user_reward_actions
-    
-    belongs_to :owner, class_name: 'User'
-    belongs_to :group
-
-    has_one :news_feed_link
-    after_create :approve_link
-    
-    accepts_nested_attributes_for :news_feed_link, :allow_destroy => true
-    
-    delegate :increment_view, :to => :news_feed_link
-    delegate :total_views, :to => :news_feed_link
-    delegate :unique_views, :to => :news_feed_link
-
-    validates :group_id,    presence: true
-    validates :subject,     presence: true
-    validates :content,     presence: true
-    validates :owner_id,    presence: true
-
-    alias_attribute :author, :owner
-
-    after_create :build_default_link
-
-    scope :of_segments, ->(segment_ids) {
-      gm_condtions = ["group_messages_segments.segment_id IS NULL"]
-      gm_condtions << "group_messages_segments.segment_id IN (#{ segment_ids.join(",") })" unless segment_ids.empty?
-      joins("LEFT JOIN group_messages_segments ON group_messages_segments.group_message_id = group_messages.id")
-      .where(gm_condtions.join(" OR "))
-    }
-
   has_many :group_messages_segments, dependent: :destroy
 
   has_many :segments, through: :group_messages_segments, :before_remove => :remove_segment_association
@@ -43,7 +9,7 @@ class GroupMessage < BaseClass
   belongs_to :owner, class_name: 'User'
   belongs_to :group
 
-  has_one :news_feed_link, :dependent => :destroy
+  has_one :news_feed_link
   after_create :approve_link
 
   accepts_nested_attributes_for :news_feed_link, :allow_destroy => true
