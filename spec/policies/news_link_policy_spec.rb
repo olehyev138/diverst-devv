b6c057b5d2d06237c7853fd370e44a6992246774
Derek Owusu-Frimpong <deepsky_5@live.com>
2019-04-01 08:30:39 +0000

    news_link_policy_spec.rb

--- spec/policies/news_link_policy_spec.rb
+++ spec/policies/news_link_policy_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe NewsLinkPolicy, :type => :policy do
   let(:no_access) { create(:user) }
   let(:news_link){ create(:news_link, author: user) }
 
-  subject { described_class }
+  subject { NewsLinkPolicy.new(user, news_link) }
 
   before {
     user.policy_group.manage_all = false
@@ -19,22 +19,60 @@ RSpec.describe NewsLinkPolicy, :type => :policy do
     no_access.policy_group.save!
   }
 
-  permissions :index?, :create?, :manage?, :update? do
-    it 'allows access to user with correct permissions' do
-      expect(subject).to permit(user, news_link)
+  describe 'for users with access' do 
+    it { is_expected.to permit_actions([:index, :show, :create, :update, :destroy]) }
+
+    context 'when ONLY manage_all is true' do 
+      before { user.policy_group.update news_links_manage: false, news_links_create: false, news_links_index: false, manage_all: true }
+      it { is_expected.to permit_actions([:index, :show, :create, :update, :destroy]) }
     end
 
-    it 'denies access to user with incorrect permissions' do
-      expect(subject).to_not permit(no_access, news_link)
+    context 'when ONLY news_links_index is true' do 
+      before { user.policy_group.update news_links_index: true, news_links_create: false, news_links_manage: false, manage_all: false }
+      it { is_expected.to permit_actions([:index, :show, :update, :destroy]) }
+    end
+
+    context 'when ONLY news_links_index is true and current user is not author' do 
+      before do 
+        news_link.author = create(:user)
+        user.policy_group.update news_links_index: true, news_links_create: false, news_links_manage: false, manage_all: false
+      end
+
+      it { is_expected.to permit_actions([:index, :show]) }
+    end
+
+    context 'when ONLY news_links_create is true' do 
+      before { user.policy_group.update news_links_index: false, news_links_create: true, news_links_manage: false, manage_all: false }
+      it { is_expected.to permit_actions([:index, :show, :create, :update, :destroy]) }
+    end
+
+    context 'when ONLY news_links_create is true and current user is not author' do 
+      before do 
+        news_link.author = create(:user)
+        user.policy_group.update news_links_index: false, news_links_create: true, news_links_manage: false, manage_all: false
+      end
+
+      it { is_expected.to permit_actions([:index, :show, :create]) }
+    end
+
+    context 'when ONLY news_links_manage is true' do 
+      before { user.policy_group.update news_links_manage: true, news_links_create: false, news_links_index: false, manage_all: false }
+      it { is_expected.to permit_actions([:index, :show, :create, :update, :destroy]) }
+    end
+
+    context 'when ONLY news_links_manage is true and current user is not author' do 
+      before do 
+        news_link.author = create(:user)
+        user.policy_group.update news_links_manage: true, news_links_create: false, news_links_index: false, manage_all: false
+      end
+
+      it { is_expected.to permit_actions([:index, :show, :create]) }
     end
   end
 
-  permissions :index?, :create?, :update? do
-    it 'allows access to non managers' do
-      user.policy_group.news_links_manage = false
-
-      expect(subject).to permit(user, news_link)
-    end
+  describe 'for users with no access' do 
+    let!(:user) { no_access }
+    before { news_link.author = create(:user) }
+    it { is_expected.to forbid_actions([:index, :show, :create, :update, :destroy]) }
   end
-
 end
