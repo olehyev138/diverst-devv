a1f952da10ae38e0b532ca1fb137aa54a9c4af98
ImOnMars <markr4848@gmail.com>
2019-04-18 15:39:43 -0400

    Fix group seeding

--- db/seeds/development/groups.seeds.rb
+++ db/seeds/development/groups.seeds.rb
@@ -1,14 +1,12 @@
 def self.populate_group(enterprise, group)
-  group_members = []
   enterprise.users.each do |user|
     # calling create fails with 'default_mentor_group not a method' for some unknown reason
     # using import for user groups is probably better anyway
-    group_members << UserGroup.new(group_id: group.id, user_id: user.id,
+    earliest_date = user.created_at > group.created_at ? user.created_at : group.created_at
+    UserGroup.create(group_id: group.id, user_id: user.id,
                                    accepted_member: true,
-                                   created_at: Faker::Time.between(2.years.ago, Time.current - 2.days)) if rand(100) < 25
+                                   created_at: Faker::Time.between(earliest_date, Time.current - 2.days)) if rand(100) < 25
   end
-
-  UserGroup.import group_members
 end
 
 
@@ -21,7 +19,7 @@ after 'development:users' do
     'Asian Blue Community',
     'Global Network',
     'Latina Cultural Network',
-    'Pride Network',
+    'Pride Community',
     'Women\'s Network',
     'Ability Network',
     'Veterans Network',
@@ -39,8 +37,8 @@ after 'development:users' do
       # shuffle group_names & take first 10
       group_names.shuffle.slice(0..no_groups).each do |group_name|
         group_name = 'BAD ENTERPRISE ' + group_name if enterprise.name != 'Diverst Inc'
-        group = enterprise.groups.create(name: group_name, description: "",
-                                         created_at: Faker::Time.between(2.years.ago, Time.current - 2.days))
+        group = enterprise.groups.create!(name: group_name, description: "",
+                                         created_at: Faker::Time.between(enterprise.created_at, Time.current - 2.days))
 
         # create subgroups
         (0..rand(0..max_subgroups)).each do |i|
@@ -48,8 +46,8 @@ after 'development:users' do
           subgroup_name = subgroup_prefix + ' Chapter ' + (65 + i).chr
           subgroup_name = 'BAD ENTERPRISE ' + subgroup_name if enterprise.name != 'Diverst Inc'
 
-          subgroup = group.children.create(name: subgroup_name, description: "",
-                                            created_at: Faker::Time.between(2.years.ago, Time.current - 2.days))
+          subgroup = group.children.create!(name: subgroup_name, description: "",
+                                            created_at: Faker::Time.between(enterprise.created_at, Time.current - 2.days))
 
           self.populate_group(enterprise, subgroup)
         end
@@ -59,9 +57,10 @@ after 'development:users' do
 
       # always create mentor network group
       mentor_group_name = enterprise.name == 'Diverst Inc' ? 'The Mentor Network' : 'BAD ENTERPRISE The Mentor Network'
-      mentor_group = enterprise.groups.create(
+      mentor_group = enterprise.groups.create!(
         name: mentor_group_name,
         description: "The Mentor Network is designed to assist associates with their ongoing career development through engagement with a mentor or mentee. The goal is to create a community in which mentors and mentees can learn from one another to foster career enrichment and advancement.",
+        created_at: Faker::Time.between(enterprise.created_at, Time.current - 2.days),
         default_mentor_group: true
       )
 
