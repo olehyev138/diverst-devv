ccd2a2c2e8f87dc80b23610dcff9e4b23d4ada4b
Derek Owusu-Frimpong <deepsky_5@live.com>
2019-05-01 14:45:30 +0000

    update users_download_job_spec.rb

--- spec/jobs/users_download_job_spec.rb
+++ spec/jobs/users_download_job_spec.rb
@@ -3,37 +3,58 @@ require 'rails_helper'
 RSpec.describe UsersDownloadJob, type: :job do
     include ActiveJob::TestHelper
 
+    let(:enterprise) { create(:enterprise) }
+    let(:user) { create(:user, :enterprise => enterprise) }
+
+
     describe "#perform" do
-        it "creates a downloadable csv file for all users" do
-            enterprise = create(:enterprise)
-            user = create(:user, :enterprise => enterprise)
-            
-            expect{ subject.perform(user.id, "all_users") }
-              .to change(CsvFile, :count).by(1)
+        
+    	context "for all users" do 
+	        it "creates a downloadable csv file for all users" do
+    	        expect{ subject.perform(user.id, "all_users") }
+        	      .to change(CsvFile, :count).by(1)
+        	end
+
+        	it "file name is all_users.csv" do 
+        		subject.perform(user.id, "all_users")
+        		expect(CsvFile.last.download_file_file_name).to eq "all_users.csv"
+        	end
         end
 
-        it "creates a downloadable csv file for only active users" do
-            enterprise = create(:enterprise)
-            user = create(:user, :enterprise => enterprise)
+        context "for all active users" do
+	        it "creates a downloadable csv file for only active users" do
+    	        expect{ subject.perform(user.id, "active_users") }
+        	      .to change(CsvFile, :count).by(1)
+        	end
 
-            expect{ subject.perform(user.id, "active_users") }
-              .to change(CsvFile, :count).by(1)
+        	it "file name is active_users.csv" do 
+        		subject.perform(user.id, "active_users")
+        		expect(CsvFile.last.download_file_file_name).to eq "active_users.csv"
+        	end
         end
 
-        it "creates a downloadable csv file for inactive users" do
-            enterprise = create(:enterprise)
-            user = create(:user, :enterprise => enterprise)
+        context "for all inactive users" do 
+        	it "creates a downloadable csv file for inactive users" do
+            	expect{ subject.perform(user.id, "inactive_users") }
+              	.to change(CsvFile, :count).by(1)
+        	end
 
-            expect{ subject.perform(user.id, "inactive_users") }
-              .to change(CsvFile, :count).by(1)
+        	it "file name is inactive_users.csv" do 
+        		subject.perform(user.id, "inactive_users")
+        		expect(CsvFile.last.download_file_file_name).to eq "inactive_users.csv"
+        	end
         end
 
-        it "creates a downloadable csv file for group leaders" do
-            enterprise = create(:enterprise)
-            user = create(:user, :enterprise => enterprise)
-            
-            expect{ subject.perform(user.id,  "group_leaders") }
-              .to change(CsvFile, :count).by(1)
-        end
+        context "for all group leaders" do 
+	        it "creates a downloadable csv file for group leaders" do
+	            expect{ subject.perform(user.id,  "group_leaders") }
+	              .to change(CsvFile, :count).by(1)
+	        end
+
+	        it "file name is group_leaders.csv" do 
+        		subject.perform(user.id, "group_leaders")
+        		expect(CsvFile.last.download_file_file_name).to eq "group_leaders.csv"
+        	end
+	    end
     end
 end
