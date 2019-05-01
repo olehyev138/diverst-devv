5ccf413fd8560897394768eb429e50c6c1440e1f
Derek Owusu-Frimpong <deepsky_5@live.com>
2019-05-01 02:40:30 +0000

    ability to export group leaders

--- spec/jobs/users_download_job_spec.rb
+++ spec/jobs/users_download_job_spec.rb
@@ -4,11 +4,35 @@ RSpec.describe UsersDownloadJob, type: :job do
     include ActiveJob::TestHelper
 
     describe "#perform" do
-        it "creates a downloadable csv file" do
+        it "creates a downloadable csv file for all users" do
+            enterprise = create(:enterprise)
+            user = create(:user, :enterprise => enterprise)
+            
+            expect{ subject.perform(user.id, "all_users") }
+              .to change(CsvFile, :count).by(1)
+        end
+
+        it "creates a downloadable csv file for only active users" do
             enterprise = create(:enterprise)
             user = create(:user, :enterprise => enterprise)
 
-            expect{ subject.perform(user.id) }
+            expect{ subject.perform(user.id, "active_users") }
+              .to change(CsvFile, :count).by(1)
+        end
+
+        it "creates a downloadable csv file for inactive users" do
+            enterprise = create(:enterprise)
+            user = create(:user, :enterprise => enterprise)
+
+            expect{ subject.perform(user.id, "inactive_users") }
+              .to change(CsvFile, :count).by(1)
+        end
+
+        it "creates a downloadable csv file for group leaders" do
+            enterprise = create(:enterprise)
+            user = create(:user, :enterprise => enterprise)
+            
+            expect{ subject.perform(user.id,  "group_leaders") }
               .to change(CsvFile, :count).by(1)
         end
     end
