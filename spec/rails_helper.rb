e19bbc01316232a12692373e44dcf8288d638b7c
Matthew Baldwin <matt@diverst.com>
2019-05-02 23:57:31 -0400

    Undo accidental changes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,14 +25,12 @@ SimpleCov.start do
   add_filter "spec"
 end
 
-#@Capybara.javascript_driver = :poltergeist
-## https://stackoverflow.com/questions/25673890/poltergeist-throws-js-errors-when-js-errors-false
-## https://stackoverflow.com/questions/42766660/capybarapoltergeistmouseeventfailed-poltergeist-detected-another-element
-#Capybara.register_driver :poltergeist do |app|
-#  Capybara::Poltergeist::Driver.new(app, {js_errors: false, window_size: [1600, 1200]})
-#end
-#
-Capybara.javascript_driver = :selenium_chrome
+Capybara.javascript_driver = :poltergeist
+# https://stackoverflow.com/questions/25673890/poltergeist-throws-js-errors-when-js-errors-false
+# https://stackoverflow.com/questions/42766660/capybarapoltergeistmouseeventfailed-poltergeist-detected-another-element
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, {js_errors: false, window_size: [1600, 1200]})
+end
 
 Capybara.asset_host = 'http://localhost:3000'
 
