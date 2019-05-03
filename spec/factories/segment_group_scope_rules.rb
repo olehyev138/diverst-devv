fdbdc00a643675acdf87d55bf6e3b6323c17b28a
Matthew Baldwin <matt@diverst.com>
2019-05-03 12:09:30 -0400

    fix segment_group_scope_rules factory

--- spec/factories/segment_group_scope_rules.rb
+++ spec/factories/segment_group_scope_rules.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     operator SegmentGroupScopeRule.operators[SegmentGroupScopeRule.operators.keys.sample]
 
     before(:create) do |segment_group_scope_rule|
-      segment_group_scope_rule.segment_group_scope_rule_groups create_list(:segment_group_scope_rule_groups, rand(1..3), segment_group_scope_rule: segment_group_scope_rule)
+      segment_group_scope_rule.segment_group_scope_rule_groups create_list(:segment_group_scope_rule_group, rand(1..3), segment_group_scope_rule: segment_group_scope_rule)
     end
   end
 end
