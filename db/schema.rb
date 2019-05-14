a87d859ec5c03413546cec5569697563cf3c16eb
AlexOxorn <alex.oxorn@gmail.com>
2019-05-14 11:12:04 -0400

    updates schema file

--- db/schema.rb
+++ db/schema.rb
@@ -1427,6 +1427,7 @@ ActiveRecord::Schema.define(version: 20190509185932) do
     t.integer  "group_id",          limit: 4
     t.integer  "folder_id",         limit: 4
     t.integer  "resource_id",       limit: 4
+    t.integer  "view_count",        limit: 4
   end
 
   create_table "yammer_field_mappings", force: :cascade do |t|
