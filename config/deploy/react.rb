f67c15de522fb4f7f107a0abd1f82521ea48fa71
Matthew Baldwin <matt@diverst.com>
2019-05-30 18:04:02 -0400

    Configure capistrano nvm

--- config/deploy/react.rb
+++ config/deploy/react.rb
@@ -5,9 +5,15 @@ set :rails_env, :development
 
 set :rollbar_env, 'react'
 
+# npm
 set :npm_target_path, -> { release_path.join('client') } # default not set
 set :npm_flags, '' # default
 set :npm_roles, :all                                     # default
 set :npm_env_variables, {}                               # default
+
+# nvm
+set :nvm_node, 'v10.13.0'
+set :nvm_type, :user
+
 set :ci, 'install' # default
 set :bundle_without, %w{test}.join(' ') # this is default
