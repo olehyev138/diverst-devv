cac9de2de2aa5ab677cde8d24c5dffb16dba4022
Matthew Baldwin <matt@diverst.com>
2019-05-30 15:43:41 -0400

    Fix factories

--- spec/factories/csv_file.rb
+++ spec/factories/csv_file.rb
@@ -2,7 +2,8 @@ include ActionDispatch::TestProcess
 
 FactoryBot.define do
   factory :csv_file do
-    import_file { File.new("#{Rails.root}/spec/fixtures/files/diverst_csv_import.csv") }
+    # Paperclip
+    # import_file { File.new("#{Rails.root}/spec/fixtures/files/diverst_csv_import.csv") }
     user
   end
 end
