97557b0c9a1d1fb68be82738b680f2bda14bf067
Matthew Baldwin <matt@diverst.com>
2019-06-11 14:57:38 -0400

    wip - add console error spec to notfoundpage

--- client/app/containers/NotFoundPage/tests/index.test.js
+++ client/app/containers/NotFoundPage/tests/index.test.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { render } from 'react-testing-library';
 import { IntlProvider } from 'react-intl';
 
-import NotFoundPage from '../index';
+import ConnectedNotFoundPage, { NotFoundPage } from '../index';
 
-xdescribe('<NotFoundPage />', () => {
+describe('<NotFoundPage />', () => {
+  it('Expect to not log errors in console', () => {
+    const spy = jest.spyOn(global.console, 'error');
+    const dispatch = jest.fn();
+
+    render(
+      <IntlProvider locale='en'>
+        <NotFoundPage />
+      </IntlProvider>
+    );
+
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+
   xit('should render and match the snapshot', () => {
     const {
       container: { firstChild },
