6b7e1607cd21490ffaa01de234fac47fca7132b7
Girgy <girgalicious@gmail.com>
2019-06-11 05:23:18 +0000

    API - Pillar request spec tests

--- config/routes.rb
+++ config/routes.rb
@@ -82,6 +82,7 @@ Diverst::Application.routes.draw do
       resources :news_link_photos
       resources :news_link_segments
       resources :outcomes
+      resources :pillars
       resources :policy_group_templates
       resources :resources
       resources :rewards
