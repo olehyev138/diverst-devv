7ca57bde6a250771d50e4fb4ee6147d7e3a7bd51
Mark Roy <markr4848@gmail.com>
2019-06-18 17:33:02 -0400

    Fix base pager spec to use a Request object with user so that the Policy Scope can be checked

--- spec/framework/base_pager_spec.rb
+++ spec/framework/base_pager_spec.rb
@@ -1,21 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe BasePager, type: :model do
+  let(:enterprise) { create(:enterprise) }
+  let(:user) { create(:user, enterprise: enterprise) }
+
   describe '#pager' do
     it 'returns the users as items and the total' do
-      create_list(:user, 10)
-      expect(User.index({}, {}).total).to eq(10)
+      create_list(:user, 10, enterprise: enterprise)
+      expect(User.index(Request.create_request(user), {}).total).to eq(11) # One more because of the initial user
     end
 
     it 'returns the groups as items and the total for the correct enterprise' do
-      enterprise_1 = create(:enterprise)
       enterprise_2 = create(:enterprise)
+      user_2 = create(:user, enterprise: enterprise_2)
 
-      create_list(:group, 5,  enterprise_id: enterprise_1.id)
+      create_list(:group, 5,  enterprise_id: enterprise.id)
       create_list(:group, 10, enterprise_id: enterprise_2.id)
 
-      expect(Group.index({}, { enterprise_id: enterprise_1.id }).total).to eq(5)
-      expect(Group.index({}, { enterprise_id: enterprise_2.id }).total).to eq(10)
+      expect(Group.index(Request.create_request(user), { enterprise_id: enterprise.id }).total).to eq(5)
+      expect(Group.index(Request.create_request(user_2), { enterprise_id: enterprise_2.id }).total).to eq(10)
     end
   end
 end
