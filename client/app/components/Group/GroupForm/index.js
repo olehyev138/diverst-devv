1c74148914661c8b3685d2d621f94dad7ca79879
Matthew Baldwin <matt@diverst.com>
2019-06-26 00:52:00 -0400

    Add some basic elements to the group form

--- client/app/components/Group/GroupForm/index.js
+++ client/app/components/Group/GroupForm/index.js
@@ -8,10 +8,44 @@ import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import { compose } from 'redux';
 
-export function GroupForm(props) {
+import {
+  Button, Card, CardActions, CardContent, Grid, TextField, Hidden
+} from '@material-ui/core';
+
+import { Field, Formik, Form } from 'formik';
+
+export function GroupFormInner(props) {
   return (
-    <React.Fragment />
+    <Card>
+      <CardContent>
+        <Field
+          component={TextField}
+          fullWidth
+          label={'Name'}
+        />
+        <Field
+          component={TextField}
+          fullWidth
+          label={'Summary'}
+        />
+        <Field
+          component={TextField}
+          fullWidth
+          label={'Description'}
+        />
+      </CardContent>
+      <CardActions>
+        <Button type='submit'>Create</Button>
+        <Button type='submit'>Cancel</Button>
+      </CardActions>
+    </Card>
   );
+}
+
+export function GroupForm(props) {
+  return (<Formik
+    render={props => <GroupFormInner {...props} /> }
+  />);
 }
 
 export default compose(
