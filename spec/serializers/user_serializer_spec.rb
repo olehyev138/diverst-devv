ebd0a0dba89e7f898460a5a9a6cbac0ecd3ab2f5
Ubuntu <ubuntu@ip-172-31-52-194.ec2.internal>
2019-07-03 18:38:41 +0000

    API - rubocop fixes

--- spec/serializers/user_serializer_spec.rb
+++ spec/serializers/user_serializer_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe UserSerializer, type: :serializer do
   it 'returns email and last name fields but not password_digest' do
     user = create(:user)
-    create_list(:user_group, 3, :user_id => user.id)
+    create_list(:user_group, 3, user_id: user.id)
     serializer = UserSerializer.new(user)
 
     expect(serializer.serializable_hash[:email]).to eq user.email
