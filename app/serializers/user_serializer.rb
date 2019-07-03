3e5d1f9f98cd93be776541bc0b67601a21c83fe2
Ubuntu <ubuntu@ip-172-31-52-194.ec2.internal>
2019-07-03 18:31:03 +0000

    API - add user_groups to UserSerializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,7 @@
 class UserSerializer < ApplicationRecordSerializer
   attributes :enterprise, :last_name, :fields
+  
+  has_many :user_groups
 
   # Serialize all user fields, including the custom attributes listed above, and excluding the `excluded_keys`
   def serialize_all_fields
