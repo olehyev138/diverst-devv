8cc0954134c3c3ecea68fbd4b385b537b575118f
Derek Owusu-Frimpong <deepsky_5@live.com>
2019-07-04 19:30:25 +0000

    add answers_count to questions as counter cache

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190704160205) do
+ActiveRecord::Schema.define(version: 20190704191720) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id",   limit: 4
@@ -1099,13 +1099,14 @@ ActiveRecord::Schema.define(version: 20190704160205) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string   "title",       limit: 191
-    t.text     "description", limit: 65535
-    t.integer  "campaign_id", limit: 4
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.string   "title",         limit: 191
+    t.text     "description",   limit: 65535
+    t.integer  "campaign_id",   limit: 4
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.datetime "solved_at"
-    t.text     "conclusion",  limit: 65535
+    t.text     "conclusion",    limit: 65535
+    t.integer  "answers_count", limit: 4
   end
 
   create_table "resources", force: :cascade do |t|
