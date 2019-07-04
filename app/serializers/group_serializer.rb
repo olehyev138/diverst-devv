3a03648dff0bf8b9dfb267f33a828c48a0eeadb4
Matthew Baldwin <matt@diverst.com>
2019-07-04 17:43:22 -0400

    Change groups controller #index to return only parents

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,6 +1,5 @@
 class GroupSerializer < ApplicationRecordSerializer
   attributes :id, :name, :short_description, :description
 
-  belongs_to :parent
-  # has_many :children
+  has_many :children
 end
