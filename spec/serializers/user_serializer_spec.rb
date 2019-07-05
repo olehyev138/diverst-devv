ab0c40b2bed0b5e57cee14660d0503002443c358
Ubuntu <ubuntu@ip-172-31-52-194.ec2.internal>
2019-07-05 12:59:02 +0000

    API - rubocop fixes

--- spec/serializers/user_serializer_spec.rb
+++ spec/serializers/user_serializer_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe UserSerializer, type: :serializer do
     user = create(:user)
     create_list(:user_group, 3, user_id: user.id)
     serializer = UserSerializer.new(user)
-    
+
     expect(serializer.serializable_hash[:email]).to eq user.email
     expect(serializer.serializable_hash[:last_name]).to_not eq nil
     expect(serializer.serializable_hash[:user_groups].empty?).to_not be true
