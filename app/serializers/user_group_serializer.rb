6c52d1e000f5b940f05064ef6c916c0907bf9211
Ubuntu <ubuntu@ip-172-31-56-211.ec2.internal>
2019-07-08 11:34:32 +0000

    API - update user group serializer to manually serialize user and group objects

--- app/serializers/user_group_serializer.rb
+++ app/serializers/user_group_serializer.rb
@@ -1,3 +1,11 @@
 class UserGroupSerializer < ApplicationRecordSerializer
   attributes :user, :group
+  
+  def user
+    UserSerializer.new(object.user).attributes
+  end
+  
+  def group
+    GroupSerializer.new(object.group).attributes
+  end
 end
