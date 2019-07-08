fc85d41e575b092530e9ef3dd886813c572b7a9a
Ubuntu <ubuntu@ip-172-31-56-211.ec2.internal>
2019-07-08 11:36:00 +0000

    API - rubocop fixes for user_group_serializer

--- app/serializers/user_group_serializer.rb
+++ app/serializers/user_group_serializer.rb
@@ -1,10 +1,10 @@
 class UserGroupSerializer < ApplicationRecordSerializer
   attributes :user, :group
-  
+
   def user
     UserSerializer.new(object.user).attributes
   end
-  
+
   def group
     GroupSerializer.new(object.group).attributes
   end
