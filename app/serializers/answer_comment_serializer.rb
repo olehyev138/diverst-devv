5e4421c757829ab87adea5167fd80459756b6daf
Ubuntu <ubuntu@ip-172-31-56-211.ec2.internal>
2019-07-10 14:02:17 +0000

    API - rubocop fixes

--- app/serializers/answer_comment_serializer.rb
+++ app/serializers/answer_comment_serializer.rb
@@ -1,6 +1,6 @@
 class AnswerCommentSerializer < ApplicationRecordSerializer
   attributes :author, :answer
-    
+
   def serialize_all_fields
     true
   end
--- spec/serializers/answer_comments_serializer_spec.rb
+++ spec/serializers/answer_comments_serializer_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe AnswerCommentSerializer, type: :serializer do
   it 'returns all fields and associations' do
     answer_comment = create(:answer_comment)
     serializer = AnswerCommentSerializer.new(answer_comment)
-    
+
     expect(serializer.serializable_hash[:id]).to_not be nil
     expect(serializer.serializable_hash[:author]).to_not be nil
     expect(serializer.serializable_hash[:answer]).to_not be nil
