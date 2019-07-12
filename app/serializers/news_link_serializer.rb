7c89afb407171763882ed2234ae98bd5d290781c
Ubuntu <ubuntu@ip-172-31-52-194.ec2.internal>
2019-07-12 03:59:48 +0000

    API - update news_link_serializer

--- app/serializers/news_link_serializer.rb
+++ app/serializers/news_link_serializer.rb
@@ -1,6 +1,7 @@
 class NewsLinkSerializer < ApplicationRecordSerializer
-  has_one   :group
-  has_one   :author
-  has_one   :news_feed_link
-  has_many  :photos
+  attributes :group, :author, :photos
+  
+  def serialize_all_fields
+    true
+  end
 end
