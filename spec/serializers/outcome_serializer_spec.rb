25a3eabdafb25f404ad84df5659df32224fce9bb
Girgalicious <girgalicious@gmail.com>
2019-07-16 18:24:03 +0000

    API - OutcomeSerializer creation and spec test

---
+++ spec/serializers/outcome_serializer_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe OutcomeSerializer, type: :serializer do
+  it 'returns associations' do
+    group = create(:group)
+    outcome = create(:outcome, group: group)
+
+    serializer = OutcomeSerializer.new(outcome)
+
+    expect(serializer.serializable_hash[:id]).to eq(outcome.id)
+    expect(serializer.serializable_hash[:group]).to_not be nil
+  end
+end
