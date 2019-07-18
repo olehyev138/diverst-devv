92411c1ddf4579b63971b87fbc735884099a1171
Matthew Baldwin <matt@diverst.com>
2019-07-18 15:49:55 -0400

    Serialize group message comments

--- app/serializers/news_feed_link_serializer.rb
+++ app/serializers/news_feed_link_serializer.rb
@@ -1,3 +1,5 @@
 class NewsFeedLinkSerializer < ApplicationRecordSerializer
-  attributes :id, :news_feed_id, :group_message, :news_link, :social_link, :total_views, :total_likes
+  attributes :id, :news_feed_id, :news_link, :social_link, :total_views, :total_likes
+
+  belongs_to :group_message
 end
