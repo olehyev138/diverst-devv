66ad91dc13efc614e8ff75cb7503fa4f3eccc399
AlexOxorn <alex.oxorn@gmail.com>
2019-07-22 09:33:40 -0400

    add seen_onboarding flag to user factory

--- spec/controllers/saml_controller_spec.rb
+++ spec/controllers/saml_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe SamlController, type: :controller do
   let(:enterprise) { create(:enterprise, idp_sso_target_url: 'http://example.com') }
-  let(:user) { create(:user, enterprise: enterprise, seen_onboarding: true) }
+  let(:user) { create(:user, enterprise: enterprise) }
 
   describe 'GET#index' do
     before { get :index, enterprise_id: enterprise.id }
