bf558f3eee70be42d3354ffd1fe9b47e16f2a4c4
Matthew Baldwin <matt@diverst.com>
2019-07-24 13:16:21 -0400

    Start on group member list

---
+++ client/app/containers/User/UserLists/GroupMemberList/index.js
@@ -0,0 +1,55 @@
+import React, {
+  memo, useEffect, useContext
+} from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect/lib';
+import { compose } from 'redux';
+
+import { useInjectSaga } from 'utils/injectSaga';
+import { useInjectReducer } from 'utils/injectReducer';
+import reducer from 'containers/User/UserLists/reducer';
+import saga from 'containers/User/UserLists/saga';
+
+import { getUsersBegin, userListUnmount } from 'containers/User/UserLists/actions';
+import { selectPaginatedUsers } from 'containers/User/UserLists/selectors';
+import RouteService from 'utils/routeHelpers';
+
+export function GroupMemberList(props) {
+  useInjectReducer({ key: 'userList', reducer });
+  useInjectSaga({ key: 'userList', saga });
+
+  const rs = new RouteService(useContext);
+
+  useEffect(() => {
+    return () => {
+      props.userListUnmount();
+    };
+  }, []);
+
+  return (
+    <React.Fragment>
+    </React.Fragment>
+  );
+}
+
+GroupMemberList.propTypes = {
+  userListUnmount: PropTypes.func,
+};
+
+const mapStateToProps = createStructuredSelector({
+});
+
+const mapDispatchToProps = {
+  userListUnmount
+};
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+export default compose(
+  withConnect,
+  memo,
+)(GroupMemberList);
