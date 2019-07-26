f938546e4a1729ba04885757e8a36188f3fc4287
Girgalicious <girgalicious@gmail.com>
2019-07-26 07:57:57 +0000

    API - add strong params for social links

--- app/controllers/api/v1/social_links_controller.rb
+++ app/controllers/api/v1/social_links_controller.rb
@@ -2,4 +2,15 @@ class Api::V1::SocialLinksController < DiverstController
   def klass
     'SocialLink'.classify.constantize
   end
+  
+  def payload
+    params
+    .require(:social_link)
+    .permit(
+      :url,
+      :author_id,
+      :group_id,
+      news_feed_link_attributes: [:id, :approved, :news_feed_id, :link, shared_news_feed_ids: [], segment_ids: []],
+    )
+  end
 end
