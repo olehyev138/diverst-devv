8f99a0ef1551cf2d39037115fa87b3a660c6b5ef
Girgalicious <girgalicious@gmail.com>
2019-07-27 22:29:38 +0000

    API - poll response serializer updates

--- app/serializers/poll_response_serializer.rb
+++ app/serializers/poll_response_serializer.rb
@@ -1,2 +1,21 @@
 class PollResponseSerializer < ApplicationRecordSerializer
+  attributes :poll, :user, :fields
+  
+  def fields
+    fields = object.poll.fields
+    fields_hash = []
+
+    fields.each do |field|
+      fields_hash << {
+        title: field.title,
+        value: field.string_value(object.info[field])
+      }
+    end
+
+    fields_hash
+  end
+
+  def serialize_all_fields
+    true
+  end
 end
