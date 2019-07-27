a80ee8f90c1697b14b63191fb281954a6ba103c9
Derek Owusu-Frimpong <deepsky_5@live.com>
2019-07-27 20:10:05 +0000

    group_message_spec.rb

--- spec/models/group_message_spec.rb
+++ spec/models/group_message_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe GroupMessage, type: :model do
-  describe 'validations' do
+  describe 'test associations and validations' do
     let(:group_message) { build_stubbed(:group_message) }
 
     [:group_id, :subject, :content, :owner_id].each do |attribute|
       it { expect(group_message).to validate_presence_of(attribute) }
     end
+
+    it { expect(group_message).to validate_length_of(:content).is_at_most(65535) }
+    it { expect(group_message).to validate_length_of(:subject).is_at_most(191) }
 
     it { expect(group_message).to belong_to(:group) }
     it { expect(group_message).to belong_to(:owner).class_name('User') }
@@ -14,17 +17,49 @@ RSpec.describe GroupMessage, type: :model do
     it { expect(group_message).to have_many(:segments).through(:group_messages_segments) }
 
     it { expect(group_message).to have_many(:comments).class_name('GroupMessageComment').with_foreign_key(:message_id) }
-    it { expect(group_message).to have_many(:group_messages_segments) }
+    it { expect(group_message).to have_many(:group_messages_segments).dependent(:destroy) }
     it { expect(group_message).to have_many(:segments).through(:group_messages_segments) }
     it { expect(group_message).to have_one(:news_feed_link) }
+
+    it { expect(group_message).to delegate_method(:increment_view).to(:news_feed_link) }
+    it { expect(group_message).to delegate_method(:total_views).to(:news_feed_link) }
+    it { expect(group_message).to delegate_method(:unique_views).to(:news_feed_link) }
+
+    it { expect(group_message).to accept_nested_attributes_for(:news_feed_link).allow_destroy(true) }
   end
 
-  describe 'test before_create callback' do
-    let!(:group_message) { build(:group_message) }
+  describe 'test callbacks' do
+    context 'test before_create callback' do
+      let!(:group_message) { build(:group_message) }
 
-    it 'run build_default_link before create' do
-      expect(group_message).to receive(:build_default_link)
-      group_message.save
+      it 'run build_default_link before create' do
+        expect(group_message).to receive(:build_default_link)
+        group_message.save
+      end
+    end
+
+    context 'test after_create callback' do
+      let!(:group_message) { build(:group_message) }
+
+      it 'run approve_link after create' do
+        group_message.save
+        expect(group_message).to receive(:approve_link)
+        group_message.run_callbacks(:create)
+      end
+    end
+
+    context '#destroy_callbacks' do
+      it 'removes the child objects' do
+        group_message = create(:group_message)
+        group_messages_segment = create(:group_messages_segment, group_message: group_message)
+        group_message_comment = create(:group_message_comment, message: group_message)
+
+        group_message.destroy!
+
+        expect { GroupMessage.find(group_message.id) }.to raise_error(ActiveRecord::RecordNotFound)
+        expect { GroupMessagesSegment.find(group_messages_segment.id) }.to raise_error(ActiveRecord::RecordNotFound)
+        expect { GroupMessageComment.find(group_message_comment.id) }.to raise_error(ActiveRecord::RecordNotFound)
+      end
     end
   end
 
@@ -55,6 +90,37 @@ RSpec.describe GroupMessage, type: :model do
     it 'returns correct name' do
       expect(subject).to include(user.first_name)
       expect(subject).to include(user.last_name)
+    end
+  end
+
+  describe '#comments_count' do
+    let!(:group_message) { create(:group_message) }
+    let!(:approved_comments) { create_list(:group_message_comment, 2, message_id: group_message.id, approved: true) }
+    let!(:comments) { create_list(:group_message_comment, 2, message_id: group_message.id) }
+
+    it 'when enterprise has pending comments enabled' do
+      group_message.group.enterprise.update enable_pending_comments: true
+      expect(group_message.comments_count).to eq 2
+    end
+
+    it 'when enterprise has pending comments disabled' do
+      expect(group_message.comments_count).to eq 4
+    end
+  end
+
+  describe '#users' do
+    let!(:group_message) { create(:group_message, group: create(:group)) }
+    let!(:group) { group_message.group }
+    let!(:user) { create(:user, enterprise: group.enterprise) }
+    before { create(:user_group, user: user, group: group) }
+
+    it 'when segments.empty? is true' do
+      expect(group_message.users).to eq([user])
+    end
+
+    it 'when segments.empty? false' do
+      group_message.segments << create(:segment, enterprise_id: group.enterprise_id)
+      expect(group_message.users).to eq([])
     end
   end
 
@@ -123,20 +189,6 @@ RSpec.describe GroupMessage, type: :model do
         }
         expect(object.as_indexed_json).to eq(hash)
       end
-    end
-  end
-
-  describe '#destroy_callbacks' do
-    it 'removes the child objects' do
-      group_message = create(:group_message)
-      group_messages_segment = create(:group_messages_segment, group_message: group_message)
-      group_message_comment = create(:group_message_comment, message: group_message)
-
-      group_message.destroy!
-
-      expect { GroupMessage.find(group_message.id) }.to raise_error(ActiveRecord::RecordNotFound)
-      expect { GroupMessagesSegment.find(group_messages_segment.id) }.to raise_error(ActiveRecord::RecordNotFound)
-      expect { GroupMessageComment.find(group_message_comment.id) }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 end
