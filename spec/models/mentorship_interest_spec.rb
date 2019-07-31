eac99e957cf18c61f0a1916d0837d11ca4aef8b4
Derek Owusu-Frimpong <deepsky_5@live.com>
2019-07-31 23:38:46 +0000

    news_feed_link_spec.rb; fix failing test in mentorship_interest_spec.rb

--- spec/models/mentorship_interest_spec.rb
+++ spec/models/mentorship_interest_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe MentorshipInterest, type: :model do
-  let(:mentorship_interest) { build(:mentorship_interest) }
-
   describe 'validations' do
+    let(:mentorship_interest) { build(:mentorship_interest) }
+
     it { expect(mentorship_interest).to validate_presence_of(:user) }
     it { expect(mentorship_interest).to validate_presence_of(:mentoring_interest) }
 
@@ -12,14 +12,20 @@ RSpec.describe MentorshipInterest, type: :model do
   end
 
   describe '#as_indexed_json' do
+    let(:enterprise) { create(:enterprise) }
+    let(:user) { create(:user, enterprise: enterprise) }
+    let(:mentoring_interest) { create(:mentoring_interest, enterprise_id: enterprise.id) }
+    let!(:mentorship_interest) { create(:mentorship_interest, user_id: user.id, mentoring_interest_id: mentoring_interest.id) }
+
     it 'returns json' do
       hash = { 'id' => mentorship_interest.id,
                'user_id' => mentorship_interest.user_id,
                'mentoring_interest_id' => mentorship_interest.mentoring_interest.id,
-               'created_at' => nil,
-               'updated_at' => nil,
-               'user' => { 'enterprise_id' => mentorship_interest.user_id },
+               'created_at' => mentorship_interest.created_at,
+               'updated_at' => mentorship_interest.updated_at,
+               'user' => { 'enterprise_id' => mentorship_interest.mentoring_interest.enterprise_id },
                'mentoring_interest' => { 'name' => mentorship_interest.mentoring_interest.name } }
+
       expect(mentorship_interest.as_indexed_json).to eq(hash)
     end
   end
