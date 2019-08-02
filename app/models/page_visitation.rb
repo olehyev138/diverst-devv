bda3e3b7feac792ff3190e66b9d384e2d4a950a9
AlexOxorn <alex.oxorn@gmail.com>
2019-08-02 17:10:33 -0400

    add string validations

--- app/models/page_visitation.rb
+++ app/models/page_visitation.rb
@@ -1,4 +1,8 @@
 class PageVisitation < ActiveRecord::Base
+  validates_length_of :action, maximum: 191
+  validates_length_of :controller, maximum: 191
+  validates_length_of :page_site, maximum: 191
+  validates_length_of :page_name, maximum: 191
   validates :page_name, uniqueness: { scope: [:page_url, :user_id] }
   belongs_to :user
 end
