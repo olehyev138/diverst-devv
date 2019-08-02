25aa2b4e120dec6bb6e5257c864546d6468e2a2f
Derek Owusu-Frimpong <deepsky_5@live.com>
2019-08-02 12:08:21 +0000

    news_link_spec.rb

--- spec/models/news_link_spec.rb
+++ spec/models/news_link_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe NewsLink, type: :model do
-  describe 'validations' do
+  describe 'test associations and validations' do
     let(:news_link) { build_stubbed(:news_link) }
 
     it { expect(news_link).to validate_presence_of(:group_id) }
@@ -13,23 +13,35 @@ RSpec.describe NewsLink, type: :model do
     it { expect(news_link).to belong_to(:author).class_name('User') }
 
     it { expect(news_link).to have_many(:segments).through(:news_link_segments) }
-
     it { expect(news_link).to have_many(:comments).class_name('NewsLinkComment').dependent(:destroy) }
     it { expect(news_link).to have_many(:photos).class_name('NewsLinkPhoto').dependent(:destroy) }
     it { expect(news_link).to have_many(:news_link_segments).dependent(:destroy) }
     it { expect(news_link).to have_many(:news_link_photos).dependent(:destroy) }
+    it { expect(news_link).to have_many(:user_reward_actions) }
+
     it { expect(news_link).to have_one(:news_feed_link) }
+
     it { expect(news_link).to accept_nested_attributes_for(:photos).allow_destroy(true) }
+
     it { expect(news_link).to have_attached_file(:picture) }
     it { expect(news_link).to validate_attachment_content_type(:picture)
         .allowing('image/png', 'image/jpeg', 'image/jpg')
         .rejecting('text/xml', 'text/plain')
     }
 
+    it { expect(news_link).to delegate_method(:increment_view).to(:news_feed_link) }
+    it { expect(news_link).to delegate_method(:total_views).to(:news_feed_link) }
+    it { expect(news_link).to delegate_method(:unique_views).to(:news_feed_link) }
+
     it 'validates url length' do
       expect(build(:news_link, url: 'www.goodurl.com')).to be_valid
       expect(build(:news_link, url: 'badurl' * 300)).to_not be_valid
     end
+
+    it { expect(news_link).to validate_length_of(:picture_content_type).is_at_most(191) }
+    it { expect(news_link).to validate_length_of(:picture_file_name).is_at_most(191) }
+    it { expect(news_link).to validate_length_of(:description).is_at_most(65535) }
+    it { expect(news_link).to validate_length_of(:title).is_at_most(191) }
   end
 
   describe 'test callbacks' do
@@ -41,20 +53,38 @@ RSpec.describe NewsLink, type: :model do
     end
   end
 
-  describe '.of_segments' do
-    let(:author) { create(:user) }
+  describe 'test scopes' do
+    describe '.of_segments' do
+      let(:author) { create(:user) }
 
-    let(:segment1) { build :segment, enterprise: author.enterprise }
-    let(:segment2) { build :segment, enterprise: author.enterprise }
+      let(:segment1) { build :segment, enterprise: author.enterprise }
+      let(:segment2) { build :segment, enterprise: author.enterprise }
 
-    let!(:news_link_without_segment) { create(:news_link, author_id: author.id, segments: []) }
-    let!(:news_link_with_segment) { create(:news_link, author_id: author.id, segments: [segment1]) }
-    let!(:news_link_with_another_segment) {
-      create(:news_link, author_id: author.id, segments: [segment2])
-    }
+      let!(:news_link_without_segment) { create(:news_link, author_id: author.id, segments: []) }
+      let!(:news_link_with_segment) { create(:news_link, author_id: author.id, segments: [segment1]) }
+      let!(:news_link_with_another_segment) {
+        create(:news_link, author_id: author.id, segments: [segment2])
+      }
 
-    it 'returns initiatives that has specific segments or does not have any segment' do
-      expect(NewsLink.of_segments([segment1.id])).to match_array([news_link_without_segment, news_link_with_segment])
+      it 'returns initiatives that has specific segments or does not have any segment' do
+        expect(described_class.of_segments([segment1.id])).to match_array([news_link_without_segment, news_link_with_segment])
+      end
+    end
+
+    describe '.unapproved' do
+      it 'returns unapproved news_links' do
+        news_link = create(:news_link)
+        news_link.news_feed_link.update(approved: false)
+
+        expect(described_class.unapproved).to eq([news_link])
+      end
+    end
+
+    describe '.approved' do
+      it 'returns approved news_links' do
+        news_link = create(:news_link)
+        expect(described_class.approved).to eq([news_link])
+      end
     end
   end
 
@@ -84,6 +114,26 @@ RSpec.describe NewsLink, type: :model do
       news_link.remove_segment_association(segment)
 
       expect(news_link_segment.news_feed_link_segment).to_not be(nil)
+    end
+  end
+
+  describe '.comments_count' do
+    let!(:news_link) { create(:news_link) }
+    let!(:approved_comments) { create_list(:news_link_comment, 1, news_link_id: news_link.id, approved: true) }
+    let!(:unapproved_comments) { create_list(:news_link_comment,  2, news_link_id: news_link.id, approved: false) }
+
+    context 'when enable_pending_comments is set to true' do
+      before { news_link.group.enterprise.update(enable_pending_comments: true) }
+
+      it 'returns approved comments count' do
+        expect(news_link.comments_count).to eq(1)
+      end
+    end
+
+    context 'when enable_pending_comments is set to false' do
+      it 'returns comments count' do
+        expect(news_link.comments_count).to eq(3)
+      end
     end
   end
 
