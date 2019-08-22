9d56dc99115f7c3269b0f4796b3dcabb44c05c64
Derek Owusu-Frimpong <deepsky_5@live.com>
2019-08-22 21:13:43 +0000

    segment_group_scope_rule_group_spec.rb

--- spec/models/segment_group_scope_rule_group_spec.rb
+++ spec/models/segment_group_scope_rule_group_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe SegmentGroupScopeRuleGroup, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:segment_group_scope_rule) }
+  it { should belong_to(:group) }
 end
