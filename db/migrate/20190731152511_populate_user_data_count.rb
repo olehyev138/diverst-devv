7140050b58198175af3c502dcdf17ca568ead578
AlexOxorn <alex.oxorn@gmail.com>
2019-08-22 09:58:47 -0400

    Find Users In Batch 2

--- db/migrate/20190731152511_populate_user_data_count.rb
+++ db/migrate/20190731152511_populate_user_data_count.rb
@@ -1,17 +1,15 @@
 class PopulateUserDataCount < ActiveRecord::Migration
   def up
-    User.find_in_batches do |users|
-      users.pluck(:id) do |user_id|
-        User.reset_counters(user_id,
-                            :initiatives,
-                            :own_messages,
-                            :social_links,
-                            :own_news_links,
-                            :answer_comments,
-                            :message_comments,
-                            :news_link_comments
-        )
-      end
+    User.find_each do |user|
+      User.reset_counters(user.id,
+                          :initiatives,
+                          :own_messages,
+                          :social_links,
+                          :own_news_links,
+                          :answer_comments,
+                          :message_comments,
+                          :news_link_comments
+      )
     end
   end
 end
