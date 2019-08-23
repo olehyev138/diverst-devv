6e3531242b2ca4d7b9795bd4f83aff44b975366d
Girgalicious <girgalicious@gmail.com>
2019-08-23 15:14:02 +0000

    API - update group updates controller

--- app/controllers/api/v1/group_updates_controller.rb
+++ app/controllers/api/v1/group_updates_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::GroupUpdatesController < DiverstController
+  def payload
+    params.require(klass.symbol).permit(
+      klass.attribute_names - ['id', 'updated_at', 'enterprise_id']
+    )
+  end
 end
