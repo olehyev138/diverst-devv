fc7552cd92ddc13a379d8e636a1a4a75a4edd7f7
Mark Roy <markr4848@gmail.com>
2019-08-23 04:44:39 -0400

    Improve/fix the user group list

--- client/app/components/Group/UserGroupList/index.js
+++ client/app/components/Group/UserGroupList/index.js
@@ -8,7 +8,6 @@ import React, { memo, useState } from 'react';
 import PropTypes from 'prop-types';
 import { compose } from 'redux';
 
-import { NavLink } from 'react-router-dom';
 import { ROUTES } from 'containers/Shared/Routes/constants';
 
 import {
@@ -17,10 +16,10 @@ import {
 } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 
-import WrappedNavLink from 'components/Shared/WrappedNavLink';
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
 
-import { FormattedMessage } from 'react-intl';
-import messages from 'containers/Group/messages';
+import WrappedNavLink from 'components/Shared/WrappedNavLink';
 
 const styles = theme => ({
   groupListItemDescription: {
@@ -37,6 +36,15 @@ const styles = theme => ({
     borderTopLeftRadius: 4,
     borderBottomLeftRadius: 4,
   },
+  groupCardContent: {
+    paddingBottom: 28,
+  },
+  groupCardDescription: {
+    paddingTop: 8,
+  },
+  groupCardLink: {
+    textDecoration: 'none !important',
+  },
   childGroupCard: {
     marginLeft: 24,
     borderLeftWidth: 2,
@@ -45,9 +53,17 @@ const styles = theme => ({
     borderTopLeftRadius: 4,
     borderBottomLeftRadius: 4,
   },
-  expandButton: {
-    fontWeight: 600,
-    fontSize: 24,
+  expandActionAreaContainer: {
+    borderLeftWidth: 1,
+    borderLeftStyle: 'solid',
+    borderLeftColor: theme.custom.colors.lightGrey,
+  },
+  expandActionArea: {
+    padding: '4px 12px',
+    height: '100%',
+  },
+  expandIcon: {
+    fontSize: 34,
   },
 });
 
@@ -71,8 +87,8 @@ export function UserGroupList(props, context) {
   if (props.groups && Object.keys(props.groups).length !== 0 && Object.keys(expandedGroups).length <= 0) {
     const initialExpandedGroups = {};
 
-    /* eslint-disable-next-line no-return-assign */
     /* Setup initial hash, with each group set to false - do it like this because of how React works with state */
+    /* eslint-disable-next-line no-return-assign */
     Object.keys(props.groups).map((id, i) => initialExpandedGroups[id] = false);
     setExpandedGroups(initialExpandedGroups);
   }
@@ -81,91 +97,91 @@ export function UserGroupList(props, context) {
     <React.Fragment>
       <Grid container spacing={3}>
         { /* eslint-disable-next-line arrow-body-style */ }
-        {props.groups && Object.values(props.groups).map((group, i) => {
-          return (
-            <Grid item xs={12} key={group.id}>
-              <Card className={classes.groupCard}>
-                <Grid container>
-                  <Grid item xs>
-                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                    <Link
-                      component={WrappedNavLink}
-                      to={{
-                        pathname: ROUTES.group.home.path(group.id),
-                        state: { id: group.id }
+        {props.groups && Object.values(props.groups).map((group, i) => (
+          <Grid item xs={12} key={group.id}>
+            <Card className={classes.groupCard}>
+              <Grid container>
+                <Grid item xs>
+                  {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                  <Link
+                    component={WrappedNavLink}
+                    to={{
+                      pathname: ROUTES.group.home.path(group.id),
+                      state: { id: group.id }
+                    }}
+                    className={classes.groupCardLink}
+                  >
+                    <CardActionArea>
+                      <CardContent className={classes.groupCardContent}>
+                        <Typography variant='h5' component='h2' display='inline'>
+                          {group.name}
+                        </Typography>
+                        {group.description && (
+                          <Typography color='textSecondary' className={classes.groupCardDescription}>
+                            {group.description}
+                          </Typography>
+                        )}
+                      </CardContent>
+                    </CardActionArea>
+                  </Link>
+                </Grid>
+                {group.children && group.children.length > 0 && (
+                  <Grid item className={classes.expandActionAreaContainer}>
+                    <CardActionArea
+                      className={classes.expandActionArea}
+                      onClick={() => {
+                        setExpandedGroups({
+                          ...expandedGroups,
+                          [group.id]: !expandedGroups[group.id]
+                        });
                       }}
                     >
-                      <CardActionArea>
-                        <CardContent>
-                          <Typography variant='h5' component='h2' display='inline'>
-                            {group.name}
-                          </Typography>
-                          {group.description && (
-                            <Typography color='textSecondary' className={classes.groupListItemDescription}>
-                              {group.description}
-                            </Typography>
-                          )}
-                        </CardContent>
-                      </CardActionArea>
-                    </Link>
+                      {expandedGroups[group.id] ? (
+                        <RemoveIcon color='primary' className={classes.expandIcon} />
+                      ) : (
+                        <AddIcon color='primary' className={classes.expandIcon} />
+                      )}
+                    </CardActionArea>
                   </Grid>
-                  <Grid item>
-                    <CardContent>
-                      <Button
-                        className={classes.expandButton}
-                        variant='outlined'
-                        color='primary'
-                        onClick={() => {
-                          setExpandedGroups({
-                            ...expandedGroups,
-                            [group.id]: !expandedGroups[group.id]
-                          });
+                )}
+              </Grid>
+            </Card>
+            <Collapse in={expandedGroups[`${group.id}`]}>
+              <Box mt={1} />
+              <Grid container spacing={2} justify='flex-end'>
+                {group.children && group.children.map((childGroup, i) => (
+                  /* eslint-disable-next-line react/jsx-wrap-multilines */
+                  <Grid item key={childGroup.id} xs={12}>
+                    <Card className={classes.childGroupCard}>
+                      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                      <Link
+                        component={WrappedNavLink}
+                        to={{
+                          pathname: ROUTES.group.home.path(childGroup.id),
+                          state: { id: childGroup.id }
                         }}
+                        className={classes.groupCardLink}
                       >
-                        {expandedGroups[group.id] ? (
-                          <FormattedMessage {...messages.children_collapse} />
-                        ) : (
-                          <FormattedMessage {...messages.children_expand} />
-                        )}
-                      </Button>
-                    </CardContent>
-                  </Grid>
-                </Grid>
-              </Card>
-              <Collapse in={expandedGroups[`${group.id}`]}>
-                <Box mt={1} />
-                <Grid container spacing={2} justify='flex-end'>
-                  {group.children && group.children.map((childGroup, i) => (
-                    /* eslint-disable-next-line react/jsx-wrap-multilines */
-                    <Grid item key={childGroup.id} xs={12}>
-                      <Card className={classes.childGroupCard}>
-                        <CardContent>
-                          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                          <Link
-                            component={WrappedNavLink}
-                            to={{
-                              pathname: ROUTES.group.home.path(childGroup.id),
-                              state: { id: childGroup.id }
-                            }}
-                          >
+                        <CardActionArea>
+                          <CardContent className={classes.groupCardContent}>
                             <Typography variant='h5' component='h2' display='inline'>
                               {childGroup.name}
                             </Typography>
-                          </Link>
-                          {childGroup.description && (
-                            <Typography color='textSecondary' className={classes.groupListItemDescription}>
-                              {childGroup.description}
-                            </Typography>
-                          )}
-                        </CardContent>
-                      </Card>
-                    </Grid>
-                  ))}
-                </Grid>
-              </Collapse>
-            </Grid>
-          );
-        })}
+                            {childGroup.description && (
+                              <Typography color='textSecondary' className={classes.groupCardDescription}>
+                                {childGroup.description}
+                              </Typography>
+                            )}
+                          </CardContent>
+                        </CardActionArea>
+                      </Link>
+                    </Card>
+                  </Grid>
+                ))}
+              </Grid>
+            </Collapse>
+          </Grid>
+        ))}
       </Grid>
       <TablePagination
         component='div'
