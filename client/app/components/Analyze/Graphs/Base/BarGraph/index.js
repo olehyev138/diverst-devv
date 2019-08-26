3bb39cde0278858c1752234ba0f3f5eabebbf315
Matthew Baldwin <matt@diverst.com>
2019-08-26 19:54:30 -0400

    Remove bargraph animation for now

--- client/app/components/Analyze/Graphs/Base/BarGraph/index.js
+++ client/app/components/Analyze/Graphs/Base/BarGraph/index.js
@@ -28,7 +28,6 @@ export function BarGraph(props) {
         <RangeSelector updateRange={props.updateRange} />
         <FlexibleWidthXYPlot
           yType='ordinal'
-          animation='stiff'
           margin={{ left: 100 }}
           height={500}
           onMouseLeave={() => setValue(undefined)}
