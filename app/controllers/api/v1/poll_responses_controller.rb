9fe834a4fedb8abcb0fd17efd75b1d0e1aa6af9f
Girgalicious <girgalicious@gmail.com>
2019-08-30 18:51:34 +0000

    PollResponses - update API to allow creation of poll responses

--- app/controllers/api/v1/poll_responses_controller.rb
+++ app/controllers/api/v1/poll_responses_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::PollResponsesController < DiverstController
+  def payload
+    params
+    .require('custom-fields')
+
+    params.require(:poll_response).permit(:anonymous, :poll_id)
+  end
 end
--- spec/requests/api/v1/poll_responses_spec.rb
+++ spec/requests/api/v1/poll_responses_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe "#{model.pluralize}", type: :request do
   let(:api_key) { create(:api_key) }
   let(:user) { create(:user, password: 'password', enterprise: enterprise) }
   let(:group) { create(:group, enterprise: enterprise) }
+  let(:poll) { create(:poll, enterprise: enterprise, owner_id: user.id) }
   let!(:item) { create(model.constantize.table_name.singularize.to_sym) }
   let(:route) { model.constantize.table_name }
   let(:jwt) { UserTokenService.create_jwt(user) }
@@ -39,22 +40,49 @@ RSpec.describe "#{model.pluralize}", type: :request do
 
   describe '#create' do
     it 'creates an item' do
-      post "/api/v1/#{route}", params: { "#{route.singularize}" => build(route.singularize.to_sym).attributes }, headers: headers
+      field = poll.fields.first
+      payload = {
+        anonymous: false,
+        poll_id: poll.id
+      }
+      post "/api/v1/#{route}", params: { "#{route.singularize}" => payload, "custom-fields": { "#{field.id}": '1' }, }, headers: headers
       expect(response).to have_http_status(201)
     end
 
     it 'captures the error when BadRequestException' do
       allow(model.constantize).to receive(:build).and_raise(BadRequestException)
-      post "/api/v1/#{route}", params: { "#{route.singularize}" => build(route.singularize.to_sym).attributes }, headers: headers
+      field = poll.fields.first
+      payload = {
+        anonymous: false,
+        poll_id: poll.id
+      }
+      post "/api/v1/#{route}", params: { "#{route.singularize}" => payload, "custom-fields": { "#{field.id}": '1' }, }, headers: headers
       expect(response).to have_http_status(:bad_request)
     end
 
-    include_examples 'InvalidInputException when creating', model
+    it 'captures the error when InvalidInputException' do
+      allow_any_instance_of(model.constantize).to receive(:save).and_return(false)
+      allow_any_instance_of(model.constantize).to receive_message_chain(:errors, :full_messages) { [] }
+      allow_any_instance_of(model.constantize).to receive_message_chain(:errors, :messages) { [[]] }
+      field = poll.fields.first
+      payload = {
+        anonymous: false,
+        poll_id: poll.id
+      }
+      post "/api/v1/#{route}", params: { "#{route.singularize}" => payload, "custom-fields": { "#{field.id}": '1' }, }, headers: headers
+      expect(response).to have_http_status(422)
+    end
   end
 
   describe '#update' do
     it 'updates an item' do
-      patch "/api/v1/#{route}/#{item.id}", params: { "#{route.singularize}" => item.attributes }, headers: headers
+      field = poll.fields.first
+      payload = {
+        anonymous: false,
+        poll_id: poll.id
+      }
+
+      patch "/api/v1/#{route}/#{item.id}", params: { "#{route.singularize}" => payload, "custom-fields": { "#{field.id}": '1' } }, headers: headers
       expect(response).to have_http_status(:ok)
     end
 
@@ -64,7 +92,19 @@ RSpec.describe "#{model.pluralize}", type: :request do
       expect(response).to have_http_status(:bad_request)
     end
 
-    include_examples 'InvalidInputException when updating', model
+    it 'captures the error when InvalidInputException' do
+      allow_any_instance_of(model.constantize).to receive(:save).and_return(false)
+      allow_any_instance_of(model.constantize).to receive_message_chain(:errors, :full_messages) { [] }
+      allow_any_instance_of(model.constantize).to receive_message_chain(:errors, :messages) { [[]] }
+      field = poll.fields.first
+      payload = {
+        anonymous: false,
+        poll_id: poll.id
+      }
+
+      patch "/api/v1/#{route}/#{item.id}", params: { "#{route.singularize}" => payload, "custom-fields": { "#{field.id}": '1' } }, headers: headers
+      expect(response).to have_http_status(422)
+    end
   end
 
   describe '#destroy' do
