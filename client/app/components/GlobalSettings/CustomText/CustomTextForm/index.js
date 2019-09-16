e662b424172f3591874f09d3f033a6f1259ffd0b
AlexOxorn <alex.oxorn@gmail.com>
2019-09-16 15:11:12 -0400

    Temp indexes

---
+++ client/app/components/GlobalSettings/CustomText/CustomTextForm/index.js
@@ -0,0 +1,203 @@
+/**
+ *
+ * Custom Text Form Component
+ *
+ */
+
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import dig from 'object-dig';
+
+import { FormattedMessage } from 'react-intl';
+import { Field, Formik, Form } from 'formik';
+import {
+  Button, Card, CardActions, CardContent, TextField
+} from '@material-ui/core';
+
+import WrappedNavLink from 'components/Shared/WrappedNavLink';
+import messages from 'containers/GlobalSettings/CustomText/messages';
+import { buildValues } from 'utils/formHelpers';
+
+/* eslint-disable object-curly-newline */
+export function CustomTextFormInner({ handleSubmit, handleChange, handleBlur, values, buttonText, setFieldValue, setFieldTouched, ...props }) {
+  return (
+    <Card>
+      <Form>
+        <CardContent>
+          <Field
+            component={TextField}
+            onChange={handleChange}
+            fullWidth
+            id='erg'
+            name='erg'
+            label={<FormattedMessage {...messages.erg} />}
+            value={values.erg}
+          />
+          <Field
+            component={TextField}
+            onChange={handleChange}
+            fullWidth
+            id='program'
+            name='program'
+            label={<FormattedMessage {...messages.program} />}
+            value={values.program}
+          />
+          <Field
+            component={TextField}
+            onChange={handleChange}
+            fullWidth
+            id='structure'
+            name='structure'
+            label={<FormattedMessage {...messages.structure} />}
+            value={values.structure}
+          />
+          <Field
+            component={TextField}
+            onChange={handleChange}
+            fullWidth
+            id='outcome'
+            name='outcome'
+            label={<FormattedMessage {...messages.outcome} />}
+            value={values.outcome}
+          />
+          <Field
+            component={TextField}
+            onChange={handleChange}
+            fullWidth
+            id='badge'
+            name='badge'
+            label={<FormattedMessage {...messages.badge} />}
+            value={values.badge}
+          />
+          <Field
+            component={TextField}
+            onChange={handleChange}
+            fullWidth
+            id='segment'
+            name='segment'
+            label={<FormattedMessage {...messages.segment} />}
+            value={values.segment}
+          />
+          <Field
+            component={TextField}
+            onChange={handleChange}
+            fullWidth
+            id='dci_full_title'
+            name='dci_full_title'
+            label={<FormattedMessage {...messages.dci_full_title} />}
+            value={values.dci_full_title}
+          />
+          <Field
+            component={TextField}
+            onChange={handleChange}
+            fullWidth
+            id='dci_abbreviation'
+            name='dci_abbreviation'
+            label={<FormattedMessage {...messages.dci_abbreviation} />}
+            value={values.dci_abbreviation}
+          />
+          <Field
+            component={TextField}
+            onChange={handleChange}
+            fullWidth
+            id='member_preference'
+            name='member_preference'
+            label={<FormattedMessage {...messages.member_preference} />}
+            value={values.member_preference}
+          />
+          <Field
+            component={TextField}
+            onChange={handleChange}
+            fullWidth
+            id='parent'
+            name='parent'
+            label={<FormattedMessage {...messages.parent} />}
+            value={values.parent}
+          />
+          <Field
+            component={TextField}
+            onChange={handleChange}
+            fullWidth
+            id='sub_erg'
+            name='sub_erg'
+            label={<FormattedMessage {...messages.sub_erg} />}
+            value={values.sub_erg}
+          />
+          <Field
+            component={TextField}
+            onChange={handleChange}
+            fullWidth
+            id='privacy_statement'
+            name='privacy_statement'
+            label={<FormattedMessage {...messages.privacy_statement} />}
+            value={values.privacy_statement}
+          />
+        </CardContent>
+        <CardActions>
+          <Button
+            color='primary'
+            type='submit'
+          >
+            {buttonText}
+          </Button>
+        </CardActions>
+      </Form>
+    </Card>
+  );
+}
+
+export function CustomTextForm(props) {
+  const customText = dig(props, 'customText');
+
+  const initialValues = buildValues(customText, {
+    erg: { default: 'ERG' },
+    program: { default: 'Goal' },
+    structure: { default: 'Structure' },
+    outcome: { default: 'Focus Area' },
+    badge: { default: 'Badge' },
+    segment: { default: 'Segment' },
+    dci_full_title: { default: 'Engagement' },
+    dci_abbreviation: { default: 'Engagement' },
+    member_preference: { default: 'Member Survey' },
+    parent: { default: 'Parent' },
+    sub_erg: { default: 'Sub-Group' },
+    privacy_statement: { default: 'Privacy Statement' }
+  });
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      enableReinitialize
+      onSubmit={(values, actions) => {
+        props.customTextAction(values);
+      }}
+
+      render={formikProps => <CustomTextFormInner {...props} {...formikProps} />}
+    />
+  );
+}
+
+CustomTextForm.propTypes = {
+  customTextAction: PropTypes.func,
+  customText: PropTypes.object,
+  currentUser: PropTypes.object,
+  currentGroup: PropTypes.object
+};
+
+CustomTextFormInner.propTypes = {
+  handleSubmit: PropTypes.func,
+  handleChange: PropTypes.func,
+  handleBlur: PropTypes.func,
+  values: PropTypes.object,
+  buttonText: PropTypes.string,
+  setFieldValue: PropTypes.func,
+  setFieldTouched: PropTypes.func,
+  links: PropTypes.shape({
+    customTextEdit: PropTypes.string
+  })
+};
+
+export default compose(
+  memo,
+)(CustomTextForm);
---
+++ client/app/containers/GlobalSettings/CustomText/CustomTextEditPage/index.js
@@ -0,0 +1,80 @@
+import React, {
+  memo, useEffect, useState, useContext
+} from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect/lib';
+import { compose } from 'redux';
+
+import { useInjectSaga } from 'utils/injectSaga';
+import { useInjectReducer } from 'utils/injectReducer';
+
+import reducer from 'containers/GlobalSettings/CustomText/reducer';
+import saga from 'containers/GlobalSettings/CustomText/saga';
+
+import RouteService from 'utils/routeHelpers';
+import { ROUTES } from 'containers/Shared/Routes/constants';
+
+import { selectUser } from 'containers/Shared/App/selectors';
+
+import {
+  getCustomTextBegin, updateCustomTextBegin,
+  customTextUnmount
+} from 'containers/GlobalSettings/CustomText/actions';
+
+import CustomTextForm from 'components/GlobalSettings/CustomText/CustomTextForm';
+
+export function CustomTextEditPage(props) {
+  useInjectReducer({ key: 'custom_text', reducer });
+  useInjectSaga({ key: 'custom_text', saga });
+
+  const rs = new RouteService(useContext);
+  const links = {
+    customTextEdit: ROUTES.admin.system.globalSettings.customText
+  };
+
+  useEffect(() => {
+    const newsItemId = rs.params('item_id');
+    props.getCustomTextBegin({ id: newsItemId });
+
+    return () => props.customTextUnmount();
+  }, []);
+
+  const { currentUser } = props;
+
+  return (
+    <CustomTextForm
+      customTextAction={props.updateCustomTextBegin}
+      buttonText='Update'
+      currentUser={currentUser}
+      links={links}
+    />
+  );
+}
+
+CustomTextEditPage.propTypes = {
+  getCustomTextBegin: PropTypes.func,
+  updateCustomTextBegin: PropTypes.func,
+  customTextUnmount: PropTypes.func,
+  currentUser: PropTypes.object
+};
+
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectUser(),
+});
+
+const mapDispatchToProps = {
+  getCustomTextBegin,
+  updateCustomTextBegin,
+  customTextUnmount
+};
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+export default compose(
+  withConnect,
+  memo,
+)(CustomTextEditPage);
