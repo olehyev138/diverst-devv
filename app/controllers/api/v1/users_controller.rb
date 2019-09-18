6db2ad899611f5ab3049115ce2c96277ac3ccf61
Mark Roy <markr4848@gmail.com>
2019-09-18 13:20:51 -0400

    Update confusing user controller action name

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < DiverstController
-  skip_before_action :verify_jwt_token, only: [:find_user_by_email]
+  skip_before_action :verify_jwt_token, only: [:find_user_enterprise_by_email]
 
-  def find_user_by_email
+  def find_user_enterprise_by_email
     render json: User.find_user_by_email(self.diverst_request.user, params).enterprise
   end
 
