5c3da8e14aaaf809ecea48d671ef045ee61bc614
AlexOxorn <alex.oxorn@gmail.com>
2019-09-20 16:31:29 -0400

    Un-commit file

--- client/server/index.js
+++ client/server/index.js
@@ -13,13 +13,13 @@ const ngrok =
   false;
 const { resolve } = require('path');
 const app = express();
-// const proxy = require('http-proxy-middleware');
-// const apiProxy = proxy('/api', {
-//   target: process.env.API_URL,
-//   logLevel: 'debug',
-//   secure: false
-// });
-// app.use('/api', apiProxy);
+const proxy = require('http-proxy-middleware');
+const apiProxy = proxy('/api', {
+  target: process.env.API_URL,
+  logLevel: 'debug',
+  secure: false
+});
+app.use('/api', apiProxy);
 
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
