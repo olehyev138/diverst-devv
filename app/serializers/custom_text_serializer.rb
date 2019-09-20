ac2641b40a4fc0ac5409039aa5bd51778e552e98
AlexOxorn <alex.oxorn@gmail.com>
2019-09-20 13:24:53 -0400

    lint fix

--- app/serializers/custom_text_serializer.rb
+++ app/serializers/custom_text_serializer.rb
@@ -1,5 +1,4 @@
 class CustomTextSerializer < ApplicationRecordSerializer
   attributes :id, :erg, :program, :structure, :outcome, :badge, :segment, :dci_full_title,
              :dci_abbreviation, :member_preference, :parent, :sub_erg, :privacy_statement
-
 end
