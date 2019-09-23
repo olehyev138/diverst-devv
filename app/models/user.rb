2833e93c7bb5ce77b6eb09e2a2a472ca9527e4f8
Derek Owusu-Frimpong <deepsky_5@live.com>
2019-09-23 16:49:12 +0000

    make changes to comment styles in view files

--- app/models/user.rb
+++ app/models/user.rb
@@ -147,7 +147,7 @@ class User < BaseClass
   accepts_nested_attributes_for :availabilities, allow_destroy: true
 
   def email_for_notification
-    (notifications_email.present? ? notifications_email : nil) || email
+    notifications_email.present? ? notifications_email : email 
   end
 
   def last_notified_date
