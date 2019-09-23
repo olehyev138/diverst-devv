457b2ad364750d320dce792d5eade762acb9eda8
Derek Owusu-Frimpong <deepsky_5@live.com>
2019-09-23 16:40:52 +0000

    resolve lint issues

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -280,21 +280,21 @@ RSpec.describe User do
   describe '#email_for_notification' do
     let(:user) { create(:user, notifications_email: 'user@email.com') }
 
-    context 'when email_for_notification is set' do 
+    context 'when email_for_notification is set' do
       it 'notifications_email attribute should not be nil or empty string' do
         expect(user.email_for_notification).not_to be_nil
         expect(user.email_for_notification).not_to be_empty
-      end 
+      end
 
-      it 'returns selected email for notification' do 
+      it 'returns selected email for notification' do
         expect(user.email_for_notification).to eq(user.notifications_email)
       end
     end
 
-    context 'when email_for_notification is not set' do 
+    context 'when email_for_notification is not set' do
       let(:user1) { create(:user) }
 
-      it 'returns default email' do 
+      it 'returns default email' do
         expect(user1.email_for_notification).to eq(user1.email)
       end
     end
