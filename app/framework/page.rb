8ef370dc169cb508ff75253992c9eb71664a6867
Mark Roy <markr4848@gmail.com>
2019-10-02 16:10:54 -0400

    Update page to return more accurate model name

--- app/framework/page.rb
+++ app/framework/page.rb
@@ -2,6 +2,6 @@ class Page < ActiveModelSerializers::Model
   attributes :items, :total, :type
 
   def initialize(items, total)
-    super({ items: items, total: total, type: items.klass.name.downcase })
+    super({ items: items, total: total, type: items.klass.model_name.singular })
   end
 end
