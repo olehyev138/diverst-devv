89390936f40fd1c3349414879ee53e241a0803b6
Mark Roy <markr4848@gmail.com>
2019-10-09 17:08:26 -0400

    Add automatic scrolling to bottom/top of page during pagination

--- client/app/components/Shared/DiverstPagination/index.js
+++ client/app/components/Shared/DiverstPagination/index.js
@@ -1,10 +1,14 @@
-import React, { memo, useState } from 'react';
+import React, { memo, useState, useEffect } from 'react';
 import { compose } from 'redux';
 import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core/styles';
 
 import { TablePagination } from '@material-ui/core';
+
+import animateScrollTo from 'animated-scroll-to';
+
+import { CONTENT_SCROLL_CLASS_NAME } from 'components/Shared/Scrollbar';
 
 const styles = theme => ({
   paginationContainer: {
@@ -31,12 +35,27 @@ const styles = theme => ({
 export function DiverstPagination(props) {
   const { classes, ...rest } = props;
 
+  const [paginationKey] = useState(Math.random().toString(36).substring(10));
   const [page, setPage] = useState(props.page || 0);
   const [rowsPerPage, setRowsPerPage] = useState(props.rowsPerPage || 10);
+  const [doScrollToBottom, setDoScrollToBottom] = useState(false);
+
+  const paginationClassName = `pagination-${paginationKey}`;
 
   const handleChangePage = (event, newPage) => {
+    let scroll = true;
+    if (newPage < page) {
+      setDoScrollToBottom(true);
+      scroll = false;
+    }
+
     setPage(newPage);
     props.handlePagination({ count: rowsPerPage, page: newPage });
+
+    if (scroll)
+      animateScrollTo(0, {
+        elementToScroll: document.querySelector(`.${CONTENT_SCROLL_CLASS_NAME}`)
+      });
   };
 
   const handleChangeRowsPerPage = (event) => {
@@ -44,12 +63,22 @@ export function DiverstPagination(props) {
     props.handlePagination({ count: +event.target.value, page });
   };
 
+  useEffect(() => {
+    if (props.isLoading === false && doScrollToBottom === true) {
+      setDoScrollToBottom(false);
+      animateScrollTo(document.querySelector(`.${paginationClassName}`), {
+        elementToScroll: document.querySelector(`.${CONTENT_SCROLL_CLASS_NAME}`)
+      });
+    }
+  }, [props.isLoading]);
+
   if (props.isLoading === true)
     return undefined;
 
   return (
     <div className={classes.paginationContainer}>
       <TablePagination
+        className={paginationClassName}
         component='div'
         page={page}
         rowsPerPageOptions={props.rowsPerPageOptions || [5, 10, 25]}
