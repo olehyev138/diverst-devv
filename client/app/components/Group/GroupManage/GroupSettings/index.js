a597bf126c5b3c4a0f485365c4e64ad0b9dff89f
Mark Roy <markr4848@gmail.com>
2019-10-10 14:51:51 -0400

    Update group settings layout

--- client/app/components/Group/GroupManage/GroupSettings/index.js
+++ client/app/components/Group/GroupManage/GroupSettings/index.js
@@ -75,83 +75,93 @@ export function GroupSettingsInner({ classes, handleSubmit, handleChange, handle
     <Card>
       <Form>
         <CardContent>
-          <Field
-            component={Select}
-            fullWidth
-            id='pending_users'
-            name='pending_users'
-            margin='normal'
-            label='Pending Users'
-            options={SETTINGS_OPTIONS.pendingUsers}
-            value={{ value: values.pending_users, label: prettify(values.pending_users) }}
-            onChange={value => setFieldValue('pending_users', value.value)}
-          />
-          <Field
-            component={Select}
-            fullWidth
-            id='members_visibility'
-            name='members_visibility'
-            margin='normal'
-            label='Members Visibility'
-            options={SETTINGS_OPTIONS.membersVisibility}
-            value={{ value: values.members_visibility, label: prettify(values.members_visibility) }}
-            onChange={value => setFieldValue('members_visibility', value.value)}
-          />
-          <Field
-            component={Select}
-            fullWidth
-            id='event_attendance_visibility'
-            name='event_attendance_visibility'
-            margin='normal'
-            label='Event Attendance Visibility'
-            options={SETTINGS_OPTIONS.eventAttendanceVisibility}
-            value={{ value: values.event_attendance_visibility, label: prettify(values.event_attendance_visibility) }}
-            onChange={value => setFieldValue('event_attendance_visibility', value.value)}
-          />
-          <Field
-            component={Select}
-            fullWidth
-            id='messages_visibility'
-            name='messages_visibility'
-            margin='normal'
-            label='Messages Visibility'
-            options={SETTINGS_OPTIONS.messagesVisibility}
-            value={{ value: values.messages_visibility, label: prettify(values.messages_visibility) }}
-            onChange={value => setFieldValue('messages_visibility', value.value)}
-          />
-          <Field
-            component={Select}
-            fullWidth
-            id='latest_news_visibility'
-            name='latest_news_visibility'
-            margin='normal'
-            label='Latest News Visibility'
-            options={SETTINGS_OPTIONS.latestNewsVisibility}
-            value={{ value: values.latest_news_visibility, label: prettify(values.latest_news_visibility) }}
-            onChange={value => setFieldValue('latest_news_visibility', value.value)}
-          />
-          <Field
-            component={Select}
-            fullWidth
-            id='upcoming_events_visibility'
-            name='upcoming_events_visibility'
-            margin='normal'
-            label='Upcoming Events Visibility'
-            options={SETTINGS_OPTIONS.upcomingEventsVisibility}
-            value={{ value: values.upcoming_events_visibility, label: prettify(values.upcoming_events_visibility) }}
-            onChange={value => setFieldValue('upcoming_events_visibility', value.value)}
-          />
-          <Field
-            component={DiverstColorPicker}
-            id='calendar_color'
-            name='calendar_color'
-            label='Calendar Colour'
-            value={values.calendar_color}
-            onChange={value => setFieldValue('calendar_color', value)}
-            FormControlProps={{
-              margin: 'normal'
-            }}
-          />
+          <Grid container spacing={3} justify='space-around'>
+            <Grid item xs='auto'>
+              <Field
+                component={Select}
+                id='pending_users'
+                name='pending_users'
+                margin='normal'
+                label='Pending Users'
+                options={SETTINGS_OPTIONS.pendingUsers}
+                value={{ value: values.pending_users, label: prettify(values.pending_users) }}
+                onChange={value => setFieldValue('pending_users', value.value)}
+              />
+            </Grid>
+            <Grid item xs='auto'>
+              <Field
+                component={Select}
+                id='members_visibility'
+                name='members_visibility'
+                margin='normal'
+                label='Members Visibility'
+                options={SETTINGS_OPTIONS.membersVisibility}
+                value={{ value: values.members_visibility, label: prettify(values.members_visibility) }}
+                onChange={value => setFieldValue('members_visibility', value.value)}
+              />
+            </Grid>
+            <Grid item xs='auto'>
+              <Field
+                component={Select}
+                id='event_attendance_visibility'
+                name='event_attendance_visibility'
+                margin='normal'
+                label='Event Attendance Visibility'
+                options={SETTINGS_OPTIONS.eventAttendanceVisibility}
+                value={{ value: values.event_attendance_visibility, label: prettify(values.event_attendance_visibility) }}
+                onChange={value => setFieldValue('event_attendance_visibility', value.value)}
+              />
+            </Grid>
+            <Grid item xs='auto'>
+              <Field
+                component={Select}
+                id='messages_visibility'
+                name='messages_visibility'
+                margin='normal'
+                label='Messages Visibility'
+                options={SETTINGS_OPTIONS.messagesVisibility}
+                value={{ value: values.messages_visibility, label: prettify(values.messages_visibility) }}
+                onChange={value => setFieldValue('messages_visibility', value.value)}
+              />
+            </Grid>
+            <Grid item xs='auto'>
+              <Field
+                component={Select}
+                id='latest_news_visibility'
+                name='latest_news_visibility'
+                margin='normal'
+                label='Latest News Visibility'
+                options={SETTINGS_OPTIONS.latestNewsVisibility}
+                value={{ value: values.latest_news_visibility, label: prettify(values.latest_news_visibility) }}
+                onChange={value => setFieldValue('latest_news_visibility', value.value)}
+              />
+            </Grid>
+            <Grid item xs='auto'>
+              <Field
+                component={Select}
+                id='upcoming_events_visibility'
+                name='upcoming_events_visibility'
+                margin='normal'
+                label='Upcoming Events Visibility'
+                options={SETTINGS_OPTIONS.upcomingEventsVisibility}
+                value={{ value: values.upcoming_events_visibility, label: prettify(values.upcoming_events_visibility) }}
+                onChange={value => setFieldValue('upcoming_events_visibility', value.value)}
+              />
+            </Grid>
+            <Grid item xs='auto'>
+              <Field
+                component={DiverstColorPicker}
+                id='calendar_color'
+                name='calendar_color'
+                label='Calendar Colour'
+                value={values.calendar_color}
+                onChange={value => setFieldValue('calendar_color', value)}
+                FormControlProps={{
+                  margin: 'normal'
+                }}
+              />
+            </Grid>
+          </Grid>
         </CardContent>
         <Divider />
         <CardActions>
