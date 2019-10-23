7f7ad0b2b2650015e85be85eda852187b6590dd0
AlexOxorn <alex.oxorn@gmail.com>
2019-10-23 10:24:49 -0400

    simple tweeks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -182,7 +182,7 @@ class ApplicationController < ActionController::Base
 
     # This ensures unauthorized users are not accessing main page, which is admin only
     # This also ensures we don't get stuck with invitation as our previous url. Otherwise it redirects to non-existent page
-    if !current_user.seen_onboarding
+    if (!current_user.seen_onboarding rescue false)
       onboarding_index_path
     elsif prev_url && (prev_url != root_url) && (!prev_url.include? 'invitation')
       prev_url
