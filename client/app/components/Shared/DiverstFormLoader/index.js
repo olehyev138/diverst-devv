9c6d543b8c104118f85bf1003d881e4c18d018e7
Mark Roy <markr4848@gmail.com>
2019-10-25 14:36:33 -0400

    Add DiverstFormLoader wrapper component to add a loader to edit forms when the data is loading and an error overlay if there was an error loading form data

---
+++ client/app/components/Shared/DiverstFormLoader/index.js
@@ -0,0 +1,69 @@
+import React, { memo } from 'react';
+import { compose } from 'redux';
+import PropTypes from 'prop-types';
+
+import { withStyles } from '@material-ui/core/styles';
+import FormErrorIcon from '@material-ui/icons/SyncProblem';
+import { CircularProgress, Grid, Typography } from '@material-ui/core';
+
+const styles = theme => ({
+  formLoader: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    zIndex: 1,
+  },
+  formLoaderWrapper: {
+    position: 'relative',
+  },
+  formLoading: {
+    filter: 'brightness(75%) opacity(60%) grayscale(100%)',
+  },
+  formErrorIcon: {
+    fontSize: 80,
+  },
+});
+
+export function DiverstFormLoader(props) {
+  const { classes, isLoading, isError, children } = props;
+
+  const dimForm = isLoading === true || isError === true;
+
+  return (
+    <React.Fragment>
+      <div className={dimForm ? classes.formLoaderWrapper : undefined}>
+        {isLoading === true && (
+          <Grid container justify='center' alignContent='center' className={classes.formLoader}>
+            <Grid item>
+              <CircularProgress size={80} thickness={1.5} />
+            </Grid>
+          </Grid>
+        )}
+        {!isLoading && isError === true && (
+          <Grid container justify='center' alignContent='center' className={classes.formLoader}>
+            <Grid item>
+              <FormErrorIcon color='primary' className={classes.formErrorIcon} />
+            </Grid>
+          </Grid>
+        )}
+        <div className={dimForm || isError === true ? classes.formLoading : undefined}>
+          {children}
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+DiverstFormLoader.propTypes = {
+  classes: PropTypes.object,
+  isLoading: PropTypes.bool,
+  isError: PropTypes.bool,
+  children: PropTypes.node.isRequired,
+};
+
+export default compose(
+  memo,
+  withStyles(styles),
+)(DiverstFormLoader);
