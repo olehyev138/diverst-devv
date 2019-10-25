c971f69fdd946aec4b933ef06da8b073246e11e5
Mark Roy <markr4848@gmail.com>
2019-10-25 16:18:49 -0400

    Add wrapper component DiverstShowLoader to provide a loader/error display on show pages

---
+++ client/app/components/Shared/DiverstShowLoader/index.js
@@ -0,0 +1,48 @@
+import React, { memo } from 'react';
+import { compose } from 'redux';
+import PropTypes from 'prop-types';
+
+import { withStyles } from '@material-ui/core/styles';
+import FormErrorIcon from '@material-ui/icons/SyncProblem';
+import { CircularProgress, Grid, Typography } from '@material-ui/core';
+
+const styles = theme => ({
+  loader: {
+    marginTop: 48,
+  },
+  errorIcon: {
+    fontSize: 80,
+  },
+});
+
+export function DiverstShowLoader(props) {
+  const { classes, isLoading, isError, children } = props;
+
+  return (
+    <React.Fragment>
+      {isLoading || isError ? (
+        <Grid container justify='center' alignContent='center' className={classes.loader}>
+          <Grid item>
+            {isLoading === true ? (
+              <CircularProgress size={80} thickness={1.5} />
+            ) : (isError === true && (
+              <FormErrorIcon color='primary' className={classes.errorIcon} />
+            ))}
+          </Grid>
+        </Grid>
+      ) : children }
+    </React.Fragment>
+  );
+}
+
+DiverstShowLoader.propTypes = {
+  classes: PropTypes.object,
+  isLoading: PropTypes.bool,
+  isError: PropTypes.bool,
+  children: PropTypes.node,
+};
+
+export default compose(
+  memo,
+  withStyles(styles),
+)(DiverstShowLoader);
