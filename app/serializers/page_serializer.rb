d5a75f7600a4b2c0d0a0f2f82603ac3e7252cba7
AlexOxorn <alex.oxorn@gmail.com>
2019-10-28 09:42:56 -0400

    Fix given pageSerializer a custom serializer

--- app/serializers/page_serializer.rb
+++ app/serializers/page_serializer.rb
@@ -1,9 +1,11 @@
 class PageSerializer < ApplicationRecordSerializer
   attributes :total, :type, :items
 
+  has_many :items
+
   def items
     object.items.map do |item|
-      (@instance_options[:use_serializer] || "#{item.class}Serializer".constantize).new(item).as_json
+      @instance_options[:use_serializer] ? @instance_options[:use_serializer].new(item).as_json : item
     end
   end
 end
