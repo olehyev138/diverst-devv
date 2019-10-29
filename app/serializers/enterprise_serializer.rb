b47042f0849af6aecc51ac464078836333fc7ae2
Matt <mattb1397@protonmail.com>
2019-10-28 22:25:01 -0400

    Add boolean switches to form

--- app/serializers/enterprise_serializer.rb
+++ app/serializers/enterprise_serializer.rb
@@ -3,7 +3,10 @@ class EnterpriseSerializer < ApplicationRecordSerializer
              :idp_cert, :saml_first_name_mapping, :saml_last_name_mapping, :has_enabled_saml, :theme_id,
              :xml_sso_config_file_name, :xml_sso_config_content_type, :xml_sso_config_file_size,
              :time_zone, :theme, :default_from_email_address, :default_from_email_display_name,
-             :redirect_email_contact, :mentorship_module_enabled
+             :redirect_email_contact, :mentorship_module_enabled, :disable_likes, :enable_pending_comments,
+             :collaborate_module_enabled, :scope_module_enabled, :has_enabled_onboarding_email, :disable_emails,
+             :enable_rewards, :enable_social_media, :plan_module_enabled
+
 
   def theme
     return nil if object.theme.nil?
