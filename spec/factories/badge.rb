393b008e9d4a7069a50ced97429ea988bf53a577
Mark Roy <markr4848@gmail.com>
2019-11-06 14:50:38 -0500

    Switch factories to use ActiveStorage attachments instead of Paperclip

--- spec/factories/badge.rb
+++ spec/factories/badge.rb
@@ -14,5 +14,16 @@ FactoryBot.define do
       #      file = File.new(Rails.root + 'spec/fixtures/files/verizon_logo.png')
       #      image { Rack::Test::UploadedFile.new(file, 'image/png') }
     end
+
+    transient do
+      image_file { Pathname.new("#{Rails.root}/spec/fixtures/files/trophy_image.jpg") }
+
+      after(:build) do |badge, evaluator|
+        badge.image.attach(
+          io: evaluator.image_file.open,
+          filename: evaluator.image_file.basename.to_s
+        )
+      end
+    end
   end
 end
