24717ae77e216f22b3fd1f67b2a27467f22b4456
Derek Owusu-Frimpong <deepsky_5@live.com>
2019-11-08 01:23:21 +0000

    resolve lint issues

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -258,7 +258,7 @@ SimpleForm.setup do |config|
   # config.i18n_scope = 'simple_form'
 end
 
-SimpleForm::FormBuilder.class_eval do 
+SimpleForm::FormBuilder.class_eval do
   def submit_with_override(field, options = {})
     data_disable_with = { disable_with: 'Processing...' }
     options[:data] = data_disable_with.merge(options[:data] || {})
