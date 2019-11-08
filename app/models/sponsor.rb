0233d6cc1b268fc3c548f8e756749de5b92a2386
Mark Roy <markr4848@gmail.com>
2019-11-08 15:35:18 -0500

    Update sponsor attachment location methods for ActiveStorage and fix tests

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -12,14 +12,11 @@ class Sponsor < ApplicationRecord
   # has_attached_file :sponsor_media, s3_permissions: :private
   # do_not_validate_attachment_file_type :sponsor_media
 
-  def sponsor_media_url=(url)
-    self.sponsor_media = URI.parse(url)
-  end
-
   def sponsor_media_location
-    return nil if !sponsor_media.presence
+    return nil if !sponsor_media.attached?
 
-    sponsor_media.expiring_url(36000)
+    # sponsor_media.expiring_url(36000)
+    Rails.application.routes.url_helpers.url_for(sponsor_media)
   end
 
   def enterprise
