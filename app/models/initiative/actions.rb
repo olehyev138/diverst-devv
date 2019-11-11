8274334a3ca7ede9753e3b87103a118ffcf307f6
Mark Roy <markr4848@gmail.com>
2019-11-11 16:24:19 -0500

    Fix initiative QR code generation

--- app/models/initiative/actions.rb
+++ app/models/initiative/actions.rb
@@ -21,7 +21,6 @@ module Initiative::Actions
     end
 
     def generate_qr_code(diverst_request, params)
-      # TODO: Fix this for ActiveStorage
       item = show(diverst_request, params)
       hash = {
         id: item.id,
@@ -29,7 +28,7 @@ module Initiative::Actions
       }
       enc = Base64.encode64(hash.to_json)
       png = RQRCode::QRCode.new(enc).as_png
-      item.qr_code = png.to_data_url
+      item.qr_code.attach(io: StringIO.new(png.to_s), filename: "#{item.name}_qr_code")
       item.save!
       item
     end
