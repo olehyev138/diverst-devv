354425b77cc6c79944d9c08910776ec1cf4825c4
Mark Roy <markr4848@gmail.com>
2019-11-11 15:14:56 -0500

    Fix news link photo tests

--- spec/factories/news_link_photos.rb
+++ spec/factories/news_link_photos.rb
@@ -1,3 +1,5 @@
+include ActiveJob::TestHelper
+
 FactoryBot.define do
   factory :news_link_photo do
     association :news_link
@@ -5,7 +7,7 @@ FactoryBot.define do
     transient do
       file_file { Pathname.new("#{Rails.root}/spec/fixtures/files/trophy_image.jpg") }
 
-      before(:build) do |news_link_photo, evaluator|
+      after(:build) do |news_link_photo, evaluator|
         news_link_photo.file.attach(
           io: evaluator.file_file.open,
           filename: evaluator.file_file.basename.to_s
