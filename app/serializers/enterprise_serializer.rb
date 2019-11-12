8a959bb600e3885cf065d71b4db28439a05a398b
Mark Roy <markr4848@gmail.com>
2019-11-12 16:59:31 -0500

    Remove ActiveStorage attributes from serializers

--- app/serializers/enterprise_serializer.rb
+++ app/serializers/enterprise_serializer.rb
@@ -1,7 +1,6 @@
 class EnterpriseSerializer < ApplicationRecordSerializer
   attributes :id, :name, :sp_entity_id, :idp_entity_id, :idp_sso_target_url, :idp_slo_target_url,
              :idp_cert, :saml_first_name_mapping, :saml_last_name_mapping, :has_enabled_saml, :theme_id,
-             :xml_sso_config_file_name, :xml_sso_config_content_type, :xml_sso_config_file_size,
              :time_zone, :theme, :default_from_email_address, :default_from_email_display_name,
              :redirect_email_contact, :mentorship_module_enabled, :disable_likes, :enable_pending_comments,
              :collaborate_module_enabled, :scope_module_enabled, :has_enabled_onboarding_email, :disable_emails,
