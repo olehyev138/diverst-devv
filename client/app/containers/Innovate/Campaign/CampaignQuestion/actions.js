f5b12e7d4a78cec19dae5174f8f5fb8e1f9799d6
Farah Khan <farah@diverst.com>
2019-11-19 12:41:57 -0500

    Setting up the crud structure for campaign questions

---
+++ client/app/containers/Innovate/Campaign/CampaignQuestion/actions.js
@@ -0,0 +1,132 @@
+/*
+ *
+ * Campaign Question actions
+ *
+ */
+
+import {
+  GET_QUESTIONS_BEGIN, GET_QUESTIONS_SUCCESS, GET_QUESTIONS_ERROR, GET_QUESTION_BEGIN,
+  GET_QUESTION_SUCCESS, GET_QUESTION_ERROR,
+  CREATE_QUESTION_BEGIN, CREATE_QUESTION_SUCCESS, CREATE_QUESTION_ERROR,
+  UPDATE_QUESTION_BEGIN, UPDATE_QUESTION_SUCCESS, UPDATE_QUESTION_ERROR,
+  DELETE_QUESTION_BEGIN, DELETE_QUESTION_SUCCESS, DELETE_QUESTION_ERROR,
+  CAMPAIGN_QUESTIONS_UNMOUNT
+} from 'containers/Innovate/Campaign/CampaignQuestion/constants';
+
+/* Member listing */
+
+export function getQuestionsBegin(payload) {
+  return {
+    type: GET_QUESTIONS_BEGIN,
+    payload
+  };
+}
+
+export function getQuestionsSuccess(payload) {
+  return {
+    type: GET_QUESTIONS_SUCCESS,
+    payload
+  };
+}
+
+export function getQuestionsError(error) {
+  return {
+    type: GET_QUESTIONS_ERROR,
+    error,
+  };
+}
+
+export function getQuestionBegin(payload) {
+  return {
+    type: GET_QUESTION_BEGIN,
+    payload
+  };
+}
+
+export function getQuestionSuccess(payload) {
+  return {
+    type: GET_QUESTION_SUCCESS,
+    payload
+  };
+}
+
+export function getQuestionError(error) {
+  return {
+    type: GET_QUESTION_ERROR,
+    error,
+  };
+}
+/* Group creating */
+
+export function createQuestionBegin(payload) {
+  return {
+    type: CREATE_QUESTION_BEGIN,
+    payload,
+  };
+}
+
+export function createQuestionSuccess(payload) {
+  return {
+    type: CREATE_QUESTION_SUCCESS,
+    payload,
+  };
+}
+
+export function createQuestionError(error) {
+  return {
+    type: CREATE_QUESTION_ERROR,
+    error,
+  };
+}
+
+/* Member updating */
+
+export function updateQuestionBegin(payload) {
+  return {
+    type: UPDATE_QUESTION_BEGIN,
+    payload,
+  };
+}
+
+export function updateQuestionSuccess(payload) {
+  return {
+    type: UPDATE_QUESTION_SUCCESS,
+    payload,
+  };
+}
+
+export function updateQuestionError(error) {
+  return {
+    type: UPDATE_QUESTION_ERROR,
+    error,
+  };
+}
+
+/* Member deleting */
+
+export function deleteQuestionBegin(payload) {
+  return {
+    type: DELETE_QUESTION_BEGIN,
+    payload,
+  };
+}
+
+export function deleteQuestionSuccess(payload) {
+  return {
+    type: DELETE_QUESTION_SUCCESS,
+    payload,
+  };
+}
+
+export function deleteQuestionError(error) {
+  return {
+    type: DELETE_QUESTION_ERROR,
+    error,
+  };
+}
+
+export function campaignQuestionsUnmount() {
+  return {
+    type: CAMPAIGN_QUESTIONS_UNMOUNT
+  };
+}
---
+++ client/app/containers/Innovate/Campaign/CampaignQuestion/constants.js
@@ -0,0 +1,27 @@
+/*
+ *
+ * Campaign Question constants
+ *
+ */
+
+export const GET_QUESTIONS_BEGIN = 'app/Innovate/Campaign/CampaignQuestion/GET_QUESTIONS_BEGIN';
+export const GET_QUESTIONS_SUCCESS = 'app/Innovate/Campaign/CampaignQuestion/GET_QUESTIONS_SUCCESS';
+export const GET_QUESTIONS_ERROR = 'app/Innovate/Campaign/GET_QUESTIONS_ERROR';
+
+export const GET_QUESTION_BEGIN = 'app/Innovate/CampaignQuestion/GET_QUESTION_BEGIN';
+export const GET_QUESTION_SUCCESS = 'app/Innovate/Campaign/CampaignQuestion/GET_QUESTION_SUCCESS';
+export const GET_QUESTION_ERROR = 'app/Innovate/Campaign/CampaignQuestion/GET_QUESTION_ERROR';
+
+export const CREATE_QUESTION_BEGIN = 'app/Innovate/Campaign/CampaignQuestion/CREATE_QUESTION_BEGIN';
+export const CREATE_QUESTION_SUCCESS = 'app/Innovate/Campaign/CampaignQuestion/CREATE_QUESTION_SUCCESS';
+export const CREATE_QUESTION_ERROR = 'app/Innovate/Campaign/CampaignQuestion/CREATE_QUESTION_ERROR';
+
+export const UPDATE_QUESTION_BEGIN = 'app/Innovate/Campaign/CampaignQuestion/UPDATE_QUESTION_BEGIN';
+export const UPDATE_QUESTION_SUCCESS = 'app/Innovate/Campaign/CampaignQuestion/UPDATE_QUESTION_SUCCESS';
+export const UPDATE_QUESTION_ERROR = 'app/Innovate/Campaign/CampaignQuestion/UPDATE_QUESTION_ERROR';
+
+export const DELETE_QUESTION_BEGIN = 'app/Innovate/Campaign/CampaignQuestion/DELETE_QUESTION_BEGIN';
+export const DELETE_QUESTION_SUCCESS = 'app/Innovate/Campaign/CampaignQuestion/DELETE_QUESTION_SUCCESS';
+export const DELETE_QUESTION_ERROR = 'app/Innovate/Campaign/CampaignQuestion/DELETE_QUESTION_ERROR';
+
+export const CAMPAIGN_QUESTIONS_UNMOUNT = 'app/Innovate/Campaign/CampaignQuestion/CAMPAIGN_QUESTIONS_UNMOUNT';
