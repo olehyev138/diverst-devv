ecb50978cf26194d075e2d432b3eb015009b92f9
Farah Khan <farah@diverst.com>
2019-11-20 09:51:12 -0500

    created campaign question selector file

---
+++ client/app/containers/Innovate/Campaign/CampaignQuestion/selectors.js
@@ -0,0 +1,45 @@
+import { createSelector } from 'reselect/lib';
+import { initialState } from 'containers/Innovate/Campaign/CampaignQuestion/reducer';
+import {getNewsItem} from "../../../News/saga";
+
+const selectQuestionsDomain = state => state.questions || initialState;
+
+const selectPaginatedQuestions = () => createSelector(
+  selectQuestionsDomain,
+  questionsState => questionsState.questionList
+);
+
+/* Select user list & format it for a select
+ *  looks like: [ { value: <>, label: <> } ... ]
+ */
+const selectPaginatedSelectQuestions = () => createSelector(
+  selectQuestionsDomain,
+  questionsState => (
+    Object
+      .values(questionsState.questionList)
+      .map(question => ({
+        value: question.id,
+        label: `${question.question}`
+      }))
+  )
+);
+
+const selectQuestionTotal = () => createSelector(
+  selectQuestionsDomain,
+  questionsState => questionsState.questionTotal
+);
+
+const selectIsFetchingQuestions = () => createSelector(
+  selectQuestionsDomain,
+  questionsState => questionsState.isFetchingMembers
+);
+
+const selectIsCommitting = () => createSelector(
+  selectQuestionsDomain,
+  questionsState => questionsState.isCommitting
+);
+
+export {
+  selectQuestionsDomain, selectPaginatedQuestions, selectPaginatedSelectQuestions,
+  selectQuestionTotal, selectIsFetchingQuestions, selectIsCommitting
+};
