b0055d9631107790b477f53e11b4776307567963
Alexander Oxorn <31745558+AlexOxorn@users.noreply.github.com>
2019-11-26 12:41:35 -0500

    Update 20191125161919_add_full_html_to_social_link.rb

--- db/migrate/20191125161919_add_full_html_to_social_link.rb
+++ db/migrate/20191125161919_add_full_html_to_social_link.rb
@@ -5,7 +5,7 @@ class AddFullHtmlToSocialLink < ActiveRecord::Migration
     SocialLink.find_each do |sl|
       unless sl.embed_code && sl.small_embed_code
         old = sl.embed_code
-	sl.update_column(:small_embed_code, old)
+	        sl.update_column(:small_embed_code, old)
         end
       end
     end
