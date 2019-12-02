f6f6df5641050caf5c6a339c7ebeecf41b8b820a
Mark Roy <markr4848@gmail.com>
2019-12-01 23:33:25 -0500

    Fix up structure for outcome page

--- client/app/components/Group/Outcome/Outcome/index.js
+++ client/app/components/Group/Outcome/Outcome/index.js
@@ -1,41 +1,54 @@
 import React, { memo } from 'react';
 
-import { compose } from 'redux/';
+import { compose } from 'redux';
 import PropTypes from 'prop-types';
 import dig from 'object-dig';
 
 import {
-  Paper, Typography, Grid, Button
+  Typography, Grid
 } from '@material-ui/core/index';
 import { withStyles } from '@material-ui/core/styles';
 
-import classNames from 'classnames';
-
 import WrappedNavLink from 'components/Shared/WrappedNavLink';
 import messages from 'containers/Group/Outcome/messages';
-import { FormattedMessage } from 'react-intl';
+import DiverstFormattedMessage from 'components/Shared/DiverstFormattedMessage';
 
-const styles = theme => ({});
+import DiverstShowLoader from 'components/Shared/DiverstShowLoader';
+
+const styles = theme => ({
+  title: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    paddingBottom: theme.spacing(3),
+  },
+});
 
 export function Outcome(props) {
-  /* Render an Outcome */
-
   const { classes } = props;
   const outcome = dig(props, 'outcome');
 
   return (
-    (outcome) ? (
-      <React.Fragment>
-      </React.Fragment>
-    ) : <React.Fragment />
+    <DiverstShowLoader isLoading={props.isFormLoading} isError={!props.isFormLoading && !outcome}>
+      {outcome && (
+        <React.Fragment>
+          <Grid container spacing={1}>
+            <Grid item>
+              <Typography color='primary' variant='h5' component='h2' className={classes.title}>
+                {outcome.name}
+              </Typography>
+            </Grid>
+          </Grid>
+        </React.Fragment>
+      )}
+    </DiverstShowLoader>
   );
 }
 
 Outcome.propTypes = {
-  deleteOutcomeBegin: PropTypes.func,
   classes: PropTypes.object,
   outcome: PropTypes.object,
   currentUserId: PropTypes.number,
+  isFormLoading: PropTypes.bool,
   links: PropTypes.shape({
     outcomeEdit: PropTypes.string,
   })
