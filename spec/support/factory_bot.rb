b702fa7453b959692610c4dba796013e0145d607
Farah Khan <farah@diverst.com>
2019-12-11 16:14:59 -0500

    Fixed factory_bot.rb to be how it was

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -2,10 +2,12 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
   config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-    DatabaseCleaner.start
-    # FactoryBot.lint
-  ensure
-    DatabaseCleaner.clean
+    begin
+      DatabaseCleaner.clean_with(:truncation)
+      DatabaseCleaner.start
+      # FactoryBot.lint
+    ensure
+      DatabaseCleaner.clean
+    end
   end
 end
