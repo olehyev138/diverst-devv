4ae833441af3a7fba35d6d84db606d5ff94b9d9e
Farah Khan <farah@diverst.com>
2019-12-11 16:18:06 -0500

    Changed spec/rails_helper.rb to how it was

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -78,7 +78,7 @@ RSpec.configure do |config|
   end
 
   config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean_with(:deletion)
   end
 
   config.before(:each) do
@@ -86,7 +86,7 @@ RSpec.configure do |config|
   end
 
   config.before(:each, js: true) do
-    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.strategy = :deletion
   end
 
   config.before(:each) do
