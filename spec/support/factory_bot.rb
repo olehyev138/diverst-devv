f69b4b5d505a51ca1a698515611c5c45bd26c47a
Farah Khan <farah@diverst.com>
2019-12-11 11:34:22 -0500

    Fix rubocop error

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -2,12 +2,10 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
   config.before(:suite) do
-    begin
-      DatabaseCleaner.clean_with(:truncation)
-      DatabaseCleaner.start
-      # FactoryBot.lint
-    ensure
-      DatabaseCleaner.clean
-    end
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.start
+    # FactoryBot.lint
+  ensure
+    DatabaseCleaner.clean
   end
 end
