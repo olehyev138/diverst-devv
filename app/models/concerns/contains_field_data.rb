ebfad2344310e52e1feacf34e9e973dc72296a8e
AlexOxorn <alex.oxorn@gmail.com>
2019-12-23 17:03:17 -0500

    add cool function2 (to discuss)

--- app/models/concerns/contains_field_data.rb
+++ app/models/concerns/contains_field_data.rb
@@ -31,17 +31,35 @@ module ContainsFieldData
     send("#{self.class.fields_holder_name}_id")
   end
 
-  def method_missing(method_name)
-    field_data = self.field_data
-                     .eager_load(:field)
-                     .where(
-                         'lower(fields.title) = ?',
-                         method_name.to_s.downcase.gsub('_', ' ')
-                     ).limit(1)
-    if field_data.present?
-      field_data[0].deserialized_data
+  def method_missing(method_name, *arg)
+    if method_name[-1] == '='
+      field_data = self.field_data
+                       .includes(:field)
+                       .references(:field)
+                       .where(
+                           'lower(fields.title) = ?',
+                           method_name[0...-1].to_s.downcase.gsub('_', ' ')
+                       ).limit(1)
+      if field_data.present?
+        pp arg[1]
+        field_data[0].data = arg[0].to_json
+        field_data[0].save!
+      else
+        super
+      end
     else
-      super
+      field_data = self.field_data
+                       .includes(:field)
+                       .references(:field)
+                       .where(
+                           'lower(fields.title) = ?',
+                           method_name.to_s.downcase.gsub('_', ' ')
+                       ).limit(1)
+      if field_data.present?
+        field_data[0].deserialized_data
+      else
+        super
+      end
     end
   end
 end
