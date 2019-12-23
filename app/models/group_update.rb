1b6bced3ea64450d66867593982adde3e26d7386
AlexOxorn <alex.oxorn@gmail.com>
2019-12-23 10:56:50 -0500

    Revert "Too tired to write message"

--- app/models/group_update.rb
+++ app/models/group_update.rb
@@ -1,6 +1,6 @@
 class GroupUpdate < BaseClass
   include PublicActivity::Common
-  include ContainsFieldData
+  include ContainsFields
 
   belongs_to :owner, class_name: 'User'
   belongs_to :group
