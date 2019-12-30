7568136fc8c832f02f75b41fa70b0c6ebd125c50
AlexOxorn <alex.oxorn@gmail.com>
2019-12-30 09:28:09 -0500

    remove twitter tests

--- spec/factories/twitter_accounts.rb
+++ spec/factories/twitter_accounts.rb
@@ -1,7 +1,2 @@
 FactoryBot.define do
-  factory :twitter_account do
-    association :group, factory: :group
-    sequence(:name) { |n| "Alex Oxorn #{n}" }
-    account 'AOxorn'
-  end
 end
--- spec/serializers/twitter_account_serializer_spec.rb
+++ spec/serializers/twitter_account_serializer_spec.rb
@@ -1,13 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe TwitterAccountSerializer, type: :serializer do
-  it 'returns associations' do
-    twitter_account = create(:twitter_account)
-    serializer = TwitterAccountSerializer.new(twitter_account)
-
-    expect(serializer.serializable_hash[:id]).to_not be_nil
-    expect(serializer.serializable_hash[:group_id]).to_not be_nil
-    expect(serializer.serializable_hash[:account]).to_not be_nil
-    expect(serializer.serializable_hash[:name]).to_not be_nil
-  end
 end
