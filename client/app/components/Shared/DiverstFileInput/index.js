a40bd5a6f9ea4e2ff3ca6ed56f5f51562f5c1c01
Mark Roy <markr4848@gmail.com>
2020-01-02 11:51:33 -0500

    Update DiverstFileInput component to use DirectUploadProvider

--- client/app/components/Shared/DiverstFileInput/index.js
+++ client/app/components/Shared/DiverstFileInput/index.js
@@ -5,6 +5,11 @@ import PropTypes from 'prop-types';
 
 import { Button, FormControl, FormHelperText, FormLabel, Grid, Typography } from '@material-ui/core';
 
+import { DirectUploadProvider } from 'react-activestorage-provider';
+
+import config from 'app.config';
+import AuthService from 'utils/authService';
+
 const styles = theme => ({
   fileInput: {
     display: 'none',
@@ -20,8 +25,10 @@ const styles = theme => ({
 
 const inputRef = React.createRef();
 
+const apiURL = new URL(config.apiUrl);
+
 export function DiverstFileInput(props) {
-  const { classes, ...rest } = props;
+  const { classes, form, handleUploadSuccess, handleUploadError, ...rest } = props;
 
   // Form Control props
   const {
@@ -33,82 +40,149 @@ export function DiverstFileInput(props) {
     required,
     variant,
     value,
+    multiple,
     ...inputProps
   } = rest;
 
   return (
     <React.Fragment>
-      <FormControl
-        disabled={disabled}
-        error={error}
-        fullWidth={fullWidth}
-        hiddenLabel={hiddenLabel}
-        margin={margin}
-        required={required}
-        variant={variant}
-      >
-        <FormLabel
-          htmlFor={props.id}
-          disabled={disabled}
-          error={error}
-          required={required}
-        >
-          {props.label}
-        </FormLabel>
-        <input
-          accept='image/*'
-          id={props.id}
-          type='file'
-          className={classes.fileInput}
-          ref={inputRef}
-          {...inputProps}
-        />
-        <Grid container spacing={2} className={classes.uploadSection} alignItems='center'>
-          <Grid item>
-            <Button
-              component='span'
-              color='secondary'
-              variant='contained'
-              onClick={() => inputRef.current.click()}
+      <DirectUploadProvider
+        multiple={multiple}
+        origin={{
+          host: apiURL.hostname,
+          port: apiURL.port,
+          protocol: apiURL.protocol.slice(0, -1),
+        }}
+        headers={{
+          'Diverst-APIKey': config.apiKey,
+          'Diverst-UserToken': AuthService.getJwt(),
+        }}
+        onSuccess={(object) => {
+          if (multiple)
+            form.setFieldValue(props.id, object);
+          else
+            form.setFieldValue(props.id, object.pop());
+
+          handleUploadSuccess(object);
+        }}
+        onError={handleUploadError}
+        render={({ handleUpload, uploads, ready }) => (
+          <React.Fragment>
+            <FormControl
+              disabled={disabled || !ready}
+              error={error}
+              fullWidth={fullWidth}
+              hiddenLabel={hiddenLabel}
+              margin={margin}
+              required={required}
+              variant={variant}
             >
-              Upload File
-            </Button>
-          </Grid>
-          <Grid item xs>
-            {props.value && props.value.name ? (
-              <Typography variant='h6' className={classes.fileName} color='textSecondary'>
-                {props.value.name}
-              </Typography>
-            ) : (
-              <Typography variant='h6' className={classes.fileName}>
-               Nothing attached
-              </Typography>
-            )}
-          </Grid>
-        </Grid>
-        {props.showHelperText && (
-          <FormHelperText
-            id={`${props.id}-helper-text`}
-            disabled={disabled}
-            error={error}
-            required={required}
-            variant={variant}
-          >
-            {props.helperText}
-          </FormHelperText>
+              <FormLabel
+                htmlFor={props.id}
+                disabled={disabled || !ready}
+                error={error}
+                required={required}
+              >
+                {props.label}
+              </FormLabel>
+              <input
+                accept='image/*'
+                id={props.id}
+                type='file'
+                className={classes.fileInput}
+                ref={inputRef}
+                disabled={disabled || !ready}
+                onChange={e => handleUpload(e.currentTarget.files)}
+                {...inputProps}
+              />
+              <Grid container spacing={2} className={classes.uploadSection} alignItems='center'>
+                <Grid item>
+                  <Button
+                    component='span'
+                    color='secondary'
+                    variant='contained'
+                    onClick={() => inputRef.current.click()}
+                    disabled={disabled || !ready}
+                  >
+                    Upload File
+                  </Button>
+                </Grid>
+                <Grid item xs>
+                  {uploads.map((upload) => {
+                    switch (upload.state) {
+                      case 'waiting':
+                        return (
+                          <Typography variant='h6' className={classes.fileName} color='textSecondary' key={upload.id}>
+                            Waiting to upload {upload.file.name}
+                          </Typography>
+                        );
+                      case 'uploading':
+                        return (
+                          <Typography variant='h6' className={classes.fileName} color='textSecondary' key={upload.id}>
+                            Uploading {upload.file.name}: {upload.progress}%
+                          </Typography>
+                        );
+                      case 'error':
+                        return (
+                          <Typography variant='h6' className={classes.fileName} color='textSecondary' key={upload.id}>
+                            Error uploading {upload.file.name}: {upload.error}
+                          </Typography>
+                        );
+                      case 'finished':
+                        return (
+                          <Typography variant='h6' className={classes.fileName} color='textSecondary' key={upload.id}>
+                            Finished uploading {upload.file.name}
+                          </Typography>
+                        );
+                      default:
+                        return (<React.Fragment />);
+                    }
+                  })}
+
+                  {value && (
+                    <Typography variant='h6' className={classes.fileName}>
+                      File has been uploaded
+                    </Typography>
+                  )}
+
+                  {!value && (
+                    <Typography variant='h6' className={classes.fileName}>
+                      Nothing attached
+                    </Typography>
+                  )}
+                </Grid>
+              </Grid>
+              {props.showHelperText && (
+                <FormHelperText
+                  id={`${props.id}-helper-text`}
+                  disabled={disabled || !ready}
+                  error={error}
+                  required={required}
+                  variant={variant}
+                >
+                  {props.helperText}
+                </FormHelperText>
+              )}
+            </FormControl>
+          </React.Fragment>
         )}
-      </FormControl>
+      />
     </React.Fragment>
   );
 }
 
 DiverstFileInput.propTypes = {
   id: PropTypes.string,
+  name: PropTypes.string,
+  multiple: PropTypes.bool,
   classes: PropTypes.object,
+  form: PropTypes.object,
   label: PropTypes.node,
-  value: PropTypes.object,
+  value: PropTypes.any,
   helperText: PropTypes.node,
   showHelperText: PropTypes.bool,
+  handleUploadSuccess: PropTypes.func,
+  handleUploadError: PropTypes.func,
 };
 
 export default compose(
