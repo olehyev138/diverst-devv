0e4b57b143611541794260653e9681fefd86321a
Mark Roy <markr4848@gmail.com>
2020-01-03 15:16:14 -0500

    Add picture methods to event serializer

--- app/serializers/initiative_serializer.rb
+++ app/serializers/initiative_serializer.rb
@@ -1,10 +1,22 @@
 class InitiativeSerializer < ApplicationRecordSerializer
   attributes :pillar, :owner, :budget, :outcome, :budget_status,
              :expenses_status, :current_expences_sum, :leftover,
-             :full?, :picture_location, :qr_code_location
+             :full?, :picture_data, :picture, :picture_file_name
 
   def picture_location
     object.picture_location(default_style: instance_options.dig(:scope, :image_size)&.to_sym)
+  end
+
+  def picture
+    object.picture.signed_id if object.picture.attached?
+  end
+
+  def picture_file_name
+    object.picture.filename.to_s if object.picture.attached?
+  end
+
+  def picture_data
+    Base64.encode64(object.picture.download) if object.picture.attached?
   end
 
   def qr_code_location
