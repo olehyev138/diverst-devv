35c6bba5f94b12c4149e5eacdfbf5fa85c5da232
AlexOxorn <alex.oxorn@gmail.com>
2020-01-06 14:04:19 -0500

    create message file [ci skip]

---
+++ client/app/containers/Shared/Update/messages.js
@@ -0,0 +1,15 @@
+/*
+ * Field Messages
+ *
+ * This contains all the text for the Fields containers/components.
+ */
+
+import { defineMessages } from 'react-intl';
+
+export const scope = 'diverst.containers.Update';
+
+export default defineMessages({
+  newUpdate: {
+    id: `${scope}.index.button.new`,
+  },
+});
