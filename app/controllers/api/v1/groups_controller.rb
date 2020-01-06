2e891ab65c993b4d478588682912475784ecd281
AlexOxorn <alex.oxorn@gmail.com>
2020-01-06 10:44:46 -0500

    refactor create field controller

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -19,10 +19,8 @@ class Api::V1::GroupsController < DiverstController
     params[:field] = field_payload
     base_authorize(klass)
     item = klass.find(params[:id])
-    new_field = Field.build(self.diverst_request, params)
-    item.fields << new_field
 
-    render status: 201, json: new_field
+    render status: 201, json: Field.build(self.diverst_request, params, base: item.fields)
   rescue => e
     case e
     when InvalidInputException
