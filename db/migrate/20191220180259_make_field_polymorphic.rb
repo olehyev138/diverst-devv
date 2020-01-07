4af7750172eb8b59905489062cc29dc9b0e4b8f1
AlexOxorn <alex.oxorn@gmail.com>
2020-01-07 14:17:06 -0500

    fix field migrations

--- db/migrate/20191220180259_make_field_polymorphic.rb
+++ db/migrate/20191220180259_make_field_polymorphic.rb
@@ -1,24 +1,29 @@
 class MakeFieldPolymorphic < ActiveRecord::Migration[5.2]
-  def up
+  def add_poly
     add_column :fields, :field_definer_id, :bigint, first: true
     add_column :fields, :field_definer_type, :string, after: :field_definer_id
     add_index :fields, [:field_definer_id, :field_definer_type]
+  end
 
-    Field.find_each do |field|
-      field.field_definer_id = field.group_id || field.poll_id || field.initiative_id || field.enterprise_id
-      field.field_definer_type =
-          if field.group_id
-            'Group'
-          elsif field.poll_id
-            'Poll'
-          elsif field.initiative_id
-            'Initiative'
-          elsif field.enterprise_id
-            'Enterprise'
-          end
-      field.save!(validate: false)
-    end
+  def remove_poly
+    remove_index :fields, [:field_definer_id, :field_definer_type]
+    remove_column :fields, :field_definer_id
+    remove_column :fields, :field_definer_type
+  end
 
+  def add_individual
+    add_column :fields, :poll_id, :bigint, first: true
+    add_column :fields, :initiative_id, :bigint, first: true
+    add_column :fields, :group_id, :bigint, first: true
+    add_column :fields, :enterprise_id, :bigint, first: true
+
+    add_index :fields, [:enterprise_id]
+    add_index :fields, [:group_id]
+    add_index :fields, [:initiative_id]
+    add_index :fields, [:poll_id]
+  end
+
+  def remove_individual
     remove_index :fields, column: [:enterprise_id]
     remove_index :fields, column: [:group_id]
     remove_index :fields, column: [:initiative_id]
@@ -29,38 +34,56 @@ class MakeFieldPolymorphic < ActiveRecord::Migration[5.2]
     remove_column :fields, :poll_id
   end
 
+  def up
+    add_poly
+
+    Field.find_each do |field|
+      field.field_definer_id = field.group_id || field.poll_id || field.initiative_id || field.enterprise_id
+      unless field.field_definer_id.present?
+        remove_poly
+        abort('No ID')
+      end
+      field.field_definer_type =
+          if field.group_id
+            'Group'
+          elsif field.poll_id
+            'Poll'
+          elsif field.initiative_id
+            'Initiative'
+          elsif field.enterprise_id
+            'Enterprise'
+          else
+            remove_poly
+            abort('No ID')
+          end
+      field.save!
+    end
+
+    remove_individual
+  end
+
 
   def down
-    add_column :fields, :poll_id, :bigint, first: true
-    add_column :fields, :initiative_id, :bigint, first: true
-    add_column :fields, :group_id, :bigint, first: true
-    add_column :fields, :enterprise_id, :bigint, first: true
-
-    add_index :fields, [:enterprise_id]
-    add_index :fields, [:group_id]
-    add_index :fields, [:initiative_id]
-    add_index :fields, [:poll_id]
-
+    add_individual
 
     Field.find_each do |field|
       case field.field_definer_type
       when 'Enterprise'
-        field.enterprise = field.field_definer
+        field.enterprise_id = field.field_definer_id
       when 'Group'
-        field.group = field.field_definer
+        field.group_id = field.field_definer_id
       when 'Poll'
-        field.poll = field.field_definer
+        field.poll_id = field.field_definer_id
       when 'Initiative'
-        field.initiative = field.field_definer
+        field.initiative_id = field.field_definer_id
       else
-        # type code here
+        remove_individual
+        abort('invalid field_definer type')
       end
 
       field.save!(validate: false)
     end
 
-    remove_index :fields, [:field_definer_id, :field_definer_type]
-    remove_column :fields, :field_definer_id
-    remove_column :fields, :field_definer_type
+    remove_poly
   end
 end
