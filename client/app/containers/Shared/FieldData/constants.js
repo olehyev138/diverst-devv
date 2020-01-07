d5a5e0de3308265574e0e382c5cacd5546920018
AlexOxorn <alex.oxorn@gmail.com>
2020-01-07 11:04:09 -0500

    simple saga for field_data updating [ci skip]

---
+++ client/app/containers/Shared/FieldData/constants.js
@@ -0,0 +1,9 @@
+/*
+ *
+ * FieldData constants
+ *
+ */
+
+export const UPDATE_FIELD_DATA_BEGIN = 'app/Shared/FieldData/UPDATE_FIELD_DATA_BEGIN';
+export const UPDATE_FIELD_DATA_SUCCESS = 'app/Shared/FieldData/UPDATE_FIELD_DATA_SUCCESS';
+export const UPDATE_FIELD_DATA_ERROR = 'app/Shared/FieldData/UPDATE_FIELD_DATA_ERROR';
