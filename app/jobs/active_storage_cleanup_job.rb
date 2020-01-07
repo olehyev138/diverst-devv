1050fe005b7d8e0802fe62ad07775acaa7cc1282
Mark Roy <markr4848@gmail.com>
2020-01-07 18:24:12 -0500

    Improve documentation for ActiveStorageCleanupJob, add more control in the job with a new param, and make it so it runs the file purge inline within the job to respect the job queue

--- app/jobs/active_storage_cleanup_job.rb
+++ app/jobs/active_storage_cleanup_job.rb
@@ -1,10 +1,14 @@
 class ActiveStorageCleanupJob < ActiveJob::Base
   queue_as :low
 
-  def perform
-    # Clears ActiveStorage blobs and their associated files if they aren't associated with an attachment & they're older than 30 minutes
-    ActiveStorage::Blob.find_each(batch_size: 500) do |blob|
-      blob.purge_later unless blob.identified? || blob.created_at > 30.minutes.ago
+  # Clears ActiveStorage blobs and their associated files if they aren't associated with an attachment & they're too old
+  #
+  # @param blob_lifetime [ActiveSupport::Duration] (30.minutes) the amount of time a blob can live for. If the created_at is older than blob_lifetime ago then it is removed
+  # @param batch_size [Integer] (500) the batch size of the `find_each` when iterating through blobs.
+  # @note blob_lifetime should stay 30 minutes most of the time. This option is primarily for development to purge unnecessary files
+  def perform(blob_lifetime = 30.minutes, batch_size = 500)
+    ActiveStorage::Blob.find_each(batch_size: batch_size) do |blob|
+      blob.purge unless blob.identified? || blob.created_at > blob_lifetime.ago
     end
   end
 end
