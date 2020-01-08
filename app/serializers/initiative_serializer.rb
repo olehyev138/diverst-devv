24f6879a08235511579ca3ce2ab8801bacd147e6
Mark Roy <markr4848@gmail.com>
2020-01-07 19:12:22 -0500

    Add QR Code serialize methods to Initiative serializer

--- app/serializers/initiative_serializer.rb
+++ app/serializers/initiative_serializer.rb
@@ -1,7 +1,14 @@
 class InitiativeSerializer < ApplicationRecordSerializer
   attributes :pillar, :owner, :budget, :outcome, :budget_status,
-             :expenses_status, :current_expences_sum, :leftover,
-             :full?, :picture_data, :picture, :picture_file_name
+             :expenses_status, :current_expences_sum, :leftover, :full?,
+             :picture, :picture_file_name, :picture_data,
+             :qr_code, :qr_code_file_name, :qr_code_data
+
+  def serialize_all_fields
+    true
+  end
+
+  # Picture
 
   def picture_location
     object.picture_location(default_style: instance_options.dig(:scope, :image_size)&.to_sym)
@@ -19,11 +26,21 @@ class InitiativeSerializer < ApplicationRecordSerializer
     AttachmentHelper.attachment_data_string(object.picture)
   end
 
-  def qr_code_location
-    object.qr_code_location(default_style: instance_options.dig(:scope, :image_size)&.to_sym)
+  # QR Code
+
+  def qr_code
+    AttachmentHelper.attachment_signed_id(object.picture)
   end
 
-  def serialize_all_fields
-    true
+  def qr_code_file_name
+    AttachmentHelper.attachment_file_name(object.picture)
+  end
+
+  def qr_code_data
+    AttachmentHelper.attachment_data_string(object.picture)
+  end
+
+  def qr_code_location
+    object.qr_code_location(default_style: instance_options.dig(:scope, :image_size)&.to_sym)
   end
 end
