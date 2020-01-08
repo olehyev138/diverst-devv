d383fb33d841f4696b02741c200e8a1fea08118e
AlexOxorn <alex.oxorn@gmail.com>
2020-01-08 13:41:42 -0500

    down now repopulates user field data

--- db/migrate/20191218212443_make_field_data_polymorphic.rb
+++ db/migrate/20191218212443_make_field_data_polymorphic.rb
@@ -45,6 +45,26 @@ class MakeFieldDataPolymorphic < ActiveRecord::Migration[5.2]
       FieldData.destroy_all
       ActiveRecord::Base.connection.execute("TRUNCATE field_data")
 
+      User.all.each do |user|
+        # For each field id in users data hash
+        info = user.info
+        info.keys.each do |field_id|
+          # - Find field object & extract data from hash (can only be done through field object)
+          # - Serialize field data as json string
+          field = Field.find(field_id)
+          data_str = info[field].to_json
+
+          # Create new FieldData object associated to current user & current field
+          field_data = FieldData.new(
+              user_id: user.id,
+              field_id: field_id,
+              data: data_str
+          )
+
+          field_data.save!(validate: false)
+        end
+      end
+
       remove_index :field_data, name: 'index_field_data_on_field_user_id_and_field_user_type'
       remove_column :field_data, :field_user_id
       remove_column :field_data, :field_user_type
