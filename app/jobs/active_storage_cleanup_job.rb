dd8a2c9239c62f50ea889b3dba96db357a2b7bcb
Mark Roy <markr4848@gmail.com>
2020-01-08 20:04:32 -0500

    Adjust ActiveStorage Blob default lifetime

--- app/jobs/active_storage_cleanup_job.rb
+++ app/jobs/active_storage_cleanup_job.rb
@@ -5,8 +5,8 @@ class ActiveStorageCleanupJob < ActiveJob::Base
   #
   # @param blob_lifetime [ActiveSupport::Duration] (30.minutes) the amount of time a blob can live for. If the created_at is older than blob_lifetime ago then it is removed
   # @param batch_size [Integer] (500) the batch size of the `find_each` when iterating through blobs.
-  # @note blob_lifetime should stay 30 minutes most of the time. This option is primarily for development to purge unnecessary files
-  def perform(blob_lifetime = 30.minutes, batch_size = 500)
+  # @note blob_lifetime should stay default most of the time. This option is primarily for development to purge unnecessary files
+  def perform(blob_lifetime = 240.minutes, batch_size = 500)
     ActiveStorage::Blob.find_each(batch_size: batch_size) do |blob|
       blob.purge unless blob.identified? || blob.created_at > blob_lifetime.ago
     end
