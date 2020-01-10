7414cae9f2f2a509a0acd0340a0aa50ff5df2513
AlexOxorn <alex.oxorn@gmail.com>
2020-01-10 16:08:47 -0500

    test fix

--- spec/models/update_spec.rb
+++ spec/models/update_spec.rb
@@ -11,20 +11,24 @@ RSpec.describe Update, type: :model do
   end
 
   describe 'test instance methods' do
-    let!(:group) { build(:group) }
-    let!(:update) { create(:update, updatable: group, report_date: DateTime.now) }
-    let!(:previous_update) { create(:update, updatable: group, report_date: DateTime.now - 2.days) }
-    let!(:next_update) { create(:update, updatable: group, report_date: DateTime.now + 2.days) }
+    before do
+      group = build(:group)
+      @update = create(:update, updatable: group, report_date: DateTime.now)
+      @previous_update = create(:update, updatable: group, report_date: DateTime.now - 2.days)
+      @next_update = create(:update, updatable: group, report_date: DateTime.now + 2.days)
+    end
 
     context '#next' do
       it 'return the next update in chronological order' do
-        expect(update.next).to eq next_update
+        @update.reload
+        expect(@update.next).to eq @next_update
       end
     end
 
     context '#previous' do
       it 'return the previous update in chronological order' do
-        expect(update.previous).to eq previous_update
+        @update.reload
+        expect(@update.previous).to eq @previous_update
       end
     end
   end
