10a5b39122e18dc592b0175ec8f24b4e33cc317a
Matt <mattb1397@protonmail.com>
2020-01-15 13:23:21 -0500

    Completely remove old devops directory

--- devops/infrastructure/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "cloudflare_email" {}
-variable "cloudflare_token" {}
-
-variable "cloudflare_zone" {
-  default = "diverst.com"
-}
