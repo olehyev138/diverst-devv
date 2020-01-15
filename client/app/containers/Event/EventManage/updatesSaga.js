ca6a02fd9956b027ad704308addb63f0e272580e
AlexOxorn <alex.oxorn@gmail.com>
2020-01-15 09:16:50 -0500

    remove metrics

--- client/app/containers/Event/EventManage/updatesSaga.js
+++ client/app/containers/Event/EventManage/updatesSaga.js
@@ -4,7 +4,6 @@ import api from 'api/api';
 import {
   GET_UPDATE_BEGIN,
   GET_UPDATES_BEGIN,
-  GET_METRICS_BEGIN,
   CREATE_UPDATE_BEGIN,
   UPDATE_UPDATE_BEGIN,
   DELETE_UPDATE_BEGIN,
@@ -12,12 +11,11 @@ import {
 } from 'containers/Shared/Update/constants';
 
 import {
-  getUpdate, updateUpdate, deleteUpdate, getMetrics, getUpdatePrototype, getUpdates, createUpdate
+  getUpdate, updateUpdate, deleteUpdate, getUpdatePrototype, getUpdates, createUpdate
 } from 'containers/Shared/Update/saga';
 
 export default function* KpiSaga() {
   yield takeLatest(GET_UPDATES_BEGIN, action => getUpdates(action, api.initiatives));
-  yield takeLatest(GET_METRICS_BEGIN, action => getMetrics(action, api.initiatives));
   yield takeLatest(CREATE_UPDATE_BEGIN, action => createUpdate(action, api.initiatives));
   yield takeLatest(GET_UPDATE_PROTOTYPE_BEGIN, action => getUpdatePrototype(action, api.initiatives));
   yield takeLatest(GET_UPDATE_BEGIN, getUpdate);
