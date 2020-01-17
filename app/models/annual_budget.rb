11de98a9a6fc45b78e58c6b0ffc73f758c3860a6
AlexOxorn <alex.oxorn@gmail.com>
2020-01-17 15:21:16 -0500

    initiatives through budget [ci skip]

--- app/models/annual_budget.rb
+++ app/models/annual_budget.rb
@@ -4,20 +4,20 @@ class AnnualBudget < ApplicationRecord
   belongs_to :group
   has_one :enterprise, through: :group
 
-  has_many :initiatives, through: :group
-  has_many :initiative_expenses, through: :initiatives, source: :expenses
-
-  has_many :finalized_initiatives, -> { where(finished_expenses: true) }, through: :group, source: :initiatives, class_name: 'Initiative'
-  has_many :finalized_expenses, through: :finalized_initiatives, source: :expenses, class_name: 'InitiativeExpense'
-
-  has_many :unfinished_initiatives, -> { where(finished_expenses: false) }, through: :group, source: :initiatives, class_name: 'Initiative'
-  has_many :unfinished_expenses, through: :unfinished_initiatives, source: :expenses, class_name: 'InitiativeExpense'
-
   has_many :budgets, dependent: :destroy
   has_many :approved_budgets, -> { approved }, class_name: 'Budget'
 
   has_many :budget_items, through: :budgets
   has_many :approved_budget_items, through: :approved_budgets, source: :budget_items
+
+  has_many :initiatives, through: :budget_items
+  has_many :initiative_expenses, through: :initiatives, source: :expenses
+
+  has_many :finalized_initiatives, -> { where(finished_expenses: true) }, through: :budget_items, source: :initiatives, class_name: 'Initiative'
+  has_many :finalized_expenses, through: :finalized_initiatives, source: :expenses, class_name: 'InitiativeExpense'
+
+  has_many :unfinished_initiatives, -> { where(finished_expenses: false) }, through: :budget_items, source: :initiatives, class_name: 'Initiative'
+  has_many :unfinished_expenses, through: :unfinished_initiatives, source: :expenses, class_name: 'InitiativeExpense'
 
   cache :approved_budget, :reserved, :spent_budget, :finalized_expenditure, :available_budget,
         :unspent, :free, :leftover
