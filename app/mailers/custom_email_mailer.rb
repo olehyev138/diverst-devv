63e09cf6b3f7b7dd84fc719ba47cbe644d2f6b5e
Anatolii Borysov <borisano@gmail.com>
2020-01-23 16:00:58 +0200

    Fix sending emails

--- app/mailers/custom_email_mailer.rb
+++ app/mailers/custom_email_mailer.rb
@@ -1,11 +1,17 @@
 class CustomEmailMailer < ApplicationMailer
   def custom(custom_email_id, emails)
-    custom_email = Email.find custom_email_id
-    return unless custom_email.custom?
+    @custom_email = Email.find custom_email_id
+    return unless @custom_email.custom?
 
-    set_defaults(custom_email.enterprise, 'custom')
+    set_defaults(@custom_email.enterprise, 'custom')
 
     #TODO check emails are unique
-    mail(from: @from_address, bcc: emails, subject: custom_email.subject)
+    mail(from: @from_address, to: emails, subject: @custom_email.subject)
+  end
+
+  def variables
+    {
+      enterprise: @custom_email.enterprise
+    }
   end
 end
