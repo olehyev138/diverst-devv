5ced0610ea25bd10fa8c052d4de0fdd6679b1bdf
Mark Roy <markr4848@gmail.com>
2020-01-27 17:16:51 -0500

    Update CSV file serializer to have all the necessary attributes for downloads

--- app/serializers/csv_file_serializer.rb
+++ app/serializers/csv_file_serializer.rb
@@ -1,7 +1,11 @@
 class CsvFileSerializer < ApplicationRecordSerializer
-  attributes :download_file_location
+  attributes :id, :download_file_path, :file_name, :created_at
 
-  def download_file_location
+  def download_file_path
     object.path_for_download
+  end
+
+  def file_name
+    object.download_file_name + object.download_file.filename.extension_with_delimiter
   end
 end
