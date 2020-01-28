faaaed5ea3b7d5ba06af92ced7790ce784969c5e
liana <provlia1@videotron.ca>
2020-01-28 11:11:08 -0500

    Added the add a banner image functionality, Added the banner image on the home page

--- client/app/components/Shared/DiverstImg/index.js
+++ client/app/components/Shared/DiverstImg/index.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 // This component is intended for rendering images from a base64 string,
 // likely image data encoded in base64 received from a serializer.
 export function DiverstImg(props) {
-  const { classes, data, maxWidth, maxHeight, alt, styles, imgProps, ...rest } = props;
+  const { classes, data, maxWidth, maxHeight, minWidth, minHeight, alt, styles, imgProps, ...rest } = props;
 
   return (
     <React.Fragment>
@@ -14,8 +14,10 @@ export function DiverstImg(props) {
           src={`data:image/jpeg;base64,${data}`}
           alt={alt}
           style={{
-            maxWidth: `${maxWidth}px`,
-            maxHeight: `${maxHeight}px`,
+            maxWidth: maxWidth,
+            maxHeight: maxHeight,
+            minWidth: minWidth,
+            minHeight: minHeight,
             ...styles,
           }}
         />
@@ -27,16 +29,18 @@ export function DiverstImg(props) {
 DiverstImg.propTypes = {
   classes: PropTypes.object,
   data: PropTypes.string.isRequired,
-  maxWidth: PropTypes.number,
-  maxHeight: PropTypes.number,
+  maxWidth: PropTypes.string,
+  maxHeight: PropTypes.string,
+  minWidth: PropTypes.string,
+  minHeight: PropTypes.string,
   alt: PropTypes.string,
   styles: PropTypes.object,
   imgProps: PropTypes.object,
 };
 
 DiverstImg.defaultProps = {
-  maxWidth: 350,
-  maxHeight: 350,
+  maxWidth: '350px',
+  maxHeight: '350px',
 };
 
 export default compose(
