2399002207433bcc66b7a6388197fb19dfde895c
AlexOxorn <alex.oxorn@gmail.com>
2020-01-31 13:52:58 -0500

    lint

--- client/app/components/Event/EventManage/ExpensesList/index.js
+++ client/app/components/Event/EventManage/ExpensesList/index.js
@@ -10,7 +10,7 @@ import React, {
 } from 'react';
 import PropTypes from 'prop-types';
 import { compose } from 'redux';
-import {floatRound, percent} from 'utils/floatRound';
+import { floatRound, percent } from 'utils/floatRound';
 
 import {
   Button, Card, CardContent, CardActions,
