51c34375201a1497bd8b03a1b1b8d92cc62bcb27
AlexOxorn <alex.oxorn@gmail.com>
2020-02-05 13:43:04 -0500

    minor fix

--- app/serializers/budget_serializer.rb
+++ app/serializers/budget_serializer.rb
@@ -2,10 +2,6 @@ class BudgetSerializer < ApplicationRecordSerializer
   attributes :approver, :requested_amount, :available_amount, :group_id, :annual_budget_id,
              :status, :requested_at, :item_count, :description, :requester, :budget_items
 
-  def budget_items
-    object.budget_items.private_scope(scope&.dig(:current_user))
-  end
-
   def status
     object.approver.present? ? "#{object.status_title} by #{object.approver.name}" : object.status_title
   end
