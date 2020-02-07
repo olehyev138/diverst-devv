816875234d794a349e01d00fa0c6e75869d8017e
Mark Roy <markr4848@gmail.com>
2020-02-07 00:15:32 -0500

    Overhaul ThemeProvider to retrieve the Enterprise from the store and build the theme based on the Enterprise theme colors

--- client/app/containers/Shared/ThemeProvider/index.js
+++ client/app/containers/Shared/ThemeProvider/index.js
@@ -4,7 +4,7 @@
  *
  */
 
-import React, { memo } from 'react';
+import React, { memo, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -12,7 +12,7 @@ import { compose } from 'redux';
 
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
-import { makeSelectPrimary, makeSelectSecondary } from './selectors';
+import { selectEnterprise } from 'containers/Shared/App/selectors';
 
 import SnackbarProviderWrapper from 'components/Shared/SnackbarProviderWrapper';
 
@@ -22,91 +22,99 @@ import App from 'containers/Shared/App/Loadable';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import LuxonUtils from '@date-io/luxon';
 
-export class ThemeProvider extends React.PureComponent {
-  render() {
-    const defaultTheme = createMuiTheme();
+export function ThemeProvider(props) {
+  const { enterprise } = props;
 
-    const { primary, secondary } = this.props;
-    const theme = createMuiTheme({
-      palette: {
-        type: 'light',
-        background: {
-          default: '#f5f5f5',
-        },
-        primary: {
-          main: primary,
-          main25: `${primary}40`,
-          main50: `${primary}80`,
-          main75: `${primary}C0`,
-        },
-        secondary: {
-          main: secondary,
-        },
-        tertiary: {
-          main: '#8b8c8d',
-        },
-        error: {
-          main: '#D32F2F',
-        },
-        warning: {
-          main: '#ffa000',
-        },
-        info: {
-          main: primary,
-        },
-        success: {
-          main: '#43a047',
-          lightBackground: '#8bcf8e',
-        },
+  const [defaultTheme] = useState(createMuiTheme());
+
+  const buildTheme = (brandingColor, graphsColor) => createMuiTheme({
+    palette: {
+      type: 'light',
+      background: {
+        default: '#f5f5f5',
       },
-      typography: {
-        useNextVariants: true,
+      primary: {
+        main: brandingColor,
+        main25: `${brandingColor}40`,
+        main50: `${brandingColor}80`,
+        main75: `${brandingColor}C0`,
       },
-      mixins: {
-        toolbar: {
+      tertiary: {
+        main: '#8b8c8d',
+      },
+      graphs: {
+        main: graphsColor
+      },
+      error: {
+        main: '#D32F2F',
+      },
+      warning: {
+        main: '#ffa000',
+      },
+      info: {
+        main: brandingColor,
+      },
+      success: {
+        main: '#43a047',
+        lightBackground: '#8bcf8e',
+      },
+    },
+    typography: {
+      useNextVariants: true,
+    },
+    mixins: {
+      toolbar: {
+        minHeight: 90,
+        maxHeight: 90,
+        [`${defaultTheme.breakpoints.up('xs')} and (orientation: landscape)`]: {
+          minHeight: 56,
+          maxHeight: 56,
+        },
+        [defaultTheme.breakpoints.up('sm')]: {
           minHeight: 90,
           maxHeight: 90,
-          [`${defaultTheme.breakpoints.up('xs')} and (orientation: landscape)`]: {
-            minHeight: 56,
-            maxHeight: 56,
-          },
-          [defaultTheme.breakpoints.up('sm')]: {
-            minHeight: 90,
-            maxHeight: 90,
-          },
         },
       },
-      // Custom theme additions & global styles should be placed here
-      custom: {
-        colors: {
-          offBlack: '#333333',
-          darkGrey: '#4c4c4c',
-          grey: '#a7a8a9',
-          lightGrey: '#dedfe0',
-        },
+    },
+    // Custom theme additions & global styles should be placed here
+    custom: {
+      colors: {
+        offBlack: '#333333',
+        darkGrey: '#4c4c4c',
+        grey: '#a7a8a9',
+        lightGrey: '#dedfe0',
       },
-    });
+    },
+  });
 
-    return (
-      <MuiThemeProvider theme={theme}>
-        <SnackbarProviderWrapper>
-          <MuiPickersUtilsProvider utils={LuxonUtils}>
-            <App />
-          </MuiPickersUtilsProvider>
-        </SnackbarProviderWrapper>
-      </MuiThemeProvider>
-    );
-  }
+  // Defaults
+  const [brandingColor, setBrandingColor] = useState('#7B77C9');
+  const [graphsColor, setGraphsColor] = useState('#8A8A8A');
+
+  const [theme, setTheme] = useState(buildTheme(brandingColor, graphsColor));
+
+  useEffect(() => {
+    if (enterprise && enterprise.theme)
+      setTheme(buildTheme(enterprise.theme.branding_color, enterprise.theme.charts_color));
+  }, [enterprise]);
+
+  return (
+    <MuiThemeProvider theme={theme}>
+      <SnackbarProviderWrapper>
+        <MuiPickersUtilsProvider utils={LuxonUtils}>
+          <App />
+        </MuiPickersUtilsProvider>
+      </SnackbarProviderWrapper>
+    </MuiThemeProvider>
+  );
 }
 
 ThemeProvider.propTypes = {
-  primary: PropTypes.string,
-  secondary: PropTypes.string,
+  enterprise: PropTypes.object,
 };
 
 const mapStateToProps = createStructuredSelector({
-  primary: makeSelectPrimary(),
-  secondary: makeSelectSecondary(),
+  enterprise: selectEnterprise(),
 });
 
 const withConnect = connect(
