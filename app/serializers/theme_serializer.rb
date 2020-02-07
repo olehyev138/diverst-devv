0ffc7df55ac8b0bd2057af770246e1ee534b053e
Mark Roy <markr4848@gmail.com>
2020-02-07 00:11:49 -0500

    Update Theme serializer to only return currently necessary attributes

--- app/serializers/theme_serializer.rb
+++ app/serializers/theme_serializer.rb
@@ -1,9 +1,3 @@
 class ThemeSerializer < ApplicationRecordSerializer
-  attributes :id, :logo, :primary_color, :secondary_color, :logo_location
-
-  def logo_location
-    return nil if !object.logo?
-
-    ENV['DOMAIN'] + object.logo.expiring_url(24.hours)
-  end
+  attributes :branding_color, :charts_color
 end
