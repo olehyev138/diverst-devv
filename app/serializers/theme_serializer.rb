33e09f7ea6d86ac625c91cb516429e1b6d74af49
Mark Roy <markr4848@gmail.com>
2020-02-10 06:07:55 -0500

    Include fields in Theme serializer for form

--- app/serializers/theme_serializer.rb
+++ app/serializers/theme_serializer.rb
@@ -1,3 +1,3 @@
 class ThemeSerializer < ApplicationRecordSerializer
-  attributes :branding_color, :charts_color
+  attributes :branding_color, :charts_color, :use_secondary_color, :primary_color, :secondary_color
 end
