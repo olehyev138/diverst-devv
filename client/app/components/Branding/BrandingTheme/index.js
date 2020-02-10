e3fe9bddd0bccf53bec83ec6fc366486dd03b1c0
Mark Roy <markr4848@gmail.com>
2020-02-10 06:30:07 -0500

    Pass in and use isLoading and isCommitting for Enterprise branding

--- client/app/components/Branding/BrandingTheme/index.js
+++ client/app/components/Branding/BrandingTheme/index.js
@@ -19,6 +19,8 @@ import { buildValues } from 'utils/formHelpers';
 
 import DiverstColorPicker from 'components/Shared/DiverstColorPicker';
 import DiverstSwitch from 'components/Shared/DiverstSwitch';
+import DiverstSubmit from 'components/Shared/DiverstSubmit';
+import DiverstFormLoader from 'components/Shared/DiverstFormLoader';
 
 import { DEFAULT_BRANDING_COLOR, DEFAULT_CHARTS_COLOR } from 'containers/Shared/ThemeProvider';
 
@@ -31,70 +33,69 @@ const styles = theme => ({
 /* eslint-disable object-curly-newline */
 export function BrandingThemeInner({ classes, handleSubmit, handleChange, handleBlur, values, buttonText, setFieldValue, setFieldTouched, ...props }) {
   return (
-    <Card>
-      <Form>
-        <CardContent>
-          <Grid container spacing={3} alignItems='center'>
-            <Grid item xs={12} sm={6}>
-              <Field
-                component={DiverstColorPicker}
-                required
-                id='primary_color'
-                name='primary_color'
-                label='Primary Color'
-                value={values.primary_color}
-                onChange={value => setFieldValue('primary_color', value)}
-                FormControlProps={{
-                  margin: 'normal',
-                  fullWidth: true,
-                }}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Field
-                component={DiverstSwitch}
-                id='use_secondary_color'
-                name='use_secondary_color'
-                label='Use different color for graphs?'
-                margin='normal'
-                value={values.use_secondary_color}
-                onChange={(_, value) => setFieldValue('use_secondary_color', value)}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Collapse in={values.use_secondary_color}>
+    <DiverstFormLoader isLoading={props.isLoading} isError={!props.theme}>
+      <Card>
+        <Form>
+          <CardContent>
+            <Grid container spacing={3} alignItems='center'>
+              <Grid item xs={12} sm={6}>
                 <Field
                   component={DiverstColorPicker}
                   required
-                  fullWidth
-                  id='secondary_color'
-                  name='secondary_color'
-                  label='Graphs Color'
-                  value={values.secondary_color}
-                  onChange={value => setFieldValue('secondary_color', value)}
+                  id='primary_color'
+                  name='primary_color'
+                  label='Primary Color'
+                  disabled={props.isCommitting}
+                  value={values.primary_color}
+                  onChange={value => setFieldValue('primary_color', value)}
                   FormControlProps={{
                     margin: 'normal',
                     fullWidth: true,
                   }}
                 />
-              </Collapse>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Field
+                  component={DiverstSwitch}
+                  id='use_secondary_color'
+                  name='use_secondary_color'
+                  label='Use different color for graphs?'
+                  margin='normal'
+                  disabled={props.isCommitting}
+                  value={values.use_secondary_color}
+                  onChange={(_, value) => setFieldValue('use_secondary_color', value)}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Collapse in={values.use_secondary_color}>
+                  <Field
+                    component={DiverstColorPicker}
+                    required
+                    fullWidth
+                    id='secondary_color'
+                    name='secondary_color'
+                    label='Graphs Color'
+                    disabled={props.isCommitting}
+                    value={values.secondary_color}
+                    onChange={value => setFieldValue('secondary_color', value)}
+                    FormControlProps={{
+                      margin: 'normal',
+                      fullWidth: true,
+                    }}
+                  />
+                </Collapse>
+              </Grid>
             </Grid>
-          </Grid>
-        </CardContent>
-        <Divider />
-        <CardActions>
-          <Button
-            color='primary'
-            type='submit'
-          >
-            Save
-          </Button>
-          <Button>
-            Cancel
-          </Button>
-        </CardActions>
-      </Form>
-    </Card>
+          </CardContent>
+          <Divider />
+          <CardActions>
+            <DiverstSubmit isCommitting={props.isCommitting}>
+              {buttonText}
+            </DiverstSubmit>
+          </CardActions>
+        </Form>
+      </Card>
+    </DiverstFormLoader>
   );
 }
 
@@ -127,13 +128,16 @@ BrandingTheme.propTypes = {
 
 BrandingThemeInner.propTypes = {
   classes: PropTypes.object,
+  theme: PropTypes.object,
   handleSubmit: PropTypes.func,
   handleChange: PropTypes.func,
   handleBlur: PropTypes.func,
   values: PropTypes.object,
   buttonText: PropTypes.string,
   setFieldValue: PropTypes.func,
-  setFieldTouched: PropTypes.func
+  setFieldTouched: PropTypes.func,
+  isCommitting: PropTypes.bool,
+  isLoading: PropTypes.bool,
 };
 
 export default compose(
