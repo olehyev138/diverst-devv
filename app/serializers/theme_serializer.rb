6eedaf6a0603cb0f2bf529b8f1ebd949fde6d0da
Mark Roy <markr4848@gmail.com>
2020-02-11 12:51:11 -0500

    Add logo data to Theme serializer

--- app/serializers/theme_serializer.rb
+++ app/serializers/theme_serializer.rb
@@ -1,3 +1,16 @@
 class ThemeSerializer < ApplicationRecordSerializer
-  attributes :branding_color, :charts_color, :use_secondary_color, :primary_color, :secondary_color
+  attributes :branding_color, :charts_color, :use_secondary_color, :primary_color,
+             :secondary_color, :logo_redirect_url, :logo, :logo_file_name, :logo_data
+
+  def logo
+    AttachmentHelper.attachment_signed_id(object.logo)
+  end
+
+  def logo_file_name
+    AttachmentHelper.attachment_file_name(object.logo)
+  end
+
+  def logo_data
+    AttachmentHelper.attachment_data_string(object.logo)
+  end
 end
