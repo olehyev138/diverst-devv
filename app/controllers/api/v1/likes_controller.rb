f1b96bfdcb58a81eb252d8840a37cb2fbc7b1a4b
devst05 <1726271@johnabbottcollege.net>
2020-02-12 12:49:49 -0500

    Post_liking API revised Component added

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -1,27 +1,50 @@
 class Api::V1::LikesController < DiverstController
+  before_action :is_liked
+
+  def is_liked
+    if payload[:news_feed_link_id].present?
+      @isliked = Like.find_by(user: self.diverst_request.user, news_feed_link_id: payload[:news_feed_link_id])
+    else
+      @isliked = Like.find_by(user: self.diverst_request.user, answer_id: payload[:answer_id])
+    end
+  end
+
   def create
     params[klass.symbol] = payload
-    params[klass.symbol][:user_id] = current_user.id
-    params[klass.symbol][:enterprise_id] = current_user.enterprise.id
 
     if params[klass.symbol][:news_feed_link_id].present? && params[klass.symbol][:answer_id].present?
       raise InvalidInputException.new({ message: 'Can\'t like Answer and News Item', attribute: item.errors.messages.first.first})
     end
 
-    base_authorize(klass)
-
-    render status:201, jason: klass.build(self.diverst_request, params)
-  rescue => e
-    case e
-    when InvalidInputException
-      raise
+    if @isliked.present?
+      raise InvalidInputException.new({ message: 'Answer and News Item is already liked', attribute: item.errors.messages.first.first})
     else
-      raise BadRequestException.new(e.message)
+      begin
+        base_authorize(klass)
+        render status: 201, json: klass.build(self.diverst_request, params)
+      rescue => e
+        case e
+        when InvalidInputException
+          raise
+        else
+          raise BadRequestException.new(e.message)
+        end
+      end
     end
   end
 
   def unlike
-    render status: 200, json: { works: true }
+    if @isliked.present?
+      begin
+        base_authorize @isliked
+        @isliked.destroy
+        render status: 200, json: { works: true }
+      rescue => e
+        raise BadRequestException.new(e.message)
+      end
+    else
+      raise BadRequestException.new('Can\'t unlike Answer and News Item')
+    end
   end
 
   def payload
--- client/app/components/Shared/DiverstLike/index.js
+++ client/app/components/Shared/DiverstLike/index.js
@@ -2,33 +2,78 @@ import React from 'react';
 import { compose } from 'redux';
 import { withStyles, withTheme } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
-import Select from 'react-select';
+import styled from 'styled-components';
 
-import { FormControl, FormHelperText, FormLabel } from '@material-ui/core';
+const Label = styled.label`
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+`;
 
-const styles = theme => ({
-  formControl: {
-    minWidth: 150,
-  },
-  select: {
-    width: '100%',
-    paddingTop: 8,
-  },
-  formLabel: {
-    fontSize: '0.9rem',
-  },
-});
+const Span = styled.span`
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: grey;
+  border-radius: 34px;
+  &:before{
+    position: absolute;
+    content: '';
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background: white;
+    border-radius: 50%;
+  }
+`;
 
+const Input = styled.input`
+  &:checked + ${Span} {
+    background: blue;
+  }
+
+  &:checked + ${Span}:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);  }
+`;
+
+const Count = styled.label`
+  align-content: center
+`;
 export function DiverstLike(props) {
   return (
     <React.Fragment>
-
+      <div>
+        <Label>
+          <Input checked={props.isLiked}
+            type='checkbox'
+            onClick={() => {
+              if (props.newsFeedLinkId != null)
+                props.likeAction(props.newsFeedLinkId);
+              else
+                props.likeAction(props.answerId);
+            }}
+          />
+          <Span />
+        </Label>
+        <Count>{props.totalLikes}</Count>
+      </div>
     </React.Fragment>
   );
 }
 
 DiverstLike.propTypes = {
+  likes: PropTypes.object,
+  totalLikes: PropTypes.number,
+  isLiked: PropTypes.bool,
   newsFeedLinkId: PropTypes.number,
+  answerId: PropTypes.number,
   likeAction: PropTypes.func.isRequired,
   unlike: PropTypes.func.isRequired,
 };
