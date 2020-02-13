b94350aa3d2d15c14297936e10058c760ff6e741
Mark Roy <markr4848@gmail.com>
2020-02-13 12:22:45 -0500

    Remove unnecessary attributes from JWT payload in UserTokenService

--- app/services/user_token_service.rb
+++ app/services/user_token_service.rb
@@ -7,15 +7,9 @@ class UserTokenService
 
   def self.create_jwt(user, params = {})
     token = user.generate_authentication_token
+
     payload = {
-        id: user.id,
-        enterprise: AuthenticatedEnterpriseSerializer.new(user.enterprise).as_json,
-        policy_group: PolicyGroupSerializer.new(user.policy_group).as_json,
-        email: user.email,
         user_token: token,
-        role: user.user_role.role_name,
-        time_zone: ActiveSupport::TimeZone.find_tzinfo(user.time_zone).name,
-        created_at: user.created_at.as_json,
         time: (Time.now.to_f * 1000).to_i + 5000
     }
 
