b13777950dca1b9e5282bbc37a9992605d8fd9f4
Mark Roy <markr4848@gmail.com>
2020-02-13 12:23:48 -0500

    On session create return an object containing payload data, including the JWT

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,7 +3,17 @@ class Api::V1::SessionsController < DiverstController
 
   def create
     user = User.signin(params[:email], params[:password])
-    render status: 200, json: { token: UserTokenService.create_jwt(user, params) }
+
+    render status: 200, json: {
+      token: UserTokenService.create_jwt(user, params),
+      user_id: user.id,
+      enterprise: AuthenticatedEnterpriseSerializer.new(user.enterprise).as_json,
+      policy_group: PolicyGroupSerializer.new(user.policy_group).as_json,
+      email: user.email,
+      role: user.user_role.role_name,
+      time_zone: ActiveSupport::TimeZone.find_tzinfo(user.time_zone).name,
+      created_at: user.created_at.as_json,
+    }
   rescue => e
     raise BadRequestException.new(e.message)
   end
