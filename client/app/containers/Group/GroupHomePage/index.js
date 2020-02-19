6dd2a34c611dffbca00612ba9009244b27df036e
liana <provlia1@videotron.ca>
2020-02-19 14:36:38 -0500

    Add News and events layout to home page

--- client/app/containers/Group/GroupHomePage/index.js
+++ client/app/containers/Group/GroupHomePage/index.js
@@ -10,13 +10,39 @@ import reducer from 'containers/Group/reducer';
 import saga from 'containers/Group/saga';
 
 import GroupHome from 'components/Group/GroupHome';
+import { Divider, Grid } from '@material-ui/core';
+import DiverstFormattedMessage from 'components/Shared/DiverstFormattedMessage';
+
+// TODO : Change EventsPage
+import EventsPage from '../../User/UserEventsPage';
+import NewsPage from '../GroupNewsFeedPage';
 
 export function GroupHomePage(props) {
   useInjectReducer({ key: 'groups', reducer });
   useInjectSaga({ key: 'groups', saga });
 
   return (
-    <GroupHome group={props.currentGroup} />
+    <React.Fragment>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <EventsPage
+            loaderProps={{
+              transitionProps: {
+                direction: 'right',
+              },
+            }}
+          />
+        </Grid>
+        <Grid item xs='auto'>
+          <Divider orientation='vertical' />
+        </Grid>
+        <Grid item xs>
+          <NewsPage
+            currentGroup={props.currentGroup}
+          />
+        </Grid>
+      </Grid>
+    </React.Fragment>
   );
 }
 
