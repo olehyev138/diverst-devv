b299e13102b59fe7959b89ebfa01bfa442082516
Derek Owusu-Frimpong <deepsky_5@live.com>
2020-02-19 17:04:04 +0000

    modal pop up for onboarding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -184,7 +184,9 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    prev_url = session[:previous_url]
+    if session[:previous_url] == "http://localhost:3000/favicon.ico"
+      prev_url = session[:previous_url] = user_root_path 
+    end
 
     # This ensures unauthorized users are not accessing main page, which is admin only
     # This also ensures we don't get stuck with invitation as our previous url. Otherwise it redirects to non-existent page
