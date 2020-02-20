f4a2071ddc5182324b0ff8f7ac9a1ac755652788
Mark Roy <markr4848@gmail.com>
2020-02-19 20:14:08 -0500

    Cleanup unnecessary login page leftovers in forgot password page

--- client/app/containers/Session/ForgotPasswordPage/index.js
+++ client/app/containers/Session/ForgotPasswordPage/index.js
@@ -9,18 +9,13 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
-import { push } from 'connected-react-router';
 
 import { useInjectReducer } from 'utils/injectReducer';
 
 import reducer from './reducer';
 
-import { CircularProgress, Backdrop } from '@material-ui/core';
+import { loginBegin, ssoLoginBegin, ssoLinkBegin } from 'containers/Shared/App/actions';
 
-import { showSnackbar } from 'containers/Shared/Notifier/actions';
-import { loginBegin, findEnterpriseBegin, ssoLoginBegin, ssoLinkBegin } from 'containers/Shared/App/actions';
-
-import { selectEnterprise } from 'containers/Shared/App/selectors';
 import { selectIsLoggingIn, selectLoginSuccess } from './selectors';
 
 import ForgotPasswordForm from 'components/Session/ForgotPasswordForm';
@@ -31,45 +26,20 @@ export function ForgotPasswordPage(props) {
   const [email, setEmail] = useState('');
 
   return (
-    <React.Fragment>
-      <ForgotPasswordForm
-        email={email}
-      />
-    </React.Fragment>
+    <ForgotPasswordForm
+      email={email}
+    />
   );
 }
 
 ForgotPasswordPage.propTypes = {
-  enterprise: PropTypes.object,
-  location: PropTypes.shape({
-    search: PropTypes.string
-  }),
-  showSnackbar: PropTypes.func,
-  refresh: PropTypes.func,
-  loginBegin: PropTypes.func,
-  ssoLoginBegin: PropTypes.func,
-  ssoLinkBegin: PropTypes.func,
-  findEnterpriseBegin: PropTypes.func,
-  isLoggingIn: PropTypes.bool,
-  loginSuccess: PropTypes.bool,
 };
 
 const mapStateToProps = createStructuredSelector({
-  enterprise: selectEnterprise(),
-  isLoggingIn: selectIsLoggingIn(),
-  loginSuccess: selectLoginSuccess(),
 });
 
-function mapDispatchToProps(dispatch) {
-  return {
-    showSnackbar: payload => dispatch(showSnackbar({ message: payload })),
-    refresh: payload => dispatch(push(payload)),
-    loginBegin: payload => dispatch(loginBegin(payload)),
-    ssoLoginBegin: payload => dispatch(ssoLoginBegin(payload)),
-    ssoLinkBegin: payload => dispatch(ssoLinkBegin(payload)),
-    findEnterpriseBegin: payload => dispatch(findEnterpriseBegin(payload)),
-  };
-}
+const mapDispatchToProps = {
+};
 
 const withConnect = connect(
   mapStateToProps,
