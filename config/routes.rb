9b66f0f5910a9630537cb0fab291958a1f6075ba
Alex Oxorn <alex.oxorn@gmail.com>
2020-02-21 16:40:21 -0500

    remove testing controller

--- config/routes.rb
+++ config/routes.rb
@@ -54,15 +54,6 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :outlook, only: [:index] do
-    collection do
-      get 'mail', to: 'outlook#mail'
-      get 'calendar', to: 'outlook#calendar'
-      get 'contacts', to: 'outlook#contacts'
-      post 'add_event', to: 'outlook#add_event'
-    end
-  end
-
   resources :user_roles
   resources :users do
     member do
--- spec/controllers/outlook_controller_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe OutlookController, type: :controller do
-end
