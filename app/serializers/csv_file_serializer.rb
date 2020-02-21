5a571c56bd98012923a3ef5fddf58464059f3531
Alex Oxorn <alex.oxorn@gmail.com>
2020-02-21 10:34:51 -0500

    return old serialized fields

--- app/serializers/csv_file_serializer.rb
+++ app/serializers/csv_file_serializer.rb
@@ -1,7 +1,8 @@
 class CsvFileSerializer < ApplicationRecordSerializer
   attributes :id, :created_at,
              :download_file, :download_file_file_name, :download_file_data,
-             :import_file, :import_file_file_name, :import_file_data
+             :import_file, :import_file_file_name, :import_file_data,
+             :file_name, :download_file_path
 
   def download_file
     AttachmentHelper.attachment_signed_id(object.download_file)
@@ -13,6 +14,14 @@ class CsvFileSerializer < ApplicationRecordSerializer
 
   def download_file_data
     AttachmentHelper.attachment_data_string(object.download_file)
+  end
+
+  def download_file_path
+    object.path_for_download
+  end
+
+  def file_name
+    object.download_file_name + object.download_file.filename.extension_with_delimiter
   end
 
   def import_file
