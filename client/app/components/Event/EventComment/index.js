4fe4ba6e580051834a7aaeef9110d8c71f507ae6
devst05 <1726271@johnabbottcollege.net>
2020-03-02 16:33:13 -0500

    event comment initial

---
+++ client/app/components/Event/EventComment/index.js
@@ -0,0 +1,56 @@
+/**
+ *
+ * Event Comment Component
+ *
+ */
+
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+import { compose } from 'redux/';
+
+import { Button, Card, CardActions, CardContent, Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  margin: {
+    marginTop: 16,
+    marginBottom: 16,
+  }
+});
+
+export function EventComment(props) {
+  const { classes, comment, newsItem } = props;
+
+  return (
+    <Card className={classes.margin}>
+      <CardContent>
+        <Typography variant='body1'>{comment.content}</Typography>
+        <Typography variant='body2'>{comment.author.first_name}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button
+          size='small'
+          onClick={() => {
+            /* eslint-disable-next-line no-alert, no-restricted-globals */
+            if (confirm('Delete group message?'))
+              props.deleteGroupMessageCommentBegin({ group_id: newsItem.group_message.group_id, id: comment.id });
+          }}
+        >
+          Delete
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+EventComment.propTypes = {
+  classes: PropTypes.object,
+  comment: PropTypes.object,
+  deleteGroupMessageCommentBegin: PropTypes.func,
+  newsItem: PropTypes.object,
+};
+
+export default compose(
+  memo,
+  withStyles(styles)
+)(EventComment);
---
+++ client/app/components/Event/EventCommentForm/index.js
@@ -0,0 +1,116 @@
+/**
+ *
+ * Event Comment Form Component
+ *
+ */
+
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import dig from 'object-dig';
+
+import DiverstFormattedMessage from 'components/Shared/DiverstFormattedMessage';
+import { Field, Formik, Form } from 'formik';
+import {
+  Button, Card, CardActions, CardContent, CardHeader, TextField, Typography, Divider,
+} from '@material-ui/core';
+import withStyles from '@material-ui/core/styles/withStyles';
+
+import messages from 'containers/News/messages';
+import DiverstSubmit from 'components/Shared/DiverstSubmit';
+
+const styles = theme => ({
+  formTitle: {
+    fontSize: 18,
+  },
+
+});
+
+/* eslint-disable object-curly-newline */
+export function EventCommentFormInner({ classes, handleSubmit, handleChange, handleBlur, values, setFieldValue, setFieldTouched, ...props }) {
+  return (
+    <Card>
+      <Form>
+        <CardContent>
+          <Typography
+            paragraph
+          >
+            Leave a Comment
+          </Typography>
+          <Field
+            component={TextField}
+            onChange={handleChange}
+            fullWidth
+            disabled={props.isCommitting}
+            id='content'
+            name='content'
+            variant='outlined'
+            value={values.content}
+            label={<DiverstFormattedMessage {...messages.content} />}
+          />
+        </CardContent>
+        <Divider />
+        <CardActions>
+          <DiverstSubmit isCommitting={props.isCommitting}>
+            <DiverstFormattedMessage {...messages.comment_submit} />
+          </DiverstSubmit>
+        </CardActions>
+      </Form>
+    </Card>
+  );
+}
+
+export function EventCommentForm(props) {
+  // No comment editing
+
+  const initialValues = {
+    author_id: dig(props, 'currentUserId') || undefined,
+    message_id: dig(props, 'newsItem', 'group_message', 'id') || undefined,
+    content: '',
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      enableReinitialize
+      onSubmit={(values, actions) => {
+        // pass news_feed_link_id to saga to refetch news_feed_link with new comment
+        props.commentAction({
+          news_feed_link_id: dig(props, 'newsItem', 'id') || undefined,
+          attributes: values
+        });
+
+        actions.resetForm();
+      }}
+    >
+      {formikProps => <EventCommentFormInner {...props} {...formikProps} />}
+    </Formik>
+  );
+}
+
+EventCommentForm.propTypes = {
+  classes: PropTypes.object,
+  commentAction: PropTypes.func,
+  newsItem: PropTypes.object,
+  currentUserId: PropTypes.number,
+  isCommitting: PropTypes.bool,
+};
+
+EventCommentFormInner.propTypes = {
+  classes: PropTypes.object,
+  handleSubmit: PropTypes.func,
+  handleChange: PropTypes.func,
+  handleBlur: PropTypes.func,
+  values: PropTypes.object,
+  setFieldValue: PropTypes.func,
+  setFieldTouched: PropTypes.func,
+  isCommitting: PropTypes.bool,
+  links: PropTypes.shape({
+    newsFeedIndex: PropTypes.string
+  })
+};
+
+export default compose(
+  memo,
+  withStyles(styles)
+)(EventCommentForm);
