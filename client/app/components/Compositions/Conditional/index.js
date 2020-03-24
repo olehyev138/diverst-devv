57ac11a0e9c7816ff5437dedce6ba83d66c7e971
AlexOxorn <alex.oxorn@gmail.com>
2020-03-24 15:51:58 -0400

    refactor conditional

--- client/app/components/Compositions/Conditional/index.js
+++ client/app/components/Compositions/Conditional/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { push } from 'connected-react-router';
 import dig from 'object-dig';
 import NotAuthorizedPage from 'containers/Shared/NotAuthorizedPage';
 
 import PropTypes from 'prop-types';
+import { showSnackbar } from 'containers/Shared/Notifier/actions';
+import RouteService from 'utils/routeHelpers';
 
 function conditionalCheck(props, condition) {
   const parts = condition.split('.');
@@ -18,17 +22,31 @@ function valid(props, conditions, reducer) {
   return reducer(conditionsMapper(props, conditions));
 }
 
-export default function Conditional(Component, conditions, reducer = a => a.reduce((sum, v) => sum || v, false)) {
-  const WrappedComponent = props => valid(props, conditions, reducer)
-    ? <Component {...props} />
-    : <NotAuthorizedPage />;
+export default function Conditional(
+  Component,
+  conditions,
+  redirect = null,
+  message = 'You don\'t have permission to view this page',
+  reducer = a => a.reduce((sum, v) => sum || v, false)
+) {
+  const WrappedComponent = (props) => {
+    if (valid(props, conditions, reducer))
+      return <Component {...props} />;
+    if (redirect) {
+      const rs = new RouteService(useContext);
+      props.dispatch(showSnackbar({ message, options: { variant: 'warning' } }));
+      props.dispatch(push(redirect(props, rs)));
+      return <React.Fragment />;
+    }
+
+    return <NotAuthorizedPage />;
+  };
 
   WrappedComponent.propTypes = {
     show: PropTypes.bool,
     valid: PropTypes.func,
-    predicate: PropTypes.func,
+    dispatch: PropTypes.func,
   };
 
-  return compose(
-  )(WrappedComponent);
+  return connect()(WrappedComponent);
 }
