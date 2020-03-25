465ebdb62c1ef9f6855a950a798bd0f65720727a
Matt <mattb1397@protonmail.com>
2020-03-25 14:14:02 -0400

    Comment out options added to paperclip config

--- config/initializers/paperclip_attachment.rb
+++ config/initializers/paperclip_attachment.rb
@@ -9,5 +9,5 @@ Paperclip::Attachment.class_eval do
 end
 
 #Paperclip::Attachment.default_options[:url] = ":s3_domain_url"
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-east-1.amazonaws.com'
+#Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-east-1.amazonaws.com'
 
