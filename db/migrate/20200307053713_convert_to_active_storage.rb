ab1410b1c3252ec7c56998e4c11376c1bbf7cf2d
Matt <mattb1397@protonmail.com>
2020-03-25 14:46:23 -0400

    Change paperclip column regex to use content_type to avoid conflicts

--- db/migrate/20200307053713_convert_to_active_storage.rb
+++ db/migrate/20200307053713_convert_to_active_storage.rb
@@ -29,7 +29,7 @@ class ConvertToActiveStorage < ActiveRecord::Migration[5.2]
     transaction do
       models.each do |model|
         attachments = model.column_names.map do |c|
-          if c =~ /(.+)_file_name$/
+          if c =~ /(.+)_content_type$/
             $1
           end
         end.compact
