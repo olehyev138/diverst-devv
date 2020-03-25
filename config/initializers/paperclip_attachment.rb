e14c07ab3c8b7fb1db84975536925a2519f31029
Matt <mattb1397@protonmail.com>
2020-03-25 14:08:16 -0400

    Remove paperclip url option

--- config/initializers/paperclip_attachment.rb
+++ config/initializers/paperclip_attachment.rb
@@ -8,6 +8,6 @@ Paperclip::Attachment.class_eval do
   end
 end
 
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+#Paperclip::Attachment.default_options[:url] = ":s3_domain_url"
 Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-east-1.amazonaws.com'
 
