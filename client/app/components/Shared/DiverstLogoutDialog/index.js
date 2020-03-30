bcfa0c0fc3722a94fb2e6b6ad2c59ef50b32bee3
liana <provlia1@videotron.ca>
2020-03-30 16:32:39 -0400

    Logout from customText

--- client/app/components/Shared/DiverstLogoutDialog/index.js
+++ client/app/components/Shared/DiverstLogoutDialog/index.js
@@ -1,6 +1,7 @@
 import React, { memo } from 'react';
 import { compose } from 'redux';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -12,9 +13,12 @@ import Button from '@material-ui/core/Button';
 import messages from 'components/Shared/DiverstLogoutDialog/messages';
 import DiverstFormattedMessage from 'components/Shared/DiverstFormattedMessage';
 
+import { logoutBegin } from 'containers/Shared/App/actions';
+import { createStructuredSelector } from 'reselect';
+
 // This component is intended for creating a custom popup with a message and logout option
 
-export function DiverstLogoutDialog(props) {
+export function DiverstLogoutDialog(props){
   const { title, message, open, handleClose } = props;
 
   return (
@@ -23,20 +27,32 @@ export function DiverstLogoutDialog(props) {
         <DialogTitle>{ title }</DialogTitle>
         <DialogActions>
           <Button color='primary' onClick={handleClose}><DiverstFormattedMessage {...messages.later} /></Button>
-          <Button color='primary'><DiverstFormattedMessage {...messages.logout} /></Button>
+          <Button color='primary' onClick={props.logoutBegin}><DiverstFormattedMessage {...messages.logout} /></Button>
         </DialogActions>
       </Dialog>
     </React.Fragment>
   );
 }
 
+
+const mapDispatchToProps = {
+  logoutBegin
+};
+
 DiverstLogoutDialog.propTypes = {
   title: PropTypes.string,
   message: PropTypes.string,
   open: PropTypes.bool,
   handleClose: PropTypes.func,
+  logoutBegin: PropTypes.func,
 };
+
+const withConnect = connect(
+  createStructuredSelector({}),
+  mapDispatchToProps,
+);
 
 export default compose(
   memo,
+  withConnect,
 )(DiverstLogoutDialog);
