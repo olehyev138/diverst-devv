60e7d9a02fe9fcc0d575d10c771ba9e5005a6218
Mark Roy <markr4848@gmail.com>
2020-04-02 23:31:28 -0400

    Add file attributes to resource serializer

--- app/serializers/resource_serializer.rb
+++ app/serializers/resource_serializer.rb
@@ -1,7 +1,21 @@
 class ResourceSerializer < ApplicationRecordSerializer
-  attributes :enterprise, :folder, :initiative, :group, :owner, :mentoring_session, :file_location, :permissions
+  attributes :enterprise, :folder, :initiative, :group, :owner, :mentoring_session, :file_location, :permissions,
+             :file, :file_file_name, :file_data
 
   def serialize_all_fields
     true
+  end
+
+  # File
+  def file
+    AttachmentHelper.attachment_signed_id(object.file)
+  end
+
+  def file_file_name
+    AttachmentHelper.attachment_file_name(object.file)
+  end
+
+  def file_data
+    AttachmentHelper.attachment_data_string(object.file)
   end
 end
