f5b426af59acaeb1928669b7082375c6a953d23c
devst05 <1726271@johnabbottcollege.net>
2020-04-07 15:32:30 -0400

    fixed date format

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -12,6 +12,6 @@ class Activity < PublicActivity::Activity
   belongs_to :user, -> { where(activities: { owner_type: 'User' }).includes(:activities) }, foreign_key: :owner_id
 
   scope :for_group_ids, -> (group_ids) { where('activities.owner_id' => group_ids).distinct if group_ids.any? }
-  scope :joined_from, -> (from) { where('activities.created_at >= ?', from) }
-  scope :joined_to, -> (to) { where('activities.created_at <= ?', to) }
+  scope :joined_from, -> (from) { where('DATE_FORMAT(activities.created_at, '"'%Y-%m-%d'"') >= ?', from.to_time.strftime('%Y-%m-%d')) }
+  scope :joined_to, -> (to) { where('DATE_FORMAT(activities.created_at, '"'%Y-%m-%d'"') <= ?', to.to_time.strftime('%Y-%m-%d')) }
 end
