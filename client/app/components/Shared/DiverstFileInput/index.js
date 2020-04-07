e7aea9ed627049b3084c63f8b83e5515e37f6bf5
Mark Roy <markr4848@gmail.com>
2020-04-07 02:09:46 -0400

    Update file info box styling

--- client/app/components/Shared/DiverstFileInput/index.js
+++ client/app/components/Shared/DiverstFileInput/index.js
@@ -45,8 +45,8 @@ const styles = theme => ({
     borderBottom: 'none !important',
     paddingTop: 0,
     paddingBottom: 0,
-    paddingLeft: 16,
-    paddingRight: 16,
+    paddingLeft: 12,
+    paddingRight: 12,
     boxShadow: '0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12)',
     '& *': {
       lineHeight: 1.75,
