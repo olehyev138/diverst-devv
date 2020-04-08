17c8f3902e0100b0ba09a57468b217f77de5d98d
devst05 <1726271@johnabbottcollege.net>
2020-04-08 09:08:46 -0400

    changed to date type when comparison

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -11,7 +11,9 @@ class Activity < PublicActivity::Activity
 
   belongs_to :user, -> { where(activities: { owner_type: 'User' }).includes(:activities) }, foreign_key: :owner_id
 
+  # need to be revised for group filter
   scope :for_group_ids, -> (group_ids) { where('activities.owner_id' => group_ids).distinct if group_ids.any? }
-  scope :joined_from, -> (from) { where('DATE_FORMAT(activities.created_at, '"'%Y-%m-%d'"') >= ?', from.to_time.strftime('%Y-%m-%d')) }
-  scope :joined_to, -> (to) { where('DATE_FORMAT(activities.created_at, '"'%Y-%m-%d'"') <= ?', to.to_time.strftime('%Y-%m-%d')) }
+  # filter for date
+  scope :joined_from, -> (from) { where('DATE(activities.created_at) >= ?', from.to_date) }
+  scope :joined_to, -> (to) { where('DATE(activities.created_at) <= ?', to.to_date) }
 end
