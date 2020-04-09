dd46299328b8cb8caa0e8c9fd9e4bf917bde554b
devst05 <1726271@johnabbottcollege.net>
2020-04-09 08:48:42 -0400

    edited group filter scope(if it means search the logs of the users who are in selected groups)

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -11,8 +11,8 @@ class Activity < PublicActivity::Activity
 
   belongs_to :user, -> { where(activities: { owner_type: 'User' }).includes(:activities) }, foreign_key: :owner_id
 
-  # need to be revised for group filter
-  scope :for_group_ids, -> (group_ids) { where('activities.owner_id' => group_ids).distinct if group_ids.any? }
+  # filter for group (the logs of the users who are in selected groups)
+  scope :for_group_ids, -> (group_ids) { where('activities.owner_id' => (UserGroup.where('group_id' => group_ids)).map { |p| p.user_id }).distinct if group_ids.any? }
   # filter for date
   scope :joined_from, -> (from) { where('DATE(activities.created_at) >= ?', from.to_date) }
   scope :joined_to, -> (to) { where('DATE(activities.created_at) <= ?', to.to_date) }
