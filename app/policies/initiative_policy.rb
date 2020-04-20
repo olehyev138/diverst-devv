3a085115da688d360c222de0dee6c65866d686a8
AlexOxorn <alex.oxorn@gmail.com>
2020-04-20 13:57:19 -0400

    alternate way to define group in initiative_policy.rb

--- app/policies/initiative_policy.rb
+++ app/policies/initiative_policy.rb
@@ -1,6 +1,10 @@
 class InitiativePolicy < GroupBasePolicy
-  def initialize(user, record, params = {})
-    super(user, [Group.find_by_id(params[:owner_group_id]), record], params)
+  def initialize(user, context, params = {})
+    if context.is_a?(Class)
+      super(user, [Group.find_by_id(params[:owner_group_id]), context], params)
+    else
+      super(user, context, params)
+    end
   end
 
   def base_index_permission
