aa5447c4c829fa4602f6904816327e27779d9ad4
devst05 <1726271@johnabbottcollege.net>
2020-04-20 08:35:47 -0400

    removed attibutes :author/:user in serializers

--- app/serializers/initiative_comment_serializer.rb
+++ app/serializers/initiative_comment_serializer.rb
@@ -1,5 +1,5 @@
 class InitiativeCommentSerializer < ApplicationRecordSerializer
-  attributes :user, :initiative_id, :user_name, :time_since_creation, :permissions
+  attributes :initiative_id, :user_name, :time_since_creation, :permissions
 
   has_one :user
   def serialize_all_fields
--- app/serializers/news_link_comment_serializer.rb
+++ app/serializers/news_link_comment_serializer.rb
@@ -1,6 +1,4 @@
 class NewsLinkCommentSerializer < ApplicationRecordSerializer
-  attributes :author
-
   has_one :author
   def serialize_all_fields
     true
