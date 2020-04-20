0209c8e7dc6b20007f3593cfd9a90798954a353c
liana <provlia1@videotron.ca>
2020-04-20 10:46:04 -0400

    resolved changes

--- client/app/components/Branding/Sponsor/SponsorCard/index.js
+++ client/app/components/Branding/Sponsor/SponsorCard/index.js
@@ -29,16 +29,17 @@ import reducer from 'containers/Shared/Sponsors/reducer';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+
+const SponsorType = Object.freeze({
+  Group: 'group',
+  Enterprise: 'enterprise',
+});
+
 export function SponsorCard(props) {
   useInjectReducer({ key: 'sponsors', reducer });
   useInjectSaga({ key: 'sponsors', saga });
 
-  const SponsorType = Object.freeze({
-    Group: 'group',
-    Enterprise: 'enterprise',
-  });
-
-  const sponsorType = {
+  const sponsorParams = {
     orderBy: '', order: 'asc', query_scopes: ['group_sponsor'], sponsorable_id: props.currentGroup ? props.currentGroup.id : 0
   };
   const enterpriseParams = {
@@ -53,50 +54,49 @@ export function SponsorCard(props) {
     else
       props.getSponsorsBegin(enterpriseParams);
   }, []);
-  return (
-    // eslint-disable-next-line no-nested-ternary
-    sponsorList[0] ? (
-      props.sponsorTotal > 1 ? (
-        <Carousel
-          autoPlay={false}
-        >
-          {sponsorList.map(sponsor => (
-            <Card key={sponsor.id}>
-              <CardContent>
-                <Typography variant='h6'>
-                  { sponsor.sponsor_name}
-                </Typography>
-                <Typography>
-                  { sponsor.sponsor_title}
-                </Typography>
-              </CardContent>
-              <CardContent>
-                { sponsor.sponsor_message}
-              </CardContent>
-            </Card>
-          ))}
-        </Carousel>
-      )
-        : (
-          <Card>
+
+  if (props.sponsorTotal > 1)
+    return (
+      <Carousel
+        autoPlay={false}
+      >
+        {sponsorList.map(sponsor => (
+          <Card key={sponsor.id}>
             <CardContent>
               <Typography variant='h6'>
-                { sponsorList[0].sponsor_name}
+                { sponsor.sponsor_name}
               </Typography>
               <Typography>
-                { sponsorList[0].sponsor_title}
+                { sponsor.sponsor_title}
               </Typography>
             </CardContent>
             <CardContent>
-              { sponsorList[0].sponsor_message}
+              { sponsor.sponsor_message}
             </CardContent>
           </Card>
-        )
-    ) : (
-      <React.Fragment>
+        ))}
+      </Carousel>
+    );
+  if (sponsorList[0])
+    return (
+      <Card>
+        <CardContent>
+          <Typography variant='h6'>
+            { sponsorList[0].sponsor_name}
+          </Typography>
+          <Typography>
+            { sponsorList[0].sponsor_title}
+          </Typography>
+        </CardContent>
+        <CardContent>
+          { sponsorList[0].sponsor_message}
+        </CardContent>
+      </Card>
+    );
+  return (
+    <React.Fragment>
 
-      </React.Fragment>
-    )
+    </React.Fragment>
   );
 }
 
--- client/app/components/Branding/Sponsor/SponsorForm/index.js
+++ client/app/components/Branding/Sponsor/SponsorForm/index.js
@@ -48,7 +48,7 @@ export function SponsorFormInner({ classes, handleSubmit, handleChange, handleBl
                 id='sponsor_name'
                 name='sponsor_name'
                 margin='normal'
-                label={<DiverstFormattedMessage {...messages.Sponsors.sname} />}
+                label={<DiverstFormattedMessage {...messages.Sponsors.name} />}
                 value={values.sponsor_name}
               />
             </Grid>
@@ -61,7 +61,7 @@ export function SponsorFormInner({ classes, handleSubmit, handleChange, handleBl
                 id='sponsor_title'
                 name='sponsor_title'
                 margin='normal'
-                label={<DiverstFormattedMessage {...messages.Sponsors.stitle} />}
+                label={<DiverstFormattedMessage {...messages.Sponsors.title} />}
                 value={values.sponsor_title}
               />
             </Grid>
@@ -73,7 +73,7 @@ export function SponsorFormInner({ classes, handleSubmit, handleChange, handleBl
                 id='sponsor_message'
                 name='sponsor_messsage'
                 margin='normal'
-                label={<DiverstFormattedMessage {...messages.Sponsors.smessage} />}
+                label={<DiverstFormattedMessage {...messages.Sponsors.message} />}
                 value={values.sponsor_message}
               />
             </Grid>
