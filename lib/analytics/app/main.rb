baac0efadc380517008f96a94d247e7ebdf92db4
Matt <mattb1397@protonmail.com>
2020-04-20 23:21:57 -0400

    Fix up analytics lambda function, add news posts per group query

--- lib/analytics/app/main.rb
+++ lib/analytics/app/main.rb
@@ -2,39 +2,44 @@ require 'mysql2'
 require 'aws-sdk-s3'
 require 'json'
 
-def main(event:, context:)
-  dbh = Mysql2::Client.new(
+BUCKET_NAME = 'diverst-analytics'
+$data_bucket = Aws::S3::Resource.new(region: ENV['AWS_DEFAULT_REGION']).bucket(BUCKET_NAME)
+
+# TODO: env variables & secrets management
+$dbh = Mysql2::Client.new(
     :host => "devops-db.chzb878zcht6.us-east-1.rds.amazonaws.com",
     :database=> "diverst",
     :username => "mainuser",
     :password=> "password123",
     :port => 3306)
 
-  s3 = Aws::S3::Resource.new(region: ENV['AWS_DEFAULT_REGION'])
-  bucket_name = 'diverst-analytics'
-  data_bucket = s3.bucket(bucket_name)
-
+def main(event:, context:)
   graph_data = {
-      group_population: group_population(dbh).to_json,
-      group_growth: group_growth(dbh).to_json
+      group_population: group_population($dbh).to_json,
+      group_growth: group_growth($dbh).to_json,
+      news_posts_per_group: news_posts_per_group($dbh).to_json
   }
 
-  # Create s3 object per graph
-  graph_data.each do |graph, data|
-    data_bucket.object(graph.to_s).put(body: data, content_type: 'application/json')
-  end
+  upload_s3(graph_data)
 end
 
+def upload_s3(graph_data)
+  # Create s3 object per graph
+  graph_data.each do |graph, data|
+    $data_bucket.object(graph.to_s).put(body: data, content_type: 'application/json')
+  end
+end
 
 # Graph functions
 
 def group_population(dbh)
   # Retrieve membership count for each group
+  #  - left join such that we get groups with no members
 
   sql = %{
     SELECT name, count(u.group_id) AS count
     FROM groups AS g
-    LEFT OUTER JOIN user_groups AS u on u.group_id = g.id
+      LEFT JOIN user_groups AS u on u.group_id = g.id
     WHERE enterprise_id = 1
     GROUP BY g.id;
   }
@@ -51,6 +56,22 @@ def group_growth(dbh)
            COUNT(*) OVER(PARTITION by g.id ORDER BY DATE_FORMAT(u.created_at, '%Y-%m-%d')) count
     FROM user_groups u
     INNER JOIN groups g on g.id = u.group_id
+  }
+
+  dbh.query(sql).to_a
+end
+
+def news_posts_per_group(dbh)
+  # Retrieve total news posts (social, links, messages) per group
+  #  - Treats shared posts as any other
+
+  sql = %{
+    SELECT g.name, count(nl.id) AS count
+    FROM groups g
+      JOIN news_feeds AS nf on nf.group_id = g.id
+      LEFT JOIN news_feed_links nl on nl.news_feed_id = nf.id
+    WHERE enterprise_id = 1
+    GROUP BY nf.id;
   }
 
   dbh.query(sql).to_a
