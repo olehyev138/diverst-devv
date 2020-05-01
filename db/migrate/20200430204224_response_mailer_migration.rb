c1343a9e42ced82b77235c0ac382e9902c75fb2d
Derek Owusu-Frimpong <deepsky_5@live.com>
2020-05-01 00:55:46 +0000

    add default text for survey response mailer content

--- db/migrate/20200430204224_response_mailer_migration.rb
+++ db/migrate/20200430204224_response_mailer_migration.rb
@@ -6,13 +6,13 @@ class ResponseMailerMigration < ActiveRecord::Migration
       survey_response_email.update(:enterprise => enterprise,
         :mailer_name => 'poll_mailer',
         :mailer_method => 'survey_response_notification',
-        :content => "<p>Hello %{user.name},</p>\r\n\r\n<p>Your response to the survey has been received.",
-        :subject => "Acknowledgement of Receipt of Response to Survey",
+        :content => "<p>Hello %{user.name},</p>\r\n\r\n<p>This is to confirm that your response to our survey has been received. Thank you for your participtaion!</p>",
+        :subject => "Thank you for participating in our '%(survey.title) survey",
         :description => "Email that goes out to users after they have submitted a response to a survey.",
         :template => "")
 
     
-      enterprise.email_variables.where(key: ["user.name"]).each do |email_variable|
+      enterprise.email_variables.where(key: ["user.name", "survey.title", "enterprise.name"]).each do |email_variable|
         email_variable.emails << survey_response_email
       end
     end
