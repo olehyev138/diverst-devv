cd3c1ae93183a163fa5ea6084c7b7a5df925d1d8
Derek Owusu-Frimpong <deepsky_5@live.com>
2020-05-04 13:00:32 +0000

    re-write target for poll response notifier

--- app/services/notifiers/response_notifier.rb
+++ app/services/notifiers/response_notifier.rb
@@ -7,20 +7,6 @@ class Notifiers::ResponseNotifier
   end
 
   def notify!
-    targeted_users.find_each(batch_size: 100) do |user|
-      PollResponseMailer.notification(@response, user).deliver_later
-    end
-  end
-
-  private
-
-  def targeted_users
-    users = @poll.targeted_users
-    users = filter_by_initiative(users) if @initiative
-    User.where(id: users.map(&:id))
-  end
-
-  def filter_by_initiative(users)
-    users.select { |u| u.initiatives.where(id: @initiative.id).any? }
+    PollResponseMailer.notification(@response, @response.user)
   end
 end
