59aabab8ddd9039e0c2d267101fd3b55f964cfcc
Derek Owusu-Frimpong <deepsky_5@live.com>
2020-05-04 13:16:48 +0000

    modify response_notifier

--- app/services/notifiers/response_notifier.rb
+++ app/services/notifiers/response_notifier.rb
@@ -3,10 +3,9 @@ class Notifiers::ResponseNotifier
     @response = response
     @user = User.find_by(response.user_id)
     @poll = Poll.find_by(response.poll_id)
-    @initiative = @poll.initiative
   end
 
   def notify!
-    PollResponseMailer.notification(@response, @response.user)
+    PollResponseMailer.notification(@response, @user)
   end
 end
