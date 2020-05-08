4f9ddd3501ae6dd7f76b3223bde9c5444cafa873
Matt <mattb1397@protonmail.com>
2020-05-08 17:08:54 -0400

    Fix group dashboard metrics

--- client/app/components/Analyze/Metrics/GroupOverviewMetrics/index.js
+++ client/app/components/Analyze/Metrics/GroupOverviewMetrics/index.js
@@ -2,20 +2,34 @@ import React, { memo, useEffect, useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import { compose } from 'redux';
 
-import { Paper, Typography, Grid } from '@material-ui/core';
+import { Paper, Box, CardContent, Typography, Grid } from '@material-ui/core';
 
 export function GroupOverviewMetrics({ data }) {
   return (
     <React.Fragment>
       <Grid container spacing={2}>
-        <Grid item xs={6} >
+        <Grid item xs={6}>
           <Paper>
-            <Typography variant='h4'>Total groups: {data.total_groups}</Typography>
+            <CardContent>
+              <Typography display='inline' variant='h6' color='primary'>
+                Total groups: &nbsp;
+              </Typography>
+              <Typography display='inline' variant='h6'>
+                {data.total_groups}
+              </Typography>
+            </CardContent>
           </Paper>
         </Grid>
         <Grid item xs={6}>
           <Paper>
-            <Typography variant='h4'>Average members: {data.avg_nb_members_per_group}</Typography>
+            <CardContent>
+              <Typography display='inline' variant='h6' color='primary'>
+                Average members: &nbsp;
+              </Typography>
+              <Typography display='inline' variant='h6'>
+                {data.avg_nb_members_per_group}
+              </Typography>
+            </CardContent>
           </Paper>
         </Grid>
       </Grid>
