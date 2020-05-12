a2beff66ea341d9ebcc639f03ffa6500a5b77d25
Matt <mattb1397@protonmail.com>
2020-05-11 20:54:19 -0400

    Add check for aws env vars before defining s3 bucket var

--- app/models/concerns/metrics.rb
+++ app/models/concerns/metrics.rb
@@ -8,27 +8,28 @@ module Metrics
   end
 
   module ClassMethods
-    @@analytics_bucket = Aws::S3::Resource::new(region: 'us-east-1')
-                           .bucket("#{ENV['ENV_NAME']}-diverst-analytics")
+    @@analytics_bucket = ENV['AWS_ACCESS_KEY_ID'] ?
+        Aws::S3::Resource::new(region: 'us-east-1')
+                           .bucket("#{ENV['ENV_NAME']}-diverst-analytics") : nil
 
     #
     ## Group metrics
     #
 
     def group_population
-      @@analytics_bucket.object('group_population').get.body.read
+      @@analytics_bucket&.object('group_population').get.body.read
     end
 
     def growth_of_groups
-      @@analytics_bucket.object('group_growth').get.body.read
+      @@analytics_bucket&.object('group_growth').get.body.read
     end
 
     def news_posts_per_group
-      @@analytics_bucket.object('news_posts_per_group').get.body.read
+      @@analytics_bucket&.object('news_posts_per_group').get.body.read
     end
 
     def views_per_folder
-      @@analytics_bucket.object('views_per_folder').get.body.read
+      @@analytics_bucket&.object('views_per_folder').get.body.read
     end
   end
 end
