d8c909140952479a44f53f123433d18e6fa985ea
Mark Roy <markr4848@gmail.com>
2020-05-19 23:03:37 -0400

    Remove RouteService class as it doesn't make sense with new routing structure, keep findTitleForPath as a normal function

--- client/app/utils/routeHelpers.js
+++ client/app/utils/routeHelpers.js
@@ -1,68 +1,15 @@
-
 import dig from 'object-dig';
-import { RouteContext } from 'containers/Layouts/ApplicationLayout';
 
 import { ROUTES } from 'containers/Shared/Routes/constants';
 
 import { intl } from 'containers/Shared/LanguageProvider/GlobalLanguageProvider';
 
-/*
- * Provide an interface to the global routing data stored in RouteContext
- *
- *
- * routeContext looks like:
- *   {
- *     computedMatch: { params: { ... }}
- *     location: { ... }
- *   }
- *
- */
-export default class RouteService {
-  constructor(routeData) {
-    if (typeof routeData === 'function') {
-      this.contextFunc = routeData;
-      this.routeData = this.contextFunc(RouteContext);
-      this.match = this.routeData.computedMatch;
-      this.location = this.routeData.location;
-      this.history = this.routeData.history;
-    } else {
-      this.match = routeData.computedMatch;
-      this.location = routeData.location;
-    }
-  }
-
-  path() {
-    return this.location.pathname;
-  }
-
-  // Returns an array no matter what, but passing an array with a string ID as an ID to the server works
-  // Ex:   const eventId = rs.params('event_id');
-  //       props.getEventBegin({ id: eventId });
-  params(...keys) {
-    const params = [];
-    for (const key of keys)
-      params.push(dig(this.match, 'params', key));
-
-    if (params.length === 1)
-      return params[0];
-
-    if (params.length === 0)
-      return null;
-
-    return params;
-  }
-
-  queries(...keys) {
-    // todo: - return url query strings in location
-    //       - parse with querystring library
-  }
-
-  // Returns [title, isPathPrefix]
-  // title is null when not found
-  // isPathPrefix is true when the title that was found only has a pathPrefix and not a path
-  findTitleForPath({ path, object = ROUTES, params = [], textArguments = {} }) {
-    if (!object || typeof object !== 'object')
-      return [null, null];
+// Returns [title, isPathPrefix]
+// title is null when not found
+// isPathPrefix is true when the title that was found only has a pathPrefix and not a path
+export const findTitleForPath = ({ path, object = ROUTES, params = [], textArguments = {} }) => {
+  if (!object || typeof object !== 'object')
+    return [null, null];
 
     if (Object.hasOwnProperty.call(object, 'data')) {
       if (Object.hasOwnProperty.call(object, 'path')) {
