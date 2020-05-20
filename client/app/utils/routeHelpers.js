29d299e7e58ada7d1284ecda56c0cee55bfc873a
Mark Roy <markr4848@gmail.com>
2020-05-19 23:04:26 -0400

    Remove `this` calls from findTitleForPath

--- client/app/utils/routeHelpers.js
+++ client/app/utils/routeHelpers.js
@@ -11,27 +11,27 @@ export const findTitleForPath = ({ path, object = ROUTES, params = [], textArgum
   if (!object || typeof object !== 'object')
     return [null, null];
 
-    if (Object.hasOwnProperty.call(object, 'data')) {
-      if (Object.hasOwnProperty.call(object, 'path')) {
-        if (typeof object.path === 'function') {
-          if (object.path(...params) === path)
-            return [intl.formatMessage(object.data.titleMessage, textArguments), false];
-        } else if (object.path === path)
+  if (Object.hasOwnProperty.call(object, 'data')) {
+    if (Object.hasOwnProperty.call(object, 'path')) {
+      if (typeof object.path === 'function') {
+        if (object.path(...params) === path)
           return [intl.formatMessage(object.data.titleMessage, textArguments), false];
-      } else if (Object.hasOwnProperty.call(object.data, 'pathPrefix'))
-        if (typeof object.data.pathPrefix === 'function') {
-          if (object.data.pathPrefix(...params) === path)
-            return [intl.formatMessage(object.data.titleMessage, textArguments), true];
-        } else if (object.data.pathPrefix === path)
+      } else if (object.path === path)
+        return [intl.formatMessage(object.data.titleMessage, textArguments), false];
+    } else if (Object.hasOwnProperty.call(object.data, 'pathPrefix'))
+      if (typeof object.data.pathPrefix === 'function') {
+        if (object.data.pathPrefix(...params) === path)
           return [intl.formatMessage(object.data.titleMessage, textArguments), true];
-    } else {
-      const subObjects = Object.values(object);
-      for (const subObject of subObjects) {
-        const [result, isPathPrefix] = this.findTitleForPath({ path, object: subObject, params, textArguments });
-        if (result)
-          return [result, isPathPrefix];
-      }
+      } else if (object.data.pathPrefix === path)
+        return [intl.formatMessage(object.data.titleMessage, textArguments), true];
+  } else {
+    const subObjects = Object.values(object);
+    for (const subObject of subObjects) {
+      const [result, isPathPrefix] = findTitleForPath({ path, object: subObject, params, textArguments });
+      if (result)
+        return [result, isPathPrefix];
     }
+  }
 
     return [null, null];
   }
