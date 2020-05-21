b8ac7202fc5d3ffb47dfe9b7a39e785168b67d6c
elymia <provlia1@videotron.ca>
2020-05-21 18:31:37 -0400

    Some base tests added

--- client/app/containers/Group/tests/saga.test.js
+++ client/app/containers/Group/tests/saga.test.js
@@ -4,10 +4,20 @@
 
 import {
   getGroups,
+  getGroup,
+  getAnnualBudgets,
+  createGroup
 } from 'containers/Group/saga';
 
 import {
   getGroupsError,
+  getGroupsSuccess,
+  getGroupError,
+  getGroupSuccess,
+  getAnnualBudgetsSuccess,
+  getAnnualBudgetsError,
+  createGroupSuccess,
+  createGroupError
 } from 'containers/Group/actions';
 
 import { push } from 'connected-react-router';
@@ -18,6 +28,14 @@ import api from 'api/api';
 import AuthService from 'utils/authService';
 
 api.groups.all = jest.fn();
+api.groups.create = jest.fn();
+api.groups.get = jest.fn();
+api.groups.annualBudgets = jest.fn();
+
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
 
 describe('Get groups Saga', () => {
   it('Should return grouplist', async () => {
@@ -27,12 +45,84 @@ describe('Get groups Saga', () => {
   it('Should return error from the API', async () => {
     const response = { response: { data: 'ERROR!' } };
     api.groups.all.mockImplementation(() => Promise.reject(response));
-    const results = [getGroupsError(response)];
+    const notified = {
+      notification: {
+        key: 1590092641484,
+        message: 'Failed to load groups',
+        options: { variant: 'warning' }
+      },
+      type: 'app/Notifier/ENQUEUE_SNACKBAR'
+    };
+
+    jest.spyOn(Notifiers, 'showSnackbar').mockReturnValue(notified);
+    const results = [getGroupsError(response), notified];
+    const initialAction = { payload: undefined };
     const dispatched = await recordSaga(
       getGroups,
+      initialAction
     );
 
-    expect(api.user.all).toHaveBeenCalled();
+    expect(api.groups.all).toHaveBeenCalledWith(initialAction.payload);
+    expect(dispatched).toEqual(results);
+  });
+});
+
+describe('Get group Saga', () => {
+  it('Should return a group', async () => {
+
+  });
+
+  it('Should return error from the API', async () => {
+    const response = { response: { data: 'ERROR!' } };
+    api.groups.get.mockImplementation(() => Promise.reject(response));
+    const notified = {
+      notification: {
+        key: 1590092641484,
+        message: 'Failed to get group',
+        options: { variant: 'warning' }
+      },
+      type: 'app/Notifier/ENQUEUE_SNACKBAR'
+    };
+
+    jest.spyOn(Notifiers, 'showSnackbar').mockReturnValue(notified);
+    const results = [getGroupError(response), notified];
+    const initialAction = { payload: { id: 5 } };
+    const dispatched = await recordSaga(
+      getGroup,
+      initialAction
+    );
+
+    expect(api.groups.get).toHaveBeenCalledWith(initialAction.payload.id);
+    expect(dispatched).toEqual(results);
+  });
+});
+
+describe('Get annual group budget', () => {
+  it('Should return a group', async () => {
+
+  });
+
+  it('Should return error from the API', async () => {
+    const response = { response: { data: 'ERROR!' } };
+    api.groups.annualBudgets.mockImplementation(() => Promise.reject(response));
+    const notified = {
+      notification: {
+        key: 1590092641484,
+        message: 'Failed to get annual budgets',
+        options: { variant: 'warning' }
+      },
+      type: 'app/Notifier/ENQUEUE_SNACKBAR'
+    };
+
+    jest.spyOn(Notifiers, 'showSnackbar').mockReturnValue(notified);
+    const results = [getAnnualBudgetsError(response), notified];
+    const initialAction = { payload: undefined };
+    const dispatched = await recordSaga(
+      getAnnualBudgets,
+      initialAction
+    );
+
+    expect(api.groups.annualBudgets).toHaveBeenCalledWith(initialAction.payload);
     expect(dispatched).toEqual(results);
   });
 });
