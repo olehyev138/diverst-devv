a70cd3334e8b43d88d761b48644df55da6a626ae
devst05 <1726271@johnabbottcollege.net>
2020-06-01 12:28:02 -0400

    added updateNewsItem for actions test

--- client/app/containers/News/tests/actions.test.js
+++ client/app/containers/News/tests/actions.test.js
@@ -4,7 +4,9 @@ import {
   CREATE_GROUP_MESSAGE_BEGIN, CREATE_GROUP_MESSAGE_SUCCESS, CREATE_GROUP_MESSAGE_ERROR,
   UPDATE_GROUP_MESSAGE_BEGIN, UPDATE_GROUP_MESSAGE_SUCCESS, UPDATE_GROUP_MESSAGE_ERROR,
   DELETE_GROUP_MESSAGE_BEGIN, DELETE_GROUP_MESSAGE_SUCCESS, DELETE_GROUP_MESSAGE_ERROR,
-  NEWS_FEED_UNMOUNT
+  NEWS_FEED_UNMOUNT,
+  UPDATE_NEWS_ITEM_BEGIN, UPDATE_NEWS_ITEM_SUCCESS, UPDATE_NEWS_ITEM_ERROR,
+
 } from 'containers/News/constants';
 
 import {
@@ -13,7 +15,8 @@ import {
   createGroupMessageBegin, createGroupMessageSuccess, createGroupMessageError,
   updateGroupMessageBegin, updateGroupMessageSuccess, updateGroupMessageError,
   deleteGroupMessageBegin, deleteGroupMessageSuccess, deleteGroupMessageError,
-  newsFeedUnmount
+  newsFeedUnmount,
+  updateNewsItemBegin, updateNewsItemSuccess, updateNewsItemError,
 } from 'containers/News/actions';
 
 describe('News actions', () => {
@@ -80,7 +83,36 @@ describe('News actions', () => {
         expect(getNewsItemError('error')).toEqual(expected);
       });
     });
-  });
+    describe('updateNewsItemBegin', () => {
+      it('has a type of UPDATE_NEWS_ITEM_BEGIN and takes a given payload', () => {
+        const expected = {
+          type: UPDATE_NEWS_ITEM_BEGIN,
+          payload: { item: {} }
+        };
+
+        expect(updateNewsItemBegin({ item: {} })).toEqual(expected);
+      });
+    });
+    describe('updateNewsItemSuccess', () => {
+      it('has a type of UPDATE_NEWS_ITEM_SUCCESS and takes a given payload', () => {
+        const expected = {
+          type: UPDATE_NEWS_ITEM_SUCCESS,
+          payload: { item: {} }
+        };
+
+        expect(updateNewsItemSuccess({ item: {} })).toEqual(expected);
+      });
+    });
+    describe('updateNewsItemError', () => {
+      it('has a type of UPDATE_NEWS_ITEM_ERROR and takes a given error', () => {
+        const expected = {
+          type: UPDATE_NEWS_ITEM_ERROR,
+          error: 'error'
+        };
+
+        expect(updateNewsItemError('error')).toEqual(expected);
+      });
+    });  });
   describe('Group Message actions', () => {
     describe('createGroupMessageBegin', () => {
       it('has a type of CREATE_GROUP_MESSAGE_BEGIN and takes a given payload', () => {
@@ -112,6 +144,66 @@ describe('News actions', () => {
         expect(createGroupMessageError('error')).toEqual(expected);
       });
     });
+    describe('updateGroupMessageBegin', () => {
+      it('has a type of UPDATE_GROUP_MESSAGE_BEGIN and takes a given payload', () => {
+        const expected = {
+          type: UPDATE_GROUP_MESSAGE_BEGIN,
+          payload: { item: {} }
+        };
+
+        expect(updateGroupMessageBegin({ item: {} })).toEqual(expected);
+      });
+    });
+    describe('updateGroupMessageSuccess', () => {
+      it('has a type of UPDATE_GROUP_MESSAGE_SUCCESS and takes a given payload', () => {
+        const expected = {
+          type: UPDATE_GROUP_MESSAGE_SUCCESS,
+          payload: { item: {} }
+        };
+
+        expect(updateGroupMessageSuccess({ item: {} })).toEqual(expected);
+      });
+    });
+    describe('updateGroupMessageError', () => {
+      it('has a type of UPDATE_GROUP_MESSAGE_ERROR and takes a given error', () => {
+        const expected = {
+          type: UPDATE_GROUP_MESSAGE_ERROR,
+          error: 'error'
+        };
+
+        expect(updateGroupMessageError('error')).toEqual(expected);
+      });
+    });
+    describe('deleteGroupMessageBegin', () => {
+      it('has a type of DELETE_GROUP_MESSAGE_BEGIN and takes a given payload', () => {
+        const expected = {
+          type: DELETE_GROUP_MESSAGE_BEGIN,
+          payload: { item: {} }
+        };
+
+        expect(deleteGroupMessageBegin({ item: {} })).toEqual(expected);
+      });
+    });
+    describe('deleteGroupMessageSuccess', () => {
+      it('has a type of DELETE_GROUP_MESSAGE_SUCCESS and takes a given payload', () => {
+        const expected = {
+          type: DELETE_GROUP_MESSAGE_SUCCESS,
+          payload: { item: {} }
+        };
+
+        expect(deleteGroupMessageSuccess({ item: {} })).toEqual(expected);
+      });
+    });
+    describe('deleteGroupMessageError', () => {
+      it('has a type of DELETE_GROUP_MESSAGE_ERROR and takes a given error', () => {
+        const expected = {
+          type: DELETE_GROUP_MESSAGE_ERROR,
+          error: 'error'
+        };
+
+        expect(deleteGroupMessageError('error')).toEqual(expected);
+      });
+    });
   });
   describe('News unmount actions', () => {
     describe('newsFeedUnmount', () => {
@@ -122,77 +214,6 @@ describe('News actions', () => {
 
         expect(newsFeedUnmount()).toEqual(expected);
       });
-    });
-  });
-  describe('updateGroupMessageBegin', () => {
-    it('has a type of UPDATE_GROUP_MESSAGE_BEGIN and takes a given payload', () => {
-      const expected = {
-        type: UPDATE_GROUP_MESSAGE_BEGIN,
-        payload: { item: {} }
-      };
-
-      expect(updateGroupMessageBegin({ item: {} })).toEqual(expected);
-    });
-  });
-  describe('updateGroupMessageSuccess', () => {
-    it('has a type of UPDATE_GROUP_MESSAGE_SUCCESS and takes a given payload', () => {
-      const expected = {
-        type: UPDATE_GROUP_MESSAGE_SUCCESS,
-        payload: { item: {} }
-      };
-
-      expect(updateGroupMessageSuccess({ item: {} })).toEqual(expected);
-    });
-  });
-  describe('updateGroupMessageError', () => {
-    it('has a type of UPDATE_GROUP_MESSAGE_ERROR and takes a given error', () => {
-      const expected = {
-        type: UPDATE_GROUP_MESSAGE_ERROR,
-        error: 'error'
-      };
-
-      expect(updateGroupMessageError('error')).toEqual(expected);
-    });
-  });
-  describe('deleteGroupMessageBegin', () => {
-    it('has a type of DELETE_GROUP_MESSAGE_BEGIN and takes a given payload', () => {
-      const expected = {
-        type: DELETE_GROUP_MESSAGE_BEGIN,
-        payload: { item: {} }
-      };
-
-      expect(deleteGroupMessageBegin({ item: {} })).toEqual(expected);
-    });
-  });
-  describe('deleteGroupMessageSuccess', () => {
-    it('has a type of DELETE_GROUP_MESSAGE_SUCCESS and takes a given payload', () => {
-      const expected = {
-        type: DELETE_GROUP_MESSAGE_SUCCESS,
-        payload: { item: {} }
-      };
-
-      expect(deleteGroupMessageSuccess({ item: {} })).toEqual(expected);
-    });
-  });
-  describe('deleteGroupMessageError', () => {
-    it('has a type of DELETE_GROUP_MESSAGE_ERROR and takes a given error', () => {
-      const expected = {
-        type: DELETE_GROUP_MESSAGE_ERROR,
-        error: 'error'
-      };
-
-      expect(deleteGroupMessageError('error')).toEqual(expected);
-    });
-  });
-});
-describe('News unmount actions', () => {
-  describe('newsFeedUnmount', () => {
-    it('has a type of getNewsItemsSuccess', () => {
-      const expected = {
-        type: NEWS_FEED_UNMOUNT,
-      };
-
-      expect(newsFeedUnmount()).toEqual(expected);
     });
   });
 });
