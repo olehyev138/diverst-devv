b2efc1c63ad502f7d9ae2345632c23c83d1bca0a
AlexOxorn <alex.oxorn@gmail.com>
2020-06-02 15:10:41 -0400

    add new action to send the reset password request

--- client/app/containers/User/ForgotPasswordPage/actions.js
+++ client/app/containers/User/ForgotPasswordPage/actions.js
@@ -5,6 +5,9 @@
  */
 
 import {
+  REQUEST_PASSWORD_RESET_BEGIN,
+  REQUEST_PASSWORD_RESET_SUCCESS,
+  REQUEST_PASSWORD_RESET_ERROR,
   GET_USER_BY_TOKEN_BEGIN,
   GET_USER_BY_TOKEN_SUCCESS,
   GET_USER_BY_TOKEN_ERROR,
@@ -13,6 +16,27 @@ import {
   SUBMIT_PASSWORD_ERROR,
   SIGN_UP_UNMOUNT,
 } from './constants';
+
+export function requestPasswordResetBegin(payload) {
+  return {
+    type: REQUEST_PASSWORD_RESET_BEGIN,
+    payload,
+  };
+}
+
+export function requestPasswordResetSuccess(payload) {
+  return {
+    type: REQUEST_PASSWORD_RESET_SUCCESS,
+    payload,
+  };
+}
+
+export function requestPasswordResetError(error) {
+  return {
+    type: REQUEST_PASSWORD_RESET_ERROR,
+    error,
+  };
+}
 
 export function getUserByTokenBegin(payload) {
   return {
--- client/app/containers/User/ForgotPasswordPage/saga.js
+++ client/app/containers/User/ForgotPasswordPage/saga.js
@@ -7,14 +7,30 @@ import { showSnackbar } from 'containers/Shared/Notifier/actions';
 import { ROUTES } from 'containers/Shared/Routes/constants';
 
 import {
+  REQUEST_PASSWORD_RESET_BEGIN,
   GET_USER_BY_TOKEN_BEGIN,
   SUBMIT_PASSWORD_BEGIN,
 } from './constants';
 
 import {
+  requestPasswordResetSuccess, requestPasswordResetError,
   getUserByTokenSuccess, getUserByTokenError,
   submitPasswordSuccess, submitPasswordError,
 } from './actions';
+
+export function* requestPasswordReset(action) {
+  try {
+    const response = { data: 'API CALL' };
+
+    yield put(requestPasswordResetSuccess({}));
+    yield put(showSnackbar({ message: 'Successfully requested password reset', options: { variant: 'success' } }));
+  } catch (err) {
+    yield put(requestPasswordResetError(err));
+
+    // TODO: intl message
+    yield put(showSnackbar({ message: 'Failed to request password reset', options: { variant: 'warning' } }));
+  }
+}
 
 export function* getUserByToken(action) {
   try {
@@ -44,7 +60,8 @@ export function* submitPassword(action) {
 }
 
 
-export default function* ForgotpasswordpageSaga() {
+export default function* ForgotPasswordSaga() {
+  yield takeLatest(REQUEST_PASSWORD_RESET_BEGIN, requestPasswordReset);
   yield takeLatest(GET_USER_BY_TOKEN_BEGIN, getUserByToken);
   yield takeLatest(SUBMIT_PASSWORD_BEGIN, submitPassword);
 }
