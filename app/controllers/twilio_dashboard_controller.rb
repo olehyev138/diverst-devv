0929bac2c058d760dfeebb6d658e269872c1b53b
Derek Owusu-Frimpong <deepsky_5@live.com>
2020-06-05 16:47:30 +0000

    remove call to twilio rest client in controller [ci skip]

--- app/controllers/twilio_dashboard_controller.rb
+++ app/controllers/twilio_dashboard_controller.rb
@@ -3,7 +3,6 @@ class TwilioDashboardController < ApplicationController
   after_action :verify_authorized
   before_action :set_enterprise
   before_action :set_video_room, only: [:show]
-  require 'twilio-ruby'
 
   layout 'global_settings'
 
@@ -20,11 +19,6 @@ class TwilioDashboardController < ApplicationController
 
   def show
     authorize User
-
-    raise BadRequestException.new 'TWILIO_ACCOUNT_SID Required' if ENV['TWILIO_ACCOUNT_SID'].blank?
-    raise BadRequestException.new 'TWILIO_AUTH_TOKEN Required' if ENV['TWILIO_AUTH_TOKEN'].blank?
-
-    client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
 
     @participants = @video_room.video_participants
   end
