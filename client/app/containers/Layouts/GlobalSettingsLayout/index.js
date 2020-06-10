17a3e9331b077407295a2ce1206af05f76882d19
Mark Roy <markr4848@gmail.com>
2020-06-10 09:51:38 -0400

    Update Global Settings Layout to new structure

--- client/app/containers/Layouts/GlobalSettingsLayout/index.js
+++ client/app/containers/Layouts/GlobalSettingsLayout/index.js
@@ -1,12 +1,12 @@
 import React, { memo, useEffect, useState } from 'react';
 import { compose } from 'redux';
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
 
 import { withStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Fade from '@material-ui/core/Fade';
 
-import AdminLayout from '../AdminLayout';
 import GlobalSettingsLinks from 'components/GlobalSettings/GlobalSettingsLinks';
 import { showSnackbar } from 'containers/Shared/Notifier/actions';
 import { connect } from 'react-redux';
@@ -15,6 +15,7 @@ import { selectPermissions } from 'containers/Shared/App/selectors';
 import { push } from 'connected-react-router';
 import { permission } from 'utils/permissionsHelpers';
 import { ROUTES } from 'containers/Shared/Routes/constants';
+import { renderChildrenWithProps } from 'utils/componentHelpers';
 
 const styles = theme => ({});
 
@@ -29,29 +30,29 @@ const GlobalSettingsPages = Object.freeze([
   'policy_templates',
 ]);
 
-const GlobalSettingsLayout = ({ component: Component, ...rest }) => {
-  const { classes, data, defaultPage, permissions, computedMatch, location, redirectAction, showSnackbar, ...other } = rest;
+const GlobalSettingsLayout = (props) => {
+  const { classes, children, permissions, redirectAction, showSnackbar, ...rest } = props;
+
+  const location = useLocation();
 
   /* Get get first key that is in the path, ie: '/admin/system/settings/emails/1/edit/ -> emails */
   const currentPage = GlobalSettingsPages.find(page => location.pathname.includes(page));
   const [tab, setTab] = useState(currentPage);
 
   useEffect(() => {
-    if (!Component) {
-      if (permission(rest, 'enterprise_manage'))
-        redirectAction(ROUTES.admin.system.globalSettings.enterpriseConfiguration.index.path());
-      else if (permission(rest, 'fields_manage'))
-        redirectAction(ROUTES.admin.system.globalSettings.fields.index.path());
-      else if (permission(rest, 'custom_text_manage'))
-        redirectAction(ROUTES.admin.system.globalSettings.customText.index.path());
-      else if (permission(rest, 'sso_authentication'))
-        redirectAction(ROUTES.admin.system.globalSettings.ssoSettings.index.path());
-      else if (permission(rest, 'emails_manage'))
-        redirectAction(ROUTES.admin.system.globalSettings.emails.index.path());
-      else if (permissions) {
-        showSnackbar({ message: 'You do not have permission to manage global settings', options: { variant: 'warning' } });
-        redirectAction(permission(rest, 'adminPath') || ROUTES.user.home.path());
-      }
+    if (permission(rest, 'enterprise_manage'))
+      redirectAction(ROUTES.admin.system.globalSettings.enterpriseConfiguration.index.path());
+    else if (permission(rest, 'fields_manage'))
+      redirectAction(ROUTES.admin.system.globalSettings.fields.index.path());
+    else if (permission(rest, 'custom_text_manage'))
+      redirectAction(ROUTES.admin.system.globalSettings.customText.index.path());
+    else if (permission(rest, 'sso_authentication'))
+      redirectAction(ROUTES.admin.system.globalSettings.ssoSettings.index.path());
+    else if (permission(rest, 'emails_manage'))
+      redirectAction(ROUTES.admin.system.globalSettings.emails.index.path());
+    else if (permissions) {
+      showSnackbar({ message: 'You do not have permission to manage global settings', options: { variant: 'warning' } });
+      redirectAction(permission(rest, 'adminPath') || ROUTES.user.home.path());
     }
 
     if (tab !== currentPage)
@@ -59,23 +60,17 @@ const GlobalSettingsLayout = ({ component: Component, ...rest }) => {
   }, [currentPage, permissions]);
 
   return (
-    <AdminLayout
-      {...other}
-      component={matchProps => (
-        <React.Fragment>
-          <GlobalSettingsLinks
-            currentTab={tab}
-            {...matchProps}
-          />
-          <Box mb={3} />
-          <Fade in appear>
-            <div>
-              { Component ? <Component {...other} /> : <React.Fragment /> }
-            </div>
-          </Fade>
-        </React.Fragment>
-      )}
-    />
+    <React.Fragment>
+      <GlobalSettingsLinks
+        currentTab={tab}
+      />
+      <Box mb={3} />
+      <Fade in appear>
+        <div>
+          {renderChildrenWithProps(children, { ...rest })}
+        </div>
+      </Fade>
+    </React.Fragment>
   );
 };
 
@@ -95,9 +90,11 @@ const withConnect = connect(
 
 GlobalSettingsLayout.propTypes = {
   classes: PropTypes.object,
-  component: PropTypes.elementType,
-  pageTitle: PropTypes.object,
+  children: PropTypes.any,
+  permissions: PropTypes.object,
   location: PropTypes.object,
+  redirectAction: PropTypes.func,
+  showSnackbar: PropTypes.func,
 };
 
 export default compose(
