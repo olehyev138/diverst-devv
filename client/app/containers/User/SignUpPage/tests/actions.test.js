01a07757e55f16726d0234f35a0b747e60b0e3bf
elymia <provlia1@videotron.ca>
2020-06-12 16:07:35 -0400

    Signup test suite

---
+++ client/app/containers/User/SignUpPage/tests/actions.test.js
@@ -0,0 +1,97 @@
+import {
+  GET_USER_BY_TOKEN_BEGIN,
+  GET_USER_BY_TOKEN_SUCCESS,
+  GET_USER_BY_TOKEN_ERROR,
+  SUBMIT_PASSWORD_BEGIN,
+  SUBMIT_PASSWORD_SUCCESS,
+  SUBMIT_PASSWORD_ERROR,
+  SIGN_UP_UNMOUNT,
+} from 'containers/User/SignUpPage/constants';
+
+import {
+  getUserByTokenBegin,
+  getUserByTokenSuccess,
+  getUserByTokenError,
+  submitPasswordBegin,
+  submitPasswordSuccess,
+  submitPasswordError,
+  signUpUnmount
+} from "../actions";
+
+describe('signUp actions', () => {
+  describe('getUserByTokenBegin', () => {
+    it('has a type of GET_USER_BY_TOKEN_BEGIN and sets a given payload', () => {
+      const expected = {
+        type: GET_USER_BY_TOKEN_BEGIN,
+        payload: { value: 118 }
+      };
+
+      expect(getUserByTokenBegin({ value: 118 })).toEqual(expected);
+    });
+  });
+
+  describe('getUserByTokenSuccess', () => {
+    it('has a type of GET_USER_BY_TOKEN_SUCCESS and sets a given payload', () => {
+      const expected = {
+        type: GET_USER_BY_TOKEN_SUCCESS,
+        payload: { value: 118 }
+      };
+
+      expect(getUserByTokenSuccess({ value: 118 })).toEqual(expected);
+    });
+  });
+
+  describe('getUserByTokenError', () => {
+    it('has a type of GET_USER_BY_TOKEN_ERROR and sets a given error', () => {
+      const expected = {
+        type: GET_USER_BY_TOKEN_ERROR,
+        error: { value: 709 }
+      };
+
+      expect(getUserByTokenError({ value: 709 })).toEqual(expected);
+    });
+  });
+
+  describe('submitPasswordBegin', () => {
+    it('has a type of SUBMIT_PASSWORD_BEGIN and sets a given payload', () => {
+      const expected = {
+        type: SUBMIT_PASSWORD_BEGIN,
+        payload: { value: 118 }
+      };
+
+      expect(submitPasswordBegin({ value: 118 })).toEqual(expected);
+    });
+  });
+
+  describe('submitPasswordSuccess', () => {
+    it('has a type of SUBMIT_PASSWORD_SUCCESS and sets a given payload', () => {
+      const expected = {
+        type: SUBMIT_PASSWORD_SUCCESS,
+        payload: { value: 118 }
+      };
+
+      expect(submitPasswordSuccess({ value: 118 })).toEqual(expected);
+    });
+  });
+
+  describe('submitPasswordError', () => {
+    it('has a type of SUBMIT_PASSWORD_ERROR and sets a given error', () => {
+      const expected = {
+        type: SUBMIT_PASSWORD_ERROR,
+        error: { value: 709 }
+      };
+
+      expect(submitPasswordError({ value: 709 })).toEqual(expected);
+    });
+  });
+
+  describe('signUpUnmount', () => {
+    it('has a type of SIGN_UP_UNMOUNT', () => {
+      const expected = {
+        type: SIGN_UP_UNMOUNT,
+      };
+
+      expect(signUpUnmount()).toEqual(expected);
+    });
+  });
+});
---
+++ client/app/containers/User/SignUpPage/tests/saga.test.js
@@ -0,0 +1,117 @@
+/**
+ * Test sagas
+ */
+
+import {
+  getUserByToken,
+  submitPassword
+} from 'containers/User/SignUpPage/saga';
+
+import {
+  getUserByTokenBegin,
+  getUserByTokenSuccess,
+  getUserByTokenError,
+  submitPasswordBegin,
+  submitPasswordSuccess,
+  submitPasswordError,
+  signUpUnmount
+} from '../actions';
+
+import { push } from 'connected-react-router';
+import { ROUTES } from 'containers/Shared/Routes/constants';
+
+import recordSaga from 'utils/recordSaga';
+import * as Notifiers from 'containers/Shared/Notifier/actions';
+import api from 'api/api';
+
+api.users.getInvitedUser = jest.fn();
+api.users.signUpUser = jest.fn();
+
+const user = {
+  id: 1,
+  title: 'abc'
+};
+
+describe('Tests for signup saga', () => {
+  describe('Get signup Saga', () => {
+    it('Should return a user', async () => {
+      api.users.getInvitedUser.mockImplementation(() => Promise.resolve({ data: { page: { ...user } } }));
+      const results = [getUserByTokenSuccess(user)];
+
+      const initialAction = { payload: {
+        count: 5,
+      } };
+
+      const dispatched = await recordSaga(
+        getUserByToken,
+        initialAction
+      );
+      expect(api.users.getInvitedUser).toHaveBeenCalledWith(initialAction.payload);
+      expect(dispatched).toEqual(results);
+    });
+
+    it('Should return error from the API', async () => {
+      const response = { response: { data: 'ERROR!' } };
+      api.users.getInvitedUser.mockImplementation(() => Promise.reject(response));
+      const notified = {
+        notification: {
+          key: 1590092641484,
+          message: 'Failed to load polls',
+          options: { variant: 'warning' }
+        },
+        type: 'app/Notifier/ENQUEUE_SNACKBAR'
+      };
+
+      jest.spyOn(Notifiers, 'showSnackbar').mockReturnValue(notified);
+      const results = [getUserByTokenError(response), notified];
+      const initialAction = { payload: undefined };
+      const dispatched = await recordSaga(
+        getUserByToken,
+        initialAction
+      );
+
+      expect(api.users.getInvitedUser).toHaveBeenCalledWith(initialAction.payload);
+      expect(dispatched).toEqual(results);
+    });
+  });
+
+  // TODO
+  describe('Get signup Saga', () => {
+    xit('Should submit the password', async () => {
+      api.users.signUpUser.mockImplementation(() => Promise.resolve({ data: { ...poll } }));
+      const results = [submitPasswordSuccess(user)];
+      const initialAction = { payload: { id: user.id } };
+
+      const dispatched = await recordSaga(
+        submitPassword,
+        initialAction
+      );
+      expect(api.users.signUpUser).toHaveBeenCalledWith(initialAction.payload.id);
+      expect(dispatched).toEqual(results);
+    });
+
+    xit('Should return error from the API', async () => {
+      const response = { response: { data: 'ERROR!' } };
+      api.users.signUpUser.mockImplementation(() => Promise.reject(response));
+      const notified = {
+        notification: {
+          key: 1590092641484,
+          message: 'Failed to get poll',
+          options: { variant: 'warning' }
+        },
+        type: 'app/Notifier/ENQUEUE_SNACKBAR'
+      };
+
+      jest.spyOn(Notifiers, 'showSnackbar').mockReturnValue(notified);
+      const results = [submitPasswordError(response), notified];
+      const initialAction = { payload: { id: 5 } };
+      const dispatched = await recordSaga(
+        submitPassword,
+        initialAction
+      );
+
+      expect(api.users.signUpUser).toHaveBeenCalledWith(initialAction.payload.id);
+      expect(dispatched).toEqual(results);
+    });
+  });
+});
