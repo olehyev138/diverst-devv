574fee22fe2b67605f00f7004607a7330bfa82cc
Mark Roy <markr4848@gmail.com>
2020-06-15 18:45:18 -0400

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_192239) do
+ActiveRecord::Schema.define(version: 2020_06_12_003729) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.string "name", null: false
