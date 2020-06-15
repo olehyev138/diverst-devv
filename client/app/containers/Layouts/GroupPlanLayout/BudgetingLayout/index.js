34667058fa51ee8acb9d502fdbb7fc8f68358f07
Mark Roy <markr4848@gmail.com>
2020-06-15 18:52:42 -0400

    Fix Group Plan Budgeting erroring

--- client/app/containers/Layouts/GroupPlanLayout/BudgetingLayout/index.js
+++ client/app/containers/Layouts/GroupPlanLayout/BudgetingLayout/index.js
@@ -4,12 +4,15 @@ import { connect } from 'react-redux';
 import { push } from 'connected-react-router';
 import { matchPath, useLocation, useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { createStructuredSelector } from 'reselect';
 
 import Box from '@material-ui/core/Box';
 import Fade from '@material-ui/core/Fade';
 import { withStyles } from '@material-ui/core/styles';
 
 import { showSnackbar } from 'containers/Shared/Notifier/actions';
+
+import { selectGroup } from 'containers/Group/selectors';
 
 import { ROUTES } from 'containers/Shared/Routes/constants';
 
@@ -28,34 +31,37 @@ const BudgetPages = Object.freeze([
 const redirectAction = path => push(path);
 
 const BudgetLayout = (props) => {
-  const { classes, children, redirectAction, showSnackbar, ...rest } = props;
+  const { classes, children, currentGroup, redirectAction, showSnackbar, ...rest } = props;
 
   const location = useLocation();
   const { group_id: groupId } = useParams();
 
   /* Get get first key that is in the path, ie: '/admin/system/settings/budgets/1/edit/ -> budgets */
   const currentPage = BudgetPages.find(page => location.pathname.includes(page));
-  const [tab, setTab] = useState(currentPage);
+  const [tab, setTab] = useState(currentPage || BudgetPages[0]);
 
   useEffect(() => {
+    if (!currentGroup) return;
+
     if (matchPath(location.pathname, { path: ROUTES.group.plan.budget.index.path(), exact: true }))
-      if (permission(rest.currentGroup, 'annual_budgets_view?'))
+      if (permission(currentGroup, 'annual_budgets_view?'))
         redirectAction(ROUTES.group.plan.budget.overview.path(groupId));
-      else if (permission(rest.currentGroup, 'annual_budgets_manage?'))
+      else if (permission(currentGroup, 'annual_budgets_manage?'))
         redirectAction(ROUTES.group.plan.budget.editAnnualBudget.path(groupId));
       else {
         showSnackbar({ message: 'You do not have permission to manage this group\'s budget', options: { variant: 'warning' } });
         redirectAction(ROUTES.group.plan.index.path(groupId));
       }
 
-    if (tab !== currentPage)
+    if (tab !== currentPage && currentPage)
       setTab(currentPage);
-  }, [currentPage]);
+  }, [currentPage, currentGroup]);
 
   return (
     <React.Fragment>
       <BudgetLinks
         currentTab={tab}
+        currentGroup={currentGroup}
       />
       <Box mb={3} />
       <Fade in appear>
@@ -75,13 +81,17 @@ BudgetLayout.propTypes = {
   showSnackbar: PropTypes.func,
 };
 
+const mapStateToProps = createStructuredSelector({
+  currentGroup: selectGroup(),
+});
+
 const mapDispatchToProps = {
   redirectAction,
   showSnackbar,
 };
 
 const withConnect = connect(
-  undefined,
+  mapStateToProps,
   mapDispatchToProps,
 );
 
