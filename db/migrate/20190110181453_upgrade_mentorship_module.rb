7844726976f93486b719826b200f64374aa71e65
Matthew Baldwin <matt@diverst.com>
2020-06-17 13:59:27 -0400

    Specify reference type for old migration to work with legacy int types

--- db/migrate/20190110181453_upgrade_mentorship_module.rb
+++ db/migrate/20190110181453_upgrade_mentorship_module.rb
@@ -1,12 +1,20 @@
-class UpgradeMentorshipModule < ActiveRecord::Migration[5.1]
+class UpgradeMentorshipModule < ActiveRecord::Migration[5.2]
   def change
-    create_table :mentoring_session_comments do |t|
+    create_table(:mentoring_session_comments, id: :integer) do |t|
       t.text :content
-      t.references :user, index: true, foreign_key: true
-      t.references :mentoring_session, index: true, foreign_key: true
-
+      t.integer :user_id
+      t.integer :mentoring_session_id
       t.timestamps null: false
     end
+
+    MentoringSessionComment.connection.schema_cache.clear!
+    MentoringSessionComment.reset_column_information
+
+    add_foreign_key :mentoring_session_comments, :users
+    add_foreign_key :mentoring_session_comments, :mentoring_sessions
+
+    #t.references :user, index: true, foreign_key: true, type: :integer
+    #t.references :mentoring_session, index: true, foreign_key: true, type: :integer
 
     remove_column :mentorship_sessions, :attending, :boolean
     add_column :mentorship_sessions, :status, :string, :null => false, :default => "pending"
