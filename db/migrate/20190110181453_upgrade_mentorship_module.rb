0e82fccf31ab1853c2c8f3e85a28f264bbc18f69
Matthew Baldwin <matt@diverst.com>
2020-06-17 21:25:40 -0400

    Restore upgrade_mentorship_module

--- db/migrate/20190110181453_upgrade_mentorship_module.rb
+++ db/migrate/20190110181453_upgrade_mentorship_module.rb
@@ -1,20 +1,12 @@
-class UpgradeMentorshipModule < ActiveRecord::Migration[5.2]
+class UpgradeMentorshipModule < ActiveRecord::Migration[5.1]
   def change
-    create_table(:mentoring_session_comments, id: :integer) do |t|
+    create_table :mentoring_session_comments do |t|
       t.text :content
-      t.integer :user_id
-      t.integer :mentoring_session_id
+      t.references :user, index: true, foreign_key: true
+      t.references :mentoring_session, index: true, foreign_key: true
+
       t.timestamps null: false
     end
-
-    MentoringSessionComment.connection.schema_cache.clear!
-    MentoringSessionComment.reset_column_information
-
-    add_foreign_key :mentoring_session_comments, :users
-    add_foreign_key :mentoring_session_comments, :mentoring_sessions
-
-    #t.references :user, index: true, foreign_key: true, type: :integer
-    #t.references :mentoring_session, index: true, foreign_key: true, type: :integer
 
     remove_column :mentorship_sessions, :attending, :boolean
     add_column :mentorship_sessions, :status, :string, :null => false, :default => "pending"
