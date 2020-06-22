e6c7e9bb2c991c2508f70530c6d8c89951ebfc5c
devst05 <1726271@johnabbottcollege.net>
2020-06-22 13:26:42 -0400

    updated model validations/associations/scope for initiative user

--- spec/models/initiative_user_spec.rb
+++ spec/models/initiative_user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe InitiativeUser, type: :model do
   it { expect(initiative_user).to belong_to(:initiative) }
   it { expect(initiative_user).to belong_to(:user).counter_cache(:initiatives_count) }
 
-  it { expect(initiative_user).to validate_presence_of(:initiative_id) }
-  it { expect(initiative_user).to validate_presence_of(:user_id) }
+  it { expect(initiative_user).to validate_presence_of(:initiative) }
+  it { expect(initiative_user).to validate_presence_of(:user) }
   it { expect(initiative_user).to validate_uniqueness_of(:user_id).scoped_to(:initiative_id).with_message('has already joined this event') }
 end
