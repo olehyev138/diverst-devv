61ec841be4708d301ef541beacf107c5c9af9396
devst05 <1726271@johnabbottcollege.net>
2020-06-25 16:19:33 -0400

    updated model validations/associations/scope for topic

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe Topic, type: :model do
   let(:topic) { build_stubbed(:topic) }
 
   it { expect(topic).to belong_to(:enterprise) }
-  it { expect(topic).to have_many(:feedbacks).class_name('TopicFeedback') }
+  #it { expect(topic).to belong_to(:user) }
+  it { expect(topic).to have_many(:feedbacks).class_name('TopicFeedback').dependent(:destroy) }
+  it { expect(topic).to validate_length_of(:statement).is_at_most(65535)}
 
   describe '#destroy_callbacks' do
     it 'removes the child objects' do
