d5bd6da01909412e890b6c3a2a03148f1350c859
devst05 <1726271@johnabbottcollege.net>
2020-06-25 16:28:54 -0400

    lint

--- app/models/surveys_manager.rb
+++ app/models/surveys_manager.rb
@@ -1,6 +1,6 @@
 class SurveysManager < ApplicationRecord
   self.table_name = 'survey_managers'
 
-  belongs_to :survey  #there is no entity called survey??
+  belongs_to :survey # there is no entity called survey??
   belongs_to :user
 end
