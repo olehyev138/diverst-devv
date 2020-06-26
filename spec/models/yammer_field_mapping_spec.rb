0f3f6943e048f1f84585f66e9bbd34a2c9724148
devst05 <1726271@johnabbottcollege.net>
2020-06-26 16:42:31 -0400

    updated model validations/associations/scope for yammer

--- spec/models/yammer_field_mapping_spec.rb
+++ spec/models/yammer_field_mapping_spec.rb
@@ -6,5 +6,6 @@ RSpec.describe YammerFieldMapping, type: :model do
   describe 'associations' do
     it { expect(yammer_field_mapping).to belong_to(:enterprise) }
     it { expect(yammer_field_mapping).to belong_to(:diverst_field).class_name('Field') }
+    it { expect(yammer_field_mapping).to validate_length_of(:yammer_field_name).is_at_most(191) }
   end
 end
