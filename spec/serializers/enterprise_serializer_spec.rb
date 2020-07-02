e14a4a93d4021ebb6b93530d32f432bc215b9149
devst05 <1726271@johnabbottcollege.net>
2020-07-02 14:40:27 -0400

    added event serializer test

--- spec/serializers/enterprise_serializer_spec.rb
+++ spec/serializers/enterprise_serializer_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe EnterpriseSerializer, type: :serializer do
-  it 'returns field data' do
+  it 'returns enterprise' do
     enterprise = create(:enterprise)
     serializer = EnterpriseSerializer.new(enterprise, scope: serializer_scopes(create(:user)), scope_name: :scope)
 
