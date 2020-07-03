beebb1ab0ff8b1b058c0774f9ad7ee7e59bff5dd
devst05 <1726271@johnabbottcollege.net>
2020-07-03 15:07:17 -0400

    added segment field rule serializer test

---
+++ spec/serializers/segment_field_rule_serializer_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe SegmentFieldRuleSerializer, type: :serializer do
+  it 'returns associations' do
+    segment_field_rule = create(:segment_field_rule)
+    serializer = SegmentFieldRuleSerializer.new(segment_field_rule, scope: serializer_scopes(create(:user)), scope_name: :scope)
+
+    expect(serializer.serializable_hash[:id]).to eq(segment_field_rule.id)
+    expect(serializer.serializable_hash[:segment_id]).to eq(segment_field_rule.segment_id)
+    expect(serializer.serializable_hash[:field_id]).to eq(segment_field_rule.field_id)
+  end
+end
