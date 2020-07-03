e86f78e2811d33b9feceac3eb5e4202b6adfc0e6
devst05 <1726271@johnabbottcollege.net>
2020-07-03 14:06:35 -0400

    added question serializer test

--- spec/serializers/page_serializer_spec.rb
+++ spec/serializers/page_serializer_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe PageSerializer, type: :serializer do
-  it 'returns activity' do
-    activity = create(:page)
-    serializer = PageSerializer.new(activity, scope: serializer_scopes(create(:user)), scope_name: :scope)
-
-    expect(serializer.serializable_hash[:id]).to_not be nil
-    expect(serializer.serializable_hash[:owner_id]).to_not be nil
+  it 'returns pages' do
   end
 end
