3097ab0fd478c2584aeb277f557979310d50d7b6
AlexOxorn <alex.oxorn@gmail.com>
2020-07-07 12:26:46 -0400

    fix function name

--- app/mailers/diverst_mailer.rb
+++ app/mailers/diverst_mailer.rb
@@ -12,33 +12,12 @@ class DiverstMailer < ApplicationMailer
     mail(from: @from_address, to: @user.email, subject: @subject)
   end
 
-  # def reset_password_instructions(record, token, opts = {})
-  #   return if record.enterprise.disable_emails?
-  #
-  #   set_defaults(record.enterprise, method_name)
-  #   super
-  # end
-  #
-  # def headers_for(action, opts)
-  #   headers = {
-  #     subject: @subject || subject_for(action),
-  #     to: @email || resource.email_for_notification,
-  #     from: @from_address, # TODO: devise
-  #     reply_to: @from_address, # TODO: devise
-  #     template_path: template_paths,
-  #     template_name: action
-  #   }.merge(opts)
-  #
-  #   @email = headers[:to]
-  #   headers
-  # end
-
   def variables
     {
       user: @user,
       enterprise: @enterprise,
       custom_text: @enterprise.custom_text,
-      click_here: "<a href=\"#{ReactRoutes.session.sign_up(@token)}\" target=\"_blank\">Click Here</a>",
+      click_here: "<a href=\"#{ReactRoutes.session.signUp(@token)}\" target=\"_blank\">Click Here</a>",
     }
   end
 
