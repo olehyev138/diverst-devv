6976f960095659aca17d9cd68775488beacbafd2
AlexOxorn <alex.oxorn@gmail.com>
2020-07-08 18:39:06 -0400

    add suppress default in reducer switch

--- client/app/containers/User/SignUpPage/reducer.js
+++ client/app/containers/User/SignUpPage/reducer.js
@@ -26,6 +26,7 @@ export const initialState = {
 function signUpReducer(state = initialState, action) {
   /* eslint-disable consistent-return */
   return produce(state, (draft) => {
+    // eslint-disable-next-line default-case
     switch (action.type) {
       case GET_USER_BY_TOKEN_BEGIN:
         draft.isLoading = true;
