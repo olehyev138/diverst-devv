d074d9f87293ce3007728049f15d1cbc495a9309
devst05 <1726271@johnabbottcollege.net>
2020-07-09 16:39:43 -0400

    added action test for activity

--- spec/models/activity/actions_spec.rb
+++ spec/models/activity/actions_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Activity::Actions, type: :model do
-    describe 'valid_scopes' do
-      let(:activity) { create(:activity) }
-      it { expect(Activity.valid_scopes.include?('joined_from')).to eq true }
-      it { expect(Activity.valid_scopes.include?('joined_to')).to eq true }
-      it { expect(Activity.valid_scopes.include?('for_group_ids')).to eq true }
-    end
+RSpec.describe Activity::Actions, type: :action do
+  describe 'valid_scopes' do
+    it { expect(Activity.valid_scopes.include?('joined_from')).to eq true }
+    it { expect(Activity.valid_scopes.include?('joined_to')).to eq true }
+    it { expect(Activity.valid_scopes.include?('for_group_ids')).to eq true }
+  end
 
-    describe 'base_preloads' do
-      it { expect(Activity.base_preloads.include?(:owner)).to eq true }
-    end
+  describe 'base_preloads' do
+    it { expect(Activity.base_preloads.include?(:owner)).to eq true }
+  end
 end
