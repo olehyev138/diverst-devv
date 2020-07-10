2e9e49d7db0229c5425a64d59d2a273de31e43c2
devst05 <1726271@johnabbottcollege.net>
2020-07-10 15:20:21 -0400

    added action test for annual budget

---
+++ spec/models/annual_budget/actions_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe AnnualBudget::Actions, type: :action do
+  describe 'ClassMethods' do
+    describe 'order_string' do
+      it { expect(AnnualBudget.order_string('name', 'asc')).to eq 'annual_budgets.closed asc, name asc' }
+    end
+  end
+end
