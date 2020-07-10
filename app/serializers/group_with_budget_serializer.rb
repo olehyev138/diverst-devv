66eeb7d6364235a90aa208617e16415eb9977689
AlexOxorn <alex.oxorn@gmail.com>
2020-07-10 13:27:35 -0400

    move child flattening to front end

--- app/serializers/group_with_budget_serializer.rb
+++ app/serializers/group_with_budget_serializer.rb
@@ -1,6 +1,14 @@
 class GroupWithBudgetSerializer < ApplicationRecordSerializer
-  attributes :id, :name, :short_description, :description, :parent_id, :enterprise_id, :currency,
+  attributes :id, :name, :short_description, :description, :parent_id, :enterprise_id, :currency, :children,
              :annual_budget, :annual_budget_leftover, :annual_budget_approved, :annual_budget_available, :permissions
+
+  def children
+    if instance_options[:with_children]
+      object.children.map do |child|
+        GroupWithBudgetSerializer.new(child).as_json
+      end
+    end
+  end
 
   def currency
     object.annual_budget_currency
