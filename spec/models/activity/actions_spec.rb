31e6fd661e0be69693de73333f98ae5b9a93dd51
devst05 <1726271@johnabbottcollege.net>
2020-07-10 13:22:02 -0400

    added action test for activity

--- spec/models/activity/actions_spec.rb
+++ spec/models/activity/actions_spec.rb
@@ -10,4 +10,24 @@ RSpec.describe Activity::Actions, type: :action do
   describe 'base_preloads' do
     it { expect(Activity.base_preloads.include?(:owner)).to eq true }
   end
+
+  describe 'ClassMethods' do
+    let!(:group) { create(:group, name: 'test') }
+    let!(:activity) { create(:activity) }
+
+    describe 'csv_attributes' do
+      it { expect(Activity.csv_attributes.dig(:titles)).to eq %w(User_id First_name Last_name Trackable_id Trackable_type Action Created_at) }
+    end
+
+    describe 'parameter_name' do
+      it { expect(Activity.parameter_name(['for_group_ids', [group.id]] )).to eq "of groups #{group.name}" }
+      it { expect(Activity.parameter_name(['joined_to', Date.tomorrow] )).to eq "to #{Date.tomorrow.strftime('%Y-%m-%d')}" }
+      it { expect(Activity.parameter_name(['joined_from', Date.yesterday] )).to eq "from #{Date.yesterday.strftime('%Y-%m-%d')}" }
+    end
+
+    describe 'file_name' do
+      it { expect(Activity.file_name({ query_scopes: [['joined_from', Date.yesterday], ['joined_to',Date.tomorrow], ['for_group_ids', [group.id]]] })).to\
+    eq "from_#{Date.yesterday.strftime('%Y-%m-%d')}_to_#{Date.tomorrow.strftime('%Y-%m-%d')}_of_groups_#{group.name}_Logs" }
+    end
+  end
 end
