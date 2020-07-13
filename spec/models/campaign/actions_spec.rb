4813592581fc3b19bdc167c8cd98f46ed9f70497
devst05 <1726271@johnabbottcollege.net>
2020-07-13 11:08:41 -0400

    added action test for campaign

---
+++ spec/models/campaign/actions_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Campaign::Actions, type: :action do
+  describe 'base_preloads' do
+    it { expect(Campaign.base_preloads.include?(:questions)).to eq true }
+    it { expect(Campaign.base_preloads.include?(:image_attachment)).to eq true }
+    it { expect(Campaign.base_preloads.include?(:banner_attachment)).to eq true }
+    it { expect(Campaign.base_preloads.include?(:groups)).to eq true }
+  end
+end
