b50ee260d4bdb3467df4b23fd6b64cbec013b673
devst05 <1726271@johnabbottcollege.net>
2020-07-13 14:52:13 -0400

    added action test for enterprise

--- spec/models/enterprise/actions_spec.rb
+++ spec/models/enterprise/actions_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Enterprise::Actions, type: :model do
   describe 'base_preloads' do
     it { expect(Enterprise.base_preloads.include?(:theme)).to eq true }
   end
+
+  # Todo
+  describe 'sso login' do
+  end
+
+  # Todo
+  describe 'sso link' do
+  end
 end
