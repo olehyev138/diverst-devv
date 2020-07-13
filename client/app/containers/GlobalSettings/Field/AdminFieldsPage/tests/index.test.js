2a6e8d0e73d3f8c05f872b132a6564416b08db97
Liana <provlia1@videotron.ca>
2020-07-13 15:06:32 -0400

    fix test

--- client/app/containers/GlobalSettings/Field/AdminFieldsPage/tests/index.test.js
+++ client/app/containers/GlobalSettings/Field/AdminFieldsPage/tests/index.test.js
@@ -7,20 +7,25 @@
  */
 
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallowWithIntl, loadTranslation } from 'enzyme-react-intl';
 import 'utils/mockReactRouterHooks';
-import AdminFieldsPage from '../index';
+import { AdminFieldsPage } from '../index';
+import { intl } from 'tests/mocks/react-intl';
 
+loadTranslation('./app/translations/en.json');
 const props = {
   getFieldsBegin: jest.fn(),
   createFieldBegin: jest.fn(),
   updateFieldBegin: jest.fn(),
+  updateFieldPositionBegin: jest.fn(),
+  deleteFieldBegin: jest.fn(),
+  fields: [],
   fieldUnmount: jest.fn(),
 };
 describe('<AdminFieldsPage />', () => {
   it('Expect to not log errors in console', () => {
     const spy = jest.spyOn(global.console, 'error');
-    const wrapper = shallow(<AdminFieldsPage classes={{}} {...props} />);
+    const wrapper = shallowWithIntl(<AdminFieldsPage classes={{}} intl={intl} {...props} />);
 
     expect(spy).not.toHaveBeenCalled();
   });
