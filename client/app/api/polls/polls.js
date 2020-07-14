81308b41b618faad4e1a1a33dc3f1d2a0e3d721d
AlexOxorn <alex.oxorn@gmail.com>
2020-07-14 11:26:34 -0400

    add to polls api file

--- client/app/api/polls/polls.js
+++ client/app/api/polls/polls.js
@@ -1,8 +1,19 @@
 import API from 'api/base/base';
+import { appendQueryArgs } from 'utils/apiHelpers';
 const axios = require('axios');
 
 const Polls = new API({ controller: 'polls' });
 
-Object.assign(Polls, {});
+Object.assign(Polls, {
+  createAndPublish(payload) {
+    return axios.post(`${this.url}/publish`, payload);
+  },
+  updateAndPublish(id, payload) {
+    return axios.put(`${this.url}/${id}/publish`, payload);
+  },
+  publish(id) {
+    return axios.patch(`${this.url}/${id}/publish`, { polls: {} });
+  },
+});
 
 export default Polls;
