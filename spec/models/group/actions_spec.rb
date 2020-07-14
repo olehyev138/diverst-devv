b47c32312c5ec23e61dfafb655834f20baf5cfc5
devst05 <1726271@johnabbottcollege.net>
2020-07-14 10:41:14 -0400

    updated action test for group

--- spec/models/group/actions_spec.rb
+++ spec/models/group/actions_spec.rb
@@ -34,18 +34,27 @@ RSpec.describe Group::Actions, type: :model do
     it { expect(Group.base_attributes_preloads.include?(:annual_budgets)).to eq true }
     it { expect(Group.base_attributes_preloads.include?(:logo_attachment)).to eq true }
     it { expect(Group.base_attributes_preloads.include?(:banner_attachment)).to eq true }
-    it { expect(Group.base_attributes_preloads.include?({ :enterprise=>[:theme] })).to eq true }
+    it { expect(Group.base_attributes_preloads.include?({ enterprise: [:theme] })).to eq true }
   end
 
   describe 'update_child_categories' do
     let!(:parent_group) { create(:group) }
     let!(:sub_group1) { create(:group) }
     let!(:sub_group2) { create(:group) }
+    let!(:group_category_type) { create(:group_category_type) }
+    let!(:group_category) { create(:group_category, group_category_type_id: group_category_type.id) }
+
     it 'ID required' do
-      expect{ Group.update_child_categories(Request.create_request(create(:user)), {}) }.to raise_error(BadRequestException)
+      expect { Group.update_child_categories(Request.create_request(create(:user)), {}) }.to raise_error(BadRequestException)
     end
 
     it 'update categories' do
+      params = { id: parent_group.id,
+                 name: parent_group.name,
+                 group_category_type_id: group_category_type.id,
+                 children: [{ id: sub_group1.id, group_category_id: group_category.id, group_category_type_id: group_category_type.id }],
+                 group: { id: parent_group.id } }
+      expect(Group.update_child_categories(Request.create_request(create(:user)), params)).to_not be nil
     end
   end
 end
