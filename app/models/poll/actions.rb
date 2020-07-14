689f2720ec9700b57a85b551d75a3315b0a26657
AlexOxorn <alex.oxorn@gmail.com>
2020-07-14 10:05:24 -0400

    add basic actions for publishing

--- app/models/poll/actions.rb
+++ app/models/poll/actions.rb
@@ -3,6 +3,15 @@ module Poll::Actions
     klass.extend ClassMethods
   end
 
+  def publish!
+    klass.update(status: 'published')
+  rescue => e
+    case e
+    when InvalidInputException, Pundit::NotAuthorizedError then raise
+    else raise BadRequestException.new(e.message)
+    end
+  end
+
   module ClassMethods
     def base_preloads
       [:fields, :groups, :segments, :enterprise]
