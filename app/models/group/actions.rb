034e64cc3aeb02a65f0a35bfdb8143ee36e1a5d0
AlexOxorn <alex.oxorn@gmail.com>
2020-07-15 17:39:57 -0400

    add `except_id` to valid group scopes

--- app/models/group/actions.rb
+++ app/models/group/actions.rb
@@ -31,7 +31,7 @@ module Group::Actions
     end
 
     def valid_scopes
-      ['all_children', 'all_parents', 'no_children', 'is_private']
+      ['all_children', 'all_parents', 'no_children', 'is_private', 'except_id']
     end
 
     # List of all attributes to preload.
