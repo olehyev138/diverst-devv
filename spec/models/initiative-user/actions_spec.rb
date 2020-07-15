d3092b14f350086510af2637b1d9443d49a425a4
devst05 <1726271@johnabbottcollege.net>
2020-07-15 10:00:52 -0400

    added action test for initiative user

---
+++ spec/models/initiative-user/actions_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe InitiativeUser::Actions, type: :model do
+  let!(:group) { create(:group, name: 'test') }
+  let!(:activity) { create(:activity) }
+
+  describe 'csv_attributes' do
+    it { expect(InitiativeUser.csv_attributes.dig(:titles)).to eq ['First name', 'Last name', 'Email', 'Attended', 'Checked In', 'Check In Time',] }
+  end
+
+  describe 'file_name' do
+    it 'event id not valid' do
+      expect { InitiativeUser.file_name({}) }.to raise_error(ArgumentError)
+    end
+
+    it 'file name' do
+      event = create(:initiative, name: 'test event name')
+      expect(InitiativeUser.file_name({ initiative_id: event.id })).to eq 'Attendees_of_test_event_name'
+    end
+  end
+end
