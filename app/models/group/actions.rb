5258b66ad92646af0be43b030f979ebdcb6385b8
AlexOxorn <alex.oxorn@gmail.com>
2020-07-15 18:01:51 -0400

    re-added except_id

--- app/models/group/actions.rb
+++ app/models/group/actions.rb
@@ -31,7 +31,7 @@ module Group::Actions
     end
 
     def valid_scopes
-      ['all_children', 'all_parents', 'no_children', 'is_private', 'replace_with_children']
+      ['all_children', 'all_parents', 'no_children', 'is_private', 'replace_with_children', 'except_id']
     end
 
     # List of all attributes to preload.
