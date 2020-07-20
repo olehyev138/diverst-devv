91d5a1b1054fcd40754b4b1f47021294d4c4c17c
AlexOxorn <alex.oxorn@gmail.com>
2020-07-20 17:28:06 -0400

    campaign serializer spec

--- spec/serializers/campaign_serializer_spec.rb
+++ spec/serializers/campaign_serializer_spec.rb
@@ -1,12 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe CampaignSerializer, type: :serializer do
+  let(:campaign) do
+    create(
+        :campaign,
+        image: {
+            io: File.open('spec/fixtures/files/verizon_logo.png'),
+            filename: 'file.png'
+        },
+        banner: {
+            io: File.open('spec/fixtures/files/verizon_logo.png'),
+            filename: 'file.png'
+        }
+    )
+  end
+  let(:serializer) { CampaignSerializer.new(campaign, scope: serializer_scopes(create(:user))) }
+
+  include_examples 'permission container', :serializer
+
   it 'returns fields for campaign' do
-    campaign = create(:campaign,
-                      image: { io: File.open('spec/fixtures/files/verizon_logo.png'), filename: 'file.png' },
-                      banner: { io: File.open('spec/fixtures/files/verizon_logo.png'), filename: 'file.png' })
     create_list(:question, 3, campaign: campaign)
-    serializer = CampaignSerializer.new(campaign, scope: serializer_scopes(create(:user)))
 
     expect(serializer.serializable_hash[:image_location]).to_not be nil
     expect(serializer.serializable_hash[:banner_location]).to_not be nil
