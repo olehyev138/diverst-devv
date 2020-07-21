a3a6ab79fb0134e6fe2114ee7dc0ccd0bb1676c2
AlexOxorn <alex.oxorn@gmail.com>
2020-07-21 15:45:21 -0400

    added fields + refactor

--- spec/serializers/application_record_serializer_spec.rb
+++ spec/serializers/application_record_serializer_spec.rb
@@ -8,17 +8,15 @@ RSpec.describe ApplicationRecordSerializer, type: :serializer do
   #
   # d: To test basic serialization of Serializer Methods
   # e: To test with_permissions of Serializer Methods
-  # f: To test exclude_keys of Serializer Methods
 
   context 'Inheriting from ApplicationRecordSerializer' do
     let(:model) {
-      class ModelOne
+      Class.new do
         include ActiveModel::Model
         attr_accessor :a, :b, :c
         validates :a, :b, :c, presence: true
         def attributes; { a: a, b: b, c: c } end
       end
-      ModelOne
     }
     let!(:policy) {
       class ModelOnePolicy < ApplicationPolicy
@@ -33,45 +31,71 @@ RSpec.describe ApplicationRecordSerializer, type: :serializer do
       end
       ModelOnePolicy
     }
-    let(:serializer1) {
-      class ModelOneSerializer < ApplicationRecordSerializer
-        def serialize_all_fields; true end
-        def excluded_keys; [:c, :f] end
-      end
-      ModelOneSerializer
-    }
-    let(:serializer2) {
-      class ModelTwoSerializer < ApplicationRecordSerializer
-        def serialize_all_fields; false end
-        def excluded_keys; [:c, :f] end
-      end
-      ModelTwoSerializer
-    }
-    let(:permission_module1) { serializer1.permission_module }
-    let(:permission_module2) { serializer2.permission_module }
 
-    it 'should create unique permissions module for each descendants' do
-      expect(permission_module1).to be_a Module
-      expect(permission_module2).to be_a Module
-      expect(permission_module1).to_not be permission_module2
+    before :each do
+      @serializer1 = Class.new(ApplicationRecordSerializer) do
+        def excluded_keys; [:c, :f] end
+      end
+
+      @serializer2 = Class.new(ApplicationRecordSerializer) do
+        def excluded_keys; [:c, :f] end
+      end
+
+      @permission_module1 = @serializer1.permission_module
+      @permission_module2 = @serializer2.permission_module
     end
 
-    context 'serialize_all_fields' do
-      before do
-        serializer1.define_method :serialize_all_fields do true end
+    it 'should create unique permissions module for each descendants' do
+      expect(@permission_module1).to be_a Module
+      expect(@permission_module2).to be_a Module
+      expect(@permission_module1).to_not be @permission_module2
+    end
+
+    context 'serialize_all_fields is true' do
+      before :each do
+        @serializer1.define_method :serialize_all_fields do true end
       end
 
       it 'has :a and :b as attributes on first instantiation' do
-        expect(serializer1._attributes).to     be_empty
+        pp @serializer1.object_id
+        expect(@serializer1._attributes).to     be_empty
 
-        serializer1.new(model.new)
+        @serializer1.new(model.new)
 
-        expect(serializer1._attributes).to     include :a
-        expect(serializer1._attributes).to     include :b
-        expect(serializer1._attributes).to_not include :c
-        expect(serializer1._attributes).to_not include :d
-        expect(serializer1._attributes).to_not include :e
-        expect(serializer1._attributes).to_not include :f
+        expect(@serializer1._attributes).to     include :a
+        expect(@serializer1._attributes).to     include :b
+        expect(@serializer1._attributes).to_not include :c
+        expect(@serializer1._attributes).to_not include :d
+        expect(@serializer1._attributes).to_not include :e
+        expect(@serializer1._attributes).to_not include :f
+      end
+    end
+
+    context 'serialize_all_fields is false, nothing is defined' do
+      it 'has :a and :b as attributes on first instantiation' do
+        pp @serializer1.object_id
+        expect(@serializer1._attributes).to     be_empty
+
+        a = @serializer1.new(model.new)
+
+        expect(@serializer1._attributes).to     be_empty
+      end
+    end
+
+    context 'with extra fields' do
+      before :each do
+        @serializer1.attributes :d, :e
+        @serializer1.define_method :d do 2 * object.a end
+        @serializer1.define_method :e do 2 * object.b end
+      end
+
+      it 'it serializes all the fields' do
+        expect(@serializer1._attributes).to_not include :a
+        expect(@serializer1._attributes).to_not include :b
+        expect(@serializer1._attributes).to_not include :c
+        expect(@serializer1._attributes).to     include :d
+        expect(@serializer1._attributes).to     include :e
+        expect(@serializer1._attributes).to_not include :f
       end
     end
   end
