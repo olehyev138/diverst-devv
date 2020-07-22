68c3af31d879227ae85bb88276aae873e030f8be
AlexOxorn <alex.oxorn@gmail.com>
2020-07-22 10:56:26 -0400

    action and selector test update

--- client/app/containers/Poll/tests/actions.test.js
+++ client/app/containers/Poll/tests/actions.test.js
@@ -11,6 +11,15 @@ import {
   UPDATE_POLL_BEGIN,
   UPDATE_POLL_SUCCESS,
   UPDATE_POLL_ERROR,
+  CREATE_POLL_AND_PUBLISH_BEGIN,
+  CREATE_POLL_AND_PUBLISH_SUCCESS,
+  CREATE_POLL_AND_PUBLISH_ERROR,
+  UPDATE_POLL_AND_PUBLISH_BEGIN,
+  UPDATE_POLL_AND_PUBLISH_SUCCESS,
+  UPDATE_POLL_AND_PUBLISH_ERROR,
+  PUBLISH_POLL_BEGIN,
+  PUBLISH_POLL_SUCCESS,
+  PUBLISH_POLL_ERROR,
   DELETE_POLL_BEGIN,
   DELETE_POLL_SUCCESS,
   DELETE_POLL_ERROR,
@@ -19,196 +28,322 @@ import {
 
 import {
   getPollBegin,
-  getPollError,
   getPollSuccess,
+  getPollError,
   getPollsBegin,
-  getPollsError,
   getPollsSuccess,
+  getPollsError,
   createPollBegin,
-  createPollError,
   createPollSuccess,
+  createPollError,
   updatePollBegin,
-  updatePollError,
   updatePollSuccess,
+  updatePollError,
+  createPollAndPublishBegin,
+  createPollAndPublishSuccess,
+  createPollAndPublishError,
+  updatePollAndPublishBegin,
+  updatePollAndPublishSuccess,
+  updatePollAndPublishError,
+  publishPollBegin,
+  publishPollSuccess,
+  publishPollError,
   deletePollBegin,
-  deletePollError,
   deletePollSuccess,
-  pollsUnmount
+  deletePollError,
+  pollsUnmount,
 } from '../actions';
 
 describe('poll actions', () => {
-  describe('getPollBegin', () => {
-    it('has a type of GET_POLL_BEGIN and sets a given payload', () => {
-      const expected = {
-        type: GET_POLL_BEGIN,
-        payload: { value: 118 }
-      };
+  describe('poll getting actions', () => {
+    describe('getPollBegin', () => {
+      it('has a type of GET_POLL_BEGIN and sets a given payload', () => {
+        const expected = {
+          type: GET_POLL_BEGIN,
+          payload: { value: 301 }
+        };
 
-      expect(getPollBegin({ value: 118 })).toEqual(expected);
+        expect(getPollBegin({ value: 301 })).toEqual(expected);
+      });
+    });
+
+    describe('getPollSuccess', () => {
+      it('has a type of GET_POLL_SUCCESS and sets a given payload', () => {
+        const expected = {
+          type: GET_POLL_SUCCESS,
+          payload: { value: 542 }
+        };
+
+        expect(getPollSuccess({ value: 542 })).toEqual(expected);
+      });
+    });
+
+    describe('getPollError', () => {
+      it('has a type of GET_POLL_ERROR and sets a given error', () => {
+        const expected = {
+          type: GET_POLL_ERROR,
+          error: { value: 966 }
+        };
+
+        expect(getPollError({ value: 966 })).toEqual(expected);
+      });
     });
   });
 
-  describe('getPollSuccess', () => {
-    it('has a type of GET_POLL_SUCCESS and sets a given payload', () => {
-      const expected = {
-        type: GET_POLL_SUCCESS,
-        payload: { value: 865 }
-      };
+  describe('poll list actions', () => {
+    describe('getPollsBegin', () => {
+      it('has a type of GET_POLLS_BEGIN and sets a given payload', () => {
+        const expected = {
+          type: GET_POLLS_BEGIN,
+          payload: { value: 856 }
+        };
 
-      expect(getPollSuccess({ value: 865 })).toEqual(expected);
+        expect(getPollsBegin({ value: 856 })).toEqual(expected);
+      });
+    });
+
+    describe('getPollsSuccess', () => {
+      it('has a type of GET_POLLS_SUCCESS and sets a given payload', () => {
+        const expected = {
+          type: GET_POLLS_SUCCESS,
+          payload: { value: 923 }
+        };
+
+        expect(getPollsSuccess({ value: 923 })).toEqual(expected);
+      });
+    });
+
+    describe('getPollsError', () => {
+      it('has a type of GET_POLLS_ERROR and sets a given error', () => {
+        const expected = {
+          type: GET_POLLS_ERROR,
+          error: { value: 824 }
+        };
+
+        expect(getPollsError({ value: 824 })).toEqual(expected);
+      });
     });
   });
 
-  describe('getPollError', () => {
-    it('has a type of GET_POLL_ERROR and sets a given error', () => {
-      const expected = {
-        type: GET_POLL_ERROR,
-        error: { value: 709 }
-      };
+  describe('poll creating actions', () => {
+    describe('createPollBegin', () => {
+      it('has a type of CREATE_POLL_BEGIN and sets a given payload', () => {
+        const expected = {
+          type: CREATE_POLL_BEGIN,
+          payload: { value: 587 }
+        };
 
-      expect(getPollError({ value: 709 })).toEqual(expected);
+        expect(createPollBegin({ value: 587 })).toEqual(expected);
+      });
+    });
+
+    describe('createPollSuccess', () => {
+      it('has a type of CREATE_POLL_SUCCESS and sets a given payload', () => {
+        const expected = {
+          type: CREATE_POLL_SUCCESS,
+          payload: { value: 587 }
+        };
+
+        expect(createPollSuccess({ value: 587 })).toEqual(expected);
+      });
+    });
+
+    describe('createPollError', () => {
+      it('has a type of CREATE_POLL_ERROR and sets a given error', () => {
+        const expected = {
+          type: CREATE_POLL_ERROR,
+          error: { value: 551 }
+        };
+
+        expect(createPollError({ value: 551 })).toEqual(expected);
+      });
     });
   });
 
-  describe('getPollsBegin', () => {
-    it('has a type of GET_POLLS_BEGIN and sets a given payload', () => {
-      const expected = {
-        type: GET_POLLS_BEGIN,
-        payload: { value: 118 }
-      };
+  describe('poll updating actions', () => {
+    describe('updatePollBegin', () => {
+      it('has a type of UPDATE_POLL_BEGIN and sets a given payload', () => {
+        const expected = {
+          type: UPDATE_POLL_BEGIN,
+          payload: { value: 909 }
+        };
 
-      expect(getPollsBegin({ value: 118 })).toEqual(expected);
+        expect(updatePollBegin({ value: 909 })).toEqual(expected);
+      });
+    });
+
+    describe('updatePollSuccess', () => {
+      it('has a type of UPDATE_POLL_SUCCESS and sets a given payload', () => {
+        const expected = {
+          type: UPDATE_POLL_SUCCESS,
+          payload: { value: 595 }
+        };
+
+        expect(updatePollSuccess({ value: 595 })).toEqual(expected);
+      });
+    });
+
+    describe('updatePollError', () => {
+      it('has a type of UPDATE_POLL_ERROR and sets a given error', () => {
+        const expected = {
+          type: UPDATE_POLL_ERROR,
+          error: { value: 179 }
+        };
+
+        expect(updatePollError({ value: 179 })).toEqual(expected);
+      });
     });
   });
 
-  describe('getPollsSuccess', () => {
-    it('has a type of GET_POLLS_SUCCESS and sets a given payload', () => {
-      const expected = {
-        type: GET_POLLS_SUCCESS,
-        payload: { value: 118 }
-      };
+  describe('poll create and publishing actions', () => {
+    describe('createPollAndPublishBegin', () => {
+      it('has a type of CREATE_POLL_AND_PUBLISH_BEGIN and sets a given payload', () => {
+        const expected = {
+          type: CREATE_POLL_AND_PUBLISH_BEGIN,
+          payload: { value: 79 }
+        };
 
-      expect(getPollsSuccess({ value: 118 })).toEqual(expected);
+        expect(createPollAndPublishBegin({ value: 79 })).toEqual(expected);
+      });
+    });
+
+    describe('createPollAndPublishSuccess', () => {
+      it('has a type of CREATE_POLL_AND_PUBLISH_SUCCESS and sets a given payload', () => {
+        const expected = {
+          type: CREATE_POLL_AND_PUBLISH_SUCCESS,
+          payload: { value: 841 }
+        };
+
+        expect(createPollAndPublishSuccess({ value: 841 })).toEqual(expected);
+      });
+    });
+
+    describe('createPollAndPublishError', () => {
+      it('has a type of CREATE_POLL_AND_PUBLISH_ERROR and sets a given error', () => {
+        const expected = {
+          type: CREATE_POLL_AND_PUBLISH_ERROR,
+          error: { value: 398 }
+        };
+
+        expect(createPollAndPublishError({ value: 398 })).toEqual(expected);
+      });
     });
   });
 
-  describe('getPollsError', () => {
-    it('has a type of GET_POLLS_ERROR and sets a given error', () => {
-      const expected = {
-        type: GET_POLLS_ERROR,
-        error: { value: 709 }
-      };
+  describe('poll update and publishing actions', () => {
+    describe('updatePollAndPublishBegin', () => {
+      it('has a type of UPDATE_POLL_AND_PUBLISH_BEGIN and sets a given payload', () => {
+        const expected = {
+          type: UPDATE_POLL_AND_PUBLISH_BEGIN,
+          payload: { value: 833 }
+        };
 
-      expect(getPollsError({ value: 709 })).toEqual(expected);
+        expect(updatePollAndPublishBegin({ value: 833 })).toEqual(expected);
+      });
+    });
+
+    describe('updatePollAndPublishSuccess', () => {
+      it('has a type of UPDATE_POLL_AND_PUBLISH_SUCCESS and sets a given payload', () => {
+        const expected = {
+          type: UPDATE_POLL_AND_PUBLISH_SUCCESS,
+          payload: { value: 811 }
+        };
+
+        expect(updatePollAndPublishSuccess({ value: 811 })).toEqual(expected);
+      });
+    });
+
+    describe('updatePollAndPublishError', () => {
+      it('has a type of UPDATE_POLL_AND_PUBLISH_ERROR and sets a given error', () => {
+        const expected = {
+          type: UPDATE_POLL_AND_PUBLISH_ERROR,
+          error: { value: 305 }
+        };
+
+        expect(updatePollAndPublishError({ value: 305 })).toEqual(expected);
+      });
     });
   });
 
-  describe('createPollBegin', () => {
-    it('has a type of CREATE_POLL_BEGIN and sets a given payload', () => {
-      const expected = {
-        type: CREATE_POLL_BEGIN,
-        payload: { value: 118 }
-      };
+  describe('poll publishing actions', () => {
+    describe('publishPollBegin', () => {
+      it('has a type of PUBLISH_POLL_BEGIN and sets a given payload', () => {
+        const expected = {
+          type: PUBLISH_POLL_BEGIN,
+          payload: { value: 755 }
+        };
 
-      expect(createPollBegin({ value: 118 })).toEqual(expected);
+        expect(publishPollBegin({ value: 755 })).toEqual(expected);
+      });
+    });
+
+    describe('publishPollSuccess', () => {
+      it('has a type of PUBLISH_POLL_SUCCESS and sets a given payload', () => {
+        const expected = {
+          type: PUBLISH_POLL_SUCCESS,
+          payload: { value: 797 }
+        };
+
+        expect(publishPollSuccess({ value: 797 })).toEqual(expected);
+      });
+    });
+
+    describe('publishPollError', () => {
+      it('has a type of PUBLISH_POLL_ERROR and sets a given error', () => {
+        const expected = {
+          type: PUBLISH_POLL_ERROR,
+          error: { value: 37 }
+        };
+
+        expect(publishPollError({ value: 37 })).toEqual(expected);
+      });
     });
   });
 
-  describe('createPollSuccess', () => {
-    it('has a type of CREATE_POLL_SUCCESS and sets a given payload', () => {
-      const expected = {
-        type: CREATE_POLL_SUCCESS,
-        payload: { value: 118 }
-      };
+  describe('poll deleting actions', () => {
+    describe('deletePollBegin', () => {
+      it('has a type of DELETE_POLL_BEGIN and sets a given payload', () => {
+        const expected = {
+          type: DELETE_POLL_BEGIN,
+          payload: { value: 100 }
+        };
 
-      expect(createPollSuccess({ value: 118 })).toEqual(expected);
+        expect(deletePollBegin({ value: 100 })).toEqual(expected);
+      });
+    });
+
+    describe('deletePollSuccess', () => {
+      it('has a type of DELETE_POLL_SUCCESS and sets a given payload', () => {
+        const expected = {
+          type: DELETE_POLL_SUCCESS,
+          payload: { value: 404 }
+        };
+
+        expect(deletePollSuccess({ value: 404 })).toEqual(expected);
+      });
+    });
+
+    describe('deletePollError', () => {
+      it('has a type of DELETE_POLL_ERROR and sets a given error', () => {
+        const expected = {
+          type: DELETE_POLL_ERROR,
+          error: { value: 196 }
+        };
+
+        expect(deletePollError({ value: 196 })).toEqual(expected);
+      });
     });
   });
 
-  describe('createPollError', () => {
-    it('has a type of CREATE_POLL_ERROR and sets a given error', () => {
-      const expected = {
-        type: CREATE_POLL_ERROR,
-        error: { value: 709 }
-      };
+  describe('State cleaning actions', () => {
+    describe('pollsUnmount', () => {
+      it('has a type of POLLS_UNMOUNT', () => {
+        const expected = {
+          type: POLLS_UNMOUNT,
+        };
 
-      expect(createPollError({ value: 709 })).toEqual(expected);
-    });
-  });
-
-  describe('updatePollBegin', () => {
-    it('has a type of UPDATE_POLL_BEGIN and sets a given payload', () => {
-      const expected = {
-        type: UPDATE_POLL_BEGIN,
-        payload: { value: 118 }
-      };
-
-      expect(updatePollBegin({ value: 118 })).toEqual(expected);
-    });
-  });
-
-  describe('updatePollSuccess', () => {
-    it('has a type of UPDATE_POLL_SUCCESS and sets a given payload', () => {
-      const expected = {
-        type: UPDATE_POLL_SUCCESS,
-        payload: { value: 118 }
-      };
-
-      expect(updatePollSuccess({ value: 118 })).toEqual(expected);
-    });
-  });
-
-  describe('updatePollError', () => {
-    it('has a type of UPDATE_POLL_ERROR and sets a given error', () => {
-      const expected = {
-        type: UPDATE_POLL_ERROR,
-        error: { value: 709 }
-      };
-
-      expect(updatePollError({ value: 709 })).toEqual(expected);
-    });
-  });
-
-  describe('deletePollBegin', () => {
-    it('has a type of DELETE_POLL_BEGIN and sets a given payload', () => {
-      const expected = {
-        type: DELETE_POLL_BEGIN,
-        payload: { value: 118 }
-      };
-
-      expect(deletePollBegin({ value: 118 })).toEqual(expected);
-    });
-  });
-
-  describe('deletePollSuccess', () => {
-    it('has a type of DELETE_POLL_SUCCESS and sets a given payload', () => {
-      const expected = {
-        type: DELETE_POLL_SUCCESS,
-        payload: { value: 118 }
-      };
-
-      expect(deletePollSuccess({ value: 118 })).toEqual(expected);
-    });
-  });
-
-  describe('deletePollError', () => {
-    it('has a type of DELETE_POLL_ERROR and sets a given error', () => {
-      const expected = {
-        type: DELETE_POLL_ERROR,
-        error: { value: 709 }
-      };
-
-      expect(deletePollError({ value: 709 })).toEqual(expected);
-    });
-  });
-
-  describe('pollsUnmount', () => {
-    it('has a type of POLLS_UNMOUNT', () => {
-      const expected = {
-        type: POLLS_UNMOUNT,
-      };
-
-      expect(pollsUnmount()).toEqual(expected);
+        expect(pollsUnmount()).toEqual(expected);
+      });
     });
   });
 });
--- client/app/containers/Poll/tests/selectors.test.js
+++ client/app/containers/Poll/tests/selectors.test.js
@@ -9,13 +9,11 @@ import {
   selectIsCommitting,
   selectHasChanged,
 } from '../selectors';
-
 import { initialState } from '../reducer';
-import { selectIsLoggingIn } from '../../Session/LoginPage/selectors';
 
 describe('Poll selectors', () => {
   describe('selectPollDomain', () => {
-    it('should select the archives domain', () => {
+    it('should select the poll domain', () => {
       const mockedState = { polls: { poll: {} } };
       const selected = selectPollDomain(mockedState);
 
@@ -30,10 +28,97 @@ describe('Poll selectors', () => {
     });
   });
 
-  describe('selectHasChanged', () => {
-    it('should select the \'has changed\' flag', () => {
-      const mockedState = { hasChanged: false };
-      const selected = selectHasChanged().resultFunc(mockedState);
+  describe('selectPaginatedPolls', () => {
+    it('should select the paginated polls', () => {
+      const mockedState = { pollList: { id: 789, __dummy: '789' } };
+      const selected = selectPaginatedPolls().resultFunc(mockedState);
+
+      expect(selected).toEqual({ id: 789, __dummy: '789' });
+    });
+  });
+
+  describe('selectPollsTotal', () => {
+    it('should select the polls total', () => {
+      const mockedState = { pollListTotal: 974 };
+      const selected = selectPollsTotal().resultFunc(mockedState);
+
+      expect(selected).toEqual(974);
+    });
+  });
+
+  describe('selectPoll', () => {
+    it('should select the currentPoll', () => {
+      const mockedState = {
+        currentPoll: {
+          id: 374,
+          fields: [{
+            id: 123,
+            title: 'Question',
+            type: 'Type A'
+          }],
+          __dummy: '374'
+        }
+      };
+      const selected = selectPoll().resultFunc(mockedState);
+
+      expect(selected).toEqual({
+        id: 374,
+        __dummy: '374',
+        fields: [{
+          label: 'Question',
+          value: 123,
+          type: 'Type A'
+        }]
+      });
+    });
+  });
+
+  describe('selectFormPoll', () => {
+    it('should select the form poll', () => {
+      const mockedState = {
+        currentPoll: {
+          id: 374,
+          groups: [{
+            id: 123,
+            name: 'Group A',
+          }],
+          segments: [{
+            id: 123,
+            name: 'Segment A',
+          }],
+          __dummy: '374'
+        }
+      };
+      const selected = selectFormPoll().resultFunc(mockedState);
+
+      expect(selected).toEqual({
+        id: 374,
+        groups: [{
+          value: 123,
+          label: 'Group A',
+        }],
+        segments: [{
+          value: 123,
+          label: 'Segment A',
+        }],
+        __dummy: '374'
+      });
+    });
+  });
+
+  describe('selectIsFetchingPolls', () => {
+    it('should select the \'is fetching polls\' flag', () => {
+      const mockedState = { isFetchingPolls: true };
+      const selected = selectIsFetchingPolls().resultFunc(mockedState);
+
+      expect(selected).toEqual(true);
+    });
+  });
+
+  describe('selectIsFetchingPoll', () => {
+    it('should select the \'is fetching poll\' flag', () => {
+      const mockedState = { isFetchingPoll: false };
+      const selected = selectIsFetchingPoll().resultFunc(mockedState);
 
       expect(selected).toEqual(false);
     });
@@ -48,57 +133,12 @@ describe('Poll selectors', () => {
     });
   });
 
-  describe('selectPollsTotal', () => {
-    it('should select the archive total', () => {
-      const mockedState = { pollListTotal: 289 };
-      const selected = selectPollsTotal().resultFunc(mockedState);
+  describe('selectHasChanged', () => {
+    it('should select the \'has changed\' flag', () => {
+      const mockedState = { hasChanged: false };
+      const selected = selectHasChanged().resultFunc(mockedState);
 
-      expect(selected).toEqual(289);
-    });
-  });
-
-  describe('selectIsFetchingPoll', () => {
-    it('should select the \'is fetchingPoll\' flag', () => {
-      const mockedState = { isFetchingPoll: true };
-      const selected = selectIsFetchingPoll().resultFunc(mockedState);
-
-      expect(selected).toEqual(true);
-    });
-  });
-
-  describe('selectIsFetchingPolls', () => {
-    it('should select the \'is fetchingPolls\' flag', () => {
-      const mockedState = { isFetchingPolls: true };
-      const selected = selectIsFetchingPolls().resultFunc(mockedState);
-
-      expect(selected).toEqual(true);
-    });
-  });
-
-  describe('selectPoll', () => {
-    it('should select the currentPoll', () => {
-      const mockedState = { currentPoll: { id: 374, __dummy: '374' } };
-      const selected = selectPoll().resultFunc(mockedState);
-
-      expect(selected).toEqual({ id: 374, __dummy: '374' });
-    });
-  });
-
-  describe('selectPaginatedPolls', () => {
-    it('should select the paginated polls', () => {
-      const mockedState = { pollList: [{ id: 37, name: 'dummy' }] };
-      const selected = selectPaginatedPolls().resultFunc(mockedState);
-
-      expect(selected).toEqual([{ id: 37, name: 'dummy' }]);
-    });
-  });
-
-  describe('selectFormPoll', () => {
-    it('should select currentPoll and do more stuff', () => {
-      const mockedState = { currentPoll: { id: 374, __dummy: '374' } };
-      const selected = selectPoll().resultFunc(mockedState);
-
-      expect(selected).toEqual({ id: 374, __dummy: '374' });
+      expect(selected).toEqual(false);
     });
   });
 });
