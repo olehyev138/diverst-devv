16e2ca90f0b20e7d1977b7424ee96a6c7a3277bb
Liana <provlia1@videotron.ca>
2020-07-23 17:17:07 -0400

    Add message for comment deletion

--- client/app/components/News/NewsComment/index.js
+++ client/app/components/News/NewsComment/index.js
@@ -67,7 +67,7 @@ export function NewsComment(props) {
             size='small'
             onClick={() => {
               /* eslint-disable-next-line no-alert, no-restricted-globals */
-              if (confirm(intl.formatMessage(messages.group_delete_confirm)))
+              if (confirm(intl.formatMessage(messages.group_comment_delete_confirm)))
                 props.deleteCommentAction({ news_id: newsItem.id, id: comment.id });
             }}
           >
