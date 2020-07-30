31a470d3efba7433dd6cbdf846f2d92c1c84b8aa
Derek Owusu-Frimpong <deepsky_5@live.com>
2020-07-30 13:34:29 +0000

    drop departments table if it exists [skip ci]

--- db/migrate/20200727161037_create_departments.rb
+++ db/migrate/20200727161037_create_departments.rb
@@ -2,13 +2,13 @@ class CreateDepartments < ActiveRecord::Migration
   def change
     if ActiveRecord::Base.connection.table_exists?('departments')
       drop_table :departments
-      
-      create_table :departments do |t|
-        t.string :name
-        t.references :enterprise, index: true, foreign_key: true
+    end
 
-        t.timestamps null: false
-      end
+    create_table :departments do |t|
+      t.string :name
+      t.references :enterprise, index: true, foreign_key: true
+
+      t.timestamps null: false
     end
   end
 end
