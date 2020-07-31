07aa195ebd8c6b90a85b664c27d0a741f561779b
devst05 <1726271@johnabbottcollege.net>
2020-07-31 15:09:54 -0400

    updated user segment actions spec

--- spec/models/users_segment/actions_spec.rb
+++ spec/models/users_segment/actions_spec.rb
@@ -2,20 +2,42 @@ require 'rails_helper'
 
 RSpec.describe UsersSegment::Actions, type: :model do
   describe 'base_preloads' do
-    it { expect(UsersSegment.base_preloads.include?({ segment: [:field_rules, :order_rules, :group_rules, { field_rules: [:field, { field: [:field_definer] }] }],
-                                                      user: [:field_data,
-                                                             :enterprise,
-                                                             :user_groups,
-                                                             :user_role,
-                                                             :news_links,
-                                                             :avatar_attachment,
-                                                             :avatar_blob,
-                                                             { enterprise: [:theme, :mobile_fields], field_data: [:field, { field: [:field_definer] }] }] })).to eq true
+    let(:base_preloads) {
+      [
+          segment: [:field_rules,
+                    :order_rules,
+                    :group_rules,
+                    field_rules: [:field,
+                                  field: [:field_definer]
+                    ]
+          ],
+          user: [:field_data,
+                 :enterprise,
+                 :user_groups,
+                 :user_role,
+                 :news_links,
+                 :avatar_attachment,
+                 :avatar_blob,
+                 enterprise: [:theme,
+                              :mobile_fields],
+                 field_data: [:field,
+                              field: [:field_definer]
+                 ]
+          ]
+      ]
     }
+
+    it { expect(UsersSegment.base_preloads).to eq base_preloads }
   end
 
   describe 'base_includes' do
-    it { expect(UsersSegment.base_includes.include?(:user)).to eq true }
-    it { expect(UsersSegment.base_includes.include?(:segment)).to eq true }
+    let(:base_includes) {
+      [
+          :user,
+          :segment
+      ]
+    }
+
+    it { expect(UsersSegment.base_includes).to eq base_includes }
   end
 end
