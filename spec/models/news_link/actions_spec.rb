42288e34e163c6a4e7c18e8f6b650b4271c55a48
devst05 <1726271@johnabbottcollege.net>
2020-07-31 12:40:18 -0400

    updated news feed actions spec

--- spec/models/news_link/actions_spec.rb
+++ spec/models/news_link/actions_spec.rb
@@ -2,31 +2,27 @@ require 'rails_helper'
 
 RSpec.describe NewsLink::Actions, type: :model do
   describe 'base_preloads' do
-    it { expect(NewsLink.base_preloads.include?(:author)).to eq true }
-    it { expect(NewsLink.base_preloads.include?(:group)).to eq true }
-    it { expect(NewsLink.base_preloads.include?(:comments)).to eq true }
-    it { expect(NewsLink.base_preloads.include?(:photos)).to eq true }
-    it { expect(NewsLink.base_preloads.include?(:picture_attachment)).to eq true }
-    it { expect(NewsLink.base_preloads.include?(author: [:field_data,
-                                                         :enterprise,
-                                                         :user_groups,
-                                                         :user_role,
-                                                         :news_links,
-                                                         :avatar_attachment,
-                                                         :avatar_blob,
-                                                         { enterprise: [:theme, :mobile_fields],
-                                                           field_data: [:field, { field: [:field_definer] }] }],
-                                                comments: [:author,
-                                                           { author: [:field_data,
-                                                                      :enterprise,
-                                                                      :user_groups,
-                                                                      :user_role,
-                                                                      :news_links,
-                                                                      :avatar_attachment,
-                                                                      :avatar_blob,
-                                                                      { enterprise: [:theme, :mobile_fields],
-                                                                        field_data: [:field, { field: [:field_definer] }] }] }],
-                                                group: [:enterprise])).to eq true
+    let!(:base_preloads) { [:author, :group, :comments, :photos, :picture_attachment, author: [:field_data,
+                                                                                               :enterprise,
+                                                                                               :user_groups,
+                                                                                               :user_role,
+                                                                                               :news_links,
+                                                                                               :avatar_attachment,
+                                                                                               :avatar_blob,
+                                                                                               { enterprise: [:theme, :mobile_fields],
+                                                                                                 field_data: [:field, { field: [:field_definer] }] }],
+                                                                                      comments: [:author,
+                                                                                                 { author: [:field_data,
+                                                                                                            :enterprise,
+                                                                                                            :user_groups,
+                                                                                                            :user_role,
+                                                                                                            :news_links,
+                                                                                                            :avatar_attachment,
+                                                                                                            :avatar_blob,
+                                                                                                            { enterprise: [:theme, :mobile_fields],
+                                                                                                              field_data: [:field, { field: [:field_definer] }] }] }],
+                                                                                      group: [:enterprise]]
     }
+    it { expect(NewsLink.base_preloads).to eq base_preloads }
   end
 end
