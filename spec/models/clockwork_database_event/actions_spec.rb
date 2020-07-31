b4a2da6250812f8e5e7e129c7246523e67813b9d
devst05 <1726271@johnabbottcollege.net>
2020-07-31 10:33:14 -0400

    updated clockwork actions spec

--- spec/models/clockwork_database_event/actions_spec.rb
+++ spec/models/clockwork_database_event/actions_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ClockworkDatabaseEvent::Actions, type: :model do
   describe 'base_preloads' do
-    it { expect(ClockworkDatabaseEvent.base_preloads.include?(:frequency_period)).to eq true }
+    let!(:base_preloads) { [:frequency_period] }
+    it { expect(ClockworkDatabaseEvent.base_preloads).to eq base_preloads }
   end
 end
