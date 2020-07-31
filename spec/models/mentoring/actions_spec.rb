c1eca74681503622a46e3846340bca7463f88f83
devst05 <1726271@johnabbottcollege.net>
2020-07-31 12:20:37 -0400

    updated mentoring actions spec

--- spec/models/mentoring/actions_spec.rb
+++ spec/models/mentoring/actions_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Mentoring::Actions, type: :model do
   describe 'valid_includes' do
-    it { expect(Mentoring.valid_includes.include?('mentee')).to eq true }
-    it { expect(Mentoring.valid_includes.include?('mentor')).to eq true }
+    let!(:valid_includes) { %w(mentee mentor) }
+    it { expect(Mentoring.valid_includes).to eq valid_includes }
   end
 end
