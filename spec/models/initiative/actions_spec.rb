c93a0d20abca6f5b761708cef6f0eccf2383ae31
devst05 <1726271@johnabbottcollege.net>
2020-07-31 12:07:42 -0400

    updated initiative actions spec

--- spec/models/initiative/actions_spec.rb
+++ spec/models/initiative/actions_spec.rb
@@ -2,30 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Initiative::Actions, type: :model do
   describe 'base_preloads' do
-    it { expect(Initiative.base_preloads.include?(:pillar)).to eq true }
-    it { expect(Initiative.base_preloads.include?(:owner)).to eq true }
-    it { expect(Initiative.base_preloads.include?(:budget)).to eq true }
-    it { expect(Initiative.base_preloads.include?(:outcome)).to eq true }
-    it { expect(Initiative.base_preloads.include?(:group)).to eq true }
-    it { expect(Initiative.base_preloads.include?(:expenses)).to eq true }
-    it { expect(Initiative.base_preloads.include?(:picture_attachment)).to eq true }
-    it { expect(Initiative.base_preloads.include?(:qr_code_attachment)).to eq true }
-    it { expect(Initiative.base_preloads.include?(:initiative_users)).to eq true }
-    it { expect(Initiative.base_preloads.include?(:comments)).to eq true }
+    let!(:base_preloads) { [
+        :pillar,
+        :owner,
+        :budget,
+        :outcome,
+        :group,
+        :expenses,
+        :picture_attachment,
+        :qr_code_attachment,
+        :initiative_users,
+        :comments
+    ]
+    }
+    it { expect(Initiative.base_preloads).to eq base_preloads }
   end
 
   describe 'valid_scopes' do
-    it { expect(Initiative.valid_scopes.include?('upcoming')).to eq true }
-    it { expect(Initiative.valid_scopes.include?('ongoing')).to eq true }
-    it { expect(Initiative.valid_scopes.include?('past')).to eq true }
-    it { expect(Initiative.valid_scopes.include?('not_archived')).to eq true }
-    it { expect(Initiative.valid_scopes.include?('archived')).to eq true }
-    it { expect(Initiative.valid_scopes.include?('of_annual_budget')).to eq true }
-    it { expect(Initiative.valid_scopes.include?('joined_events_for_user')).to eq true }
-    it { expect(Initiative.valid_scopes.include?('available_events_for_user')).to eq true }
-    it { expect(Initiative.valid_scopes.include?('for_groups')).to eq true }
-    it { expect(Initiative.valid_scopes.include?('for_segments')).to eq true }
-    it { expect(Initiative.valid_scopes.include?('date_range')).to eq true }
+    let!(:valid_scopes) { %w(upcoming ongoing past not_archived archived of_annual_budget joined_events_for_user available_events_for_user for_groups for_segments date_range) }
+    it { expect(Initiative.valid_scopes).to eq valid_scopes }
   end
 
   describe 'generate_qr_code' do
