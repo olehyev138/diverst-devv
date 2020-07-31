559ddff2dcc5342d310ca8701f148283f8c4fe6a
devst05 <1726271@johnabbottcollege.net>
2020-07-31 14:51:10 -0400

    updated update actions spec

--- spec/models/update/actions_spec.rb
+++ spec/models/update/actions_spec.rb
@@ -2,33 +2,35 @@ require 'rails_helper'
 
 RSpec.describe Update::Actions, type: :model do
   describe 'base_preloads' do
-    let(:base_preloads) { [
-                            :field_data,
-                            :previous,
-                            :next,
-                            field_data:
-                                [:field,
-                                 field: [:field_definer]
-                                ],
-                            previous:
-                                [:field_data,
-                                 field_data: [:field,
-                                              field: [:field_definer]
-                                 ]
-                                ]
-                           ]
+    let(:base_preloads) {
+      [
+          :field_data,
+          :previous,
+          :next,
+          field_data:
+              [:field,
+               field: [:field_definer]
+              ],
+          previous:
+              [:field_data,
+               field_data: [:field,
+                            field: [:field_definer]
+               ]
+              ]
+      ]
     }
 
     it { expect(Update.base_preloads).to eq base_preloads }
   end
 
   describe 'lesser_preloads' do
-    let(:lesser_preloads) { [
-                             :field_data,
-                             field_data: [:field,
-                                          field: [:field_definer]
-                                         ]
-                            ]
+    let(:lesser_preloads) {
+      [
+          :field_data,
+          field_data: [:field,
+                       field: [:field_definer]
+                      ]
+      ]
     }
 
     it { expect(Update.lesser_preloads).to eq lesser_preloads }
