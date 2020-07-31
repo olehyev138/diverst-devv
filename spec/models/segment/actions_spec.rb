8092cd3875b9cb622e6f95fe169f8c633fd5bb5b
devst05 <1726271@johnabbottcollege.net>
2020-07-31 14:01:03 -0400

    updated segment actions spec

--- spec/models/segment/actions_spec.rb
+++ spec/models/segment/actions_spec.rb
@@ -2,9 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Segment::Actions, type: :model do
   describe 'base_preloads' do
-    it { expect(Segment.base_preloads.include?(:field_rules)).to eq true }
-    it { expect(Segment.base_preloads.include?(:order_rules)).to eq true }
-    it { expect(Segment.base_preloads.include?(:group_rules)).to eq true }
-    it { expect(Segment.base_preloads.include?({ field_rules: [:field, { field: [:field_definer] }] })).to eq true }
+    let!(:base_preloads) { [:field_rules, :order_rules, :group_rules, field_rules: [:field, { field: [:field_definer] }]] }
+    it { expect(Segment.base_preloads).to eq base_preloads }
   end
 end
