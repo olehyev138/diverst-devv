26c364da0fd2d2e8be3305a827a73eeecb376d53
devst05 <1726271@johnabbottcollege.net>
2020-07-31 14:32:16 -0400

    updated social link actions spec

--- spec/models/social_link/actions_spec.rb
+++ spec/models/social_link/actions_spec.rb
@@ -2,16 +2,21 @@ require 'rails_helper'
 
 RSpec.describe SocialLink::Actions, type: :model do
   describe 'base_preloads' do
-    it { expect(SocialLink.base_preloads.include?(:author)).to eq true }
-    it { expect(SocialLink.base_preloads.include?({ author: [:field_data,
-                                                             :enterprise,
-                                                             :user_groups,
-                                                             :user_role,
-                                                             :news_links,
-                                                             :avatar_attachment,
-                                                             :avatar_blob,
-                                                             { enterprise: [:theme, :mobile_fields], field_data: [:field, { field: [:field_definer] }] }
-    ] })).to eq true
+    let(:base_preloads) { [
+                            :author,
+                            author:
+                                [:field_data,
+                                 :enterprise,
+                                 :user_groups,
+                                 :user_role,
+                                 :news_links,
+                                 :avatar_attachment,
+                                 :avatar_blob,
+                                 enterprise: [:theme, :mobile_fields],
+                                 field_data: [:field, field: [:field_definer]]
+                                 ]
+                            ]
     }
+    it { expect(SocialLink.base_preloads).to eq base_preloads }
   end
 end
