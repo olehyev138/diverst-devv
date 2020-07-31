4fb9d21e47dbd21fe366d987113df873f6b41e77
devst05 <1726271@johnabbottcollege.net>
2020-07-31 12:40:09 -0400

    updated news feed link actions spec

--- spec/models/news_feed_link/actions_spec.rb
+++ spec/models/news_feed_link/actions_spec.rb
@@ -2,87 +2,84 @@ require 'rails_helper'
 
 RSpec.describe NewsFeedLink::Actions, type: :model do
   describe 'valid_scopes' do
-    it { expect(NewsFeedLink.valid_scopes.include?('approved')).to eq true }
-    it { expect(NewsFeedLink.valid_scopes.include?('pending')).to eq true }
-    it { expect(NewsFeedLink.valid_scopes.include?('combined_news_links')).to eq true }
-    it { expect(NewsFeedLink.valid_scopes.include?('not_archived')).to eq true }
-    it { expect(NewsFeedLink.valid_scopes.include?('archived')).to eq true }
-    it { expect(NewsFeedLink.valid_scopes.include?('pinned')).to eq true }
-    it { expect(NewsFeedLink.valid_scopes.include?('not_pinned')).to eq true }
+    let!(:valid_scopes) { %w(approved pending combined_news_links not_archived archived pinned not_pinned) }
+    it { expect(NewsFeedLink.valid_scopes).to eq valid_scopes }
   end
 
   describe 'base_preloads' do
-    it { expect(NewsFeedLink.base_preloads.include?(:group_message)).to eq true }
-    it { expect(NewsFeedLink.base_preloads.include?(:news_link)).to eq true }
-    it { expect(NewsFeedLink.base_preloads.include?(:social_link)).to eq true }
-    it { expect(NewsFeedLink.base_preloads.include?(:news_feed)).to eq true }
-    it { expect(NewsFeedLink.base_preloads.include?(:views)).to eq true }
-    it { expect(NewsFeedLink.base_preloads.include?(:likes)).to eq true }
-    it { expect(NewsFeedLink.base_preloads.include?(group_message: [:owner,
-                                                                    :group,
-                                                                    :comments,
-                                                                    { comments: [:author,
-                                                                                 { author: [:field_data,
-                                                                                            :enterprise,
-                                                                                            :user_groups,
-                                                                                            :user_role,
-                                                                                            :news_links,
-                                                                                            :avatar_attachment,
-                                                                                            :avatar_blob,
-                                                                                            { enterprise: [:theme, :mobile_fields],
-                                                                                              field_data: [:field, { field: [:field_definer] }] }] }],
-                                                                      group: [:enterprise],
-                                                                      owner: [:field_data,
-                                                                              :enterprise,
-                                                                              :user_groups,
-                                                                              :user_role,
-                                                                              :news_links,
-                                                                              :avatar_attachment,
-                                                                              :avatar_blob,
-                                                                              { enterprise: [:theme, :mobile_fields],
-                                                                                field_data: [:field, { field: [:field_definer] }] }] }],
-                                                    news_link: [:author,
-                                                                :group,
-                                                                :comments,
-                                                                :photos,
-                                                                :picture_attachment,
-                                                                { author: [:field_data,
-                                                                           :enterprise,
-                                                                           :user_groups,
-                                                                           :user_role,
-                                                                           :news_links,
-                                                                           :avatar_attachment,
-                                                                           :avatar_blob,
-                                                                           { enterprise: [:theme, :mobile_fields],
-                                                                             field_data: [:field, { field: [:field_definer] }] }],
-                                                                  comments: [:author,
-                                                                             { author: [:field_data,
-                                                                                        :enterprise,
-                                                                                        :user_groups,
-                                                                                        :user_role,
-                                                                                        :news_links,
-                                                                                        :avatar_attachment,
-                                                                                        :avatar_blob,
-                                                                                        { enterprise: [:theme, :mobile_fields],
-                                                                                          field_data: [:field, { field: [:field_definer] }] }] }],
-                                                                  group: [:enterprise] }],
-                                                    social_link: [:author,
-                                                                  { author: [:field_data,
-                                                                             :enterprise,
-                                                                             :user_groups,
-                                                                             :user_role,
-                                                                             :news_links,
-                                                                             :avatar_attachment,
-                                                                             :avatar_blob,
-                                                                             { enterprise: [:theme, :mobile_fields],
-                                                                               field_data: [:field, { field: [:field_definer] }] }] }])).to eq true
+    let!(:base_preloads) { [
+        :group_message,
+        :news_link,
+        :social_link,
+        :news_feed,
+        :views,
+        :likes,
+        group_message: [:owner,
+                        :group,
+                        :comments,
+                        { comments: [:author,
+                                     { author: [:field_data,
+                                                :enterprise,
+                                                :user_groups,
+                                                :user_role,
+                                                :news_links,
+                                                :avatar_attachment,
+                                                :avatar_blob,
+                                                { enterprise: [:theme, :mobile_fields],
+                                                  field_data: [:field, { field: [:field_definer] }] }] }],
+                          group: [:enterprise],
+                          owner: [:field_data,
+                                  :enterprise,
+                                  :user_groups,
+                                  :user_role,
+                                  :news_links,
+                                  :avatar_attachment,
+                                  :avatar_blob,
+                                  { enterprise: [:theme, :mobile_fields],
+                                    field_data: [:field, { field: [:field_definer] }] }] }],
+        news_link: [:author,
+                    :group,
+                    :comments,
+                    :photos,
+                    :picture_attachment,
+                    { author: [:field_data,
+                               :enterprise,
+                               :user_groups,
+                               :user_role,
+                               :news_links,
+                               :avatar_attachment,
+                               :avatar_blob,
+                               { enterprise: [:theme, :mobile_fields],
+                                 field_data: [:field, { field: [:field_definer] }] }],
+                      comments: [:author,
+                                 { author: [:field_data,
+                                            :enterprise,
+                                            :user_groups,
+                                            :user_role,
+                                            :news_links,
+                                            :avatar_attachment,
+                                            :avatar_blob,
+                                            { enterprise: [:theme, :mobile_fields],
+                                              field_data: [:field, { field: [:field_definer] }] }] }],
+                      group: [:enterprise] }],
+        social_link: [:author,
+                      { author: [:field_data,
+                                 :enterprise,
+                                 :user_groups,
+                                 :user_role,
+                                 :news_links,
+                                 :avatar_attachment,
+                                 :avatar_blob,
+                                 { enterprise: [:theme, :mobile_fields],
+                                   field_data: [:field, { field: [:field_definer] }] }] }]
+    ]
     }
+    it { expect(NewsFeedLink.base_preloads).to eq base_preloads }
   end
 
   describe 'base_left_joins' do
-    it { expect(NewsFeedLink.base_left_joins.include?(:group_message)).to eq true }
-    it { expect(NewsFeedLink.base_left_joins.include?(:news_link)).to eq true }
-    it { expect(NewsFeedLink.base_left_joins.include?(:social_link)).to eq true }
+    let!(:base_left_joins) { [:group_message, :news_link, :social_link] }
+    it { expect(NewsFeedLink.base_left_joins).to eq base_left_joins }
   end
 
   describe 'order_string' do
