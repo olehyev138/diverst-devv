2899be5b7e0a9a7b2fae75ac4efcf8776957bf7c
devst05 <1726271@johnabbottcollege.net>
2020-08-03 10:42:32 -0400

    reformatted array methods for resource action spec

--- spec/models/resource/actions_spec.rb
+++ spec/models/resource/actions_spec.rb
@@ -2,12 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Resource::Actions, type: :model do
   describe 'valid_scopes' do
-    let!(:valid_scopes) { %w(not_archived archived) }
+    let(:valid_scopes) {
+      %w(
+          not_archived
+          archived
+      )
+    }
+
     it { expect(Resource.valid_scopes).to eq valid_scopes }
   end
 
   describe 'base_preloads' do
-    let!(:base_preloads) { [:folder, :file_attachment] }
+    let(:base_preloads) {
+      [
+          :folder,
+          :file_attachment
+      ]
+    }
+
     it { expect(Resource.base_preloads).to eq base_preloads }
   end
 end
