f07a5e131aa75da844aff856c7b052e1576121e4
AlexOxorn <alex.oxorn@gmail.com>
2020-08-03 16:53:01 -0400

    application policy tests

--- spec/serializers/application_record_serializer_spec.rb
+++ spec/serializers/application_record_serializer_spec.rb
@@ -20,18 +20,43 @@ RSpec.describe ApplicationRecordSerializer, type: :serializer do
         def attributes; { a: a, b: b, c: c } end
       end
     }
-    let!(:policy) {
-      class ModelOnePolicy < ApplicationPolicy
+    let(:policy) {
+      Class.new(ApplicationPolicy) do
         def index?;   true  end
+
         def show?;    false end
+
         def create?;  true  end
+
         def manage?;  false end
+
         def update?;  true  end
+
         def destroy?; false end
+
         def custom1?; true  end
+
         def custom2?; false end
       end
-      ModelOnePolicy
+    }
+    let(:other_policy) {
+      Class.new(ApplicationPolicy) do
+        def index?;   false end
+
+        def show?;    true  end
+
+        def create?;  false end
+
+        def manage?;  true  end
+
+        def update?;  false end
+
+        def destroy?; true  end
+
+        def custom1?; false end
+
+        def custom2?; true  end
+      end
     }
 
     before :each do
@@ -60,9 +85,9 @@ RSpec.describe ApplicationRecordSerializer, type: :serializer do
         end
 
         it 'has :a and :b as attributes on first instantiation' do
-          expect(@serializer1._attributes).to     be_empty
+          expect(@serializer1._attributes).to be_empty
 
-          serializer = @serializer1.new(model.new({a: 1, b: 2, c: 3}))
+          serializer = @serializer1.new(model.new({ a: 1, b: 2, c: 3 }))
 
           expect(@serializer1._attributes).to     include :a
           expect(@serializer1._attributes).to     include :b
@@ -78,11 +103,11 @@ RSpec.describe ApplicationRecordSerializer, type: :serializer do
 
       context 'serialize_all_fields is false, nothing is defined' do
         it 'has :a and :b as attributes on first instantiation' do
-          expect(@serializer1._attributes).to     be_empty
+          expect(@serializer1._attributes).to be_empty
 
-          serializer = @serializer1.new(model.new({a: 1, b: 2, c: 3}))
+          serializer = @serializer1.new(model.new({ a: 1, b: 2, c: 3 }))
 
-          expect(serializer.attributes).to        be_empty
+          expect(serializer.attributes).to be_empty
         end
       end
 
@@ -101,7 +126,7 @@ RSpec.describe ApplicationRecordSerializer, type: :serializer do
           expect(@serializer1._attributes).to     include :e
           expect(@serializer1._attributes).to_not include :f
 
-          a = model.new({a: 1, b: 2, c: 3})
+          a = model.new({ a: 1, b: 2, c: 3 })
           serializer = @serializer1.new(a)
 
           expect(serializer.serializable_hash[:d]).to eq 2
@@ -123,6 +148,155 @@ RSpec.describe ApplicationRecordSerializer, type: :serializer do
           expect(@serializer1._attributes).to     include :d
           expect(@serializer1._attributes).to     include :e
           expect(@serializer1._attributes).to_not include :f
+        end
+      end
+    end
+
+    describe 'Policy' do
+      context 'with default policies' do
+        let(:serializer) do
+          Class.new(ApplicationRecordSerializer) do
+            attributes :permissions
+          end
+        end
+        context 'where policy passed as option' do
+          context 'and where current_user is defined' do
+            let(:serializer_instance) do
+              new_object = model.new
+              serializer.new(new_object, scope: {}, policy: policy.new(create(:user), new_object))
+            end
+
+            it 'serializes the permissions correctly' do
+              expect(serializer_instance.as_json).to eq({
+                                                            permissions: {
+                                                                show?: false,
+                                                                update?: true,
+                                                                destroy?: false,
+                                                            }
+                                                        })
+            end
+          end
+        end
+        context 'where policy determined with Pundit Policy Finder' do
+          before :each do
+            allow_any_instance_of(Pundit::PolicyFinder).to receive(:policy).and_return(other_policy)
+          end
+          context 'and where current_user is defined' do
+            let(:serializer_instance) do
+              serializer.new(model.new, scope: serializer_scopes(create(:user)))
+            end
+
+            it 'serializes the permissions correctly' do
+              expect(serializer_instance.as_json).to eq({
+                                                            permissions: {
+                                                              show?: true,
+                                                              update?: false,
+                                                              destroy?: true,
+                                                            }
+                                                        })
+            end
+          end
+          context 'and where current_user is not defined' do
+            let(:serializer_instance) do
+              serializer.new(model.new, scope: {})
+            end
+
+            it 'serializes the permissions correctly' do
+              expect(serializer_instance.as_json).to eq({
+                                                            permissions: {
+                                                                show?: false,
+                                                                update?: false,
+                                                                destroy?: false,
+                                                            }
+                                                        })
+            end
+          end
+        end
+      end
+      context 'with custom policies' do
+        let(:serializer) do
+          Class.new(ApplicationRecordSerializer) do
+            attributes :permissions
+            def policies
+              [
+                  :index?,
+                  :show?,
+                  :create?,
+                  :manage?,
+                  :update?,
+                  :destroy?,
+                  :custom1?,
+                  :custom2?
+              ]
+            end
+          end
+        end
+        context 'where policy passed as option' do
+          context 'and where current_user is defined' do
+            let(:serializer_instance) do
+              new_object = model.new
+              serializer.new(new_object, scope: {}, policy: policy.new(create(:user), new_object))
+            end
+            it 'serializes the permissions correctly' do
+              expect(serializer_instance.as_json).to eq({
+                                                            permissions: {
+                                                                index?: true,
+                                                                show?: false,
+                                                                create?: true,
+                                                                manage?: false,
+                                                                update?: true,
+                                                                destroy?: false,
+                                                                custom1?: true,
+                                                                custom2?: false,
+                                                            }
+                                                        })
+            end
+          end
+        end
+        context 'where policy determined with Pundit Policy Finder' do
+          before do
+            allow_any_instance_of(Pundit::PolicyFinder).to receive(:policy).and_return(other_policy)
+          end
+          context 'and where current_user is defined' do
+            let(:serializer_instance) do
+              serializer.new(model.new, scope: serializer_scopes(create(:user)))
+            end
+
+            it 'serializes the permissions correctly' do
+              expect(serializer_instance.as_json).to eq({
+                                                            permissions: {
+                                                                index?: false,
+                                                                show?: true,
+                                                                create?: false,
+                                                                manage?: true,
+                                                                update?: false,
+                                                                destroy?: true,
+                                                                custom1?: false,
+                                                                custom2?: true,
+                                                            }
+                                                        })
+            end
+          end
+          context 'and where current_user is not defined' do
+            let(:serializer_instance) do
+              serializer.new(model.new, scope: {})
+            end
+
+            it 'serializes the permissions correctly' do
+              expect(serializer_instance.as_json).to eq({
+                                                            permissions: {
+                                                              index?: false,
+                                                              show?: false,
+                                                              create?: false,
+                                                              manage?: false,
+                                                              update?: false,
+                                                              destroy?: false,
+                                                              custom1?: false,
+                                                              custom2?: false,
+                                                          }
+                                                        })
+            end
+          end
         end
       end
     end
