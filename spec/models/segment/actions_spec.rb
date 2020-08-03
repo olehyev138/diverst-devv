378ea80495af5ca0d69cdf947298b0cdf02c70b1
devst05 <1726271@johnabbottcollege.net>
2020-08-03 10:42:54 -0400

    reformatted array methods for segment action spec

--- spec/models/segment/actions_spec.rb
+++ spec/models/segment/actions_spec.rb
@@ -2,7 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Segment::Actions, type: :model do
   describe 'base_preloads' do
-    let!(:base_preloads) { [:field_rules, :order_rules, :group_rules, field_rules: [:field, { field: [:field_definer] }]] }
+    let(:base_preloads) {
+      [
+          :field_rules,
+          :order_rules,
+          :group_rules,
+          field_rules: [
+              :field,
+              field: [:field_definer]
+          ]
+      ]
+    }
+
     it { expect(Segment.base_preloads).to eq base_preloads }
   end
 end
