4a0bc96e3fe12f7421a86d30b6ca26134a7f210c
devst05 <1726271@johnabbottcollege.net>
2020-08-03 10:43:39 -0400

    reformatted array methods for social link action spec

--- spec/models/social_link/actions_spec.rb
+++ spec/models/social_link/actions_spec.rb
@@ -2,21 +2,29 @@ require 'rails_helper'
 
 RSpec.describe SocialLink::Actions, type: :model do
   describe 'base_preloads' do
-    let(:base_preloads) { [
-                            :author,
-                            author:
-                                [:field_data,
-                                 :enterprise,
-                                 :user_groups,
-                                 :user_role,
-                                 :news_links,
-                                 :avatar_attachment,
-                                 :avatar_blob,
-                                 enterprise: [:theme, :mobile_fields],
-                                 field_data: [:field, field: [:field_definer]]
-                                 ]
-                            ]
+    let(:base_preloads) {
+      [
+          :author,
+          author: [
+              :field_data,
+              :enterprise,
+              :user_groups,
+              :user_role,
+              :news_links,
+              :avatar_attachment,
+              :avatar_blob,
+              enterprise: [
+                  :theme,
+                  :mobile_fields
+              ],
+              field_data: [
+                  :field,
+                  field: [:field_definer]
+              ]
+          ]
+      ]
     }
+
     it { expect(SocialLink.base_preloads).to eq base_preloads }
   end
 end
