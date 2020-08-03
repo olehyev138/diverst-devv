2d34e9ed7b40c54d6d566f8e9c5666ae86f96b53
Liana <provlia1@videotron.ca>
2020-08-03 13:36:31 -0400

    Skip job when a record has been deleted before the job has been executed

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -3,5 +3,9 @@ class ApplicationJob < ActiveJob::Base
   # retry_on ActiveRecord::Deadlocked
 
   # Most jobs are safe to ignore if the underlying records are no longer available
+
   # discard_on ActiveJob::DeserializationError
+  rescue_from(ActiveJob::DeserializationError) do |exception|
+    # Skip job when record is not found
+  end
 end
