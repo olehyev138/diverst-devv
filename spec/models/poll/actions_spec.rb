6428844e5e5795fc9938b55d6d3a41b1508af66d
devst05 <1726271@johnabbottcollege.net>
2020-08-03 10:23:40 -0400

    reformatted array methods for poll action spec

--- spec/models/poll/actions_spec.rb
+++ spec/models/poll/actions_spec.rb
@@ -2,7 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Poll::Actions, type: :model do
   describe 'base_preloads' do
-    let!(:base_preloads) { [:fields, :groups, :segments, :enterprise] }
+    let(:base_preloads) {
+      [
+          :fields,
+          :groups,
+          :segments,
+          :enterprise
+      ]
+    }
+
     it { expect(Poll.base_preloads).to eq base_preloads }
   end
 end
--- spec/models/poll_response/actions_spec.rb
+++ spec/models/poll_response/actions_spec.rb
@@ -2,7 +2,18 @@ require 'rails_helper'
 
 RSpec.describe PollResponse::Actions, type: :model do
   describe 'base_preloads' do
-    let!(:base_preloads) { [:poll, :user, :field_data, field_data: [:field, { field: [:field_definer] }]] }
+    let(:base_preloads) {
+      [
+          :poll,
+          :user,
+          :field_data,
+          field_data: [
+              :field,
+              field: [:field_definer]
+          ]
+      ]
+    }
+
     it { expect(PollResponse.base_preloads).to eq base_preloads }
   end
 end
