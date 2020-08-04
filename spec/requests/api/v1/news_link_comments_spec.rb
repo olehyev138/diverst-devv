d84ad6187555307e9d4c0031e4c8d248d466298a
devst05 <1726271@johnabbottcollege.net>
2020-08-04 16:16:04 -0400

    updated news_link_comments_spec

--- spec/requests/api/v1/news_link_comments_spec.rb
+++ spec/requests/api/v1/news_link_comments_spec.rb
@@ -25,9 +25,16 @@ RSpec.describe "#{model.pluralize}", type: :request do
   end
 
   describe '#show' do
-    it 'gets a item' do
+    before do
       get "/api/v1/#{route}/#{item.id}", headers: headers
+    end
+
+    it 'gets a item' do
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'JSON body response contains expected attributes' do
+      expect(JSON.parse(response.body)['news_link_comment']).to include('id' => item.id)
     end
 
     it 'captures the error' do
@@ -38,9 +45,18 @@ RSpec.describe "#{model.pluralize}", type: :request do
   end
 
   describe '#create' do
+    let!(:new_item) { build(route.singularize.to_sym) }
+    before do
+      post "/api/v1/#{route}", params: { "#{route.singularize}" => new_item.attributes }, headers: headers
+    end
+
     it 'creates an item' do
-      post "/api/v1/#{route}", params: { "#{route.singularize}": build(route.singularize.to_sym).attributes }, headers: headers
       expect(response).to have_http_status(201)
+    end
+
+    it 'contains expected attributes' do
+      id = JSON.parse(response.body)['news_link_comment']['id']
+      expect(model.constantize.find(id).content).to eq new_item.content
     end
 
     it 'captures the error when BadRequestException' do
@@ -53,9 +69,17 @@ RSpec.describe "#{model.pluralize}", type: :request do
   end
 
   describe '#update' do
+    let!(:new_params) { { id: item.id, content: 'test' } }
+    before do
+      patch "/api/v1/#{route}/#{item.id}", params: { "#{route.singularize}" => new_params }, headers: headers
+    end
+
     it 'updates an item' do
-      patch "/api/v1/#{route}/#{item.id}", params: { "#{route.singularize}": item.attributes }, headers: headers
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'contains expected attributes' do
+      expect(model.constantize.find(item.id).content).to eq new_params[:content]
     end
 
     it 'captures the error when BadRequestException' do
