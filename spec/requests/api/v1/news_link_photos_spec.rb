98b60aeed000b64be20770babfd1a4b99e1ea808
devst05 <1726271@johnabbottcollege.net>
2020-08-05 14:23:47 -0400

    updated news_link_photos_spec

--- spec/requests/api/v1/news_link_photos_spec.rb
+++ spec/requests/api/v1/news_link_photos_spec.rb
@@ -8,15 +8,23 @@ RSpec.describe "#{model.pluralize}", type: :request do
   let(:api_key) { create(:api_key) }
   let(:user) { create(:user, password: 'password', enterprise: enterprise) }
   let(:group) { create(:group, enterprise: enterprise) }
-  let!(:item) { create(model.constantize.table_name.singularize.to_sym) }
+  let(:news_link) { create(:news_link, author: user, group: group) }
+  let!(:item) { create(model.constantize.table_name.singularize.to_sym, news_link: news_link) }
   let(:route) { model.constantize.table_name }
   let(:jwt) { UserTokenService.create_jwt(user) }
   let(:headers) { { 'HTTP_DIVERST_APIKEY' => api_key.key, 'Diverst-UserToken' => jwt } }
 
   describe '#index' do
-    it 'gets all items' do
+    before do
       get "/api/v1/#{route}", headers: headers
+    end
+
+    it 'gets all items' do
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'JSON body response contains expected attributes' do
+      expect(JSON.parse(response.body)['page']['items'].first).to include('id' => item.id)
     end
 
     it 'captures the error' do
@@ -27,9 +35,16 @@ RSpec.describe "#{model.pluralize}", type: :request do
   end
 
   describe '#show' do
-    it 'gets a item' do
+    before do
       get "/api/v1/#{route}/#{item.id}", headers: headers
+    end
+
+    it 'gets a item' do
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'JSON body response contains expected attributes' do
+      expect(JSON.parse(response.body)['news_link_photo']).to include('id' => item.id)
     end
 
     it 'captures the error' do
@@ -40,12 +55,19 @@ RSpec.describe "#{model.pluralize}", type: :request do
   end
 
   describe '#create' do
-    it 'creates an item' do
-      params = build(route.singularize.to_sym).attributes
-      params[:file] = fixture_file_upload('spec/fixtures/files/verizon_logo.png', 'image/png')
+    let!(:new_item) { build(route.singularize.to_sym).attributes }
+    before do
+      new_item[:file] = fixture_file_upload('spec/fixtures/files/verizon_logo.png', 'image/png')
+      post "/api/v1/#{route}", params: { "#{route.singularize}" => new_item }, headers: headers
+    end
 
-      post "/api/v1/#{route}", params: { "#{route.singularize}" => params }, headers: headers
+    it 'creates an item' do
       expect(response).to have_http_status(201)
+    end
+
+    it 'contains file' do
+      id = JSON.parse(response.body)['news_link_photo']['id']
+      expect(model.constantize.find(id).file.attached?).to be true
     end
 
     it 'captures the error when BadRequestException' do
@@ -58,9 +80,19 @@ RSpec.describe "#{model.pluralize}", type: :request do
   end
 
   describe '#update' do
+    let!(:new_params) { { id: item.id, file: nil } }
+    let!(:old_file_location) { model.constantize.find(item.id).file_location }
+    before do
+      new_params[:file] = fixture_file_upload('spec/fixtures/files/verizon_logo.png', 'image/png')
+      patch "/api/v1/#{route}/#{item.id}", params: { "#{route.singularize}" => new_params }, headers: headers
+    end
+
     it 'updates an item' do
-      patch "/api/v1/#{route}/#{item.id}", params: { "#{route.singularize}" => item.attributes.merge({ file: AttachmentHelper.attachment_signed_id(item.file) }) }, headers: headers
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'contains expected attributes' do
+      expect(model.constantize.find(item.id).file_location).to_not eq old_file_location
     end
 
     it 'captures the error when BadRequestException' do
