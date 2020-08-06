acdf5a9ebc0dc0f4d1d508b6aea7ae1e0009bc2c
Liana <provlia1@videotron.ca>
2020-08-06 15:50:31 -0400

    Messages for GlobalSettings snackbars

--- client/app/containers/GlobalSettings/EnterpriseConfiguration/messages.js
+++ client/app/containers/GlobalSettings/EnterpriseConfiguration/messages.js
@@ -7,7 +7,7 @@
 import { defineMessages } from 'react-intl';
 
 export const scope = 'diverst.containers.GlobalSettings.Configurations';
-
+export const snackbar = 'diverst.snackbars.GlobalSettings.EnterpriseConfiguration'
 export default defineMessages({
   update: {
     id: `${scope}.button.update`,
@@ -98,4 +98,19 @@ export default defineMessages({
       id: `${scope}.settings.units.weeks`,
     },
   },
+  snackbars: {
+    errors: {
+      load: {
+        id: `${snackbar}.errors.load`,
+      },
+      update: {
+        id: `${snackbar}.errors.update`,
+      },
+    },
+    success: {
+      update: {
+        id: `${snackbar}.success.update`,
+      },
+    }
+  }
 });
