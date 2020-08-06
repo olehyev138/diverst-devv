c2a69d70c69e21661212cbf3d5a59c57c5321e25
devst05 <1726271@johnabbottcollege.net>
2020-08-06 15:00:32 -0400

    updated initiative_users_spec

--- spec/requests/api/v1/initiative_users_spec.rb
+++ spec/requests/api/v1/initiative_users_spec.rb
@@ -5,15 +5,24 @@ RSpec.describe "#{model.pluralize}", type: :request do
   let(:enterprise) { create(:enterprise) }
   let(:api_key) { create(:api_key) }
   let(:user) { create(:user, password: 'password', enterprise: enterprise) }
-  let!(:item) { create(model.constantize.table_name.singularize.to_sym) }
+  let(:group) { create(:group, enterprise: enterprise) }
+  let!(:initiative) { create(:initiative, owner_group: group) }
+  let!(:item) { create(model.constantize.table_name.singularize.to_sym, initiative: initiative) }
   let(:route) { model.constantize.table_name }
   let(:jwt) { UserTokenService.create_jwt(user) }
   let(:headers) { { 'HTTP_DIVERST_APIKEY' => api_key.key, 'Diverst-UserToken' => jwt } }
 
   describe '#index' do
+    before do
+      get "/api/v1/#{route}?initiative_id=#{initiative.id}", headers: headers
+    end
+
     it 'gets all items' do
-      get "/api/v1/#{route}", headers: headers
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'JSON body response contains expected attributes' do
+      expect(JSON.parse(response.body)['page']['items'].first).to include('id' => item.id)
     end
 
     it 'captures the error' do
@@ -24,9 +33,16 @@ RSpec.describe "#{model.pluralize}", type: :request do
   end
 
   describe '#show' do
-    it 'gets a item' do
+    before do
       get "/api/v1/#{route}/#{item.id}", headers: headers
+    end
+
+    it 'gets a item' do
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'JSON body response contains expected attributes' do
+      expect(JSON.parse(response.body)['initiative_user']).to include('id' => item.id)
     end
 
     it 'captures the error' do
@@ -37,9 +53,18 @@ RSpec.describe "#{model.pluralize}", type: :request do
   end
 
   describe '#create' do
+    let!(:new_item) { build(route.singularize.to_sym) }
+    before do
+      post "/api/v1/#{route}", params: { "#{route.singularize}" => new_item.attributes }, headers: headers
+    end
+
     it 'creates an item' do
-      post "/api/v1/#{route}", params: { "#{route.singularize}": build(route.singularize.to_sym).attributes }, headers: headers
       expect(response).to have_http_status(201)
+    end
+
+    it 'contains expected attributes' do
+      id = JSON.parse(response.body)['initiative_user']['id']
+      expect(model.constantize.find(id).user_id).to eq new_item.user_id
     end
 
     it 'captures the error when BadRequestException' do
@@ -52,13 +77,17 @@ RSpec.describe "#{model.pluralize}", type: :request do
   end
 
   describe '#update' do
+    let!(:new_params) { { id: item.id, attended: true, check_in_time: Time.now } }
+    before do
+      patch "/api/v1/#{route}/#{item.id}", params: { "#{route.singularize}" => new_params }, headers: headers
+    end
+
     it 'updates an item' do
-      patch "/api/v1/#{route}/#{item.id}", params: { "#{route.singularize}": { attended: true, check_in_time: Time.now } }, headers: headers
-      Clipboard.copy response.body
-      initiative_user = JSON.parse(response.body)['initiative_user']
-      expect(initiative_user['attended']).to eq(true)
-      expect(initiative_user['attended']).to_not be nil
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'contains expected attributes' do
+      expect(model.constantize.find(item.id).attended).to eq new_params[:attended]
     end
 
     it 'captures the error when BadRequestException' do
@@ -71,9 +100,17 @@ RSpec.describe "#{model.pluralize}", type: :request do
   end
 
   describe '#destroy' do
-    it 'deletes an item' do
+    before do
       delete "/api/v1/#{route}/#{item.id}", headers: headers
+    end
+
+    it 'deletes an item' do
       expect(response).to have_http_status(:no_content)
+    end
+
+    it 'returns nil' do
+      record = model.constantize.find(item.id) rescue nil
+      expect(record).to eq nil
     end
 
     it 'captures the error' do
@@ -84,9 +121,14 @@ RSpec.describe "#{model.pluralize}", type: :request do
   end
 
   describe '#join' do
-    it 'join a group' do
-      post "/api/v1/#{route}/join", params: { "#{route.singularize}" => build(route.singularize.to_sym).attributes }, headers: headers
+    let!(:event) { create(:initiative) }
+    let!(:new_params) { { initiative_id: event.id } }
+
+    it 'join an event' do
+      post "/api/v1/#{route}/join", params: { "#{route.singularize}" => new_params }, headers: headers
       expect(response).to have_http_status(:created)
+      id = JSON.parse(response.body)['initiative_user']['id']
+      expect(model.constantize.find(id).initiative_id).to eq new_params[:initiative_id]
     end
 
     it 'captures the error' do
@@ -103,9 +145,17 @@ RSpec.describe "#{model.pluralize}", type: :request do
   end
 
   describe '#leave' do
-    it 'leave a group' do
-      post "/api/v1/#{route}/leave", params: { "#{route.singularize}" => build(route.singularize.to_sym).attributes }, headers: headers
+    let!(:current_user) { create(:user, password: 'password', enterprise: enterprise) }
+    let!(:initiative_user) { create(model.constantize.table_name.singularize.to_sym, user: current_user) }
+    let!(:new_headers) { { 'HTTP_DIVERST_APIKEY' => api_key.key, 'Diverst-UserToken' => UserTokenService.create_jwt(current_user) } }
+
+    let!(:new_params) { { initiative_id: initiative_user.initiative_id } }
+
+    it 'leave an event' do
+      post "/api/v1/#{route}/leave", params: { "#{route.singularize}" => new_params }, headers: new_headers
       expect(response).to have_http_status(:no_content)
+      record = model.constantize.find_by(initiative_id: initiative_user.initiative_id, current_user: item.user_id) rescue nil
+      expect(record).to eq nil
     end
 
     it 'captures the error' do
