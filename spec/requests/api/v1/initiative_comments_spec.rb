7f12749d5c22f19d8f23565e9948f4c8f3dc36b9
devst05 <1726271@johnabbottcollege.net>
2020-08-06 09:41:18 -0400

    updated initiative_comments_spec

--- spec/requests/api/v1/initiative_comments_spec.rb
+++ spec/requests/api/v1/initiative_comments_spec.rb
@@ -5,15 +5,22 @@ RSpec.describe 'InitiativeComments', type: :request do
   let(:enterprise) { create(:enterprise) }
   let(:api_key) { create(:api_key) }
   let(:user) { create(:user, password: 'password', enterprise: enterprise) }
-  let!(:item) { create(model.constantize.table_name.singularize.to_sym) }
+  let!(:item) { create(model.constantize.table_name.singularize.to_sym, user: user) }
   let(:route) { model.constantize.table_name }
   let(:jwt) { UserTokenService.create_jwt(user) }
   let(:headers) { { 'HTTP_DIVERST_APIKEY' => api_key.key, 'Diverst-UserToken' => jwt } }
 
   describe '#index' do
-    it 'gets all items' do
+    before do
       get "/api/v1/#{route}", headers: headers
+    end
+
+    it 'gets all items' do
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'JSON body response contains expected attributes' do
+      expect(JSON.parse(response.body)['page']['items'].first).to include('id' => item.id)
     end
 
     it 'captures the error' do
@@ -24,9 +31,16 @@ RSpec.describe 'InitiativeComments', type: :request do
   end
 
   describe '#show' do
-    it 'gets a item' do
+    before do
       get "/api/v1/#{route}/#{item.id}", headers: headers
+    end
+
+    it 'gets a item' do
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'JSON body response contains expected attributes' do
+      expect(JSON.parse(response.body)['initiative_comment']).to include('id' => item.id)
     end
 
     it 'captures the error' do
@@ -37,9 +51,18 @@ RSpec.describe 'InitiativeComments', type: :request do
   end
 
   describe '#create' do
+    let!(:new_item) { build(route.singularize.to_sym, content: 'test') }
+    before do
+      post "/api/v1/#{route}", params: { "#{route.singularize}" => new_item.attributes }, headers: headers
+    end
+
     it 'creates an item' do
-      post "/api/v1/#{route}", params: { "#{route.singularize}" => build(route.singularize.to_sym).attributes }, headers: headers
-      expect(response).to have_http_status(:created)
+      expect(response).to have_http_status(201)
+    end
+
+    it 'contains expected attributes' do
+      id = JSON.parse(response.body)['initiative_comment']['id']
+      expect(model.constantize.find(id).content).to eq new_item.content
     end
 
     it 'captures the error when BadRequestException' do
@@ -52,9 +75,17 @@ RSpec.describe 'InitiativeComments', type: :request do
   end
 
   describe '#update' do
+    let!(:new_params) { { id: item.id, content: 'test content' } }
+    before do
+      patch "/api/v1/#{route}/#{item.id}", params: { "#{route.singularize}" => new_params }, headers: headers
+    end
+
     it 'updates an item' do
-      patch "/api/v1/#{route}/#{item.id}", params: { "#{route.singularize}" => item.attributes }, headers: headers
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'contains expected attributes' do
+      expect(model.constantize.find(item.id).content).to eq new_params[:content]
     end
 
     it 'captures the error when BadRequestException' do
@@ -66,9 +97,17 @@ RSpec.describe 'InitiativeComments', type: :request do
   end
 
   describe '#destroy' do
-    it 'deletes an item' do
+    before do
       delete "/api/v1/#{route}/#{item.id}", headers: headers
+    end
+
+    it 'deletes an item' do
       expect(response).to have_http_status(:no_content)
+    end
+
+    it 'returns nil' do
+      record = model.constantize.find(item.id) rescue nil
+      expect(record).to eq nil
     end
 
     it 'captures the error' do
