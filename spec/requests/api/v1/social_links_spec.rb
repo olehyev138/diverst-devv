6e70c163335d30236c8af8364585c3bc7902548a
devst05 <1726271@johnabbottcollege.net>
2020-08-07 11:43:46 -0400

    updated social_links_spec

--- spec/requests/api/v1/social_links_spec.rb
+++ spec/requests/api/v1/social_links_spec.rb
@@ -6,15 +6,22 @@ RSpec.describe "#{model.pluralize}", type: :request do
   let(:api_key) { create(:api_key) }
   let(:user) { create(:user, password: 'password', enterprise: enterprise) }
   let(:group) { create(:group, enterprise: enterprise) }
-  let!(:item) { create(:social_link) }
+  let!(:item) { create(:social_link, group: group, author: user) }
   let(:route) { 'social_links' }
   let(:jwt) { UserTokenService.create_jwt(user) }
   let(:headers) { { 'HTTP_DIVERST_APIKEY' => api_key.key, 'Diverst-UserToken' => jwt } }
 
   describe '#index' do
-    it 'gets all items' do
+    before do
       get "/api/v1/#{route}", headers: headers
+    end
+
+    it 'gets all items' do
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'JSON body response contains expected attributes' do
+      expect(JSON.parse(response.body)['page']['items'].first).to include('id' => item.id)
     end
 
     it 'captures the error' do
@@ -25,9 +32,16 @@ RSpec.describe "#{model.pluralize}", type: :request do
   end
 
   describe '#show' do
-    it 'gets a item' do
+    before do
       get "/api/v1/#{route}/#{item.id}", headers: headers
+    end
+
+    it 'gets an item' do
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'JSON body response contains expected attributes' do
+      expect(JSON.parse(response.body)['social_link']).to include('id' => item.id)
     end
 
     it 'captures the error' do
@@ -38,9 +52,18 @@ RSpec.describe "#{model.pluralize}", type: :request do
   end
 
   describe '#create' do
+    let!(:new_item) { build(route.singularize.to_sym) }
+    before do
+      post "/api/v1/#{route}", params: { "#{route.singularize}" => new_item.attributes }, headers: headers
+    end
+
     it 'creates an item' do
-      post "/api/v1/#{route}", params: { "#{route.singularize}" => build(route.singularize.to_sym).attributes }, headers: headers
       expect(response).to have_http_status(201)
+    end
+
+    it 'contains expected attributes' do
+      id = JSON.parse(response.body)['social_link']['id']
+      expect(model.constantize.find(id).group_id).to eq new_item.group_id
     end
 
     it 'captures the error when BadRequestException' do
@@ -53,9 +76,17 @@ RSpec.describe "#{model.pluralize}", type: :request do
   end
 
   describe '#update' do
+    let!(:new_params) { { id: item.id, url: 'https://twitter.com/CNN/status/1291758499563700225' } }
+    before do
+      patch "/api/v1/#{route}/#{item.id}", params: { "#{route.singularize}" => new_params }, headers: headers
+    end
+
     it 'updates an item' do
-      patch "/api/v1/#{route}/#{item.id}", params: { "#{route.singularize}" => item.attributes }, headers: headers
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'contains expected attributes' do
+      expect(model.constantize.find(item.id).url).to eq new_params[:url]
     end
 
     it 'captures the error when BadRequestException' do
@@ -68,9 +99,17 @@ RSpec.describe "#{model.pluralize}", type: :request do
   end
 
   describe '#destroy' do
-    it 'deletes an item' do
+    before do
       delete "/api/v1/#{route}/#{item.id}", headers: headers
+    end
+
+    it 'deletes an item' do
       expect(response).to have_http_status(:no_content)
+    end
+
+    it 'returns nil' do
+      record = model.constantize.find(item.id) rescue nil
+      expect(record).to eq nil
     end
 
     it 'captures the error' do
