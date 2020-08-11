e802a8e6fa31f1907ee9f1a034a23d921b407a32
devst05 <1726271@johnabbottcollege.net>
2020-08-11 09:10:33 -0400

    added spacing

--- spec/requests/api/v1/group_messages_segments_spec.rb
+++ spec/requests/api/v1/group_messages_segments_spec.rb
@@ -55,6 +55,7 @@ RSpec.describe 'GroupMessagesSegments', type: :request do
 
   describe '#create' do
     let!(:new_item) { build(route.singularize.to_sym) }
+
     before do
       post "/api/v1/#{route}", params: { "#{route.singularize}" => new_item.attributes }, headers: headers
     end
@@ -80,6 +81,7 @@ RSpec.describe 'GroupMessagesSegments', type: :request do
   describe '#update' do
     let!(:new_segment) { create(:segment) }
     let!(:new_params) { { id: item.id, segment_id: new_segment.id } }
+
     before do
       patch "/api/v1/#{route}/#{item.id}", params: { "#{route.singularize}" => new_params }, headers: headers
     end
