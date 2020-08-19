2916785e658be04166b8b77d4f8ac83cc3e57deb
devst05 <1726271@johnabbottcollege.net>
2020-08-19 15:23:24 -0400

    improved files object

--- client/app/utils/fileInputHelpers.js
+++ client/app/utils/fileInputHelpers.js
@@ -3,24 +3,18 @@ import DiverstFormattedMessage from 'components/Shared/DiverstFormattedMessage';
 import messages from 'components/Shared/DiverstFileInput/messages';
 
 const supportedFiles = {
-  png: { type: 'image/png', message: <DiverstFormattedMessage {...messages.fileTypes.png} /> },
-  jpg: { type: 'image/jpg', message: <DiverstFormattedMessage {...messages.fileTypes.jpg} /> },
-  jpeg: { type: 'image/jpeg', message: <DiverstFormattedMessage {...messages.fileTypes.jpeg} /> },
-  svg: { type: 'image/svg', message: <DiverstFormattedMessage {...messages.fileTypes.svg} /> },
-  gif: { type: 'image/gif', message: <DiverstFormattedMessage {...messages.fileTypes.gif} /> },
-  pdf: { type: 'application/pdf', message: <DiverstFormattedMessage {...messages.fileTypes.pdf} /> },
-  csv: { type: 'text/csv', message: <DiverstFormattedMessage {...messages.fileTypes.csv} /> },
-  xml: { type: 'text/xml', message: <DiverstFormattedMessage {...messages.fileTypes.xml} /> },
-  json: { type: 'application/json', message: <DiverstFormattedMessage {...messages.fileTypes.json} /> },
-  zip: { type: 'application/zip', message: <DiverstFormattedMessage {...messages.fileTypes.zip} /> },
-  plain: { type: 'text/plain', message: <DiverstFormattedMessage {...messages.fileTypes.plain} /> },
-  html: { type: 'text/html', message: <DiverstFormattedMessage {...messages.fileTypes.html} /> },
-};
-const supportedImageFiles = {
-  png: { type: 'image/png', message: <DiverstFormattedMessage {...messages.fileTypes.png} /> },
-  jpg: { type: 'image/jpg', message: <DiverstFormattedMessage {...messages.fileTypes.jpg} /> },
-  jpeg: { type: 'image/jpeg', message: <DiverstFormattedMessage {...messages.fileTypes.jpeg} /> },
-  svg: { type: 'image/svg', message: <DiverstFormattedMessage {...messages.fileTypes.svg} /> },
+  png: { type: 'image/png', message: <DiverstFormattedMessage {...messages.fileTypes.png} />, category: 'image' },
+  jpg: { type: 'image/jpg', message: <DiverstFormattedMessage {...messages.fileTypes.jpg} />, category: 'image' },
+  jpeg: { type: 'image/jpeg', message: <DiverstFormattedMessage {...messages.fileTypes.jpeg} />, category: 'image' },
+  svg: { type: 'image/svg', message: <DiverstFormattedMessage {...messages.fileTypes.svg} />, category: 'image' },
+  gif: { type: 'image/gif', message: <DiverstFormattedMessage {...messages.fileTypes.gif} />, category: 'other' },
+  pdf: { type: 'application/pdf', message: <DiverstFormattedMessage {...messages.fileTypes.pdf} />, category: 'other' },
+  csv: { type: 'text/csv', message: <DiverstFormattedMessage {...messages.fileTypes.csv} />, category: 'other' },
+  xml: { type: 'text/xml', message: <DiverstFormattedMessage {...messages.fileTypes.xml} />, category: 'other' },
+  json: { type: 'application/json', message: <DiverstFormattedMessage {...messages.fileTypes.json} />, category: 'other' },
+  zip: { type: 'application/zip', message: <DiverstFormattedMessage {...messages.fileTypes.zip} />, category: 'other' },
+  plain: { type: 'text/plain', message: <DiverstFormattedMessage {...messages.fileTypes.plain} />, category: 'other' },
+  html: { type: 'text/html', message: <DiverstFormattedMessage {...messages.fileTypes.html} />, category: 'other' },
 };
 
 export function getAllSupportedFileTypes() {
@@ -32,11 +26,11 @@ export function getAllSupportedFileKeys() {
 }
 
 export function getSupportedImageFileTypes() {
-  return Object.values(supportedImageFiles).map(obj => obj.type);
+  return Object.values(supportedFiles).filter(item => item.category === 'image').map(obj => obj.type);
 }
 
 export function getSupportedImageFileKeys() {
-  return Object.keys(supportedImageFiles);
+  return Object.keys(supportedFiles).filter(key => supportedFiles[key].category === 'image');
 }
 
 export function getFileMessages(fileType) {
