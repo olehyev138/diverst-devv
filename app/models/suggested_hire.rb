daba0d3db1b8bd676e0d539cf010fbb3a53049f1
Derek Owusu-Frimpong <deepsky_5@live.com>
2020-08-28 15:20:32 +0000

    make close button on modal more prominent; provide email field to suggest hire modal [skip ci]

--- app/models/suggested_hire.rb
+++ app/models/suggested_hire.rb
@@ -1,22 +1,13 @@
 class SuggestedHire < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
-  belongs_to :suggested_hire, class_name: 'User', foreign_key: :suggested_hire_id
   has_many :user_reward_actions
-  
-  validates :suggested_hire_id, presence: true
+
+  validates :email, uniqueness: true, format: { with: Devise.email_regexp }
   has_attached_file :resume, s3_permissions: 'private'
   validates_attachment_content_type :resume,
                                     content_type: ['text/plain', 'application/pdf'],
                                     message: 'This format is not supported'
-
-  def avatar
-    suggested_hire.avatar unless suggested_hire.nil?
-  end
-
-  def name 
-    suggested_hire.name unless suggested_hire.nil?
-  end
 
   def resume_extension
     File.extname(resume_file_name)[1..-1].downcase
--- spec/models/suggested_hire_spec.rb
+++ spec/models/suggested_hire_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe SuggestedHire, type: :model do
-  let(:suggested_hire) { build(:suggested_hire, suggested_hire_id: create(:user).id) }
+  let(:suggested_hire) { build(:suggested_hire, email: 'derek@diverst.com') }
 
   describe 'tests associations and validations' do
     it { expect(suggested_hire).to belong_to(:user) }
     it { expect(suggested_hire).to belong_to(:group) }
-    it { expect(suggested_hire).to belong_to(:suggested_hire).class_name('User').with_foreign_key(:suggested_hire_id) }
-    it { expect(suggested_hire).to validate_presence_of(:suggested_hire_id) }
+    it { expect(suggested_hire).to validate_uniqueness_of(:email) }
     it { expect(suggested_hire).to have_attached_file(:resume) }
     it { expect(suggested_hire).to validate_attachment_content_type(:resume).allowing('text/plain', 'application/pdf') }
   end
 
   describe '#resume_extension' do
     it "returns '' " do
-      suggested_hire = create(:suggested_hire, suggested_hire_id: create(:user).id)
+      suggested_hire = create(:suggested_hire, email: 'derek@diverst.com')
       expect(suggested_hire.resume_extension).to eq('')
     end
   end
