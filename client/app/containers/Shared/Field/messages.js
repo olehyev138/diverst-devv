616071709cfff25df39ae5f56ad01c59295cf7ab
Liana <provlia1@videotron.ca>
2020-09-02 18:17:53 -0400

    Saga messages fixed

--- client/app/containers/Shared/Field/messages.js
+++ client/app/containers/Shared/Field/messages.js
@@ -7,7 +7,7 @@
 import { defineMessages } from 'react-intl';
 
 export const scope = 'diverst.containers.Field';
-export const snackbar = 'diverst.snackbars.Shared.Field';
+export const snackbar = 'diverst.snackbars.Shared.Fields';
 
 export default defineMessages({
   newTextField: {
