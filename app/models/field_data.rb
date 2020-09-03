9601cf41a572a1de56f9a6fa041557995a78e76e
AlexOxorn <alex.oxorn@gmail.com>
2020-09-03 11:42:26 -0400

    add field data validation for numeric fields

--- app/models/field_data.rb
+++ app/models/field_data.rb
@@ -6,6 +6,7 @@ class FieldData < ApplicationRecord
 
   validates_presence_of :field
   validates_presence_of :field_user
+  validates :validate_numeric_limit, if: -> { field.is_a? NumericField }
 
   def deserialized_data
     field.deserialize_value(data)
@@ -16,6 +17,14 @@ class FieldData < ApplicationRecord
   def same_parent
     unless field.field_definer_id == field_user.field_definer_id
       errors.add(:field, 'Field and field_user must have same parent')
+    end
+  end
+
+  def validate_numeric_limit
+    if data.to_i > field.max
+      errors.add(:data, "can't be greater than the max value")
+      elsif data.to_i < field.min
+      errors.add(:data, "can't be less than the min value")
     end
   end
 end
