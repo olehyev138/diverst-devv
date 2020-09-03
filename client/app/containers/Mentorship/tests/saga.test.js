4b5986f4efc61f0572d3b7ed7b30b21f97810b98
Liana <provlia1@videotron.ca>
2020-09-03 12:33:35 -0400

    Comment out empty saga test

--- client/app/containers/Mentorship/tests/saga.test.js
+++ client/app/containers/Mentorship/tests/saga.test.js
@@ -1,37 +1,37 @@
-import {
-  getUser,
-  getUsers,
-  updateUser
-} from '../saga';
-
-import {
-  getUsersSuccess, getUsersError,
-  getUserSuccess, getUserError,
-  updateUserSuccess, updateUserError,
-} from 'containers/Mentorship/actions';
-
-import { push } from 'connected-react-router';
-import { ROUTES } from 'containers/Shared/Routes/constants';
-import recordSaga from 'utils/recordSaga';
-import * as Notifiers from 'containers/Shared/Notifier/actions';
-import api from 'api/api';
-
-import messages from '../messages';
-import { intl } from 'containers/Shared/LanguageProvider/GlobalLanguageProvider';
-
-api.users.all = jest.fn();
-api.users.get = jest.fn();
-api.users.update = jest.fn();
-
-beforeEach(() => {
-  jest.resetAllMocks();
-});
-
-const budget = {
-  id: 5,
-};
-
-// TODO: Testing to be done : Manual implementation due to additional serializer
-describe('Mentorship saga', () => {
-
-});
+// import {
+//   getUser,
+//   getUsers,
+//   updateUser
+// } from '../saga';
+//
+// import {
+//   getUsersSuccess, getUsersError,
+//   getUserSuccess, getUserError,
+//   updateUserSuccess, updateUserError,
+// } from 'containers/Mentorship/actions';
+//
+// import { push } from 'connected-react-router';
+// import { ROUTES } from 'containers/Shared/Routes/constants';
+// import recordSaga from 'utils/recordSaga';
+// import * as Notifiers from 'containers/Shared/Notifier/actions';
+// import api from 'api/api';
+//
+// import messages from '../messages';
+// import { intl } from 'containers/Shared/LanguageProvider/GlobalLanguageProvider';
+//
+// api.users.all = jest.fn();
+// api.users.get = jest.fn();
+// api.users.update = jest.fn();
+//
+// beforeEach(() => {
+//   jest.resetAllMocks();
+// });
+//
+// const budget = {
+//   id: 5,
+// };
+//
+// // TODO: Testing to be done : Manual implementation due to additional serializer
+// describe('Mentorship saga', () => {
+//
+// });
