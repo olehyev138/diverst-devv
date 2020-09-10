fdbe038cfc4e58601a3b6888f6d97186214e2989
Mark Roy <markr4848@gmail.com>
2020-09-10 14:52:29 -0400

    Update tests for SSOLandingPage index

--- client/app/containers/Session/SSOLandingPage/tests/index.test.js
+++ client/app/containers/Session/SSOLandingPage/tests/index.test.js
@@ -1,6 +1,6 @@
 /**
  *
- * Tests for LoginPage
+ * Tests for SSOLandingPage
  *
  *
  */
@@ -17,50 +17,35 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import 'utils/mockReactRouterHooks';
 
-import { LoginPage } from 'containers/Session/LoginPage/index';
-import LoginForm from 'components/Session/LoginForm';
-import EnterpriseForm from 'components/Session/EnterpriseForm';
+import { SSOLandingPage } from 'containers/Session/SSOLandingPage/index';
+import SSOLanding from 'components/Session/SSOLanding';
 
-describe('<LoginPage />', () => {
+describe('<SSOLandingPage />', () => {
   it('Expect to not log errors in console', () => {
     const spy = jest.spyOn(global.console, 'error');
-    const wrapper = shallow(<LoginPage />);
+    const props = {
+      enterprise: { id: 1, has_enabled_saml: true },
+      showSnackbar: jest.fn(),
+      refresh: jest.fn(),
+      ssoLinkBegin: jest.fn(),
+      ssoLoginBegin: jest.fn(),
+    };
+    const wrapper = shallow(<SSOLandingPage {...props} />);
+    wrapper.setProps(props);
 
     expect(spy).not.toHaveBeenCalled();
     expect(wrapper.exists()).toBe(true);
-    expect(wrapper.find(EnterpriseForm).length).toBe(0);
-    expect(wrapper.find(LoginForm).length).toBe(1);
-  });
-
-  it('Displays the password form', () => {
-    const wrapper = shallow(<LoginPage />);
-    wrapper.setProps({
-      enterprise: { id: 1, has_enabled_saml: false }
-    });
-
-    expect(wrapper.find(LoginForm).length).toBe(1);
-  });
-
-  // TODO: useEffect & shallow rendering dont work together
-  xit('Calls ssoLinkBegin', () => {
-    const wrapper = shallow(<LoginPage />);
-    const props = {
-      enterprise: { id: 1, has_enabled_saml: true },
-      ssoLinkBegin: () => {}
-    };
-    const spy = jest.spyOn(props, 'ssoLinkBegin');
-
-    wrapper.setProps(props);
-
-    expect(spy).toHaveBeenCalledTimes(1);
-    expect(wrapper.find(EnterpriseForm).length).toBe(1);
+    expect(wrapper.find(SSOLanding).length).toBe(1);
   });
 
   xit('Calls refresh', () => {
-    const wrapper = shallow(<LoginPage />);
+    const wrapper = shallow(<SSOLandingPage />);
     const props = {
-      refresh: () => {},
-      showSnackbar: () => {},
+      enterprise: { id: 1, has_enabled_saml: true },
+      showSnackbar: jest.fn(),
+      refresh: jest.fn(),
+      ssoLinkBegin: jest.fn(),
+      ssoLoginBegin: jest.fn(),
       location: {
         search: '?errorMessage=Hello, How are you'
       }
@@ -76,11 +61,15 @@ describe('<LoginPage />', () => {
   });
 
   xit('Calls ssoLoginBegin', () => {
-    const wrapper = shallow(<LoginPage />);
+    const wrapper = shallow(<SSOLandingPage />);
     const props = {
-      ssoLoginBegin: () => {},
+      enterprise: { id: 1, has_enabled_saml: true },
+      showSnackbar: jest.fn(),
+      refresh: jest.fn(),
+      ssoLinkBegin: jest.fn(),
+      ssoLoginBegin: jest.fn(),
       location: {
-        search: '?userToken=Hello&policyGroupId=1'
+        search: '?userToken=Hello'
       }
     };
 
