4b3f85aa7af647a11acec3fcf0ce20dd0fce70a3
Mark Roy <markr4848@gmail.com>
2020-09-10 14:52:35 -0400

    Update tests for SSOLanding index

--- client/app/components/Session/SSOLanding/tests/index.test.js
+++ client/app/components/Session/SSOLanding/tests/index.test.js
@@ -1,86 +1,26 @@
 /**
  *
- * Tests for LoginForm
+ * Tests for SSOLanding
+ *
+ * @see https://github.com/react-boilerplate/react-boilerplate/tree/master/docs/testing
  *
  */
 
-
 import React from 'react';
 import { shallowWithIntl, loadTranslation } from 'enzyme-react-intl';
-import { unwrap } from '@material-ui/core/test-utils';
-
-import { LoginFormInner, StyledLoginForm } from '../index';
-import PropTypes from 'prop-types';
-
-const LoginFormNaked = unwrap(StyledLoginForm);
-
-/**
- * TODO:
- *  - test correct actions are dispatched
- *  - test validations
- */
-
+import { SSOLanding } from '../index';
+import { intl } from 'tests/mocks/react-intl';
 
 loadTranslation('./app/translations/en.json');
-
-const innerProps = {
-  classes: {},
-  handleSubmit: jest.fn,
-  handleChange: jest.fn,
-  handleBlur: jest.fn,
-  errors: {
-    email: '',
-    password: ''
-  },
-  touched: {
-    email: false,
-    password: false,
-  },
-  values: {
-    email: '',
-    password: ''
-  }
-};
-
 const props = {
   classes: {},
-  width: '',
-  loginBegin: jest.fn(),
-  email: 'email@email.com',
-  formErrors: {
-    email: null,
-    password: null,
-  },
+  handleEnter: jest.fn(),
 };
+describe('<SSOLanding />', () => {
+  it('Expect to not log errors in console', () => {
+    const spy = jest.spyOn(global.console, 'error');
+    const wrapper = shallowWithIntl(<SSOLanding intl={intl} {...props} />);
 
-describe('<LoginForm />', () => {
-  describe('<LoginFormInner />', () => {
-    it('Expect to not log errors in console', () => {
-      const spy = jest.spyOn(global.console, 'error');
-      const wrapper = shallowWithIntl(<LoginFormInner {...innerProps} />);
-
-      expect(spy).not.toHaveBeenCalled();
-    });
-
-    it('Should render and match the snapshot', () => {
-      const wrapper = shallowWithIntl(<LoginFormInner {...innerProps} />);
-
-      expect(wrapper).toMatchSnapshot();
-    });
-  });
-
-  describe('Formik', () => {
-    it('Expect to not log errors in console', () => {
-      const spy = jest.spyOn(global.console, 'error');
-      const wrapper = shallowWithIntl(<LoginFormNaked {...props} />);
-
-      expect(spy).not.toHaveBeenCalled();
-    });
-
-    it('Should render and match the snapshot', () => {
-      const wrapper = shallowWithIntl(<LoginFormNaked {...props} />);
-
-      expect(wrapper).toMatchSnapshot();
-    });
+    expect(spy).not.toHaveBeenCalled();
   });
 });
