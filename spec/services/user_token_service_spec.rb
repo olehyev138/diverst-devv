d883fe7d462d2d2d8bc47808547d9db12ba05e95
AlexOxorn <alex.oxorn@gmail.com>
2020-09-17 11:24:51 -0400

    token service spec

---
+++ spec/services/user_token_service_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe UserTokenService, type: :service do
+  describe '#verify_jwt_token' do
+    it 'verifies the user' do
+      user = create(:user)
+      token = UserTokenService.create_jwt user
+      token_user = UserTokenService.verify_jwt_token token
+      expect(token_user).to eq(user)
+    end
+    it 'raises an error because of a decode error' do
+      expect { UserTokenService.verify_jwt_token '02032013' }.to raise_error BadRequestException
+    end
+    it 'raises an error because user doesnt exist' do
+      user = create(:user)
+      token = UserTokenService.create_jwt user
+      user.reload.destroy
+      expect { UserTokenService.verify_jwt_token token }.to raise_error BadRequestException
+    end
+  end
+end
