16abbe5eb1d66cfa8295c8650e8ecd95528be12e
Matthew Baldwin <matt@diverst.com>
2020-09-18 14:12:26 -0400

    Update cci config & work on dangerfile

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,6 @@
 import {message, danger} from 'danger'
 
-const modifiedMD = danger.git.modified_files.join('- ')
-message('Changed Files in this PR: \n - ' + modifiedMD)
+if (!github.pr_body.include("jira"))
+    warn('Missing Jira Issue Link!')
+
+
