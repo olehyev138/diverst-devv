bed246a2837b333ca07678d8e40c4cd476a3dbb5
AlexOxorn <alex.oxorn@gmail.com>
2020-10-02 14:38:47 -0400

    fix tests

--- spec/models/news_link/actions_spec.rb
+++ spec/models/news_link/actions_spec.rb
@@ -2,52 +2,21 @@ require 'rails_helper'
 
 RSpec.describe NewsLink::Actions, type: :model do
   describe 'base_preloads' do
-    let(:base_preloads) {
+    let(:base_preloads) do
       [
           :author,
           :group,
           :comments,
           :photos,
           :picture_attachment,
-          author: [
-              :field_data,
-              :enterprise,
-              :user_groups,
-              :user_role,
-              :news_links,
-              :avatar_attachment,
-              :avatar_blob,
-              enterprise: [
-                  :theme,
-                  :mobile_fields
-              ],
-              field_data: [
-                  :field,
-                  field: [:field_definer]
+          {
+              comments: [
+                  :author
               ]
-          ],
-          comments: [
-              :author,
-              author: [:field_data,
-                       :enterprise,
-                       :user_groups,
-                       :user_role,
-                       :news_links,
-                       :avatar_attachment,
-                       :avatar_blob,
-                       enterprise: [
-                           :theme,
-                           :mobile_fields
-                       ],
-                       field_data: [
-                           :field,
-                           field: [:field_definer]
-                       ]
-              ]
-          ],
-          group: [:enterprise]
+          }
       ]
-    }
+
+    end
 
     it { expect(NewsLink.base_preloads).to eq base_preloads }
   end
