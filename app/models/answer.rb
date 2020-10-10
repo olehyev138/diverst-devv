38f57fbd5f000515ac9e3f1b063a12d14e891a64
Matthew Baldwin <matt@diverst.com>
2020-10-10 19:41:08 -0400

    Remove legacy paperclip validations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -18,19 +18,9 @@ class Answer < ApplicationRecord
 
   # TODO Remove after Paperclip to ActiveStorage migration
   has_attached_file :supporting_document_paperclip, s3_permissions: 'private'
-
-  # Legacy attribute
   has_attached_file :video_upload_paperclip, s3_permissions: 'private'
-  validates_attachment_content_type :video_upload_paperclip,
-                                    content_type: ['video/mp4', 'video/webm'],
-                                    message: 'This format is not supported', if: Proc.new { |a| a.content.blank? && a.supporting_document.blank? }
-
   has_attached_file :supporting_document_from_sponsor_paperclip, s3_permissions: 'private'
-  validates_attachment_content_type :supporting_document_from_sponsor_paperclip,
-                                    content_type: ['text/plain', 'application/pdf'],
-                                    message: 'This format is not supported'
 
-  # Legacy attributes - active storage equivalent
   has_one_attached :video_upload
   has_one_attached :supporting_document_from_sponsor
 
@@ -43,7 +33,7 @@ class Answer < ApplicationRecord
   validates :content, presence: true
   validates :contributing_group, presence: true
 
-  # Legacy
+  # Legacy paperclip validations
   validates_length_of :video_upload_content_type, maximum: 191
   validates_length_of :video_upload_file_name, maximum: 191
   validates_length_of :supporting_document_content_type, maximum: 191
