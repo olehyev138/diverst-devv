96d98dc7f54a331e0e1e0732437ce727da34ac90
AlexOxorn <alex.oxorn@gmail.com>
2020-10-14 16:04:44 -0400

    group message comments

--- app/serializers/group_message_serializer.rb
+++ app/serializers/group_message_serializer.rb
@@ -1,7 +1,13 @@
 class GroupMessageSerializer < ApplicationRecordSerializer
-  attributes :comments_count, :news_feed_link_id, :owner
+  attributes :news_feed_link_id, :owner
 
-  has_many :comments
+  attributes :comments, if: :show_action?
+
+  def comments
+    object.comments.map do |comment|
+      GroupMessageCommentSerializer.new(comment).as_json
+    end
+  end
 
   def news_feed_link_id
     object.news_feed_link.id
