ccccd0cd108e4009996b44276ffa741c0c943693
AlexOxorn <alex.oxorn@gmail.com>
2020-10-14 11:47:19 -0400

    question_serializer.rb

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,6 +1,14 @@
 class QuestionSerializer < ApplicationRecordSerializer
-  attributes :title, :description, :campaign, :conclusion, :solved_at, :permissions
-  has_many :answers
+  attributes :title, :description, :conclusion, :solved_at, :permissions
+
+  attributes_with_permission :campaign, :answers, if: :show_action?
+
+  def answers
+    object.answers.map do |ans|
+      AnswerSerializer.new(ans, **instance_options)
+    end
+  end
+
   def serialize_all_fields
     true
   end
