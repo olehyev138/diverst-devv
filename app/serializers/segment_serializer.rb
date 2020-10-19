bbaa02550994162ac9ccf9cf43228a6be803708c
AlexOxorn <alex.oxorn@gmail.com>
2020-10-19 10:22:20 -0400

    segment fix

--- app/serializers/segment_serializer.rb
+++ app/serializers/segment_serializer.rb
@@ -1,9 +1,25 @@
 class SegmentSerializer < ApplicationRecordSerializer
-  attributes :owner, :all_rules_count, :active_users_filter, :limit, :permissions
+  attributes :owner, :active_users_filter, :limit, :permissions
 
-  has_many :field_rules
-  has_many :order_rules
-  has_many :group_rules
+  attributes_with_permission :all_rules_count, :field_rules, :order_rules, :group_rules, if: :show_action?
+
+  def field_rules
+    object.field_rules.map do |fr|
+      SegmentFieldRuleSerializer.new(fr, **instance_options).as_json
+    end
+  end
+
+  def order_rules
+    object.order_rules.map do |orl|
+      SegmentOrderRuleSerializer.new(orl, **instance_options).as_json
+    end
+  end
+
+  def group_rules
+    object.group_rules.map do |gr|
+      SegmentGroupScopeRuleSerializer.new(gr, **instance_options).as_json
+    end
+  end
 
   def serialize_all_fields
     true
