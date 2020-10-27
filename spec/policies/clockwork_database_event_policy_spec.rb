77b1d6ae40c3c13fa12d7011d01ea6f978040584
AlexOxorn <alex.oxorn@gmail.com>
2020-10-27 10:59:09 -0400

    lint

--- spec/policies/clockwork_database_event_policy_spec.rb
+++ spec/policies/clockwork_database_event_policy_spec.rb
@@ -19,15 +19,15 @@ RSpec.describe ClockworkDatabaseEventPolicy, type: :policy do
 
   describe 'for users with access' do
     context 'when manage_all is true' do
-        before { user.policy_group.update manage_all: true }
-        it { is_expected.to permit_actions([:update, :index, :show]) }
-        it { is_expected.to forbid_actions([:create, :destroy]) }
+      before { user.policy_group.update manage_all: true }
+      it { is_expected.to permit_actions([:update, :index, :show]) }
+      it { is_expected.to forbid_actions([:create, :destroy]) }
     end
 
     context 'when branding manage is true' do
-        before { user.policy_group.update branding_manage: true }
-        it { is_expected.to permit_actions([:update, :index, :show]) }
-        it { is_expected.to forbid_actions([:create, :destroy]) }
+      before { user.policy_group.update branding_manage: true }
+      it { is_expected.to permit_actions([:update, :index, :show]) }
+      it { is_expected.to forbid_actions([:create, :destroy]) }
     end
   end
 
