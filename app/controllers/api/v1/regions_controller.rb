dcd2bed41cc3fe3d7be037f138a0b4be181839d3
Mark Roy <markr4848@gmail.com>
2020-10-27 00:05:12 -0400

    Make RegionsController with the payload defined

---
+++ app/controllers/api/v1/regions_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::RegionsController < DiverstController
+
+  private
+
+  def payload
+    params
+    .require(klass.symbol)
+    .permit(
+      :name,
+      :short_description,
+      :description,
+      :home_message,
+      :private,
+      :parent_id,
+      :position,
+      child_ids: [],
+    )
+  end
+end
