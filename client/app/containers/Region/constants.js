7069625907ecb4982607e9a4a0f96cbe27caf41c
Mark Roy <markr4848@gmail.com>
2020-10-27 00:28:46 -0400

    Create initial Region redux constants & actions

---
+++ client/app/containers/Region/constants.js
@@ -0,0 +1,29 @@
+/*
+ *
+ * Region constants
+ *
+ */
+
+export const GET_REGIONS_BEGIN = 'app/Region/GET_REGIONS_BEGIN';
+export const GET_REGIONS_SUCCESS = 'app/Region/GET_REGIONS_SUCCESS';
+export const GET_REGIONS_ERROR = 'app/Region/GET_REGIONS_ERROR';
+
+export const GET_REGION_BEGIN = 'app/Region/GET_REGION_BEGIN';
+export const GET_REGION_SUCCESS = 'app/Region/GET_REGION_SUCCESS';
+export const GET_REGION_ERROR = 'app/Region/GET_REGION_ERROR';
+
+export const CREATE_REGION_BEGIN = 'app/Region/CREATE_REGION_BEGIN';
+export const CREATE_REGION_SUCCESS = 'app/Region/CREATE_REGION_SUCCESS';
+export const CREATE_REGION_ERROR = 'app/Region/CREATE_REGION_ERROR';
+
+export const UPDATE_REGION_BEGIN = 'app/Region/UPDATE_REGION_BEGIN';
+export const UPDATE_REGION_SUCCESS = 'app/Region/UPDATE_REGION_SUCCESS';
+export const UPDATE_REGION_ERROR = 'app/Region/UPDATE_REGION_ERROR';
+
+export const DELETE_REGION_BEGIN = 'app/Region/DELETE_REGION_BEGIN';
+export const DELETE_REGION_SUCCESS = 'app/Region/DELETE_REGION_SUCCESS';
+export const DELETE_REGION_ERROR = 'app/Region/DELETE_REGION_ERROR';
+
+export const REGION_LIST_UNMOUNT = 'app/Region/REGION_LIST_UNMOUNT';
+export const REGION_ALL_UNMOUNT = 'app/Region/REGION_ALL_UNMOUNT';
+export const REGION_FORM_UNMOUNT = 'app/Region/REGION_FORM_UNMOUNT';
