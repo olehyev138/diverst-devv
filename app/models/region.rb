cf452dc80e148bdc700ef1bdabaa58debc2ec75a
Mark Roy <markr4848@gmail.com>
2020-10-27 00:02:58 -0400

    Create Region model file with associations, basic validations, and relevant shared Group methods, scopes, and hooks

---
+++ app/models/region.rb
@@ -0,0 +1,32 @@
+class Region < ApplicationRecord
+  include PublicActivity::Common
+  include Region::Actions
+
+  belongs_to :parent, class_name: 'Group'
+  has_many :children, class_name: 'Group', foreign_key: :region_id
+
+  validates :name, presence: true
+  validates_length_of :name, maximum: 191
+  validates_length_of :home_message, maximum: 65535
+  validates_length_of :short_description, maximum: 65535
+  validates_length_of :description, maximum: 65535
+
+  after_create :set_position
+
+  scope :is_private,        -> { where(private: true) }
+  scope :non_private,       -> { where(private: false) }
+
+  def to_s
+    name
+  end
+
+  def file_safe_name
+    name.gsub(/[^0-9A-Za-z.\-]/, '_')
+  end
+
+  private
+
+  def set_position
+    self.position = self.id
+  end
+end
