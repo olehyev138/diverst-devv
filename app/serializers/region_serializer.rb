b07b7b0d2b2fa2e7d5c202c368001922e4836f70
Mark Roy <markr4848@gmail.com>
2020-10-28 18:15:00 -0400

    Add relevant attributes to region_serializer

--- app/serializers/region_serializer.rb
+++ app/serializers/region_serializer.rb
@@ -1,7 +1,7 @@
 class RegionSerializer < ApplicationRecordSerializer
   attributes :id, :permissions
 
-  attributes_with_permission :name, :short_description, :private, if: :family?
+  attributes_with_permission :name, :short_description, :description, :parent_id, :private, :position, :children, if: :family?
 
   attributes_with_permission :name, :short_description, :description, :parent_id, :private, :home_message, :position,
                              :children, :parent, if: :show?
