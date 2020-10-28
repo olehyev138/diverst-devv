024427407b0aab2ca28c4fba3f23e01f542dc2fd
Mark Roy <markr4848@gmail.com>
2020-10-28 18:16:39 -0400

    Implement 'group_regions' controller action for RegionsController which retrieves the regions for a given `group_id` (TODO: authorization)

--- app/controllers/api/v1/regions_controller.rb
+++ app/controllers/api/v1/regions_controller.rb
@@ -1,4 +1,13 @@
 class Api::V1::RegionsController < DiverstController
+  def group_regions
+    group = Group.find(params[:group_id])
+    # TODO base_authorize(group)
+
+    render status: 200, json: klass.index(self.diverst_request, params.permit!.merge({ parent_id: group.id })), family: true
+  rescue => e
+    raise BadRequestException.new(e.message)
+  end
+
   private
 
   def payload
