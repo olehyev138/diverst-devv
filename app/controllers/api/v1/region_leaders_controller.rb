ae0117b7e384ec34744123631bae13d9d3a6881c
AlexOxorn <alex.oxorn@gmail.com>
2020-10-28 16:26:43 -0400

    create test

--- app/controllers/api/v1/region_leaders_controller.rb
+++ app/controllers/api/v1/region_leaders_controller.rb
@@ -2,10 +2,16 @@ class Api::V1::RegionLeadersController < DiverstController
   before_action :polymorphize_leader_of_query, only: [:show, :index]
   before_action :polymorphize_leader_of_commit, only: [:create, :update]
 
+  protected def klass
+    GroupLeader
+  end
+
   private
 
   def polymorphize_leader_of_commit
-    polymorphize_leader_of_query(params[:region_leader])
+    params[:group_leader] = params[:region_leader]
+    params.delete(:region_leader)
+    polymorphize_leader_of_query(params[:group_leader])
   end
 
   def polymorphize_leader_of_query(temp_params)
