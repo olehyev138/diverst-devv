7673a8a487a96b07050bdeef63a62616994a0493
AlexOxorn <alex.oxorn@gmail.com>
2020-10-28 12:38:46 -0400

    permissions backend (RegionPolicy) (updated)

--- app/policies/region_policy.rb
+++ app/policies/region_policy.rb
@@ -1,12 +1,15 @@
 class RegionPolicy < GroupBasePolicy
+  attr_reader :parent_policy
+
   def initialize(*args)
     super
     @parent_policy = GroupPolicy.new(@user, @group) if @group
   end
 
   def group_association; :parent end
-
   def group_id_param; :parent_id end
+
+  delegate :index?, :show?, :create?, :update?, :destroy?, :manage?, to: :parent_policy
 
   private def method_missing(symbol, *args)
     if @parent_policy.respond_to?(symbol)
