0aab439642e192643cd562c0e5be0a31091ec16d
Mark Roy <markr4848@gmail.com>
2020-10-29 19:20:36 -0400

    Update GroupRegionsListPage & associated list component with relevant CRUD links, using proper permissions & translation messages, improve styling

--- client/app/components/Region/GroupRegionsList/index.js
+++ client/app/components/Region/GroupRegionsList/index.js
@@ -9,6 +9,7 @@ import { compose } from 'redux';
 import PropTypes from 'prop-types';
 import DiverstFormattedMessage from 'components/Shared/DiverstFormattedMessage';
 import { withStyles } from '@material-ui/core/styles';
+import { intlShape } from 'react-intl';
 
 import WrappedNavLink from 'components/Shared/WrappedNavLink';
 import { ROUTES } from 'containers/Shared/Routes/constants';
@@ -16,13 +17,16 @@ import { ROUTES } from 'containers/Shared/Routes/constants';
 import messages from 'containers/Region/messages';
 
 import {
-  Button, Card, CardContent, Grid, Divider, Box, Typography,
+  Button, Card, CardContent, CardActions, Grid, Divider, Box, Typography,
 } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 
 import DiverstShowLoader from 'components/Shared/DiverstShowLoader';
 import DiverstLoader from 'components/Shared/DiverstLoader';
 import DiverstPagination from 'components/Shared/DiverstPagination';
+
+import Permission from 'components/Shared/DiverstPermission';
+import { permission } from 'utils/permissionsHelpers';
 
 const styles = theme => ({
   pageTitle: {
@@ -34,10 +38,13 @@ const styles = theme => ({
   emptyText: {
     fontSize: '1.25rem',
   },
+  errorButton: {
+    color: theme.palette.error.main,
+  },
 });
 
 export function GroupRegionsList(props) {
-  const { classes, group, regions, isLoading, isRegionsLoading } = props;
+  const { classes, group, regions, isLoading, isRegionsLoading, intl } = props;
 
   return (
     <React.Fragment>
@@ -46,7 +53,7 @@ export function GroupRegionsList(props) {
           <React.Fragment>
             <Grid container spacing={3} justify='space-between'>
               <Grid item>
-                <Typography color='secondary' variant='h5' className={classes.pageTitle}>
+                <Typography color='textSecondary' variant='h5' className={classes.pageTitle}>
                   <Box component='span' className={classes.groupName}>
                     {group.name}
                   </Box>
@@ -55,16 +62,18 @@ export function GroupRegionsList(props) {
                 </Typography>
               </Grid>
               <Grid item>
-                <Button
-                  variant='contained'
-                  to={ROUTES.admin.manage.groups.regions.new.path(group.id)}
-                  color='primary'
-                  size='large'
-                  startIcon={<AddIcon />}
-                  component={WrappedNavLink}
-                >
-                  <DiverstFormattedMessage {...messages.new} />
-                </Button>
+                <Permission show={permission(props, 'groups_create')}>
+                  <Button
+                    variant='contained'
+                    to={ROUTES.admin.manage.groups.regions.new.path(group.id)}
+                    color='primary'
+                    size='large'
+                    startIcon={<AddIcon />}
+                    component={WrappedNavLink}
+                  >
+                    <DiverstFormattedMessage {...messages.new} />
+                  </Button>
+                </Permission>
               </Grid>
             </Grid>
             <Box mb={3} />
@@ -74,12 +83,45 @@ export function GroupRegionsList(props) {
                   {regions && regions.map((region, i) => (
                     <Grid item key={region.id} xs={12}>
                       <CardContent>
-                        <Box pt={2} pb={2}>
+                        <Box pt={1} pb={1}>
                           <Typography color='primary' variant='h6'>
                             {region.name}
                           </Typography>
+                          {region.short_description && (
+                            <React.Fragment>
+                              <Box pt={1} />
+                              <Typography color='textSecondary'>
+                                {region.short_description}
+                              </Typography>
+                            </React.Fragment>
+                          )}
                         </Box>
                       </CardContent>
+                      <CardActions>
+                        <Permission show={permission(group, 'update?')}>
+                          <Button
+                            size='small'
+                            color='primary'
+                            to={ROUTES.admin.manage.groups.regions.edit.path(group.id, region.id)}
+                            component={WrappedNavLink}
+                          >
+                            <DiverstFormattedMessage {...messages.edit} />
+                          </Button>
+                        </Permission>
+                        <Permission show={permission(group, 'destroy?')}>
+                          <Button
+                            size='small'
+                            className={classes.errorButton}
+                            onClick={() => {
+                              /* eslint-disable-next-line no-alert, no-restricted-globals */
+                              if (confirm(intl.formatMessage(messages.delete_confirm)))
+                                props.deleteRegionBegin({ region_id: region.id, group_id: group.id });
+                            }}
+                          >
+                            <DiverstFormattedMessage {...messages.delete} />
+                          </Button>
+                        </Permission>
+                      </CardActions>
                       {i < regions.length - 1 && (
                         <Divider />
                       )}
@@ -88,7 +130,7 @@ export function GroupRegionsList(props) {
                   {regions && regions.length <= 0 && (
                     <Grid item xs={12} align='center'>
                       <Box pt={3} pb={3} pl={1} pr={1}>
-                        <Typography color='secondary' className={classes.emptyText}>
+                        <Typography color='textSecondary' className={classes.emptyText}>
                           <DiverstFormattedMessage {...messages.empty} />
                         </Typography>
                       </Box>
@@ -119,10 +161,13 @@ GroupRegionsList.propTypes = {
   group: PropTypes.object,
   regions: PropTypes.array,
   regionTotal: PropTypes.number,
+  deleteRegionBegin: PropTypes.func,
   isLoading: PropTypes.bool,
   isRegionsLoading: PropTypes.bool,
   handlePagination: PropTypes.func,
   params: PropTypes.object,
+  permissions: PropTypes.object,
+  intl: intlShape.isRequired,
 };
 
 export default compose(
