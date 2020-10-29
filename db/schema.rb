a28c98b33402b95d936bb86a1a4789c1670499be
AlexOxorn <alex.oxorn@gmail.com>
2020-10-29 16:35:34 -0400

    add migration to add new default roles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_133233) do
+ActiveRecord::Schema.define(version: 2020_10_29_202350) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.string "name", null: false
