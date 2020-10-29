f9a36bdac488493b8a756c5bfae5e2ba287dead6
AlexOxorn <alex.oxorn@gmail.com>
2020-10-29 10:59:12 -0400

    region policy

--- app/policies/region_policy.rb
+++ app/policies/region_policy.rb
@@ -3,14 +3,22 @@ class RegionPolicy < GroupBasePolicy
 
   def initialize(*args)
     super
-    @parent_policy = GroupPolicy.new(@user, @group) if @group
+    @parent_policy = GroupPolicy.new(@user, @group || Group)
   end
 
   def group_association; :parent end
 
   def group_id_param; :parent_id end
 
-  delegate :index?, :show?, :create?, :update?, :destroy?, :manage?, to: :parent_policy
+  delegate :show?, :update?, :destroy?, :manage?, to: :parent_policy
+
+  def index?
+    group.present? ? parent_policy.show? : parent_policy.index?
+  end
+
+  def create?
+    parent_policy.update?
+  end
 
   private def method_missing(symbol, *args)
     if parent_policy.respond_to?(symbol)
