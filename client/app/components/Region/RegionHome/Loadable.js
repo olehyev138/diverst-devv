343c487af40cf94fc0a13b5be8c2486cce0fd533
Mark Roy <markr4848@gmail.com>
2020-11-01 23:12:29 -0500

    Implement RegionHome component with basic description display + basic test & loadable files

---
+++ client/app/components/Region/RegionHome/Loadable.js
@@ -0,0 +1,9 @@
+/**
+ *
+ * Asynchronously loads the component for RegionHome
+ *
+ */
+
+import loadable from 'utils/loadable';
+
+export default loadable(() => import('components/Region/RegionHome/index'));
