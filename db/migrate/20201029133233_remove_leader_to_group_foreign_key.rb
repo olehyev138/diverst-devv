b36c4facaf5e1a3754bbe823cf09af6819eb365d
AlexOxorn <alex.oxorn@gmail.com>
2020-11-04 10:05:07 -0500

    fix migration (ROLLBACK TO BEFORE `AddPolymorphismToGroupLeader` BEFORE PULLING)

--- db/migrate/20201029133233_remove_leader_to_group_foreign_key.rb
+++
@@ -1,9 +0,0 @@
-class RemoveLeaderToGroupForeignKey < ActiveRecord::Migration[5.2]
-  def up
-    remove_foreign_key 'group_leaders', 'groups'
-  end
-
-  def down
-    add_foreign_key 'group_leaders', 'groups', column: 'leader_of_id'
-  end
-end
