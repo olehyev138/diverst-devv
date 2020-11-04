03fe1fca7d7dce75c5feddbad2664aa9cb6b1357
AlexOxorn <alex.oxorn@gmail.com>
2020-11-04 14:15:48 -0500

    fix leader serializer

--- app/serializers/group_leader_serializer.rb
+++ app/serializers/group_leader_serializer.rb
@@ -1,7 +1,7 @@
 class GroupLeaderSerializer < ApplicationRecordSerializer
   attributes :user_role, :position_name, :pending_member_notifications_enabled,
              :pending_comments_notifications_enabled, :pending_posts_notifications_enabled,
-             :default_group_contact, :user, :group, :region
+             :default_group_contact, :user, :group, :region, :group_id, :region_id
 
   def serialize_all_fields
     true
