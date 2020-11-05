7cecc5312138538e71627e95a3818e784d64e422
AlexOxorn <alex.oxorn@gmail.com>
2020-11-05 10:16:18 -0500

    make migration reversible

--- db/migrate/20201103175354_remove_not_null_restriction_on_views_user.rb
+++ db/migrate/20201103175354_remove_not_null_restriction_on_views_user.rb
@@ -1,5 +1,10 @@
 class RemoveNotNullRestrictionOnViewsUser < ActiveRecord::Migration[5.2]
-  def change
-    change_column :views, :user_id, :bigint, :null => true
+  def up
+    change_column :views, :user_id, :bigint, null: true
+  end
+
+  def down
+    View.where(user_id: nil).destroy_all
+    change_column :views, :user_id, :bigint, null: false
   end
 end
