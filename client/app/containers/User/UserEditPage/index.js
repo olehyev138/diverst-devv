8e289b8c3651d0e976d1d8a4e1b420f978f0efa4
liana <provlia1@videotron.ca>
2020-11-16 15:53:32 -0500

    lint

--- client/app/containers/User/UserEditPage/index.js
+++ client/app/containers/User/UserEditPage/index.js
@@ -58,7 +58,7 @@ export function UserEditPage(props) {
 
   return (
     <React.Fragment>
-      <DiverstBreadcrumbs customTexts={props.customTexts}/>
+      <DiverstBreadcrumbs customTexts={props.customTexts} />
       <UserForm
         edit
         userAction={props.updateUserBegin}
