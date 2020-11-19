c635a782394bc5ad193852dff9c670bebf907b24
liana <provlia1@videotron.ca>
2020-11-19 17:13:59 -0500

    Change prop passed to buttonText

--- client/app/containers/Resource/EnterpriseResource/ResourceEditPage/index.js
+++ client/app/containers/Resource/EnterpriseResource/ResourceEditPage/index.js
@@ -60,7 +60,7 @@ export function ResourceEditPage(props) {
       getFoldersBegin={props.getFoldersBegin}
       selectFolders={props.folders}
       resourceAction={props.updateResourceBegin}
-      buttonText={props.intl.formatMessage(messages.update, props.customTexts)}
+      buttonText={messages.update}
       currentUser={currentUser}
       currentEnterprise={currentEnterprise}
       resource={currentResource}
