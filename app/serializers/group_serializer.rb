fe85fcd41dc3f6333f401014c8bbc994a0e45eaa
Mark Roy <markr4848@gmail.com>
2020-11-23 14:22:23 -0500

    Comment use of new AttachmentHelper resize method in GroupSerializer for logo_data, change resulting size to max at 125x125

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -156,7 +156,8 @@ class GroupSerializer < ApplicationRecordSerializer
   end
 
   def logo_data
-    AttachmentHelper.image_resize_variant_data_string(object.logo, 175, 175)
+    # Thumbnail the logo to at most 125x125
+    AttachmentHelper.image_resize_variant_data_string(object.logo, 125, 125)
   end
 
   def logo_content_type
