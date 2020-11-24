e323286b7e2fce2f5b89c1cadfaee68f0289565a
AlexOxorn <alex.oxorn@gmail.com>
2020-11-24 10:57:40 -0500

    create materialized tables (not migrated)

---
+++ db/migrate/20201124154238_create_materialized_sum_tables.rb
@@ -0,0 +1,34 @@
+class CreateMaterializedSumTables < ActiveRecord::Migration[5.2]
+  def change
+    create_table :budget_users_sums do |t|
+      t.belongs_to :budget_user
+      t.decimal :spent, precision: 20, scale: 4
+    end
+
+    create_table :budget_item_sums do |t|
+      t.belongs_to :budget_item
+      t.decimal :spent, precision: 20, scale: 4
+      t.decimal :reserved, precision: 20, scale: 4
+      t.decimal :finalized_expenditures, precision: 20, scale: 4
+    end
+
+    create_table :budgets_sums do |t|
+      t.belongs_to :budget
+      t.decimal :spent, precision: 20, scale: 4
+      t.decimal :reserved, precision: 20, scale: 4
+      t.decimal :requested_amount, precision: 20, scale: 4
+      t.decimal :available, precision: 20, scale: 4
+      t.decimal :unspent, precision: 20, scale: 4
+    end
+
+    create_table :annual_budgets_sums do |t|
+      t.belongs_to :budget
+      t.decimal :spent, precision: 20, scale: 4
+      t.decimal :reserved, precision: 20, scale: 4
+      t.decimal :requested_amount, precision: 20, scale: 4
+      t.decimal :available, precision: 20, scale: 4
+      t.decimal :approved, precision: 20, scale: 4
+      t.decimal :unspent, precision: 20, scale: 4
+    end
+  end
+end
