f3d0cf9b0dba3e9bdd03b37fa78db9dc2105e2ae
AlexOxorn <alex.oxorn@gmail.com>
2020-12-02 13:12:05 -0500

    form input

--- client/app/components/Group/GroupPlan/AnnualBudgetForm/index.js
+++ client/app/components/Group/GroupPlan/AnnualBudgetForm/index.js
@@ -10,7 +10,7 @@ import React, {
 import { compose } from 'redux';
 import PropTypes from 'prop-types';
 import Select from 'components/Shared/DiverstSelect';
-import { Field, Formik, Form, getIn } from 'formik';
+import {Field, Formik, Form, getIn, FastField} from 'formik';
 import DiverstFormattedMessage from 'components/Shared/DiverstFormattedMessage';
 import { withStyles } from '@material-ui/core/styles';
 
@@ -22,10 +22,10 @@ import { ROUTES } from 'containers/Shared/Routes/constants';
 
 import messages from 'containers/Group/GroupPlan/AnnualBudget/messages';
 import appMessages from 'containers/Shared/App/messages';
-import { buildValues, mapFields } from 'utils/formHelpers';
+import { buildValues, buildValuesOfArray, mapFields } from 'utils/formHelpers';
 
 import {
-  Button, Card, CardActions, CardContent, Grid, Paper,
+  Button, Card, CardActions, CardContent, Grid, Paper, Box, CardHeader,
   TextField, InputAdornment, Input, FormControl, InputLabel, Typography,
 } from '@material-ui/core';
 
@@ -48,70 +48,79 @@ const styles = theme => ({
 export function AnnualBudgetFormInner(
   { classes, handleSubmit, handleChange, handleBlur, values, buttonText, setFieldValue, setFieldTouched, intl, annualBudgets, ...props }
 ) {
-  const annualBudget = annualBudgets[0];
-
   return (
-    <DiverstFormLoader isLoading={props.isFormLoading} isError={props.edit && !annualBudget}>
-      <Card>
-        <Form>
-          <CardContent>
-            <DiverstMoneyField
-              label={intl.formatMessage(formMessages.amount)}
-              name='amount'
-              id='amount'
-              margin='dense'
-              fullWidth
-              disabled={props.isCommitting}
-              value={values.amount}
-              onChange={value => setFieldValue('amount', value)}
+    <DiverstFormLoader isLoading={props.isFormLoading} isError={props.edit && !annualBudgets}>
+      <Form>
+        { values.map((value, index) => (
+          <React.Fragment>
+            <Card>
+              <CardHeader
+                title={annualBudgets[index].name}
+              />
+              <CardContent>
+                <FastField
+                  component={DiverstMoneyField}
+                  label={intl.formatMessage(formMessages.amount)}
+                  name={`[${index}].amount`}
+                  id={`[${index}].amount`}
+                  margin='dense'
+                  fullWidth
+                  disabled={props.isCommitting}
+                  value={value.amount}
+                  onChange={value => setFieldValue(`[${index}].amount`, value)}
 
-              currencyForm
-              currency={getCurrency(values.currency)}
-              currency_name='currency'
-              currency_id='currency'
-              onCurrencyChange={value => setFieldValue('currency', value.value)}
-            />
-          </CardContent>
-          { annualBudget && (
-            <CardContent>
-              <Grid
-                container
-                justify='space-between'
-                spacing={3}
-                alignContent='stretch'
-                alignItems='center'
-              >
-                <Grid item sm={6}>
-                  <Typography color='primary' variant='h6' component='h2'>
-                    <DiverstFormattedMessage {...formMessages.leftover} />
-                  </Typography>
-                </Grid>
-                <Grid item sm={6}>
-                  <Typography color='primary' variant='h6' component='h2'>
-                    <DiverstFormattedMessage {...formMessages.approved} />
-                  </Typography>
-                </Grid>
-              </Grid>
-              <Grid
-                container
-                justify='space-between'
-                spacing={3}
-                alignContent='stretch'
-                alignItems='center'
-              >
-                <Grid item sm={6}>
-                  <Typography color='secondary' variant='body1' component='h3'>
-                    {toCurrencyString(intl, annualBudget.leftover || 0, annualBudget.currency)}
-                  </Typography>
-                </Grid>
-                <Grid item sm={6}>
-                  <Typography color='secondary' variant='body1' component='h3'>
-                    {toCurrencyString(intl, annualBudget.approved || 0, annualBudget.currency)}
-                  </Typography>
-                </Grid>
-              </Grid>
-            </CardContent>
-          )}
+                  currencyForm
+                  currency={getCurrency(value.currency)}
+                  currency_name={`[${index}].currency`}
+                  currency_id={`[${index}].currency`}
+                  onCurrencyChange={value => setFieldValue(`[${index}].currency`, value.value)}
+                />
+              </CardContent>
+              { annualBudgets[index] && (
+                <CardContent>
+                  <Grid
+                    container
+                    justify='space-between'
+                    spacing={3}
+                    alignContent='stretch'
+                    alignItems='center'
+                  >
+                    <Grid item sm={6}>
+                      <Typography color='primary' variant='h6' component='h2'>
+                        <DiverstFormattedMessage {...formMessages.leftover} />
+                      </Typography>
+                    </Grid>
+                    <Grid item sm={6}>
+                      <Typography color='primary' variant='h6' component='h2'>
+                        <DiverstFormattedMessage {...formMessages.approved} />
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Grid
+                    container
+                    justify='space-between'
+                    spacing={3}
+                    alignContent='stretch'
+                    alignItems='center'
+                  >
+                    <Grid item sm={6}>
+                      <Typography color='secondary' variant='body1' component='h3'>
+                        {toCurrencyString(intl, annualBudgets[index].leftover || 0, annualBudgets[index].currency)}
+                      </Typography>
+                    </Grid>
+                    <Grid item sm={6}>
+                      <Typography color='secondary' variant='body1' component='h3'>
+                        {toCurrencyString(intl, annualBudgets[index].approved || 0, annualBudgets[index].currency)}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </CardContent>
+              )}
+            </Card>
+            <Box mb={2} />
+          </React.Fragment>
+        ))}
+        <Card>
           <CardActions>
             <DiverstSubmit isCommitting={props.isCommitting}>
               <DiverstFormattedMessage {...formMessages.setAnnualBudget} />
@@ -123,14 +132,14 @@ export function AnnualBudgetFormInner(
               <DiverstFormattedMessage {...formMessages.cancel} />
             </DiverstCancel>
           </CardActions>
-        </Form>
-      </Card>
+        </Card>
+      </Form>
     </DiverstFormLoader>
   );
 }
 
 export function AnnualBudgetForm(props) {
-  const initialValues = buildValues(props.annualBudgets[0], {
+  const initialValues = buildValuesOfArray(props.annualBudgets, {
     id: { default: '' },
     amount: { default: '' },
     currency: { default: 'USD' },
