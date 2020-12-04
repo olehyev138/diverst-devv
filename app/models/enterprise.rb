1f750813f7d96f08f9e2219c3f0a2f394cd3089d
Anatolii Borysov <borisano@gmail.com>
2020-12-04 15:39:49 +0200

    Only show enterprise custom emails on enterprise custom emails list

--- app/models/enterprise.rb
+++ app/models/enterprise.rb
@@ -36,7 +36,7 @@ class Enterprise < BaseClass
   has_many :yammer_field_mappings, dependent: :destroy
 
   has_many :emails, -> { where custom: false }, dependent: :destroy
-  has_many :custom_emails, -> { where custom: true }, class_name: 'Email', dependent: :destroy
+  has_many :custom_emails, -> { where custom: true, group_id: nil }, class_name: 'Email', dependent: :destroy
 
   has_many :email_variables, class_name: 'EnterpriseEmailVariable', dependent: :destroy
   has_many :total_page_visitations
