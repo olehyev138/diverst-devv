5330d11aa776b1acf84940d0340f33336a48def3
liana <provlia1@videotron.ca>
2020-12-07 16:47:18 -0500

    Migrate and remove all mentions of "Policy template" from user role creation

---
+++ db/migrate/20201207212551_remove_policy_template_from_user_role_creation.rb
@@ -0,0 +1,5 @@
+class RemovePolicyTemplateFromUserRoleCreation < ActiveRecord::Migration[5.2]
+  def change
+    PolicyGroupTemplate.update_all('name = replace(name, " Policy Template", "")')
+  end
+end
