08309c50ed07d106616710039af538037f1964c3
Mark Roy <markr4848@gmail.com>
2020-12-09 15:54:14 -0500

    Implement Group Selector dialog child navigation UX with proper transitions

--- client/app/components/Shared/GroupSelector/dialog.js
+++ client/app/components/Shared/GroupSelector/dialog.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 import DiverstPagination from 'components/Shared/DiverstPagination';
 import GroupSelectorItem from './item';
-import { Typography, Fade, Grid, TextField, Box, IconButton, withStyles } from '@material-ui/core';
+import { Typography, Fade, Grid, TextField, Box, IconButton, Button, withStyles } from '@material-ui/core';
 import DiverstLoader from 'components/Shared/DiverstLoader';
 import messages from 'containers/Group/messages';
 import DiverstFormattedMessage from 'components/Shared/DiverstFormattedMessage';
@@ -43,18 +43,29 @@ const styles = {
 };
 
 const GroupListSelector = (props) => {
-  const { groups, classes, intl, customTexts, ...rest } = props;
+  const { groups, classes, intl, customTexts, displayParentUI, parentData, setDisplayParentUI, setParentData, ...rest } = props;
 
   const [params, setParams] = useState({ count: 10, page: 0, query_scopes: union(props.queryScopes, props.dialogQueryScopes) });
   const [searchKey, setSearchKey] = useState('');
 
-  const groupSearchAction = (searchKey = searchKey, params = params) => props.inputCallback(props, searchKey, params);
+  const groupSearchAction = (searchKey = searchKey, params = params, clearParentUI = true) => {
+    if (clearParentUI) setDisplayParentUI(false);
+
+    props.inputCallback(props, searchKey, params);
+  };
+
   const delayedSearchAction = useDelayedTextInputCallback(groupSearchAction);
 
   useEffect(() => {
     if (props.open)
-      groupSearchAction(searchKey, params);
+      groupSearchAction(searchKey, params, false);
   }, [props.open]);
+
+  useEffect(() => {
+    if (parentData === undefined || !parentData?.id) return;
+
+    groupSearchAction('', { ...params, page: 0, query_scopes: union(props.queryScopes, [['children_of', parentData?.id]]) }, false);
+  }, [parentData?.id]);
 
   const header = (
     <Typography color='secondary' variant='body1'>
@@ -64,35 +75,82 @@ const GroupListSelector = (props) => {
 
   const searchBar = (
     <React.Fragment>
-      <Grid container justify='space-between' alignContent='center' alignItems='center'>
-        <Grid item style={{ flex: 1 }}>
-          <TextField
-            placeholder={intl.formatMessage(messages.selectorDialog.searchPlaceholder, customTexts)}
-            margin='dense'
-            id='search key'
-            fullWidth
-            type='text'
-            onChange={(e) => {
-              setSearchKey(e.target.value);
-              delayedSearchAction(e.target.value, params);
-            }}
-            value={searchKey}
-            InputProps={{
-              endAdornment: searchKey && (
-                <IconButton
-                  className={classes.clearSearchTextButton}
-                  onClick={() => {
-                    setSearchKey('');
-                    groupSearchAction('', params);
-                  }}
-                >
-                  <ClearIcon />
-                </IconButton>
-              ),
-            }}
-          />
+      <Fade
+        in={!props.isDisplayingChildren}
+        appear
+        mountOnEnter
+        unmountOnExit
+        onExited={props.handleFinishExitTransition}
+      >
+        <Grid container justify='space-between' alignContent='center' alignItems='center'>
+          <Grid item style={{ flex: 1 }}>
+            <TextField
+              placeholder={intl.formatMessage(messages.selectorDialog.searchPlaceholder, customTexts)}
+              margin='dense'
+              id='search key'
+              fullWidth
+              type='text'
+              onChange={(e) => {
+                setSearchKey(e.target.value);
+                delayedSearchAction(e.target.value, params);
+              }}
+              value={searchKey}
+              InputProps={{
+                endAdornment: searchKey && (
+                  <IconButton
+                    className={classes.clearSearchTextButton}
+                    onClick={() => {
+                      setSearchKey('');
+                      groupSearchAction('', params);
+                    }}
+                  >
+                    <ClearIcon />
+                  </IconButton>
+                ),
+              }}
+            />
+          </Grid>
         </Grid>
-      </Grid>
+      </Fade>
+
+      <Fade
+        in={displayParentUI}
+        appear
+        mountOnEnter
+        unmountOnExit
+        onExited={() => {
+          setParentData(null);
+          setSearchKey('');
+        }}
+      >
+        <Box pt={1} style={{ overflow: 'hidden' }}>
+          <Grid container spacing={1}>
+            <Grid item>
+              <Button
+                size='small'
+                color='primary'
+                variant='contained'
+                onClick={() => {
+                  setParentData({ name: parentData?.name });
+                  groupSearchAction('', { ...params, page: 0, query_scopes: union(props.queryScopes, props.dialogQueryScopes) });
+                }}
+              >
+                <DiverstFormattedMessage {...messages.back} />
+              </Button>
+            </Grid>
+            <Grid item xs align='center'>
+              <Typography component='span' color='primary' variant='h5' className={classes.headerText}>
+                {parentData?.name}
+              </Typography>
+              &nbsp;
+              &nbsp;
+              <Typography component='span' color='textSecondary' variant='h5' className={classes.headerText}>
+                <DiverstFormattedMessage {...messages.childList} />
+              </Typography>
+            </Grid>
+          </Grid>
+        </Box>
+      </Fade>
     </React.Fragment>
   );
 
@@ -168,8 +226,12 @@ GroupListSelector.propTypes = {
   getGroupsBegin: PropTypes.func,
 
   parentData: PropTypes.object,
+  setParentData: PropTypes.func,
   displayParentUI: PropTypes.bool,
+  setDisplayParentUI: PropTypes.func,
+  isDisplayingChildren: PropTypes.bool,
   handleParentExpand: PropTypes.func,
+  handleFinishExitTransition: PropTypes.func,
 
   open: PropTypes.bool,
   addGroup: PropTypes.func.isRequired,
