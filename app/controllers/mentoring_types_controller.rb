915e9e5419c723baf88da2c78d317e8eb0aa1fc8
Derek Owusu-Frimpong <deepsky_5@live.com>
2020-12-20 07:04:25 +0000

    add ability to CRUD mentoring types [skip ci]

---
+++ app/controllers/mentoring_types_controller.rb
@@ -0,0 +1,66 @@
+class MentoringTypesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_type, only: [:show, :edit, :update, :destroy]
+
+  layout 'mentorship'
+
+  def index
+    authorize MentoringType
+    @types = current_user.enterprise.mentoring_types
+  end
+
+  def new
+    authorize MentoringType
+    @type = current_user.enterprise.mentoring_types.new
+  end
+
+  def create
+    authorize MentoringType
+    @type = current_user.enterprise.mentoring_types.new(type_params)
+
+    if @type.save
+      flash[:notice] = 'Your mentoring type was created'
+      redirect_to action: :index
+    else
+      flash[:alert] = 'Your mentoring type was not created. Please fix the errors.'
+      render :new
+    end
+  end
+
+  def edit
+    authorize MentoringType
+  end
+
+  def update
+    authorize MentoringType
+
+    if @type.update(type_params)
+      flash[:notice] = 'The type was updated'
+      redirect_to action: :index
+    else
+      flash[:alert] = 'The type was not updated. Please fix the errors'
+      render :edit
+    end
+  end
+
+  def destroy
+    authorize MentoringType
+
+    @type.destroy
+    redirect_to :back
+  end
+
+  private 
+
+  def set_type
+    @type = current_user.enterprise.mentoring_types.find(params[:id])
+  end
+
+  def type_params
+    params
+      .require(:mentoring_type)
+      .permit(
+          :name
+      )
+  end
+end
