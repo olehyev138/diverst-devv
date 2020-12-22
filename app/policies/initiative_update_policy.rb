ac976c09a1bb3bf42b8235b103b77dd9ca9164d9
AlexOxorn <alex.oxorn@gmail.com>
2020-12-22 14:37:55 -0500

    test fixes

--- app/policies/initiative_update_policy.rb
+++ app/policies/initiative_update_policy.rb
@@ -11,7 +11,15 @@ class InitiativeUpdatePolicy < InitiativeBasePolicy
     'initiatives_manage'
   end
 
+  def update?
+    record.owner == user || super
+  end
+
   class Scope < Scope
+    def initiative_base
+      initiative.updates
+    end
+
     def resolve
       super(policy.base_index_permission)
     end
