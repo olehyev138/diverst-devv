8dc6c7af2eb0dd13857a069f27d5e8a035e66ed0
boatdiverst <arlene@diverst.com>
2020-12-24 13:56:06 -0500

    updated test

--- spec/models/segment_field_rule_spec.rb
+++ spec/models/segment_field_rule_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe SegmentFieldRule, type: :model do
     let(:segment_field_rule) { build_stubbed(:segment_field_rule) }
 
     it { expect(segment_field_rule).to belong_to(:field) }
-    it { expect(segment_field_rule).to belong_to(:segment) }
+    it { expect(segment_field_rule).to belong_to(:segment).dependent(:delete) }
 
     it { is_expected.to validate_presence_of(:operator) }
     it { is_expected.to validate_presence_of(:field) }
