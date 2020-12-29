eb99c4d792b5034769310e0d29f2cfd395279ab6
liana <provlia1@videotron.ca>
2020-12-29 09:50:48 -0500

    update migration to consider the case of edits made recently

--- db/migrate/20201207212551_remove_policy_template_from_user_role_creation.rb
+++ db/migrate/20201207212551_remove_policy_template_from_user_role_creation.rb
@@ -1,5 +1,6 @@
 class RemovePolicyTemplateFromUserRoleCreation < ActiveRecord::Migration[5.2]
   def change
     PolicyGroupTemplate.update_all('name = replace(name, " Policy Template", "")')
+    PolicyGroupTemplate.update_all('name = replace(name, "Policy Template", "")')
   end
 end
