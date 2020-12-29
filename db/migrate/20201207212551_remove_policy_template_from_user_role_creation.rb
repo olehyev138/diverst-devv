7459f2e8b15b00f2384d2f8e2f044c46f656a97c
liana <provlia1@videotron.ca>
2020-12-29 10:20:47 -0500

    Update migration to set the name to the user role name

--- db/migrate/20201207212551_remove_policy_template_from_user_role_creation.rb
+++ db/migrate/20201207212551_remove_policy_template_from_user_role_creation.rb
@@ -1,6 +1,8 @@
 class RemovePolicyTemplateFromUserRoleCreation < ActiveRecord::Migration[5.2]
   def change
-    PolicyGroupTemplate.update_all('name = replace(name, " Policy Template", "")')
-    PolicyGroupTemplate.update_all('name = replace(name, "Policy Template", "")')
+    PolicyGroupTemplate.find_each do |policy|
+      policy.name = UserRole.find(policy.id).role_name
+      policy.save!
+    end
   end
 end
