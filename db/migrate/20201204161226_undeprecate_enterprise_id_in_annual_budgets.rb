8a84865bc74f611eab7a8edc90671a3bbcd20239
Alex Oxorn <alex.oxorn@gmail.com>
2021-01-06 13:47:10 -0500

    set budget heads before undeprecate enterprise id

--- db/migrate/20201204161226_undeprecate_enterprise_id_in_annual_budgets.rb
+++ db/migrate/20201204161226_undeprecate_enterprise_id_in_annual_budgets.rb
@@ -5,7 +5,7 @@ class UndeprecateEnterpriseIdInAnnualBudgets < ActiveRecord::Migration[5.2]
         remove_foreign_key :annual_budgets, column: :deprecated_enterprise_id
       end
       dir.down do
-        add_foreign_key :annual_budgets, :enetrprises, column: :deprecated_enterprise_id
+        add_foreign_key :annual_budgets, :enterprises, column: :deprecated_enterprise_id
       end
     end
     rename_column :annual_budgets, :deprecated_enterprise_id, :enterprise_id
@@ -13,7 +13,7 @@ class UndeprecateEnterpriseIdInAnnualBudgets < ActiveRecord::Migration[5.2]
     up_only do
       AnnualBudget.column_reload!
       AnnualBudget.find_each do |ab|
-        ab.update(enterprise: ab.budget_head.enterprise) if ab.enterprise_id.blank?
+        ab.update(enterprise: ab.budget_head&.enterprise) if ab.enterprise_id.blank?
       end
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_28_210444) do
+ActiveRecord::Schema.define(version: 2020_12_21_182556) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.string "name", null: false
