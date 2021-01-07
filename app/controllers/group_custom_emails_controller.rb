ae89a65b0fd379ca67909163a047c8035e8a410f
Anatolii Borysov <borisano@gmail.com>
2021-01-07 18:20:40 +0200

    Fix creation and edit/update actions

--- app/controllers/group_custom_emails_controller.rb
+++ app/controllers/group_custom_emails_controller.rb
@@ -2,6 +2,7 @@
 class GroupCustomEmailsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_group
+  before_action :set_custom_email, only: [:edit, :update]
   #before_action :set_field, only: [:time_series]
   after_action :verify_authorized
 
@@ -18,19 +19,19 @@ class GroupCustomEmailsController < ApplicationController
 
     @custom_email = @group.custom_emails.new
 
-    @submit_url = create_new_email_group_path(@group)
+    @submit_url = group_group_custom_emails_path(@group.id)
     @submit_method = 'post'
   end
 
   def create
-    authorize @group, :new_email?
+    authorize @group
 
     @custom_email = @group.custom_emails.new(custom_email_params)
 
     if @custom_email.save
       # TODO track activity
       flash[:notice] = 'Your custom email was created'
-      redirect_to emails_group_path(@group)
+      redirect_to group_group_custom_emails_path(@group)
     else
       flash.now[:alert] = 'Your custom email was not created, please fix errors'
       render :new_email
@@ -39,23 +40,19 @@ class GroupCustomEmailsController < ApplicationController
 
   def edit
     authorize @group
-    #TODO throw exception if email_id param is not present
-    email_id = params[:email_id]
-    @custom_email = @group.custom_emails.find(email_id)
 
-    @submit_url = update_email_group_path(@group)
+    @submit_url = group_group_custom_email_path(@group, @custom_email)
     @submit_method = 'patch'
   end
 
   def update
     authorize @group, :edit_email?
 
-    @custom_email = @group.custom_emails.find params[:email_id]
     if @custom_email.update(custom_email_params)
       flash[:notice] = 'Your custom email was updated'
       # TODO track activity
       # track_activity(@email, :update)
-      redirect_to emails_group_path(@group)
+      redirect_to group_group_custom_emails_path(@group)
     else
       flash[:alert] = 'Your custom email was not updated. Please fix the errors'
       render :edit
@@ -66,7 +63,7 @@ class GroupCustomEmailsController < ApplicationController
   protected
 
   def set_custom_email
-
+    @custom_email = @group.custom_emails.find params[:id]
   end
 
   def set_group
